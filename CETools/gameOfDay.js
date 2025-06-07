dc7c9ed1b4c837ec3caf7094c02634189940c68c
Icondor <ivanfigus@gmail.com>
2025-06-07 13:59:46 +0200

    Create gameOfDay.js

---
+++ CETools/gameOfDay.js
@@ -0,0 +1,65 @@
+var gameOfDay =
+{
+    "game": {
+        "gameType": 0,
+        "platformId": 1528880,
+        "CEId": "4a9f7e74-2e61-46e6-b788-66a42b212d86",
+        "name": "Time to GOLF",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1528880/header.jpg?t=1613660718",
+        "genre": "Arcade",
+        "tier": 2,
+        "points": 30,
+        "createdAt": "2022-06-07T03:11:15.000Z",
+        "updatedAt": "2025-05-24T21:45:49.000Z",
+        "playersCompleted": 9,
+        "playersStarted": 0,
+        "playersTotal": 48,
+        "priceData": {
+            "USD": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 52000,
+                "final": 52000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 750,
+                "final": 750,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 569,
+                "final": 569,
+                "discountPercent": 0
+            }
+        },
+        "milestones": {
+            "primary": 1,
+            "primaryText": "No Mulligan Necessary [30\u2605]\nClear in under 100 seconds.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 11
+        }
+    },
+    "extras": {
+        "reviews": {
+            "description": "Positive",
+            "total": 15,
+            "positive": 13,
+            "negative": 2
+        }
+    }
+}
