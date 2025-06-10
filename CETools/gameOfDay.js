75bc1ff3210f1520f6e8a05538b7df693d7b5408
Icondor <ivanfigus@gmail.com>
2025-06-10 09:38:18 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,65 +2,65 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1159450,
-        "CEId": "a2a176d3-40ae-412b-a690-faba912064d3",
-        "name": "Hayfever",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/1159450/header.jpg?t=1583498702",
-        "genre": "Platformer",
+        "platformId": 217270,
+        "CEId": "6f98bdad-bd2e-4861-9e3f-7d7d510cde3e",
+        "name": "Karateka",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/217270/header.jpg?t=1580746358",
+        "genre": "Action",
         "tier": 2,
         "points": 20,
-        "createdAt": "2020-06-09T19:30:19.000Z",
+        "createdAt": "2023-06-10T19:38:19.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 18,
+        "playersCompleted": 2,
         "playersStarted": 0,
-        "playersTotal": 56,
+        "playersTotal": 49,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 289,
+                "final": 289,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 148000,
-                "final": 148000,
+                "initial": 41000,
+                "final": 41000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 595,
+                "final": 595,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 465,
+                "final": 465,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Bless You [20\u2605]\nAchieve 100% in-game completion.",
+            "primaryText": "Path of the Martial Artist [20\u2605]\nReach the final boss without getting hit once. / Beat the final boss as the \"True Love\" character. ",
             "community": 0,
             "communityText": "",
-            "achievements": 30
+            "achievements": 12
         }
     },
     "extras": {
         "reviews": {
-            "description": "Very Positive",
-            "total": 54,
-            "positive": 51,
-            "negative": 3
+            "description": "Mostly Positive",
+            "total": 372,
+            "positive": 263,
+            "negative": 109
         }
     }
 }
-var gameOfDayDate = '2025-06-09'
+var gameOfDayDate = '2025-06-10'
