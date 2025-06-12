8a0a549db26d02ee1a9c1af630b776ea64731950
Icondor <ivanfigus@gmail.com>
2025-06-12 08:56:21 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,96 +2,95 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 663130,
-        "CEId": "d852a7ba-a2e2-4efb-a748-3879708d43f2",
-        "name": "Game Tengoku CruisinMix Special",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/663130/header.jpg?t=1697063945",
-        "genre": "Bullet Hell",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2024-06-11T05:18:28.000Z",
+        "platformId": 269670,
+        "CEId": "08615566-f2ee-4379-8e80-0e7175088d9a",
+        "name": "BADLAND: Game of the Year Edition",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/269670/header.jpg?t=1569395419",
+        "genre": "Arcade",
+        "tier": 4,
+        "points": 100,
+        "createdAt": "2022-06-12T10:24:42.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
         "playersCompleted": 1,
-        "playersStarted": 1,
-        "playersTotal": 20,
+        "playersStarted": 0,
+        "playersTotal": 199,
         "priceData": {
             "USD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2899,
-                "final": 2899,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 699,
+                "final": 699,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 303500,
-                "final": 303500,
+                "initial": 98000,
+                "final": 98000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 4395,
-                "final": 4395,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 3899,
-                "final": 3899,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Wreck-It Yui  [10\u2605]\nBeat both loops of Arcade mode without continuing on Normal or higher.\n\nCruisin Lore [10\u2605]\nClear the game in Arrange mode without continuing on Normal or higher.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 17
-        },
-        "information": "Play on \"usertest\" branch to avoid freezes."
+            "primaryText": "We Are The Saviours [60\u2605]\nSave 3000 clones in total in co-op. / Complete 300 missions in co-op.\n\nMission Possible [40\u2605]\nComplete all 300 Missions (excluding \"Eternal Day\").",
+            "community": 1,
+            "communityText": "An Everlasting Day\nComplete all \"Eternal Day\" missions.",
+            "achievements": 78
+        }
     },
     "extras": {
         "reviews": {
-            "description": "Mostly Positive",
-            "total": 92,
-            "positive": 71,
-            "negative": 21
+            "description": "Very Positive",
+            "total": 3081,
+            "positive": 2640,
+            "negative": 441
         },
         "dlc": {
-            "description": "Game Tengoku - Clarice\n\nGame Tengoku - Homura Banto\n\nGame Tengoku - Voice Mix\n\nGame Tengoku - TATSUJIN",
-            "count": 4,
+            "description": "BADLAND: Game of the Year Edition - Digital Art Booklet & Ambient Soundtrack",
+            "count": 1,
             "priceData": {
                 "USD": {
-                    "initial": 3996,
-                    "final": 3996
+                    "initial": 299,
+                    "final": 299
                 },
                 "EUR": {
-                    "initial": 3900,
-                    "final": 3900
+                    "initial": 299,
+                    "final": 299
                 },
                 "GBP": {
-                    "initial": 3400,
-                    "final": 3400
+                    "initial": 199,
+                    "final": 199
                 },
                 "JPY": {
-                    "initial": 440000,
-                    "final": 440000
+                    "initial": 29800,
+                    "final": 29800
                 },
                 "AUD": {
-                    "initial": 5800,
-                    "final": 5800
+                    "initial": 0,
+                    "final": 0
                 },
                 "CAD": {
-                    "initial": 5196,
-                    "final": 5196
+                    "initial": 329,
+                    "final": 329
                 }
             }
         }
     }
 }
-var gameOfDayDate = '2025-06-11'
+var gameOfDayDate = '2025-06-12'
