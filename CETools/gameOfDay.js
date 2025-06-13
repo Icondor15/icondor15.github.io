6340d2f5839f6d4427cb91eab7a09ee4443178ae
Icondor <ivanfigus@gmail.com>
2025-06-13 09:38:18 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,95 +2,65 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 269670,
-        "CEId": "08615566-f2ee-4379-8e80-0e7175088d9a",
-        "name": "BADLAND: Game of the Year Edition",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/269670/header.jpg?t=1569395419",
-        "genre": "Arcade",
-        "tier": 4,
-        "points": 100,
-        "createdAt": "2022-06-12T10:24:42.000Z",
+        "platformId": 2169300,
+        "CEId": "37067e78-c1d1-482e-b0ea-e1d286dfa6b7",
+        "name": "Dizzy Rogues",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2169300/header.jpg?t=1686624581",
+        "genre": "Action",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2023-06-13T21:36:29.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 1,
+        "playersCompleted": 7,
         "playersStarted": 0,
-        "playersTotal": 199,
+        "playersTotal": 15,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 699,
-                "final": 699,
+                "initial": 349,
+                "final": 349,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 98000,
-                "final": 98000,
+                "initial": 49900,
+                "final": 49900,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 599,
+                "final": 599,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "We Are The Saviours [60\u2605]\nSave 3000 clones in total in co-op. / Complete 300 missions in co-op.\n\nMission Possible [40\u2605]\nComplete all 300 Missions (excluding \"Eternal Day\").",
-            "community": 1,
-            "communityText": "An Everlasting Day\nComplete all \"Eternal Day\" missions.",
-            "achievements": 78
+            "primary": 1,
+            "primaryText": "Bear the Dizziness [5\u2605]\nSeparately, beat the game without buying anything, and without the blacksmith or doctor.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 16
         }
     },
     "extras": {
         "reviews": {
-            "description": "Very Positive",
-            "total": 3081,
-            "positive": 2640,
-            "negative": 441
-        },
-        "dlc": {
-            "description": "BADLAND: Game of the Year Edition - Digital Art Booklet & Ambient Soundtrack",
-            "count": 1,
-            "priceData": {
-                "USD": {
-                    "initial": 299,
-                    "final": 299
-                },
-                "EUR": {
-                    "initial": 299,
-                    "final": 299
-                },
-                "GBP": {
-                    "initial": 199,
-                    "final": 199
-                },
-                "JPY": {
-                    "initial": 29800,
-                    "final": 29800
-                },
-                "AUD": {
-                    "initial": 0,
-                    "final": 0
-                },
-                "CAD": {
-                    "initial": 329,
-                    "final": 329
-                }
-            }
+            "description": "Positive",
+            "total": 49,
+            "positive": 45,
+            "negative": 4
         }
     }
 }
-var gameOfDayDate = '2025-06-12'
+var gameOfDayDate = '2025-06-13'
