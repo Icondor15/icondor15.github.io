74abbc517a14cd7804b393caaf9ec6e3971db334
Icondor <ivanfigus@gmail.com>
2025-06-14 09:02:49 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,66 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 2169300,
-        "CEId": "37067e78-c1d1-482e-b0ea-e1d286dfa6b7",
-        "name": "Dizzy Rogues",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2169300/header.jpg?t=1686624581",
-        "genre": "Action",
+        "gameType": 3,
+        "platformId": 11320,
+        "CEId": "09846c29-9d16-44ac-aa4b-b7cba5656b9f",
+        "name": "Gran Turismo",
+        "header": "https://i.imgur.com/oALEFek.png",
+        "genre": "Arcade",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2023-06-13T21:36:29.000Z",
+        "points": 15,
+        "createdAt": "2024-06-14T19:14:19.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 7,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 15,
-        "priceData": {
-            "USD": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 349,
-                "final": 349,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 49900,
-                "final": 49900,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 599,
-                "final": 599,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            }
-        },
+        "playersTotal": 10,
         "milestones": {
             "primary": 1,
-            "primaryText": "Bear the Dizziness [5\u2605]\nSeparately, beat the game without buying anything, and without the blacksmith or doctor.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 16
-        }
-    },
-    "extras": {
-        "reviews": {
-            "description": "Positive",
-            "total": 49,
-            "positive": 45,
-            "negative": 4
+            "primaryText": "Certified 90's Driver [15\u2605]\nObtain every gold medal in all license tests.",
+            "community": 1,
+            "communityText": "Licensed to Refill\nComplete all primary objectives from all site-listed games in the Gran Turismo franchise. See verification requirements for complete list.",
+            "achievements": 68
         }
     }
 }
-var gameOfDayDate = '2025-06-13'
+var gameOfDayDate = '2025-06-14'
