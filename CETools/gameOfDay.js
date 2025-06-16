13626b480cc35847eb0a41747000eb432cbe2b94
Icondor <ivanfigus@gmail.com>
2025-06-16 09:38:35 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,65 +2,95 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 963450,
-        "CEId": "8b9fcbe3-b305-4eb4-a9e4-868c8b2f0d48",
-        "name": "The Eternal Castle [REMASTERED]",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/963450/header.jpg?t=1587515231",
+        "platformId": 907670,
+        "CEId": "ce890e5f-851c-438d-863f-a0cabe4b19ad",
+        "name": "Super Crush KO",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/907670/header.jpg?t=1619758080",
         "genre": "Action",
         "tier": 2,
-        "points": 26,
-        "createdAt": "2024-06-15T00:19:51.000Z",
+        "points": 20,
+        "createdAt": "2023-06-16T06:34:28.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 2,
-        "playersTotal": 26,
+        "playersCompleted": 1,
+        "playersStarted": 3,
+        "playersTotal": 18,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 599,
-                "final": 599,
+                "initial": 1479,
+                "final": 1479,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 599,
-                "final": 599,
+                "initial": 1279,
+                "final": 1279,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 98700,
-                "final": 98700,
+                "initial": 170000,
+                "final": 170000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 2195,
+                "final": 2195,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1149,
-                "final": 1149,
+                "initial": 1949,
+                "final": 1949,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "2-Bit Tyrant [20\u2605]\nProve yourself.\n\nThe Eternal Gamer (PARTIALLY UNCLEARED)[6\u2605 (Partial: 5\u2605)]\nFinish the third run.",
+            "primaryText": "Spectacular+ [15\u2605]\nAchieve S+ rank on all levels.\n\nRobot Crusher [5\u2605]\nProve yourself.",
             "community": 0,
             "communityText": "",
-            "achievements": 35
+            "achievements": 20
         }
     },
     "extras": {
         "reviews": {
             "description": "Very Positive",
-            "total": 743,
-            "positive": 645,
-            "negative": 98
+            "total": 210,
+            "positive": 203,
+            "negative": 7
+        },
+        "dlc": {
+            "description": "Super Crush KO Original Soundtrack",
+            "count": 1,
+            "priceData": {
+                "USD": {
+                    "initial": 699,
+                    "final": 699
+                },
+                "EUR": {
+                    "initial": 569,
+                    "final": 569
+                },
+                "GBP": {
+                    "initial": 519,
+                    "final": 519
+                },
+                "JPY": {
+                    "initial": 72000,
+                    "final": 72000
+                },
+                "AUD": {
+                    "initial": 995,
+                    "final": 995
+                },
+                "CAD": {
+                    "initial": 799,
+                    "final": 799
+                }
+            }
         }
     }
 }
-var gameOfDayDate = '2025-06-15'
+var gameOfDayDate = '2025-06-16'
