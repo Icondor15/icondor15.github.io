6fc8b2082151a5ddc112ca9efbd614356d29a7c6
Icondor <ivanfigus@gmail.com>
2025-06-22 09:20:03 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,66 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 253750,
-        "CEId": "3ad152d0-13e1-4e5c-b4b0-a59f3911a316",
-        "name": "Ikaruga",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/253750/header.jpg?t=1447358720",
-        "genre": "Bullet Hell",
-        "tier": 4,
-        "points": 115,
-        "createdAt": "2020-06-21T09:13:08.000Z",
+        "gameType": 3,
+        "platformId": 708,
+        "CEId": "d80873d4-ac33-4283-ad39-e2a2e3984f52",
+        "name": "Kirby's Pinball Land",
+        "header": "https://file.garden/Z1JRULbjjD-R7wit/kirby%20pinball.jpg",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2024-12-06T01:15:58.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 11,
-        "playersStarted": 10,
-        "playersTotal": 279,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 899,
-                "final": 899,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 699,
-                "final": 699,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 98000,
-                "final": 98000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1450,
-                "final": 1450,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1099,
-                "final": 1099,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 2,
+        "playersStarted": 0,
+        "playersTotal": 5,
         "milestones": {
-            "primary": 2,
-            "primaryText": "Kuro to Shiro [100\u2605]\nComplete all chapters in any mode with a grade of S++.\n\nButsutekkai [15\u2605]\nClear the game in any mode without using Continue.",
-            "community": 2,
-            "communityText": "Color Eater\nClear the game in any mode without continues and without shooting at all.\n\nMetempsychosis\nBeat the game in double play without using continues. (Any difficulty)",
-            "achievements": 25
-        }
-    },
-    "extras": {
-        "reviews": {
-            "description": "Very Positive",
-            "total": 2225,
-            "positive": 2106,
-            "negative": 119
+            "primary": 1,
+            "primaryText": "Pink Baller [5\u2605]\nDefeat King Dedede and beat the game.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 27
         }
     }
 }
-var gameOfDayDate = '2025-06-21'
+var gameOfDayDate = '2025-06-22'
