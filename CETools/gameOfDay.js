efe8e2059f315c5e10e7abf81b42035356a397d3
Icondor <ivanfigus@gmail.com>
2025-06-23 10:58:17 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,96 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 708,
-        "CEId": "d80873d4-ac33-4283-ad39-e2a2e3984f52",
-        "name": "Kirby's Pinball Land",
-        "header": "https://file.garden/Z1JRULbjjD-R7wit/kirby%20pinball.jpg",
-        "genre": "Arcade",
+        "gameType": 0,
+        "platformId": 412670,
+        "CEId": "6bd79f05-d56d-469d-a657-978d588c39fc",
+        "name": "Bullet Heaven 2",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/412670/header.jpg?t=1568759243",
+        "genre": "Bullet Hell",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2024-12-06T01:15:58.000Z",
+        "points": 15,
+        "createdAt": "2020-06-23T11:37:19.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 2,
+        "playersCompleted": 30,
         "playersStarted": 0,
-        "playersTotal": 5,
+        "playersTotal": 132,
+        "priceData": {
+            "USD": {
+                "initial": 1499,
+                "final": 1499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1379,
+                "final": 1379,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1099,
+                "final": 1099,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 150000,
+                "final": 150000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1599,
+                "final": 1599,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
             "primary": 1,
-            "primaryText": "Pink Baller [5\u2605]\nDefeat King Dedede and beat the game.",
+            "primaryText": "Heavenly Bullets [15\u2605]\nGet a star rank in all levels on \"Heavenly\" difficulty.",
             "community": 0,
             "communityText": "",
-            "achievements": 27
+            "achievements": 75
+        }
+    },
+    "extras": {
+        "reviews": {
+            "description": "Very Positive",
+            "total": 494,
+            "positive": 485,
+            "negative": 9
+        },
+        "dlc": {
+            "description": "Bullet Heaven 2 Soundtrack",
+            "count": 1,
+            "priceData": {
+                "USD": {
+                    "initial": 999,
+                    "final": 999
+                },
+                "EUR": {
+                    "initial": 799,
+                    "final": 799
+                },
+                "GBP": {
+                    "initial": 699,
+                    "final": 699
+                },
+                "JPY": {
+                    "initial": 101000,
+                    "final": 101000
+                },
+                "AUD": {
+                    "initial": 1450,
+                    "final": 1450
+                },
+                "CAD": {
+                    "initial": 1149,
+                    "final": 1149
+                }
+            }
         }
     }
 }
-var gameOfDayDate = '2025-06-22'
+var gameOfDayDate = '2025-06-23'
