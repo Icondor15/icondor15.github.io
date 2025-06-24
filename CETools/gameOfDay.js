a9dc5ef8af1aef5f31569e9f9e2a1389faa2ad29
Icondor <ivanfigus@gmail.com>
2025-06-24 09:37:26 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,67 +2,68 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 412670,
-        "CEId": "6bd79f05-d56d-469d-a657-978d588c39fc",
-        "name": "Bullet Heaven 2",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/412670/header.jpg?t=1568759243",
-        "genre": "Bullet Hell",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2020-06-23T11:37:19.000Z",
+        "platformId": 1533420,
+        "CEId": "23dfa792-591a-4f55-99ae-1c34180b22c8",
+        "name": "Neon White",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1533420/header.jpg?t=1655938828",
+        "genre": "First-Person",
+        "tier": 3,
+        "points": 65,
+        "createdAt": "2022-06-24T23:32:39.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 30,
-        "playersStarted": 0,
-        "playersTotal": 132,
+        "playersCompleted": 82,
+        "playersStarted": 84,
+        "playersTotal": 401,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1379,
-                "final": 1379,
+                "initial": 2299,
+                "final": 2299,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1099,
-                "final": 1099,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 150000,
-                "final": 150000,
-                "discountPercent": 0
-            },
-            "AUD": {
                 "initial": 1999,
                 "final": 1999,
                 "discountPercent": 0
             },
+            "JPY": {
+                "initial": 325000,
+                "final": 325000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 3695,
+                "final": 3695,
+                "discountPercent": 0
+            },
             "CAD": {
-                "initial": 1599,
-                "final": 1599,
+                "initial": 3299,
+                "final": 3299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Heavenly Bullets [15\u2605]\nGet a star rank in all levels on \"Heavenly\" difficulty.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 75
-        }
+            "primary": 4,
+            "primaryText": "Demon Exterminator [30\u2605]\nClear White's Hell Rush.\n\nRocket League [10\u2605]\nClear Mikey's Hell Rush.\n\n\"I just keep getting better and better.\" [20\u2605]\nEarn all Red Medals.\n\nHeaven's Janitor [5\u2605]\nProve yourself.",
+            "community": 5,
+            "communityText": "\"Not bad for a dead guy, huh?\"\nReach a total time of 50:00.00 or less on the global leaderboard.\n\nAim for the Heavens\nObtain a Sapphire Medal on any level within the NeonLite mod.\n\nEmerald City\nObtain an Emerald Medal on all levels within the NeonLite mod.\n\nNeon Maniac\nReach a total time of 40:00.00 or less on the global leaderboard.\n\nUltraviolet\nObtain an Amethyst Medal on all levels in a single non-boss chapter within the NeonLite mod.",
+            "achievements": 63
+        },
+        "information": " For Community Objectives, the NeonLite mod can be downloaded from: <https://github.com/Faustas156/NeonLite/releases>"
     },
     "extras": {
         "reviews": {
-            "description": "Very Positive",
-            "total": 494,
-            "positive": 485,
-            "negative": 9
+            "description": "Overwhelmingly Positive",
+            "total": 17846,
+            "positive": 17518,
+            "negative": 328
         },
         "dlc": {
-            "description": "Bullet Heaven 2 Soundtrack",
+            "description": "Neon White - Original Soundtrack",
             "count": 1,
             "priceData": {
                 "USD": {
@@ -70,27 +71,27 @@ var gameOfDay =
                     "final": 999
                 },
                 "EUR": {
+                    "initial": 929,
+                    "final": 929
+                },
+                "GBP": {
                     "initial": 799,
                     "final": 799
                 },
-                "GBP": {
-                    "initial": 699,
-                    "final": 699
-                },
                 "JPY": {
-                    "initial": 101000,
-                    "final": 101000
+                    "initial": 135000,
+                    "final": 135000
                 },
                 "AUD": {
-                    "initial": 1450,
-                    "final": 1450
+                    "initial": 1495,
+                    "final": 1495
                 },
                 "CAD": {
-                    "initial": 1149,
-                    "final": 1149
+                    "initial": 1349,
+                    "final": 1349
                 }
             }
         }
     }
 }
-var gameOfDayDate = '2025-06-23'
+var gameOfDayDate = '2025-06-24'
