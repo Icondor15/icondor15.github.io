04c83f9a6988d9ec1018a7d0d472adb06095fd6d
Icondor <ivanfigus@gmail.com>
2025-06-25 09:38:06 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,96 +2,65 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1533420,
-        "CEId": "23dfa792-591a-4f55-99ae-1c34180b22c8",
-        "name": "Neon White",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1533420/header.jpg?t=1655938828",
-        "genre": "First-Person",
-        "tier": 3,
-        "points": 65,
-        "createdAt": "2022-06-24T23:32:39.000Z",
+        "platformId": 223810,
+        "CEId": "e77c672e-3f9a-4509-9e73-2bd09d93348c",
+        "name": "Ys I",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/223810/header.jpg?t=1589525326",
+        "genre": "Action",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2020-06-25T19:48:15.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 82,
-        "playersStarted": 84,
-        "playersTotal": 401,
+        "playersCompleted": 46,
+        "playersStarted": 0,
+        "playersTotal": 149,
         "priceData": {
             "USD": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2299,
-                "final": 2299,
+                "initial": 1199,
+                "final": 1199,
                 "discountPercent": 0
             },
             "GBP": {
+                "initial": 999,
+                "final": 999,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 148000,
+                "final": 148000,
+                "discountPercent": 0
+            },
+            "AUD": {
                 "initial": 1999,
                 "final": 1999,
                 "discountPercent": 0
             },
-            "JPY": {
-                "initial": 325000,
-                "final": 325000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 3695,
-                "final": 3695,
-                "discountPercent": 0
-            },
             "CAD": {
-                "initial": 3299,
-                "final": 3299,
+                "initial": 1699,
+                "final": 1699,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Demon Exterminator [30\u2605]\nClear White's Hell Rush.\n\nRocket League [10\u2605]\nClear Mikey's Hell Rush.\n\n\"I just keep getting better and better.\" [20\u2605]\nEarn all Red Medals.\n\nHeaven's Janitor [5\u2605]\nProve yourself.",
-            "community": 5,
-            "communityText": "\"Not bad for a dead guy, huh?\"\nReach a total time of 50:00.00 or less on the global leaderboard.\n\nAim for the Heavens\nObtain a Sapphire Medal on any level within the NeonLite mod.\n\nEmerald City\nObtain an Emerald Medal on all levels within the NeonLite mod.\n\nNeon Maniac\nReach a total time of 40:00.00 or less on the global leaderboard.\n\nUltraviolet\nObtain an Amethyst Medal on all levels in a single non-boss chapter within the NeonLite mod.",
-            "achievements": 63
-        },
-        "information": " For Community Objectives, the NeonLite mod can be downloaded from: <https://github.com/Faustas156/NeonLite/releases>"
+            "primary": 1,
+            "primaryText": "Adol Begins [15\u2605]\nComplete Ys I on Nightmare difficulty. / Complete Ys I Time Attack.",
+            "community": 1,
+            "communityText": "Adol the Red\nComplete all primary objectives from all site-listed games in the Ys franchise. See verification requirements for complete list.",
+            "achievements": 17
+        }
     },
     "extras": {
         "reviews": {
-            "description": "Overwhelmingly Positive",
-            "total": 17846,
-            "positive": 17518,
-            "negative": 328
-        },
-        "dlc": {
-            "description": "Neon White - Original Soundtrack",
-            "count": 1,
-            "priceData": {
-                "USD": {
-                    "initial": 999,
-                    "final": 999
-                },
-                "EUR": {
-                    "initial": 929,
-                    "final": 929
-                },
-                "GBP": {
-                    "initial": 799,
-                    "final": 799
-                },
-                "JPY": {
-                    "initial": 135000,
-                    "final": 135000
-                },
-                "AUD": {
-                    "initial": 1495,
-                    "final": 1495
-                },
-                "CAD": {
-                    "initial": 1349,
-                    "final": 1349
-                }
-            }
+            "description": "Very Positive",
+            "total": 2748,
+            "positive": 2446,
+            "negative": 302
         }
     }
 }
-var gameOfDayDate = '2025-06-24'
+var gameOfDayDate = '2025-06-25'
