db12fdb44259b993aa234144173b76d1f2521250
Icondor <ivanfigus@gmail.com>
2025-06-27 09:38:54 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,65 +2,65 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1876560,
-        "CEId": "5dc49366-4bc0-45ea-886a-76bb50e64fae",
-        "name": "Save the Reactor",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1876560/header.jpg?t=1656496630",
-        "genre": "Arcade",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2022-06-26T10:32:40.000Z",
+        "platformId": 1566410,
+        "CEId": "7d0aae8e-6d89-490d-9ef4-ae9ad428cc4a",
+        "name": "Touhou Kouryudou ~ Unconnected Marketeers.",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1566410/header.jpg?t=1621839528",
+        "genre": "Bullet Hell",
+        "tier": 3,
+        "points": 65,
+        "createdAt": "2023-06-27T00:26:39.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 15,
-        "playersStarted": 9,
-        "playersTotal": 51,
+        "playersCompleted": 5,
+        "playersStarted": 12,
+        "playersTotal": 75,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1249,
+                "final": 1249,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 429,
-                "final": 429,
+                "initial": 1139,
+                "final": 1139,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 58000,
-                "final": 58000,
+                "initial": 154000,
+                "final": 154000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 2150,
+                "final": 2150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 649,
-                "final": 649,
+                "initial": 1749,
+                "final": 1749,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Did It All For The Cookie [10\u2605]\nGet the gold medal in Endless Mode by scoring 3300 points or more.\n\nOrdinary Janitor [5\u2605]\nGet the bronze medal in Endless Mode by scoring 2500 points or more. / Produce 4500 energy on Day 10.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 21
+            "primary": 4,
+            "primaryText": "Black Monday [40\u2605]\nBeat the game on Lunatic without deaths.\n\nMarket Crash [15\u2605]\nBeat the game on Lunatic without continues.\n\nCentipede Slayer [5\u2605]\nBeat the Extra mode.\n\nShopping Spree [5\u2605]\nBeat the game on Normal or higher without continues.",
+            "community": 2,
+            "communityText": "Golden Centipede\nBeat the Extra mode without dying, bombing and equipping cards.\n\nOut of Stock\nBeat the game on Lunatic without dying, bombing and equipping cards.",
+            "achievements": 0
         }
     },
     "extras": {
         "reviews": {
-            "description": "Very Positive",
-            "total": 118,
-            "positive": 112,
-            "negative": 6
+            "description": "Overwhelmingly Positive",
+            "total": 1776,
+            "positive": 1738,
+            "negative": 38
         }
     }
 }
-var gameOfDayDate = '2025-06-26'
+var gameOfDayDate = '2025-06-27'
