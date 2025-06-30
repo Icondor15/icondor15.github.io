8b42c85e64fd9441cf9b748362ee9c38a831c157
Icondor <ivanfigus@gmail.com>
2025-06-30 10:13:04 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,65 +2,65 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1606210,
-        "CEId": "15eafeba-cdd5-4b79-9caf-b3bf4b60ff63",
-        "name": "Heck Deck",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1606210/header.jpg?t=1641916235",
+        "platformId": 2228030,
+        "CEId": "b84c079f-d721-4baa-8d51-07876c632131",
+        "name": "Dr. Fetus' Mean Meat Machine",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2228030/header.jpg?t=1687769270",
         "genre": "Arcade",
         "tier": 1,
-        "points": 16,
-        "createdAt": "2022-06-29T01:36:21.000Z",
+        "points": 10,
+        "createdAt": "2023-06-30T02:17:38.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 4,
-        "playersTotal": 37,
+        "playersCompleted": 8,
+        "playersStarted": 0,
+        "playersTotal": 55,
         "priceData": {
             "USD": {
-                "initial": 399,
-                "final": 79,
-                "discountPercent": 80
+                "initial": 999,
+                "final": 119,
+                "discountPercent": 88
             },
             "EUR": {
-                "initial": 399,
-                "final": 79,
-                "discountPercent": 80
+                "initial": 999,
+                "final": 119,
+                "discountPercent": 88
             },
             "GBP": {
-                "initial": 289,
-                "final": 57,
-                "discountPercent": 80
+                "initial": 799,
+                "final": 95,
+                "discountPercent": 88
             },
             "JPY": {
-                "initial": 41000,
-                "final": 8200,
-                "discountPercent": 80
+                "initial": 120000,
+                "final": 14400,
+                "discountPercent": 88
             },
             "AUD": {
-                "initial": 595,
-                "final": 119,
-                "discountPercent": 80
+                "initial": 1450,
+                "final": 174,
+                "discountPercent": 88
             },
             "CAD": {
-                "initial": 465,
-                "final": 93,
-                "discountPercent": 80
+                "initial": 1299,
+                "final": 155,
+                "discountPercent": 88
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Bullet Heck (UNCLEARED) [1\u2605]\nBeat real time mode as all characters without deaths.\n\nYou Only Heck Once [10\u2605]\nBeat the game as all characters without dying. / Clear the real time mode without dying.\n\nSUPERHECK [5\u2605]\nBeat the game as all characters. / Clear the real time mode.",
+            "primary": 1,
+            "primaryText": "Meat Flipper [10\u2605]\nGet the A+ Rank on all Levels",
             "community": 0,
             "communityText": "",
-            "achievements": 19
+            "achievements": 30
         }
     },
     "extras": {
         "reviews": {
-            "description": "Very Positive",
-            "total": 62,
-            "positive": 57,
-            "negative": 5
+            "description": "Mixed",
+            "total": 220,
+            "positive": 143,
+            "negative": 77
         }
     }
 }
-var gameOfDayDate = '2025-06-29'
+var gameOfDayDate = '2025-06-30'
