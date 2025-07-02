3ff746655cdec2f8b771344c76369bd4065608f1
Icondor <ivanfigus@gmail.com>
2025-07-02 09:21:44 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,65 +2,65 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 233150,
-        "CEId": "90954c44-0e5c-433e-876f-a06f609d2b17",
-        "name": "LUFTRAUSERS",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/233150/header.jpg?t=1615922153",
+        "platformId": 774181,
+        "CEId": "28977539-4671-4634-8683-b5a192bf0059",
+        "name": "Rhythm Doctor",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/774181/header.jpg?t=1656535686",
         "genre": "Arcade",
         "tier": 1,
-        "points": 10,
-        "createdAt": "2022-07-01T20:52:18.000Z",
+        "points": 15,
+        "createdAt": "2022-07-02T21:17:05.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 22,
+        "playersCompleted": 40,
         "playersStarted": 0,
-        "playersTotal": 196,
+        "playersTotal": 298,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 199,
-                "discountPercent": 80
+                "initial": 1599,
+                "final": 1439,
+                "discountPercent": 10
             },
             "EUR": {
-                "initial": 975,
-                "final": 195,
-                "discountPercent": 80
+                "initial": 1579,
+                "final": 1421,
+                "discountPercent": 10
             },
             "GBP": {
-                "initial": 850,
-                "final": 170,
-                "discountPercent": 80
+                "initial": 1349,
+                "final": 1214,
+                "discountPercent": 10
             },
             "JPY": {
-                "initial": 120000,
-                "final": 24000,
-                "discountPercent": 80
+                "initial": 180000,
+                "final": 162000,
+                "discountPercent": 10
             },
             "AUD": {
-                "initial": 1450,
-                "final": 290,
-                "discountPercent": 80
+                "initial": 2350,
+                "final": 2115,
+                "discountPercent": 10
             },
             "CAD": {
-                "initial": 1299,
-                "final": 259,
-                "discountPercent": 80
+                "initial": 2079,
+                "final": 1871,
+                "discountPercent": 10
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Flying Ace [10\u2605]\nBeat all missions and launch the [CLASSIFIED].",
+            "primaryText": "Polyrhythmic Practitioner [15\u2605]\nGet an S+ rank on every song.",
             "community": 0,
             "communityText": "",
-            "achievements": 12
+            "achievements": 24
         }
     },
     "extras": {
         "reviews": {
-            "description": "Very Positive",
-            "total": 4428,
-            "positive": 4022,
-            "negative": 406
+            "description": "Overwhelmingly Positive",
+            "total": 21283,
+            "positive": 20951,
+            "negative": 332
         }
     }
 }
-var gameOfDayDate = '2025-07-01'
+var gameOfDayDate = '2025-07-02'
