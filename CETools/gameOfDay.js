71ec913714927fec24bb05e05a128e6b2b1f1774
Icondor <ivanfigus@gmail.com>
2025-07-04 09:40:23 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,95 +2,96 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1472540,
-        "CEId": "115d23f3-cc07-41d9-949b-26defdc5b6f0",
-        "name": "Save me Mr Tako: Definitive Edition",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1472540/header.jpg?t=1687264251",
+        "platformId": 331190,
+        "CEId": "42633477-6588-4243-8feb-0662dff7cea9",
+        "name": "Master Spy",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/331190/header.jpg?t=1632849122",
         "genre": "Platformer",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2023-07-03T23:33:03.000Z",
+        "tier": 2,
+        "points": 30,
+        "createdAt": "2023-07-04T00:36:02.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
         "playersCompleted": 1,
-        "playersStarted": 0,
-        "playersTotal": 14,
+        "playersStarted": 1,
+        "playersTotal": 88,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 749,
-                "discountPercent": 50
+                "initial": 999,
+                "final": 99,
+                "discountPercent": 90
             },
             "EUR": {
-                "initial": 1249,
-                "final": 624,
-                "discountPercent": 50
+                "initial": 899,
+                "final": 89,
+                "discountPercent": 90
             },
             "GBP": {
-                "initial": 1139,
-                "final": 569,
-                "discountPercent": 50
+                "initial": 699,
+                "final": 69,
+                "discountPercent": 90
             },
             "JPY": {
-                "initial": 152000,
-                "final": 76000,
-                "discountPercent": 50
+                "initial": 98000,
+                "final": 9800,
+                "discountPercent": 90
             },
             "AUD": {
-                "initial": 2150,
-                "final": 1075,
-                "discountPercent": 50
+                "initial": 1450,
+                "final": 145,
+                "discountPercent": 90
             },
             "CAD": {
-                "initial": 1749,
-                "final": 874,
-                "discountPercent": 50
+                "initial": 1099,
+                "final": 109,
+                "discountPercent": 90
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Hattako [15\u2605]\nBeat all the bosses consecutively in Boss Mode. / Beat the game without seeing the Game Over screen.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 55
-        }
+            "primary": 3,
+            "primaryText": "Stealth Master [15\u2605]\nComplete each mission with a Master rank on Operative difficulty.\n\nUndercover Virtuoso [10\u2605]\nComplete the game on Master difficulty or above.\n\nEntry-Level Espionage [5\u2605]\nComplete the game and all VR levels on Operative difficulty or above (level select is allowed).",
+            "community": 1,
+            "communityText": "Mission: Impossible\nStart a new game and complete every mission with a Master rank on Operative difficulty or above.",
+            "achievements": 0
+        },
+        "information": "The game only shows Master ranks upon completing a chapter in the campaign, which is highly inconvenient. So, you're allowed to simulate them through level select. For Master ranks, you are not allowed to get caught or use resets. For Mission 1, the sum of level times must be 1:30 or less. For Missions 2 through 5 individually, 8:00 or less."
     },
     "extras": {
         "reviews": {
-            "description": "Positive",
-            "total": 43,
-            "positive": 35,
-            "negative": 8
+            "description": "Mostly Positive",
+            "total": 386,
+            "positive": 303,
+            "negative": 83
         },
         "dlc": {
-            "description": "Save me Mr Tako Original Soundtrack",
+            "description": "Master Spy OST",
             "count": 1,
             "priceData": {
                 "USD": {
-                    "initial": 599,
-                    "final": 599
+                    "initial": 499,
+                    "final": 84
                 },
                 "EUR": {
-                    "initial": 499,
-                    "final": 499
+                    "initial": 399,
+                    "final": 67
                 },
                 "GBP": {
-                    "initial": 479,
-                    "final": 479
+                    "initial": 399,
+                    "final": 67
                 },
                 "JPY": {
-                    "initial": 62000,
-                    "final": 62000
+                    "initial": 49800,
+                    "final": 8400
                 },
                 "AUD": {
-                    "initial": 850,
-                    "final": 850
+                    "initial": 750,
+                    "final": 127
                 },
                 "CAD": {
-                    "initial": 669,
-                    "final": 669
+                    "initial": 549,
+                    "final": 93
                 }
             }
         }
     }
 }
-var gameOfDayDate = '2025-07-03'
+var gameOfDayDate = '2025-07-04'
