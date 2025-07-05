f672d7621f78293760e652bce3fc7607622ea171
Icondor <ivanfigus@gmail.com>
2025-07-05 09:37:39 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,18 +2,18 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 331190,
-        "CEId": "42633477-6588-4243-8feb-0662dff7cea9",
-        "name": "Master Spy",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/331190/header.jpg?t=1632849122",
-        "genre": "Platformer",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2023-07-04T00:36:02.000Z",
+        "platformId": 1202970,
+        "CEId": "9c6729e1-7044-4ed3-a5ac-e8f2eb2f6e3f",
+        "name": "Mad Tracks",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1202970/header.jpg?t=1702891972",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2024-07-05T00:22:05.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 1,
-        "playersTotal": 88,
+        "playersCompleted": 5,
+        "playersStarted": 0,
+        "playersTotal": 42,
         "priceData": {
             "USD": {
                 "initial": 999,
@@ -21,18 +21,18 @@ var gameOfDay =
                 "discountPercent": 90
             },
             "EUR": {
-                "initial": 899,
-                "final": 89,
+                "initial": 999,
+                "final": 99,
                 "discountPercent": 90
             },
             "GBP": {
-                "initial": 699,
-                "final": 69,
+                "initial": 719,
+                "final": 71,
                 "discountPercent": 90
             },
             "JPY": {
-                "initial": 98000,
-                "final": 9800,
+                "initial": 101000,
+                "final": 10100,
                 "discountPercent": 90
             },
             "AUD": {
@@ -41,57 +41,26 @@ var gameOfDay =
                 "discountPercent": 90
             },
             "CAD": {
-                "initial": 1099,
-                "final": 109,
+                "initial": 1149,
+                "final": 114,
                 "discountPercent": 90
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Stealth Master [15\u2605]\nComplete each mission with a Master rank on Operative difficulty.\n\nUndercover Virtuoso [10\u2605]\nComplete the game on Master difficulty or above.\n\nEntry-Level Espionage [5\u2605]\nComplete the game and all VR levels on Operative difficulty or above (level select is allowed).",
-            "community": 1,
-            "communityText": "Mission: Impossible\nStart a new game and complete every mission with a Master rank on Operative difficulty or above.",
-            "achievements": 0
-        },
-        "information": "The game only shows Master ranks upon completing a chapter in the campaign, which is highly inconvenient. So, you're allowed to simulate them through level select. For Master ranks, you are not allowed to get caught or use resets. For Mission 1, the sum of level times must be 1:30 or less. For Missions 2 through 5 individually, 8:00 or less."
+            "primary": 1,
+            "primaryText": "Mighty Mad Micro Machines [5\u2605]\nProve yourself.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 13
+        }
     },
     "extras": {
         "reviews": {
-            "description": "Mostly Positive",
-            "total": 386,
-            "positive": 303,
-            "negative": 83
-        },
-        "dlc": {
-            "description": "Master Spy OST",
-            "count": 1,
-            "priceData": {
-                "USD": {
-                    "initial": 499,
-                    "final": 84
-                },
-                "EUR": {
-                    "initial": 399,
-                    "final": 67
-                },
-                "GBP": {
-                    "initial": 399,
-                    "final": 67
-                },
-                "JPY": {
-                    "initial": 49800,
-                    "final": 8400
-                },
-                "AUD": {
-                    "initial": 750,
-                    "final": 127
-                },
-                "CAD": {
-                    "initial": 549,
-                    "final": 93
-                }
-            }
+            "description": "Very Positive",
+            "total": 263,
+            "positive": 231,
+            "negative": 32
         }
     }
 }
-var gameOfDayDate = '2025-07-04'
+var gameOfDayDate = '2025-07-05'
