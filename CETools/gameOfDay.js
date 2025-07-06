657d19193c63c527727b839a9a4eb9f725b79f04
Icondor <ivanfigus@gmail.com>
2025-07-06 08:39:24 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,66 +1,34 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1202970,
-        "CEId": "9c6729e1-7044-4ed3-a5ac-e8f2eb2f6e3f",
-        "name": "Mad Tracks",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1202970/header.jpg?t=1702891972",
-        "genre": "Arcade",
+        "gameType": 1,
+        "platformId": 1961770,
+        "CEId": "66011c02-3adb-460b-b20b-98cc0bc6e55c",
+        "name": "Ball Go Home",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1961770/header.jpg?t=1675958332",
+        "genre": "Platformer",
         "tier": 1,
         "points": 5,
-        "createdAt": "2024-07-05T00:22:05.000Z",
+        "createdAt": "2024-07-06T17:48:26.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 5,
+        "playersCompleted": 8,
         "playersStarted": 0,
-        "playersTotal": 42,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 99,
-                "discountPercent": 90
-            },
-            "EUR": {
-                "initial": 999,
-                "final": 99,
-                "discountPercent": 90
-            },
-            "GBP": {
-                "initial": 719,
-                "final": 71,
-                "discountPercent": 90
-            },
-            "JPY": {
-                "initial": 101000,
-                "final": 10100,
-                "discountPercent": 90
-            },
-            "AUD": {
-                "initial": 1450,
-                "final": 145,
-                "discountPercent": 90
-            },
-            "CAD": {
-                "initial": 1149,
-                "final": 114,
-                "discountPercent": 90
-            }
-        },
+        "playersTotal": 75,
         "milestones": {
             "primary": 1,
-            "primaryText": "Mighty Mad Micro Machines [5\u2605]\nProve yourself.",
+            "primaryText": "Homecoming [5\u2605]\nFinish all levels.",
             "community": 0,
             "communityText": "",
-            "achievements": 13
+            "achievements": 0
         }
     },
     "extras": {
         "reviews": {
-            "description": "Very Positive",
-            "total": 263,
-            "positive": 231,
-            "negative": 32
+            "description": "Positive",
+            "total": 11,
+            "positive": 10,
+            "negative": 1
         }
     }
 }
-var gameOfDayDate = '2025-07-05'
+var gameOfDayDate = '2025-07-06'
