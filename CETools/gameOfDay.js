f11a34b96209e53294a9f35899135531d080cd84
Icondor <ivanfigus@gmail.com>
2025-07-07 10:31:14 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,34 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 1961770,
-        "CEId": "66011c02-3adb-460b-b20b-98cc0bc6e55c",
-        "name": "Ball Go Home",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1961770/header.jpg?t=1675958332",
-        "genre": "Platformer",
+        "gameType": 3,
+        "platformId": 17650,
+        "CEId": "f9773360-2f64-4cbb-9231-8631e936e42d",
+        "name": "Olympic Trials",
+        "header": "https://i.imgur.com/abJpmzj.png",
+        "genre": "Arcade",
         "tier": 1,
         "points": 5,
-        "createdAt": "2024-07-06T17:48:26.000Z",
+        "createdAt": "2024-07-07T05:05:32.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 8,
+        "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 75,
+        "playersTotal": 4,
         "milestones": {
             "primary": 1,
-            "primaryText": "Homecoming [5\u2605]\nFinish all levels.",
+            "primaryText": "Olympic Visionary [5\u2605]\nBeat the world record in all 6 events and finish a full game loop.",
             "community": 0,
             "communityText": "",
-            "achievements": 0
-        }
-    },
-    "extras": {
-        "reviews": {
-            "description": "Positive",
-            "total": 11,
-            "positive": 10,
-            "negative": 1
+            "achievements": 9
         }
     }
 }
-var gameOfDayDate = '2025-07-06'
+var gameOfDayDate = '2025-07-07'
