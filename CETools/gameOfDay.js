7180f7c14b95114392b6da308c7887436d8ba99d
Icondor <ivanfigus@gmail.com>
2025-07-10 09:12:38 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,66 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 349680,
-        "CEId": "c956a227-1f07-46f9-9127-7fe0d4d27537",
-        "name": "Supercharged Robot VULKAISER",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/349680/header.jpg?t=1598071324",
-        "genre": "Bullet Hell",
+        "gameType": 3,
+        "platformId": 7994,
+        "CEId": "278f8af4-c737-4955-904d-0a058f2d07ab",
+        "name": "Guitar Hero",
+        "header": "https://upload.cedb.me/E3ZexI.png",
+        "genre": "Arcade",
         "tier": 2,
-        "points": 25,
-        "createdAt": "2022-07-09T11:13:26.000Z",
+        "points": 20,
+        "createdAt": "2024-07-10T21:49:35.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 6,
+        "playersCompleted": 5,
         "playersStarted": 0,
-        "playersTotal": 55,
-        "priceData": {
-            "USD": {
-                "initial": 499,
-                "final": 199,
-                "discountPercent": 60
-            },
-            "EUR": {
-                "initial": 499,
-                "final": 199,
-                "discountPercent": 60
-            },
-            "GBP": {
-                "initial": 429,
-                "final": 171,
-                "discountPercent": 60
-            },
-            "JPY": {
-                "initial": 49800,
-                "final": 19900,
-                "discountPercent": 60
-            },
-            "AUD": {
-                "initial": 750,
-                "final": 300,
-                "discountPercent": 60
-            },
-            "CAD": {
-                "initial": 649,
-                "final": 259,
-                "discountPercent": 60
-            }
-        },
+        "playersTotal": 8,
         "milestones": {
             "primary": 1,
-            "primaryText": "GO, GO, VULKAISER! [25\u2605]\nClear the game in Super-Hard Mode.",
+            "primaryText": "Harmonix [20\u2605]\nGet an Expert Five Star clear on all 47 songs.",
             "community": 0,
             "communityText": "",
-            "achievements": 33
-        }
-    },
-    "extras": {
-        "reviews": {
-            "description": "Very Positive",
-            "total": 320,
-            "positive": 300,
-            "negative": 20
+            "achievements": 79
         }
     }
 }
-var gameOfDayDate = '2025-07-09'
+var gameOfDayDate = '2025-07-10'
