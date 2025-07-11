dd7a5f9816e70cc2941ea6aab3aeade340697e9b
Icondor <ivanfigus@gmail.com>
2025-07-11 08:06:07 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,59 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 7994,
-        "CEId": "278f8af4-c737-4955-904d-0a058f2d07ab",
-        "name": "Guitar Hero",
-        "header": "https://upload.cedb.me/E3ZexI.png",
-        "genre": "Arcade",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2024-07-10T21:49:35.000Z",
+        "gameType": 0,
+        "platformId": 1088790,
+        "CEId": "9357297c-ae7d-4997-9472-a205bd602732",
+        "name": "Rebel Inc: Escalation",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1088790/header.jpg?t=1676464547",
+        "genre": "Strategy",
+        "tier": 3,
+        "points": 51,
+        "createdAt": "2023-07-11T06:06:20.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 5,
-        "playersStarted": 0,
-        "playersTotal": 8,
+        "playersCompleted": 0,
+        "playersStarted": 3,
+        "playersTotal": 129,
+        "priceData": {
+            "USD": {
+                "initial": 1499,
+                "final": 1499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1479,
+                "final": 1479,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1279,
+                "final": 1279,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 170000,
+                "final": 170000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2195,
+                "final": 2195,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1949,
+                "final": 1949,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Harmonix [20\u2605]\nGet an Expert Five Star clear on all 47 songs.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 79
-        }
+            "primary": 5,
+            "primaryText": "Synergistic Commanders (UNCLEARED) [1\u2605]\nComplete all 9 stages of a multiplayer co-op campaign on Mega-Brutal difficulty.\n\nWorld Domination [25\u2605]\nBeat Black Caves, Azure Dam, Opium Trail, Golden Sands, and Devil's Peak with every governor, and all maps as tank commander on Mega-Brutal difficulty.\n\nUnyielding Conquest [10\u2605]\nComplete every Official Scenario on Mega-Brutal difficulty.\n\n Vive La R\u00e9sistance! [10\u2605]\nBeat Campaign Mode on Mega-Brutal difficulty.\n\nFreedom's Cry [5\u2605]\nBeat the first five maps with every governor on Brutal difficulty. / Beat Campaign Mode on Brutal difficulty.",
+            "community": 2,
+            "communityText": "Scorched Earth\nBeat all maps with all governors on Mega-Brutal difficulty.\n\nFive-Star General\nBeat Campaign Mode on Mega-Brutal difficulty with a five-star rating.",
+            "achievements": 99
+        },
+        "information": "Dollars & Disasters, and Sand & Secrets DLC are required for World Domination and Unyielding Conquest POs."
     }
 }
-var gameOfDayDate = '2025-07-10'
+var gameOfDayDate = '2025-07-11'
