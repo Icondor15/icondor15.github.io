5ef0673f1771501f27a4481d0d235ddc0767bc0c
Icondor <ivanfigus@gmail.com>
2025-07-12 09:12:25 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,58 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1088790,
-        "CEId": "9357297c-ae7d-4997-9472-a205bd602732",
-        "name": "Rebel Inc: Escalation",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1088790/header.jpg?t=1676464547",
-        "genre": "Strategy",
-        "tier": 3,
-        "points": 51,
-        "createdAt": "2023-07-11T06:06:20.000Z",
+        "platformId": 250580,
+        "CEId": "057827ec-de03-4d6c-9791-509255e1970d",
+        "name": "Paranautical Activity: Deluxe Atonement Edition",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/250580/header.jpg?t=1593028220",
+        "genre": "First-Person",
+        "tier": 2,
+        "points": 25,
+        "createdAt": "2024-07-12T04:07:18.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 3,
-        "playersTotal": 129,
+        "playersCompleted": 2,
+        "playersStarted": 0,
+        "playersTotal": 95,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1479,
-                "final": 1479,
+                "initial": 699,
+                "final": 699,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1279,
-                "final": 1279,
+                "initial": 699,
+                "final": 699,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 170000,
-                "final": 170000,
+                "initial": 98000,
+                "final": 98000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2195,
-                "final": 2195,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1949,
-                "final": 1949,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 5,
-            "primaryText": "Synergistic Commanders (UNCLEARED) [1\u2605]\nComplete all 9 stages of a multiplayer co-op campaign on Mega-Brutal difficulty.\n\nWorld Domination [25\u2605]\nBeat Black Caves, Azure Dam, Opium Trail, Golden Sands, and Devil's Peak with every governor, and all maps as tank commander on Mega-Brutal difficulty.\n\nUnyielding Conquest [10\u2605]\nComplete every Official Scenario on Mega-Brutal difficulty.\n\n Vive La R\u00e9sistance! [10\u2605]\nBeat Campaign Mode on Mega-Brutal difficulty.\n\nFreedom's Cry [5\u2605]\nBeat the first five maps with every governor on Brutal difficulty. / Beat Campaign Mode on Brutal difficulty.",
-            "community": 2,
-            "communityText": "Scorched Earth\nBeat all maps with all governors on Mega-Brutal difficulty.\n\nFive-Star General\nBeat Campaign Mode on Mega-Brutal difficulty with a five-star rating.",
-            "achievements": 99
-        },
-        "information": "Dollars & Disasters, and Sand & Secrets DLC are required for World Domination and Unyielding Conquest POs."
+            "primary": 2,
+            "primaryText": "Picky Eater [15\u2605]\nBeat the game without taking any items.\n\nAtonement [10\u2605]\nProve yourself.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 45
+        }
     }
 }
-var gameOfDayDate = '2025-07-11'
+var gameOfDayDate = '2025-07-12'
