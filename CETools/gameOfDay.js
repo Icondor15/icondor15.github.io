68700b5cf525883d1cbc6085f7dec665bfe8f570
Icondor <ivanfigus@gmail.com>
2025-07-14 09:13:04 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2390650,
-        "CEId": "1fa27e34-8c82-4e71-aaf7-140d878e1f91",
-        "name": "Boomeraxe",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2390650/header.jpg?t=1708058799",
-        "genre": "Bullet Hell",
+        "platformId": 1304420,
+        "CEId": "ef102261-6ec4-4325-819a-620f7b70c793",
+        "name": "Super Cable Boy",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1304420/header.jpg?t=1635417304",
+        "genre": "Platformer",
         "tier": 1,
-        "points": 10,
-        "createdAt": "2024-07-13T06:37:34.000Z",
+        "points": 15,
+        "createdAt": "2022-07-14T11:55:22.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 2,
-        "playersTotal": 9,
+        "playersCompleted": 12,
+        "playersStarted": 0,
+        "playersTotal": 60,
         "priceData": {
             "USD": {
-                "initial": 199,
-                "final": 199,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 199,
-                "final": 199,
+                "initial": 1399,
+                "final": 1399,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 169,
-                "final": 169,
+                "initial": 1239,
+                "final": 1239,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 23500,
-                "final": 23500,
+                "initial": 164000,
+                "final": 164000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 295,
-                "final": 295,
+                "initial": 2295,
+                "final": 2295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 259,
-                "final": 259,
+                "initial": 1849,
+                "final": 1849,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Return to Sender [5\u2605]\nWin a run without taking any powerups.\n\nIt Axes and Comes Backses [5\u2605]\nBeat the game.",
+            "primary": 1,
+            "primaryText": "Omusubi Connoisseur [15\u2605]\nCollect all Onigiris.",
             "community": 0,
             "communityText": "",
-            "achievements": 12
+            "achievements": 41
         }
     }
 }
-var gameOfDayDate = '2025-07-13'
+var gameOfDayDate = '2025-07-14'
