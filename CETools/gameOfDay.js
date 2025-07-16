fa38ab17a221cc59717ffaedd6bca87a29e2798c
Icondor <ivanfigus@gmail.com>
2025-07-16 09:13:53 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,18 +2,18 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1779180,
-        "CEId": "ebae404f-0ea1-4b51-b624-949bc0c90aef",
-        "name": "Knight's Try",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1779180/header.jpg?t=1643317706",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2022-07-15T23:02:50.000Z",
+        "platformId": 1797680,
+        "CEId": "67572fba-5a2b-45fe-ad40-2a8ff2dd7f26",
+        "name": "Wee Tanks!",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1797680/header.jpg?t=1688732888",
+        "genre": "Action",
+        "tier": 4,
+        "points": 185,
+        "createdAt": "2023-07-16T20:24:38.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 14,
+        "playersCompleted": 1,
+        "playersStarted": 9,
+        "playersTotal": 74,
         "priceData": {
             "USD": {
                 "initial": 999,
@@ -21,18 +21,18 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 819,
-                "final": 819,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 719,
-                "final": 719,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 101000,
-                "final": 101000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
@@ -47,12 +47,12 @@ var gameOfDay =
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "First Try [10\u2605]\nComplete the game in a single try.",
+            "primary": 3,
+            "primaryText": "One Last Weep [125\u2605]\nFinish the full campaign on any difficulty without getting killed once. / Complete the True Run challenge solo on grandpa difficulty.\n\nLucky Cadet [45\u2605]\nFinish the full campaign on at least kid difficulty in one session. / Defeat the King Tank on adult difficulty without any armor.\n\nCrisis Core [15\u2605]\nObtain all achievements in survival mode.",
             "community": 0,
             "communityText": "",
-            "achievements": 13
+            "achievements": 51
         }
     }
 }
-var gameOfDayDate = '2025-07-15'
+var gameOfDayDate = '2025-07-16'
