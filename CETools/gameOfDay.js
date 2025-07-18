27832523cf965282a689d28ba4684098e3fb0f64
Icondor <ivanfigus@gmail.com>
2025-07-18 09:12:28 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,58 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2326350,
-        "CEId": "d1cddef6-5077-4bdd-afe6-431d10248fa1",
-        "name": "Grey Area",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2326350/header.jpg?t=1694769595",
-        "genre": "Platformer",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2024-07-17T07:34:08.000Z",
+        "platformId": 2860150,
+        "CEId": "1017571b-2377-492e-b991-df1ab23720c4",
+        "name": "Asteroid Drift",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2860150/header.jpg?t=1718004677",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2024-07-18T02:52:02.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
         "playersCompleted": 2,
         "playersStarted": 0,
-        "playersTotal": 7,
+        "playersTotal": 6,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 299,
+                "final": 299,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1950,
-                "final": 1950,
+                "initial": 299,
+                "final": 299,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1675,
-                "final": 1675,
+                "initial": 249,
+                "final": 249,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 230000,
-                "final": 230000,
+                "initial": 35000,
+                "final": 35000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2950,
-                "final": 2950,
+                "initial": 450,
+                "final": 450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2599,
-                "final": 2599,
+                "initial": 389,
+                "final": 389,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Greybound [25\u2605]\nBeat the game on Punished difficulty.\n\nCausal Defect [5\u2605]\nReach the alternate ending on any difficulty.",
+            "primary": 1,
+            "primaryText": "Fasteroid [5\u2605]\nAchieve a Divine time on all levels.",
             "community": 1,
-            "communityText": "50 Shades of Grey Area\nReach the alternate ending on Punished difficulty.",
-            "achievements": 17
-        },
-        "information": "Reaching the alternate ending requires unlocking and beating the secret level, which requires finding all collectibles and then using the soul attack ability on the Goddess of Ichor.\n\nIn regards to CO proof, the bunny jump ability automatically recharges on Punished difficulty exclusively."
+            "communityText": "In the Nick of Time\nBeat Nick's Time of 7:21:64.",
+            "achievements": 32
+        }
     }
 }
-var gameOfDayDate = '2025-07-17'
+var gameOfDayDate = '2025-07-18'
