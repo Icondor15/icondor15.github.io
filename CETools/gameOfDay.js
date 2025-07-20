09b1d96439137e4fdaabd68cf57f3e4c47d97ec4
Icondor <ivanfigus@gmail.com>
2025-07-20 08:52:08 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,27 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1766810,
-        "CEId": "586ffe39-18a4-42ed-8f0e-293dedc66a88",
-        "name": "SnOut 2",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1766810/header.jpg?t=1645531215",
+        "gameType": 3,
+        "platformId": 5377,
+        "CEId": "86723119-e5b6-4621-be5e-243b3785726f",
+        "name": "Kuru Kuru Kururin",
+        "header": "https://upload.cedb.me/3i4JYy.png",
         "genre": "Arcade",
-        "tier": 3,
-        "points": 50,
-        "createdAt": "2022-07-19T05:54:53.000Z",
+        "tier": 2,
+        "points": 25,
+        "createdAt": "2024-07-20T21:04:15.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 2,
-        "playersTotal": 14,
-        "priceData": {
-            "USD": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 339,
-                "final": 339,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 47000,
-                "final": 47000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 595,
-                "final": 595,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 11,
+        "playersStarted": 1,
+        "playersTotal": 19,
         "milestones": {
             "primary": 2,
-            "primaryText": "Anaconda [40\u2605]\nAchieve 10,000 points in a single Endless Mode run.\n\nFusion-Ha [10\u2605]\nProve yourself.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 13
-        }
+            "primaryText": "Helirin Hero [15\u2605]\nBeat the Master time on all adventure and challenge levels\n\nCopter Captain [10\u2605]\nComplete all adventure and challenge levels without taking a hit / Collect all collectibles.",
+            "community": 1,
+            "communityText": "Whirler of Worlds\nComplete all primary objectives from all site-listed games in the Kuru Kuru Kururin franchise. See verification requirements for complete list.",
+            "achievements": 64
+        },
+        "information": "Copter Captain - The final 3x levels in Adventure Mode, and 5x levels in Challenge Mode unlock when all previous levels in that mode have a perfect (no hit) rating.\n\nHelirin Hero - Master times become visible in game after beating all \"P\" times. The achievement will not unlock if there are tied times, even if the leaderboard shows your name first.\n\nAll master times can be found here:\nhttps://docs.google.com/spreadsheets/d/1Znf-trw9suYPw295mvdcTGaCSGX-bZTSM26ITp1z02g/"
     }
 }
-var gameOfDayDate = '2025-07-19'
+var gameOfDayDate = '2025-07-20'
