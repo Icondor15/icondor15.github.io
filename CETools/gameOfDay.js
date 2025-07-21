43da10b3a4920f5aa609e5209985ccaad48ace6a
Icondor <ivanfigus@gmail.com>
2025-07-21 09:09:51 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,26 +2,26 @@ var gameOfDay =
 {
     "game": {
         "gameType": 3,
-        "platformId": 5377,
-        "CEId": "86723119-e5b6-4621-be5e-243b3785726f",
-        "name": "Kuru Kuru Kururin",
-        "header": "https://upload.cedb.me/3i4JYy.png",
+        "platformId": 13772,
+        "CEId": "faa802aa-2730-439b-9422-22f3cad60cf3",
+        "name": "Kururin Paradise",
+        "header": "https://upload.cedb.me/MCDIIR.png",
         "genre": "Arcade",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2024-07-20T21:04:15.000Z",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2024-07-21T04:59:30.000Z",
         "updatedAt": "2025-06-08T19:48:04.000Z",
-        "playersCompleted": 11,
+        "playersCompleted": 8,
         "playersStarted": 1,
-        "playersTotal": 19,
+        "playersTotal": 15,
         "milestones": {
-            "primary": 2,
-            "primaryText": "Helirin Hero [15\u2605]\nBeat the Master time on all adventure and challenge levels\n\nCopter Captain [10\u2605]\nComplete all adventure and challenge levels without taking a hit / Collect all collectibles.",
-            "community": 1,
-            "communityText": "Whirler of Worlds\nComplete all primary objectives from all site-listed games in the Kuru Kuru Kururin franchise. See verification requirements for complete list.",
-            "achievements": 64
+            "primary": 3,
+            "primaryText": "Like Father, Like Son [20\u2605]\nBeat all of Totorin's times.\n\nOne Helirin of a Show [10\u2605]\nGet a Perfect in every stage in Neo Land.\n\nTrouble in Paradise [10\u2605]\nGet a Perfect in every stage in the first four worlds in Adventure mode, and every stage in Challenge mode.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 69
         },
-        "information": "Copter Captain - The final 3x levels in Adventure Mode, and 5x levels in Challenge Mode unlock when all previous levels in that mode have a perfect (no hit) rating.\n\nHelirin Hero - Master times become visible in game after beating all \"P\" times. The achievement will not unlock if there are tied times, even if the leaderboard shows your name first.\n\nAll master times can be found here:\nhttps://docs.google.com/spreadsheets/d/1Znf-trw9suYPw295mvdcTGaCSGX-bZTSM26ITp1z02g/"
+        "information": "The final 4x levels in Adventure Mode (the \"One Helirin of a Show\" PO), and Challenge Lvl 11 levels in Challenge Mode unlock when all previous levels in that mode have a perfect (no hit) rating.\n\nLike Father, Like Son - Totorin's times become visible in game after beating all of the Baron's times (\"B\" on the leaderboard). The achievement will not unlock if there are tied times, even if the leaderboard shows your name first.\n\nAll dad/Totorin times can be found here:\nhttps://docs.google.com/spreadsheets/d/1qRX1DIkDDytZr3c3M5zpcHooht9SwxkXODjNqy1h1nI"
     }
 }
-var gameOfDayDate = '2025-07-20'
+var gameOfDayDate = '2025-07-21'
