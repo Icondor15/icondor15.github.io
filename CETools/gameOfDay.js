c9cfcb21751bc88151a67326ff271c7894e68728
Icondor <ivanfigus@gmail.com>
2025-07-23 07:14:10 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1410600,
-        "CEId": "3819697c-6048-49a6-8eae-e4a6cd0b225d",
-        "name": "MIRAY",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1410600/header.jpg?t=1679712426",
-        "genre": "Arcade",
-        "tier": 3,
-        "points": 50,
-        "createdAt": "2024-07-22T17:27:56.000Z",
+        "gameType": 1,
+        "platformId": 1602330,
+        "CEId": "09f100aa-caa7-4154-a224-1c3e9277eea4",
+        "name": "Clown Town",
+        "header": "https://i.imgur.com/kV34yQG.png",
+        "genre": "Action",
+        "tier": 0,
+        "points": 0,
+        "createdAt": "2022-07-23T23:19:08.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 7,
-        "playersStarted": 10,
-        "playersTotal": 33,
-        "priceData": {
-            "USD": {
-                "initial": 699,
-                "final": 699,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 569,
-                "final": 569,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 519,
-                "final": 519,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 72000,
-                "final": 72000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 995,
-                "final": 995,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 79,
+        "playersStarted": 0,
+        "playersTotal": 79,
         "milestones": {
-            "primary": 3,
-            "primaryText": "Mirror Master [25\u2605]\nClear all songs on hardmode with no misses.\n\nAngular Artist [15\u2605]\nClear all three extra songs with no misses.\n\nRhythmic Reflector [10\u2605]\nClear songs 1 through 15 and Ending with no misses.",
+            "primary": 29,
+            "primaryText": "Apollo (UNCLEARED) [0\u2605]\nAdd a game to CE that single handedly doubles the total amount of UNCLEARED objectives on the entire site, then apologize profusely.\n\nCE Japan [0\u2605]\n\u3042\u306a\u305f\u306e\u52aa\u529b\u3068\u732e\u8eab\u306b\u6df1\u304f\u611f\u8b1d\u3057\u307e\u3059\u3002\n\nCraziiPanduh [0\u2605]\nSuccessfully scare people out of the voice chat with your rage pistol.\n\negg [0\u2605]\nBeat the high score of 1,000 direct messages to an Admin in a single hour.\n\nFlash [0\u2605]\nHave your discord message ratio be 10:1 stickers to non-stickers.\n\nFolkius [0\u2605]\nBe the only admin in CE history that hasn't rage quit from the responsibilities. (Now he's stuck here forever or else this objective will be a meme).\n\nGooey [0\u2605]\nPlay games in the worst and least efficient way possible, then claim nothing on the site is properly valued.\n\nHazelnut Hex, Possibly the Best Game Ever [0\u2605]\nHave the steam curator recommend you to the group over a dozen times in a row.\n\nJarvis [0\u2605]\nFail every difficult challenge you ever try on the last possible frame.\n\nKing Bing Bong [0\u2605]\nBe born after CE was founded (in October of 2019).\n\nLaura [0\u2605]\nBe the most wonderful, beautiful, kind, intelligent person in the whole world \ud83e\udd70\n\nLegit \"Leg it\" Legion the Legend [0\u2605]\nObtain the prestigious hidden role of Hentai Weeb Furry Grandmaster\n\nMega [0\u2605]\nBecome the owner of CE without actually knowing anything about how the group or system work.\n\nMini, PO Terrorist [0\u2605]\nConvince a dev to add an objective so hard that no one in the world has done it, then go on to complete it.\n\nMiyu [0\u2605]\nRecommend a legitimately T6 game unironically at 80 points, and then fight to the death to keep it there.\n\nNashi [0\u2605]\nComplete every game on CE without anyone noticing, then crash the proof-submissions channel.\n\nPig [0\u2605]\nRuin your sleep schedule so badly that you have to take micro naps in between speaking.\n\nqetc [0\u2605]\nhttps://www.youtube.com/watch?v=8-OtzZA9jCY\n\nRamia [0\u2605]\nInsist that you are burnt out on challenging games, and then go on to earn 500 points a month for 18 consecutive months.\n\nScarlet [0\u2605]\nObtain the impossible rank of SSSSS (by obtaining, losing, and re-obtaining S rank 5 separate times).\n\nSchmole [0\u2605]\nEarn each of the following roles: Legendary Spreadsheet Challenger, Black Spreadsheet Grandmaster, and T5 Spreadsheet Enthusiast.\n\nSoundodger 2, Defender of CE [0\u2605]\nAs CE assistant rampaged through the site, devouring everything in its wake, one game rose up to defend all that remains.\n\nsp1nz [0\u2605]\nOwn every game on the site, then buy a physical copy of each in every language, then proceed to play the same two games for the rest of your life.\n\nTeddy [0\u2605]\nSeparately; Know a billionaire personally, be a professional Esports coach, and take over a year to finish writing the script for a single video because it was so emotionally powerful, the world wasn't ready for it.\n\nThe Entirety of the Celeste, Pogostuck, and Geometry Dash Discords [0\u2605]\nWhere do I post proof?\n\nTomato Final Boss [0\u2605]\nCreate a thread that's such a bruh moment, someone leaves the group.\n\nVantier [0\u2605]\nSuccessfully argue against your own points for 8 hours straight, then fall asleep.\n\nVitas [0\u2605]\nBorn your own children and teach him how to become nice gamer for farm points.\n\nZelda [0\u2605]\nThis is potentially an objective.",
             "community": 1,
-            "communityText": "Lightspeed Legend\nClear all songs on 2x speed with no misses.",
-            "achievements": 59
+            "communityText": "Achrotone\nAdd in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO  for adding in a CO  for adding in a CO for adding in a CO for adding in a CO for adding in a CO for adding in a CO.",
+            "achievements": 0
         }
     }
 }
-var gameOfDayDate = '2025-07-22'
+var gameOfDayDate = '2025-07-23'
