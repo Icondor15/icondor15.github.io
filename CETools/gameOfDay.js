c0e44d9c17e6f784c71e8a9075155cf982708c47
Icondor <ivanfigus@gmail.com>
2025-07-30 09:13:02 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,25 +2,25 @@ var gameOfDay =
 {
     "game": {
         "gameType": 3,
-        "platformId": 25493,
-        "CEId": "9b209163-a21a-462d-875e-f3cdd97dbe8e",
-        "name": "Kururin Squash!",
-        "header": "https://i.imgur.com/yZJhd8E.png",
+        "platformId": 2022,
+        "CEId": "10bf9fae-37b9-4ef3-a39b-686edd4433ef",
+        "name": "Tetris",
+        "header": "https://i.postimg.cc/Y9pzBNLV/tetris-title-nes-cover.png",
         "genre": "Arcade",
         "tier": 2,
-        "points": 25,
-        "createdAt": "2024-07-29T14:33:28.000Z",
+        "points": 35,
+        "createdAt": "2024-07-30T12:59:04.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 8,
-        "playersStarted": 0,
-        "playersTotal": 13,
+        "playersCompleted": 15,
+        "playersStarted": 5,
+        "playersTotal": 31,
         "milestones": {
             "primary": 2,
-            "primaryText": "Squash the Competition [15\u2605]\nGet Perfect in all stages\n\nDad Would be Proud [10\u2605]\nBeat all Dad times in Adventure mode.",
-            "community": 1,
-            "communityText": "Super Player\nBeat all times shown in the \"Super Guide\" videos.",
-            "achievements": 66
+            "primaryText": "Excavation Mission [25\u2605]\nSeparately; clear Type B with Level 9 Height 5 and clear Type B with Level 18 Height 3.\n\nStackin' it Old School [10\u2605]\nScore over 120,000 in Type A and get the final cutscene.",
+            "community": 2,
+            "communityText": "Block Party\nComplete at least 1 primary objective from each game in the Tetris franchise. See verification requirements for complete list.\n\nJourney of a Billion Blocks\nComplete all primary objectives from each game in the Tetris franchise. See verification requirements for complete list.",
+            "achievements": 37
         }
     }
 }
-var gameOfDayDate = '2025-07-29'
+var gameOfDayDate = '2025-07-30'
