6d8e63b472d9d109e0e771afe05863622ff05c52
Icondor <ivanfigus@gmail.com>
2025-07-31 09:09:45 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,59 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 2022,
-        "CEId": "10bf9fae-37b9-4ef3-a39b-686edd4433ef",
-        "name": "Tetris",
-        "header": "https://i.postimg.cc/Y9pzBNLV/tetris-title-nes-cover.png",
-        "genre": "Arcade",
-        "tier": 2,
-        "points": 35,
-        "createdAt": "2024-07-30T12:59:04.000Z",
+        "gameType": 0,
+        "platformId": 1400910,
+        "CEId": "7918bc97-0c7b-48f9-b833-e9d781b91b95",
+        "name": "Soundodger 2",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1400910/header.jpg?t=1681393861",
+        "genre": "Bullet Hell",
+        "tier": 4,
+        "points": 120,
+        "createdAt": "2023-07-31T21:52:38.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 15,
-        "playersStarted": 5,
-        "playersTotal": 31,
+        "playersCompleted": 9,
+        "playersStarted": 2,
+        "playersTotal": 97,
+        "priceData": {
+            "USD": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1950,
+                "final": 1950,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1675,
+                "final": 1675,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 230000,
+                "final": 230000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2950,
+                "final": 2950,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 2599,
+                "final": 2599,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
             "primary": 2,
-            "primaryText": "Excavation Mission [25\u2605]\nSeparately; clear Type B with Level 9 Height 5 and clear Type B with Level 18 Height 3.\n\nStackin' it Old School [10\u2605]\nScore over 120,000 in Type A and get the final cutscene.",
-            "community": 2,
-            "communityText": "Block Party\nComplete at least 1 primary objective from each game in the Tetris franchise. See verification requirements for complete list.\n\nJourney of a Billion Blocks\nComplete all primary objectives from each game in the Tetris franchise. See verification requirements for complete list.",
-            "achievements": 37
-        }
+            "primaryText": "Bullet Dancer[100\u2605 (Partial: 20\u2605)]\nObtain an S+ rank on the hard and challenge versions of every level \"purist\" style (level finished with a heart, without powerups, and without getting hit simultaneously). *Partial points are awarded as a progress milestone.\n\nAudio Avoider [20\u2605]\nObtain an S+ rank on the easy version of every level \"purist\" style (level finished with a heart, without powerups, and without getting hit simultaneously).",
+            "community": 0,
+            "communityText": "",
+            "achievements": 27
+        },
+        "information": "Curated on Steam: https://store.steampowered.com/app/1400910/Soundodger_2/?curator_clanid=36185934\n\nHero of the March 18 Massacre."
     }
 }
-var gameOfDayDate = '2025-07-30'
+var gameOfDayDate = '2025-07-31'
