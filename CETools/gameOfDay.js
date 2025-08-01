7fbc395b19f522f16714f2fc0e69cd7c0e791297
Icondor <ivanfigus@gmail.com>
2025-08-01 09:13:56 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,58 +2,53 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1400910,
-        "CEId": "7918bc97-0c7b-48f9-b833-e9d781b91b95",
-        "name": "Soundodger 2",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1400910/header.jpg?t=1681393861",
-        "genre": "Bullet Hell",
-        "tier": 4,
-        "points": 120,
-        "createdAt": "2023-07-31T21:52:38.000Z",
+        "platformId": 251130,
+        "CEId": "51431149-3450-45a5-b574-b223800b5d76",
+        "name": "Chroma Squad",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/251130/header.jpg?t=1601931904",
+        "genre": "Strategy",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2022-08-01T17:42:22.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 9,
-        "playersStarted": 2,
-        "playersTotal": 97,
+        "playersCompleted": 10,
+        "playersStarted": 0,
+        "playersTotal": 224,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1950,
-                "final": 1950,
+                "initial": 1479,
+                "final": 1479,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1675,
-                "final": 1675,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 230000,
-                "final": 230000,
+                "initial": 1279,
+                "final": 1279,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2950,
-                "final": 2950,
+                "initial": 2195,
+                "final": 2195,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2599,
-                "final": 2599,
+                "initial": 1949,
+                "final": 1949,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Bullet Dancer[100\u2605 (Partial: 20\u2605)]\nObtain an S+ rank on the hard and challenge versions of every level \"purist\" style (level finished with a heart, without powerups, and without getting hit simultaneously). *Partial points are awarded as a progress milestone.\n\nAudio Avoider [20\u2605]\nObtain an S+ rank on the easy version of every level \"purist\" style (level finished with a heart, without powerups, and without getting hit simultaneously).",
+            "primary": 1,
+            "primaryText": "Mighty Morphin' Power Rainbow [5\u2605]\nComplete the game on \"Challenging\" difficulty.",
             "community": 0,
             "communityText": "",
-            "achievements": 27
+            "achievements": 43
         },
-        "information": "Curated on Steam: https://store.steampowered.com/app/1400910/Soundodger_2/?curator_clanid=36185934\n\nHero of the March 18 Massacre."
+        "information": "Curated on Steam: https://store.steampowered.com/app/251130/Chroma_Squad/?curator_clanid=36185934"
     }
 }
-var gameOfDayDate = '2025-07-31'
+var gameOfDayDate = '2025-08-01'
