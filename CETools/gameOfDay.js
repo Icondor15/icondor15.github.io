a64f3a6e8c8a0b389d25c733daf0042a0a506d53
Icondor <ivanfigus@gmail.com>
2025-08-06 08:33:59 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1515490,
-        "CEId": "0b5b2ae4-0b88-4612-843c-7e10b839f18b",
-        "name": "System Purge",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1515490/header.jpg?t=1649142136",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2022-08-05T11:39:33.000Z",
+        "gameType": 1,
+        "platformId": 2088520,
+        "CEId": "44dfdb5f-82e3-4f7c-9241-c4b8013a6d94",
+        "name": "Shooting Game KARI",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2088520/header.jpg?t=1664197661",
+        "genre": "Bullet Hell",
+        "tier": 3,
+        "points": 55,
+        "createdAt": "2022-08-06T16:08:40.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 4,
-        "playersStarted": 0,
-        "playersTotal": 30,
-        "priceData": {
-            "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 52000,
-                "final": 52000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 569,
-                "final": 569,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 10,
+        "playersStarted": 3,
+        "playersTotal": 134,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Closed the Loop [10\u2605]\nFinish speedrun mode in under 40 minutes and 3 or less deaths.",
+            "primary": 2,
+            "primaryText": "This is Shooting Game [35\u2605]\nBeat the extra boss on Hard.\n\nPrimary Objective KIBISH\u012a [20\u2605]\nClear the game on hard. / Clear the game without taking damage.",
             "community": 0,
             "communityText": "",
-            "achievements": 18
+            "achievements": 8
         }
     }
 }
-var gameOfDayDate = '2025-08-05'
+var gameOfDayDate = '2025-08-06'
