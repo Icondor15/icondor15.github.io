06f93dafab1a0a0c616132944caca6b28e78c7ad
Icondor <ivanfigus@gmail.com>
2025-08-08 08:51:25 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 982220,
-        "CEId": "81b955a8-1398-4c78-9892-088bb1bcab40",
-        "name": "Globesweeper",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/982220/header.jpg?t=1667200580",
-        "genre": "Strategy",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2023-08-07T16:34:34.000Z",
+        "platformId": 1523670,
+        "CEId": "f3e83a35-b65b-4ce6-8fc8-0e2633735861",
+        "name": "Eggy",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1523670/header.jpg?t=1626847301",
+        "genre": "Platformer",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2021-08-08T17:46:02.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 44,
-        "playersStarted": 24,
-        "playersTotal": 133,
+        "playersCompleted": 17,
+        "playersStarted": 0,
+        "playersTotal": 32,
         "priceData": {
             "USD": {
-                "initial": 299,
-                "final": 299,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 299,
-                "final": 299,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 249,
-                "final": 249,
+                "initial": 429,
+                "final": 429,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 35000,
-                "final": 35000,
+                "initial": 58000,
+                "final": 58000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 450,
-                "final": 450,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 389,
-                "final": 389,
+                "initial": 649,
+                "final": 649,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Myriahedral Triathlon [15\u2605]\nComplete a colossal triangle game with at least standard mine density.\n\nAround the World in 52 Minutes [10\u2605]\nComplete a colossal globe within 52 minutes and 35.7 seconds.",
-            "community": 1,
-            "communityText": "Salt Mine\nComplete a colossal triangle game with insane mine density.",
-            "achievements": 16
+            "primary": 1,
+            "primaryText": "https://en.wikipedia.org/wiki/Egg [10\u2605]\nVenture into the upside down and conquer the death avatar.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 14
         }
     }
 }
-var gameOfDayDate = '2025-08-07'
+var gameOfDayDate = '2025-08-08'
