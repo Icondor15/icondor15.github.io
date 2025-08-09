6877ff82141a4295c2503eaff814614eaa2ae30a
Icondor <ivanfigus@gmail.com>
2025-08-09 09:13:15 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1523670,
-        "CEId": "f3e83a35-b65b-4ce6-8fc8-0e2633735861",
-        "name": "Eggy",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1523670/header.jpg?t=1626847301",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2021-08-08T17:46:02.000Z",
+        "platformId": 3065720,
+        "CEId": "82c5249d-6175-4012-ab79-321f82b9e29d",
+        "name": "life is not auto",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/3065720/header.jpg?t=1720085434",
+        "genre": "Arcade",
+        "tier": 2,
+        "points": 20,
+        "createdAt": "2024-08-09T00:57:17.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 17,
+        "playersCompleted": 5,
         "playersStarted": 0,
-        "playersTotal": 32,
+        "playersTotal": 14,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
+                "initial": 399,
+                "final": 239,
+                "discountPercent": 40
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
+                "initial": 399,
+                "final": 239,
+                "discountPercent": 40
             },
             "GBP": {
-                "initial": 429,
-                "final": 429,
-                "discountPercent": 0
+                "initial": 339,
+                "final": 203,
+                "discountPercent": 40
             },
             "JPY": {
-                "initial": 58000,
-                "final": 58000,
-                "discountPercent": 0
+                "initial": 47000,
+                "final": 28200,
+                "discountPercent": 40
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
+                "initial": 595,
+                "final": 357,
+                "discountPercent": 40
             },
             "CAD": {
-                "initial": 649,
-                "final": 649,
-                "discountPercent": 0
+                "initial": 499,
+                "final": 299,
+                "discountPercent": 40
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "https://en.wikipedia.org/wiki/Egg [10\u2605]\nVenture into the upside down and conquer the death avatar.",
+            "primaryText": "Mr. Auto [20\u2605]\nAchieve all perfects, on all levels, on hard mode / Complete 3 rounds of the challenge",
             "community": 0,
             "communityText": "",
-            "achievements": 14
+            "achievements": 45
         }
     }
 }
-var gameOfDayDate = '2025-08-08'
+var gameOfDayDate = '2025-08-09'
