0d9d09eaffb1f7363fbd49c0a5cc851a377255a0
Icondor <ivanfigus@gmail.com>
2025-08-12 08:55:08 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,27 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 274290,
-        "CEId": "0da927de-a778-413c-b88d-af505bcb1cd4",
-        "name": "Gods Will Be Watching",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/274290/header.jpg?t=1684858773",
+        "gameType": 3,
+        "platformId": 14853,
+        "CEId": "efc10f10-df8e-46b6-932a-d83d46019cfb",
+        "name": "Pokemon Mystery Dungeon: Explorers of Sky",
+        "header": "https://i.imgur.com/pNHvp0g.png",
         "genre": "Strategy",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2023-08-11T12:22:38.000Z",
+        "tier": 2,
+        "points": 25,
+        "createdAt": "2024-08-12T16:29:35.000Z",
         "updatedAt": "2025-07-20T17:38:01.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 114,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 975,
-                "final": 975,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 850,
-                "final": 850,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 120000,
-                "final": 120000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1450,
-                "final": 1450,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1299,
-                "final": 1299,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 1,
+        "playersStarted": 2,
+        "playersTotal": 17,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Big Heavens [10\u2605]\nEarn all achievements in the game.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 35
-        }
+            "primary": 4,
+            "primaryText": "Destiny's Ascent [10\u2605]\nClear Destiny Tower.\n\nPerfect Purist [5\u2605]\nClear Zero Isle South.\n\nAdept Purist [5\u2605]\nClear Zero Isle East and Zero Isle West.\n\nCenter of the Earth [5\u2605]\nClear Zero Isle North and Zero Isle Center.",
+            "community": 1,
+            "communityText": "No Stone Left Unturned\nReach Guildmaster Rank and recruit all Pokemon.",
+            "achievements": 130
+        },
+        "information": "For those interested in RA mastery: there are 5 missable achievements."
     }
 }
-var gameOfDayDate = '2025-08-11'
+var gameOfDayDate = '2025-08-12'
