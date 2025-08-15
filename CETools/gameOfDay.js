52ddad2b2744cbfcb0c688dda9753a8bfcfe2d1f
Icondor <ivanfigus@gmail.com>
2025-08-15 08:50:31 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,27 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1301230,
-        "CEId": "79d73929-1a91-4695-8626-e6503b384c2b",
-        "name": "Astrodogs",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1301230/header.jpg?t=1617894064",
-        "genre": "Arcade",
+        "gameType": 3,
+        "platformId": 658,
+        "CEId": "7149490c-03fc-46ef-9b75-ce55ae5e58af",
+        "name": "Mortal Kombat 3",
+        "header": "https://i.imgur.com/kS195vp.png",
+        "genre": "Action",
         "tier": 1,
-        "points": 10,
-        "createdAt": "2022-08-14T02:49:48.000Z",
+        "points": 15,
+        "createdAt": "2024-08-15T07:51:51.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
         "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 19,
-        "priceData": {
-            "USD": {
-                "initial": 1299,
-                "final": 1299,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 1079,
-                "final": 1079,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 1029,
-                "final": 1029,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 132000,
-                "final": 132000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1850,
-                "final": 1850,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1449,
-                "final": 1449,
-                "discountPercent": 0
-            }
-        },
+        "playersTotal": 3,
         "milestones": {
             "primary": 1,
-            "primaryText": "Star Doge [10\u2605]\nGet all S ranks.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 15
-        }
+            "primaryText": "Ruler of Outworld [15\u2605]\nBeat the Master Destiny tower with no continues on Very Hard difficulty.",
+            "community": 1,
+            "communityText": "Randper Kombat\nComplete any tower with the Switcharoo cheat enabled.",
+            "achievements": 31
+        },
+        "information": "Cheat for Switcharoo mode:\nX, B, A, Y, Up, Left, Down, Right, Down - At the start game screen"
     }
 }
-var gameOfDayDate = '2025-08-14'
+var gameOfDayDate = '2025-08-15'
