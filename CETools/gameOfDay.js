7384c824e5b2384fcc19e5601fcbb4bc62ac4efc
Icondor <ivanfigus@gmail.com>
2025-08-16 09:12:58 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,27 +1,58 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 658,
-        "CEId": "7149490c-03fc-46ef-9b75-ce55ae5e58af",
-        "name": "Mortal Kombat 3",
-        "header": "https://i.imgur.com/kS195vp.png",
-        "genre": "Action",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2024-08-15T07:51:51.000Z",
+        "gameType": 0,
+        "platformId": 243360,
+        "CEId": "ba80ac2a-186d-4b55-8981-d4bce6a34663",
+        "name": "TrackMania\u00b2 Valley",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/243360/header.jpg?t=1588603133",
+        "genre": "Arcade",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2022-08-29T07:21:24.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 3,
-        "milestones": {
-            "primary": 1,
-            "primaryText": "Ruler of Outworld [15\u2605]\nBeat the Master Destiny tower with no continues on Very Hard difficulty.",
-            "community": 1,
-            "communityText": "Randper Kombat\nComplete any tower with the Switcharoo cheat enabled.",
-            "achievements": 31
+        "playersCompleted": 7,
+        "playersStarted": 4,
+        "playersTotal": 77,
+        "priceData": {
+            "USD": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1699,
+                "final": 1699,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 264000,
+                "final": 264000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2995,
+                "final": 2995,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            }
         },
-        "information": "Cheat for Switcharoo mode:\nX, B, A, Y, Up, Left, Down, Right, Down - At the start game screen"
+        "milestones": {
+            "primary": 2,
+            "primaryText": "Well-Stacked in Tracks [30\u2605]\nObtain 65 Author Medals.\n\nRoad Rager [10\u2605]\nObtain 65 Gold Medals.",
+            "community": 3,
+            "communityText": "Alpine All-Star\nGet all Author Medals in the TM1 Alpine Title Pack.\n\nSpeed All-Star\nGet all Author Medals in the TM1 Speed Title Pack.\n\nValley All-Star\nGet all Author Medals in the StarTrack Title Pack.",
+            "achievements": 0
+        }
     }
 }
-var gameOfDayDate = '2025-08-15'
+var gameOfDayDate = '2025-08-16'
