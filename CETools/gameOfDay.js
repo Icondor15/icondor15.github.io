562e38a775c05993d86505c891dc561c41e821f0
Icondor <ivanfigus@gmail.com>
2025-08-19 08:50:37 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 643880,
-        "CEId": "256ec130-6f8c-46f6-b0e3-d690b9ea9f92",
-        "name": "Strikey Sisters",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/643880/header.jpg?t=1667211591",
-        "genre": "Arcade",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2023-08-18T21:19:34.000Z",
+        "gameType": 1,
+        "platformId": 1533280,
+        "CEId": "4b07fa14-ef52-43ed-ae1e-2d10efd1f721",
+        "name": "Eternal Cave Escape",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1533280/header.jpg?t=1628641209",
+        "genre": "Platformer",
+        "tier": 2,
+        "points": 25,
+        "createdAt": "2021-08-19T13:12:52.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
         "playersCompleted": 8,
         "playersStarted": 0,
-        "playersTotal": 110,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 899,
-                "final": 899,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 120000,
-                "final": 120000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1450,
-                "final": 1450,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1299,
-                "final": 1299,
-                "discountPercent": 0
-            }
-        },
+        "playersTotal": 149,
         "milestones": {
             "primary": 1,
-            "primaryText": "Brick Breaker Siblings [10\u2605]\nProve yourself.",
+            "primaryText": "Speedlunking Out of It [25\u2605]\nEscape the Expert Map Cave in under 30 minutes.",
             "community": 0,
             "communityText": "",
             "achievements": 10
         }
     }
 }
-var gameOfDayDate = '2025-08-18'
+var gameOfDayDate = '2025-08-19'
