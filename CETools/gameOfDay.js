a20607412dd08687e12c01e13533767eb0f4646f
Icondor <ivanfigus@gmail.com>
2025-08-20 09:16:26 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,25 +2,25 @@ var gameOfDay =
 {
     "game": {
         "gameType": 1,
-        "platformId": 1533280,
-        "CEId": "4b07fa14-ef52-43ed-ae1e-2d10efd1f721",
-        "name": "Eternal Cave Escape",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1533280/header.jpg?t=1628641209",
-        "genre": "Platformer",
+        "platformId": 1333090,
+        "CEId": "ff73f8f0-e5c6-4c90-9702-add88b698e03",
+        "name": "Gravity",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1333090/header.jpg?t=1686975474",
+        "genre": "Arcade",
         "tier": 2,
-        "points": 25,
-        "createdAt": "2021-08-19T13:12:52.000Z",
+        "points": 30,
+        "createdAt": "2022-08-20T20:02:48.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 8,
-        "playersStarted": 0,
-        "playersTotal": 149,
+        "playersCompleted": 3,
+        "playersStarted": 9,
+        "playersTotal": 152,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Speedlunking Out of It [25\u2605]\nEscape the Expert Map Cave in under 30 minutes.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 10
+            "primary": 2,
+            "primaryText": "Space-Time Anomaly [15\u2605]\nComplete the Zeta Pack.\n\nGravitron [15\u2605]\nComplete all other official level packs.",
+            "community": 2,
+            "communityText": "F = G(m1m2/r^2)\nReach the teleporter in \"The Ultimate Trial\".\n\nThe Sign of Zeta\nComplete the Zeta Pack without dying.",
+            "achievements": 23
         }
     }
 }
-var gameOfDayDate = '2025-08-19'
+var gameOfDayDate = '2025-08-20'
