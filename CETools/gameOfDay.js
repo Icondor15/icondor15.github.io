cd51373d3fdc380382ed2443fc4ead3cd47d11ed
Icondor <ivanfigus@gmail.com>
2025-08-23 08:01:11 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 531510,
-        "CEId": "f2571e56-0b55-472d-a318-9c8775c4c5eb",
-        "name": "Just Shapes & Beats",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/531510/header.jpg?t=1627067406",
-        "genre": "Bullet Hell",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2022-01-03T21:24:11.000Z",
+        "platformId": 1595770,
+        "CEId": "e59b8b36-793e-4f48-84f9-2e19d587b9f9",
+        "name": "SUFFER 2",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1595770/header.jpg?t=1673662655",
+        "genre": "First-Person",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2024-08-23T09:10:45.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 30,
-        "playersStarted": 134,
-        "playersTotal": 462,
+        "playersCompleted": 4,
+        "playersStarted": 0,
+        "playersTotal": 6,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1599,
-                "final": 1599,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 220000,
-                "final": 220000,
+                "initial": 52000,
+                "final": 52000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2599,
-                "final": 2599,
+                "initial": 569,
+                "final": 569,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Shapes [20\u2605]\nObtain an S rank on all songs on Hardcore.\n\nBeats [5\u2605]\nProve yourself.",
-            "community": 1,
-            "communityText": "Beat Rush\nCreate a playlist with all boss songs and achieve S rank on them all in a row.",
-            "achievements": 26
+            "primary": 1,
+            "primaryText": "Insufferable [15\u2605]\nBeat the game on Brutal mode.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 15
         }
     }
 }
-var gameOfDayDate = '2025-08-22'
+var gameOfDayDate = '2025-08-23'
