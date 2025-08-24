85c589a80cc73c4d8037318318f4b1d4c7fecf4f
Icondor <ivanfigus@gmail.com>
2025-08-24 07:56:27 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,58 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1595770,
-        "CEId": "e59b8b36-793e-4f48-84f9-2e19d587b9f9",
-        "name": "SUFFER 2",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1595770/header.jpg?t=1673662655",
-        "genre": "First-Person",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2024-08-23T09:10:45.000Z",
+        "platformId": 1001970,
+        "CEId": "b852615d-311d-4622-8476-fc0b25a47430",
+        "name": "Rhythmy",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1001970/header.jpg?t=1622385883",
+        "genre": "Bullet Hell",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2022-04-12T23:46:53.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 4,
+        "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 6,
+        "playersTotal": 13,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 399,
-                "final": 399,
+                "initial": 739,
+                "final": 739,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 399,
-                "final": 399,
+                "initial": 619,
+                "final": 619,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 52000,
-                "final": 52000,
+                "initial": 93000,
+                "final": 93000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 1295,
+                "final": 1295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 569,
-                "final": 569,
+                "initial": 1029,
+                "final": 1029,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Insufferable [15\u2605]\nBeat the game on Brutal mode.",
+            "primaryText": "Mutator Master [40\u2605]\nComplete every level with \"All Mutators Challenge\" enabled.",
             "community": 0,
             "communityText": "",
-            "achievements": 15
-        }
+            "achievements": 60
+        },
+        "information": "Game is balanced around its last update from 2021, while still on Early Access."
     }
 }
-var gameOfDayDate = '2025-08-23'
+var gameOfDayDate = '2025-08-24'
