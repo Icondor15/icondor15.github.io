14b41820fa40d0db5c115b6f44b85575e1ce7030
Icondor <ivanfigus@gmail.com>
2025-08-26 09:02:05 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,58 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 78000,
-        "CEId": "f1eab776-645c-42d1-b640-244cbf4a590d",
-        "name": "Bejeweled\u00ae 3",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/78000/header.jpg?t=1584804978",
-        "genre": "Arcade",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2020-08-25T17:35:19.000Z",
+        "platformId": 1110910,
+        "CEId": "869b0b40-e4c8-404e-8648-0b4ec86a55ed",
+        "name": "Mortal Shell",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1110910/header.jpg?t=1629787787",
+        "genre": "Action",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2021-08-26T17:12:54.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 55,
-        "playersStarted": 0,
-        "playersTotal": 337,
+        "playersCompleted": 14,
+        "playersStarted": 2,
+        "playersTotal": 171,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 425,
-                "final": 425,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 49800,
-                "final": 49800,
+                "initial": 308000,
+                "final": 308000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 4495,
+                "final": 4495,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 3299,
+                "final": 3299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Bedazzling [15\u2605]\nProve yourself.",
+            "primary": 2,
+            "primaryText": "Immortal Shell [25\u2605]\nComplete a run in The Virtuous Cycle with the Dark Form Obsidian Shell Shade.\n\nEternal Suffering [15\u2605]\nSeparately, beat the game without bonding with a shell, and beat the game in Obsidian Dark form.",
             "community": 0,
             "communityText": "",
-            "achievements": 65
-        }
+            "achievements": 37
+        },
+        "information": "DLC Required:\n-The Virtuous Cycle"
     }
 }
-var gameOfDayDate = '2025-08-25'
+var gameOfDayDate = '2025-08-26'
