3bc204b00ce205a926737e2b3695c09026b1bf4f
Icondor <ivanfigus@gmail.com>
2025-08-27 08:18:47 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,58 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1110910,
-        "CEId": "869b0b40-e4c8-404e-8648-0b4ec86a55ed",
-        "name": "Mortal Shell",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1110910/header.jpg?t=1629787787",
-        "genre": "Action",
-        "tier": 3,
-        "points": 40,
-        "createdAt": "2021-08-26T17:12:54.000Z",
+        "platformId": 569530,
+        "CEId": "0821c9da-3d94-4e05-9bfc-44f5f4ca8739",
+        "name": "Sky Noon",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/569530/header.jpg?t=1667240838",
+        "genre": "First-Person",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2024-08-27T10:56:37.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 14,
-        "playersStarted": 2,
-        "playersTotal": 171,
+        "playersCompleted": 1,
+        "playersStarted": 0,
+        "playersTotal": 26,
         "priceData": {
             "USD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 1249,
+                "final": 1249,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 1139,
+                "final": 1139,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 308000,
-                "final": 308000,
+                "initial": 152000,
+                "final": 152000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 4495,
-                "final": 4495,
+                "initial": 2150,
+                "final": 2150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 3299,
-                "final": 3299,
+                "initial": 1749,
+                "final": 1749,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Immortal Shell [25\u2605]\nComplete a run in The Virtuous Cycle with the Dark Form Obsidian Shell Shade.\n\nEternal Suffering [15\u2605]\nSeparately, beat the game without bonding with a shell, and beat the game in Obsidian Dark form.",
+            "primary": 1,
+            "primaryText": "It's Sky Noon! [15\u2605]\nGet platinum medals in all time trial levels.",
             "community": 0,
             "communityText": "",
-            "achievements": 37
-        },
-        "information": "DLC Required:\n-The Virtuous Cycle"
+            "achievements": 0
+        }
     }
 }
-var gameOfDayDate = '2025-08-26'
+var gameOfDayDate = '2025-08-27'
