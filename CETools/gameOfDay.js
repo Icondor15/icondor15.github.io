73443fddda5ec8c112e8ad03f11fd01f045ccec9
Icondor <ivanfigus@gmail.com>
2025-08-28 08:43:48 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 569530,
-        "CEId": "0821c9da-3d94-4e05-9bfc-44f5f4ca8739",
-        "name": "Sky Noon",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/569530/header.jpg?t=1667240838",
-        "genre": "First-Person",
+        "platformId": 283080,
+        "CEId": "c94a0769-2ea6-42bb-9e50-d766080e1bd8",
+        "name": "Super Chibi Knight",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/283080/header.jpg?t=1568743916",
+        "genre": "Action",
         "tier": 1,
-        "points": 15,
-        "createdAt": "2024-08-27T10:56:37.000Z",
+        "points": 10,
+        "createdAt": "2022-08-28T21:06:10.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 1,
+        "playersCompleted": 12,
         "playersStarted": 0,
-        "playersTotal": 26,
+        "playersTotal": 88,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 429,
+                "final": 429,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 152000,
-                "final": 152000,
+                "initial": 58000,
+                "final": 58000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 649,
+                "final": 649,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "It's Sky Noon! [15\u2605]\nGet platinum medals in all time trial levels.",
+            "primaryText": "Knight of Legend [10\u2605]\nProve yourself.",
             "community": 0,
             "communityText": "",
-            "achievements": 0
+            "achievements": 72
         }
     }
 }
-var gameOfDayDate = '2025-08-27'
+var gameOfDayDate = '2025-08-28'
