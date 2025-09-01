f675fb1c1b54846c2cd9ac92291456f162e5da26
Icondor <ivanfigus@gmail.com>
2025-09-01 09:12:35 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1647920,
-        "CEId": "73b43c5e-d713-4be7-8033-03c918fc60de",
-        "name": "Valfaris: Mecha Therion",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1647920/header.jpg?t=1724775738",
+        "platformId": 1884300,
+        "CEId": "d88b865b-b991-4d5f-bc05-5d6193470b87",
+        "name": "Deathsmiles I\u30fbII",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1884300/header.jpg?t=1676875376",
         "genre": "Bullet Hell",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2024-08-31T07:23:10.000Z",
+        "tier": 2,
+        "points": 20,
+        "createdAt": "2023-09-01T03:32:53.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
         "playersCompleted": 2,
-        "playersStarted": 0,
-        "playersTotal": 44,
+        "playersStarted": 2,
+        "playersTotal": 24,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 3990,
+                "final": 3990,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2450,
-                "final": 2450,
+                "initial": 3490,
+                "final": 3490,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 2099,
-                "final": 2099,
+                "initial": 3099,
+                "final": 3099,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 280000,
-                "final": 280000,
+                "initial": 495000,
+                "final": 495000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 3650,
-                "final": 3650,
+                "initial": 5695,
+                "final": 5695,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 3250,
-                "final": 3250,
+                "initial": 4549,
+                "final": 4549,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Full Mecha Mayhem [10\u2605]\nProve yourself.",
+            "primary": 2,
+            "primaryText": "Fever Christmas [15\u2605]\nProve yourself on the X mode.\n\nHeaping Snow [5\u2605]\nClear DSII's Arcade mode without continuing.",
             "community": 0,
             "communityText": "",
-            "achievements": 44
+            "achievements": 43
         }
     }
 }
-var gameOfDayDate = '2025-08-31'
+var gameOfDayDate = '2025-09-01'
