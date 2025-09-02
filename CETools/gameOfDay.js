31d6e2bb16524e1f2f6ae2f5498cb83c67fc6577
Icondor <ivanfigus@gmail.com>
2025-09-02 08:59:07 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,27 +2,27 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1884300,
-        "CEId": "d88b865b-b991-4d5f-bc05-5d6193470b87",
-        "name": "Deathsmiles I\u30fbII",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1884300/header.jpg?t=1676875376",
-        "genre": "Bullet Hell",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2023-09-01T03:32:53.000Z",
+        "platformId": 1003590,
+        "CEId": "d28e20d0-b092-45c6-8c5b-25e448b09215",
+        "name": "Tetris\u00ae Effect: Connected",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1003590/header.jpg?t=1629881521",
+        "genre": "Arcade",
+        "tier": 6,
+        "points": 600,
+        "createdAt": "2021-09-02T00:55:07.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 2,
-        "playersTotal": 24,
+        "playersCompleted": 15,
+        "playersStarted": 89,
+        "playersTotal": 289,
         "priceData": {
             "USD": {
-                "initial": 3990,
-                "final": 3990,
+                "initial": 3999,
+                "final": 3999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 3490,
-                "final": 3490,
+                "initial": 3399,
+                "final": 3399,
                 "discountPercent": 0
             },
             "GBP": {
@@ -31,8 +31,8 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 495000,
-                "final": 495000,
+                "initial": 450000,
+                "final": 450000,
                 "discountPercent": 0
             },
             "AUD": {
@@ -47,12 +47,12 @@ var gameOfDay =
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Fever Christmas [15\u2605]\nProve yourself on the X mode.\n\nHeaping Snow [5\u2605]\nClear DSII's Arcade mode without continuing.",
-            "community": 0,
-            "communityText": "",
+            "primary": 5,
+            "primaryText": "Tetrified [425\u2605]\nClear Master Mode.\n\nSeriously Godlike [75\u2605]\nGet an SS rank everywhere possible.\n\nSeriously Skilled [60\u2605]\nGet an S rank everywhere possible.\n\nConnected [25\u2605]\nDefeat Area 5 Tetrimidion with SS rank.\n\nCome Follow Me [15\u2605]\nProve yourself.",
+            "community": 8,
+            "communityText": "Devoted Sprinter\nClear Sprint Mode in under 60 seconds.\n\nElite Sprinter\nClear Sprint Mode in under 50 seconds.\n\nGodlike Sprinter\nClear Sprint Mode in under 40 seconds. \n\nGrand Master Guardian\nReach Tier 10 in multiplayer.\n\nInfinitris Ascendant\nClear 23 lines in one zone (not in multiplayer).\n\nMaxed Out Master\nClear the non-endless Master Mode with 303 total lines. \n\nRainbow Connection\nEarn an SS rank in multiplayer Zone Battle, Score Attack, Classic Score Attack, and Connected Vs as the Boss.\n\nZodiac Maniac\nDefeat Area 5 Tetrimidion in Maniac Mode with SS rank.",
             "achievements": 43
         }
     }
 }
-var gameOfDayDate = '2025-09-01'
+var gameOfDayDate = '2025-09-02'
