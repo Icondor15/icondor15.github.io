2b968c2403cb151a11c14160bb2df62733acdacf
Icondor <ivanfigus@gmail.com>
2025-09-04 07:50:00 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,18 +2,18 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1261960,
-        "CEId": "da2d40fc-95ed-4eda-9492-dc50e85499b3",
-        "name": "STRIKERS 1945",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1261960/header.jpg?t=1611907317",
+        "platformId": 841660,
+        "CEId": "20cb9527-1ecf-4554-a4ef-03e01863330e",
+        "name": "The Treehouse Man",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/841660/header.jpg?t=1573752606",
         "genre": "Bullet Hell",
-        "tier": 4,
-        "points": 105,
-        "createdAt": "2022-09-03T03:39:46.000Z",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2022-09-04T10:18:52.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 5,
-        "playersStarted": 1,
-        "playersTotal": 29,
+        "playersCompleted": 17,
+        "playersStarted": 0,
+        "playersTotal": 45,
         "priceData": {
             "USD": {
                 "initial": 999,
@@ -21,18 +21,18 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 899,
-                "final": 899,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 719,
-                "final": 719,
+                "initial": 850,
+                "final": 850,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 99000,
-                "final": 99000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
@@ -41,18 +41,18 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1149,
-                "final": 1149,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Literally 1945 [80\u2605]\nClear both loops of the game.\n\nHeavy Weapon [15\u2605]\nClear the game on the maximum difficulty level without using continues.\n\nSupreme Squad [10\u2605]\nClear the game on easy or higher with all gold medals on all characters.",
+            "primary": 1,
+            "primaryText": "The Silence [5\u2605]\nComplete any river from each region without taking damage.",
             "community": 0,
             "communityText": "",
-            "achievements": 32
+            "achievements": 14
         }
     }
 }
-var gameOfDayDate = '2025-09-03'
+var gameOfDayDate = '2025-09-04'
