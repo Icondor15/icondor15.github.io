ef5345cec84a89c4e71ec4b269f2af836607c153
Icondor <ivanfigus@gmail.com>
2025-09-05 07:39:49 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 841660,
-        "CEId": "20cb9527-1ecf-4554-a4ef-03e01863330e",
-        "name": "The Treehouse Man",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/841660/header.jpg?t=1573752606",
-        "genre": "Bullet Hell",
+        "platformId": 1012570,
+        "CEId": "445c29a1-2357-4d51-8157-3a27aa77c8c5",
+        "name": "Knuckle Sandwich",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1012570/header.jpg?t=1716169693",
+        "genre": "Arcade",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2022-09-04T10:18:52.000Z",
+        "points": 10,
+        "createdAt": "2024-09-05T22:57:45.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 17,
+        "playersCompleted": 1,
         "playersStarted": 0,
-        "playersTotal": 45,
+        "playersTotal": 6,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1950,
+                "final": 1950,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 850,
-                "final": 850,
+                "initial": 1675,
+                "final": 1675,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 120000,
-                "final": 120000,
+                "initial": 230000,
+                "final": 230000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 2950,
+                "final": 2950,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 2599,
+                "final": 2599,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "The Silence [5\u2605]\nComplete any river from each region without taking damage.",
+            "primaryText": "UnderWare [10\u2605]\nProve yourself.",
             "community": 0,
             "communityText": "",
-            "achievements": 14
+            "achievements": 32
         }
     }
 }
-var gameOfDayDate = '2025-09-04'
+var gameOfDayDate = '2025-09-05'
