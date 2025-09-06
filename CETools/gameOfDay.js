95011d751173ca67b1ccae9300159b6f8ea01d6b
Icondor <ivanfigus@gmail.com>
2025-09-06 08:48:43 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1012570,
-        "CEId": "445c29a1-2357-4d51-8157-3a27aa77c8c5",
-        "name": "Knuckle Sandwich",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1012570/header.jpg?t=1716169693",
+        "platformId": 1492420,
+        "CEId": "fcb2bef9-e847-4390-9c12-6315fa149e3f",
+        "name": "EQUALINE",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1492420/header.jpg?t=1612777216",
         "genre": "Arcade",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2024-09-05T22:57:45.000Z",
+        "tier": 3,
+        "points": 65,
+        "createdAt": "2022-09-06T17:37:06.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 1,
+        "playersCompleted": 50,
         "playersStarted": 0,
-        "playersTotal": 6,
+        "playersTotal": 134,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 1199,
+                "final": 1199,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1950,
-                "final": 1950,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1675,
-                "final": 1675,
+                "initial": 929,
+                "final": 929,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 230000,
-                "final": 230000,
+                "initial": 122000,
+                "final": 122000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2950,
-                "final": 2950,
+                "initial": 1695,
+                "final": 1695,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2599,
-                "final": 2599,
+                "initial": 1349,
+                "final": 1349,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "UnderWare [10\u2605]\nProve yourself.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 32
+            "primaryText": "Order of Operations [65\u2605]\nClear a mission with more than 120 seconds remaining. / Get a score of 1 million or more in every mission.",
+            "community": 3,
+            "communityText": "Divide By Zero\nAchieve a Maxout (999,999,999 points) in any of the Endless modes.\n\nEuler\nDefeat a Lv. MAX CPU with \"\"999 Sec.\", \"Easy\" and \"100 Questions\" selected in Calculation Battle.\n\nGauss\nDefeat a Lv. MAX CPU with \"999 Sec.\", \"Expert\" and \"100 Questions\" selected in Calculation Battle.",
+            "achievements": 24
         }
     }
 }
-var gameOfDayDate = '2025-09-05'
+var gameOfDayDate = '2025-09-06'
