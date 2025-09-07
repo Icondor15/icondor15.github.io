85dd4c1ddfe37870477035e183eb9f21c3a429ff
Icondor <ivanfigus@gmail.com>
2025-09-07 09:04:42 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1492420,
-        "CEId": "fcb2bef9-e847-4390-9c12-6315fa149e3f",
-        "name": "EQUALINE",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1492420/header.jpg?t=1612777216",
-        "genre": "Arcade",
+        "platformId": 969200,
+        "CEId": "e01e34c0-1f0c-483d-a0b2-7fba2954e356",
+        "name": "OkunoKA Madness",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/969200/header.jpg?t=1692364837",
+        "genre": "Platformer",
         "tier": 3,
-        "points": 65,
-        "createdAt": "2022-09-06T17:37:06.000Z",
+        "points": 60,
+        "createdAt": "2024-09-07T18:17:44.000Z",
         "updatedAt": "2025-08-13T06:36:01.000Z",
-        "playersCompleted": 50,
+        "playersCompleted": 0,
         "playersStarted": 0,
-        "playersTotal": 134,
+        "playersTotal": 2,
         "priceData": {
             "USD": {
-                "initial": 1199,
-                "final": 1199,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 739,
+                "final": 739,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 929,
-                "final": 929,
+                "initial": 619,
+                "final": 619,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 122000,
-                "final": 122000,
+                "initial": 93000,
+                "final": 93000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1695,
-                "final": 1695,
+                "initial": 1295,
+                "final": 1295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1349,
-                "final": 1349,
+                "initial": 1029,
+                "final": 1029,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Order of Operations [65\u2605]\nClear a mission with more than 120 seconds remaining. / Get a score of 1 million or more in every mission.",
-            "community": 3,
-            "communityText": "Divide By Zero\nAchieve a Maxout (999,999,999 points) in any of the Endless modes.\n\nEuler\nDefeat a Lv. MAX CPU with \"\"999 Sec.\", \"Easy\" and \"100 Questions\" selected in Calculation Battle.\n\nGauss\nDefeat a Lv. MAX CPU with \"999 Sec.\", \"Expert\" and \"100 Questions\" selected in Calculation Battle.",
-            "achievements": 24
+            "primary": 2,
+            "primaryText": "King of Ka [45\u2605]\nSeparately, complete each world in Time Attack mode without dying.\n\nSpirit Saviour [15\u2605]\n100% the game.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 27
         }
     }
 }
-var gameOfDayDate = '2025-09-06'
+var gameOfDayDate = '2025-09-07'
