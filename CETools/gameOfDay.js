77d3b1e16d10437716d64e62946766c9a1a770ba
Icondor <ivanfigus@gmail.com>
2025-09-10 09:13:04 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,18 +2,18 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1486630,
-        "CEId": "0701886d-5002-4850-ae10-d14dd0191403",
-        "name": "Intravenous",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1486630/header.jpg?t=1629288196",
+        "platformId": 314710,
+        "CEId": "e65f151c-37d4-4ea6-aae7-18d78ba88d89",
+        "name": "Mighty No. 9",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/314710/header.jpg?t=1608197459",
         "genre": "Action",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2021-09-09T00:44:53.000Z",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2024-09-10T18:47:32.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 23,
-        "playersStarted": 0,
-        "playersTotal": 451,
+        "playersCompleted": 1,
+        "playersStarted": 2,
+        "playersTotal": 78,
         "priceData": {
             "USD": {
                 "initial": 1999,
@@ -26,33 +26,33 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1675,
-                "final": 1675,
+                "initial": 1779,
+                "final": 1779,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 230000,
-                "final": 230000,
+                "initial": 238900,
+                "final": 238900,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2950,
-                "final": 2950,
+                "initial": 2995,
+                "final": 2995,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2599,
-                "final": 2599,
+                "initial": 2749,
+                "final": 2749,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Take In Vain [15\u2605]\nProve yourself.",
+            "primary": 3,
+            "primaryText": "Bright Ray [15\u2605]\nGet S ranks on each stage on Hyper & Maniac difficulties as Ray. / Get S rank on the \"Vermillion Destroyer\" stage on Hyper & Maniac difficulties.\n\nBeefed Up [15\u2605]\nGet S ranks on each stage of the base game on Hyper & Maniac difficulties (as Beck).\n\nI'll See You In Xel [10\u2605]\nProve yourself.",
             "community": 0,
             "communityText": "",
-            "achievements": 45
+            "achievements": 70
         }
     }
 }
-var gameOfDayDate = '2025-09-09'
+var gameOfDayDate = '2025-09-10'
