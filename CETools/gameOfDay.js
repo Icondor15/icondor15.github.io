5fdab9e8f9884732fbce1c961faadd79c3e86b21
Icondor <ivanfigus@gmail.com>
2025-09-11 09:12:30 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 314710,
-        "CEId": "e65f151c-37d4-4ea6-aae7-18d78ba88d89",
-        "name": "Mighty No. 9",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/314710/header.jpg?t=1608197459",
-        "genre": "Action",
+        "platformId": 1404080,
+        "CEId": "45850979-c6c0-4e7a-957b-221ad28838ec",
+        "name": "Rotund Takeoff",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1404080/header.jpg?t=1650385871",
+        "genre": "Platformer",
         "tier": 3,
-        "points": 40,
-        "createdAt": "2024-09-10T18:47:32.000Z",
+        "points": 50,
+        "createdAt": "2022-09-11T07:18:05.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 2,
-        "playersTotal": 78,
+        "playersCompleted": 3,
+        "playersStarted": 0,
+        "playersTotal": 43,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 659,
+                "final": 659,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1779,
-                "final": 1779,
+                "initial": 579,
+                "final": 579,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 238900,
-                "final": 238900,
+                "initial": 82000,
+                "final": 82000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2995,
-                "final": 2995,
+                "initial": 1150,
+                "final": 1150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2749,
-                "final": 2749,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Bright Ray [15\u2605]\nGet S ranks on each stage on Hyper & Maniac difficulties as Ray. / Get S rank on the \"Vermillion Destroyer\" stage on Hyper & Maniac difficulties.\n\nBeefed Up [15\u2605]\nGet S ranks on each stage of the base game on Hyper & Maniac difficulties (as Beck).\n\nI'll See You In Xel [10\u2605]\nProve yourself.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 70
+            "primary": 2,
+            "primaryText": "Chubbit Enthusiast [40\u2605]\nObtain all 50 Flag times.\n\nBeautiful Gold [10\u2605]\nObtain all 50 Gold Medals.",
+            "community": 1,
+            "communityText": "Rotund Revelry\nComplete all primary objectives from all site-listed games in the Rotund Rabbit franchise. See verification requirements for complete list.",
+            "achievements": 18
         }
     }
 }
-var gameOfDayDate = '2025-09-10'
+var gameOfDayDate = '2025-09-11'
