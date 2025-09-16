c745b03cf7b9c3f6b4d639491022dc5b1eedab76
Icondor <ivanfigus@gmail.com>
2025-09-16 08:44:54 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,52 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 982290,
-        "CEId": "febdee99-bbee-4787-abc1-bad267e2165a",
-        "name": "Airborne Kingdom",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/982290/header.jpg?t=1736784478",
-        "genre": "Strategy",
+        "platformId": 420070,
+        "CEId": "50002fa2-2d45-4455-8879-59d3380b77f4",
+        "name": "Kill The Plumber",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/420070/header.jpg?t=1463668968",
+        "genre": "Platformer",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2025-06-25T06:34:39.000Z",
+        "points": 15,
+        "createdAt": "2022-09-16T22:32:14.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 0,
-        "playersTotal": 18,
+        "playersCompleted": 1,
+        "playersStarted": 2,
+        "playersTotal": 21,
         "priceData": {
             "USD": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2299,
-                "final": 2299,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1949,
-                "final": 1949,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 280000,
-                "final": 280000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 3650,
-                "final": 3650,
+                "initial": 49800,
+                "final": 49800,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 3249,
-                "final": 3249,
+                "initial": 549,
+                "final": 549,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Airborne King [5\u2605]\nRescue the Tundra Explorers in Hard Mode with the Default Town Center.",
+            "primary": 2,
+            "primaryText": "Massacre the Marios [10\u2605]\nBeat the time limit for each world speedrun.\n\nPlumber Plunking [5\u2605]\nGet 3 stars on every level.",
             "community": 0,
             "communityText": "",
-            "achievements": 30
+            "achievements": 27
         }
     }
 }
-var gameOfDayDate = '2025-09-15'
+var gameOfDayDate = '2025-09-16'
