78c374e374293b909d7b4907457407ee6aa243db
Icondor <ivanfigus@gmail.com>
2025-09-18 09:12:59 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1049890,
-        "CEId": "dc9e2542-0e9d-448c-94cd-dc388826e419",
-        "name": "Little Witch Nobeta",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1049890/header.jpg?t=1694012578",
-        "genre": "Action",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2023-09-17T05:51:29.000Z",
+        "platformId": 238010,
+        "CEId": "76b60daf-0df0-4855-a576-be5cb40e61a1",
+        "name": "Deus Ex: Human Revolution - Director's Cut",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/238010/header.jpg?t=1661266769",
+        "genre": "First-Person",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2022-09-18T22:32:00.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 6,
-        "playersStarted": 7,
-        "playersTotal": 137,
+        "playersCompleted": 31,
+        "playersStarted": 0,
+        "playersTotal": 402,
         "priceData": {
             "USD": {
-                "initial": 2499,
-                "final": 2499,
-                "discountPercent": 0
+                "initial": 1999,
+                "final": 299,
+                "discountPercent": 85
             },
             "EUR": {
-                "initial": 2199,
-                "final": 2199,
-                "discountPercent": 0
+                "initial": 1999,
+                "final": 299,
+                "discountPercent": 85
             },
             "GBP": {
-                "initial": 1999,
-                "final": 1999,
-                "discountPercent": 0
+                "initial": 1649,
+                "final": 247,
+                "discountPercent": 85
             },
             "JPY": {
-                "initial": 290000,
-                "final": 290000,
-                "discountPercent": 0
+                "initial": 277000,
+                "final": 41500,
+                "discountPercent": 85
             },
             "AUD": {
-                "initial": 2499,
-                "final": 2499,
-                "discountPercent": 0
+                "initial": 2995,
+                "final": 449,
+                "discountPercent": 85
             },
             "CAD": {
-                "initial": 2499,
-                "final": 2499,
-                "discountPercent": 0
+                "initial": 2599,
+                "final": 389,
+                "discountPercent": 85
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "A Witch's Ascent [20\u2605]\nClear the Trial Tower Mode without dying. \n\nWizardry Trials [10\u2605]\nClear the Trial Tower Mode.",
+            "primary": 1,
+            "primaryText": "Human at the Core [10\u2605]\nSeparately, beat the game without triggering any alarms, without killing anyone and on the hardest difficulty. / Complete \"Hei Zhen Zhu\" and \"Rifleman Bank Station\" using no Praxis kits, weapons, grenades or mines.",
             "community": 0,
             "communityText": "",
-            "achievements": 42
+            "achievements": 59
         }
     }
 }
-var gameOfDayDate = '2025-09-17'
+var gameOfDayDate = '2025-09-18'
