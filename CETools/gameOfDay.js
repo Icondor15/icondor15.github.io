ac71105ae1139353d0fb188d87f4d73e4b6f5669
Icondor <ivanfigus@gmail.com>
2025-09-19 09:12:40 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 238010,
-        "CEId": "76b60daf-0df0-4855-a576-be5cb40e61a1",
-        "name": "Deus Ex: Human Revolution - Director's Cut",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/238010/header.jpg?t=1661266769",
+        "platformId": 1841070,
+        "CEId": "60163c66-c6f4-488c-b66d-5c94697c1db6",
+        "name": "Bhop Craze",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1841070/header.jpg?t=1661906806",
         "genre": "First-Person",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2022-09-18T22:32:00.000Z",
+        "tier": 5,
+        "points": 220,
+        "createdAt": "2022-09-19T08:44:29.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 31,
-        "playersStarted": 0,
-        "playersTotal": 402,
+        "playersCompleted": 2,
+        "playersStarted": 3,
+        "playersTotal": 55,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 299,
-                "discountPercent": 85
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 299,
-                "discountPercent": 85
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
             },
             "GBP": {
-                "initial": 1649,
-                "final": 247,
-                "discountPercent": 85
+                "initial": 339,
+                "final": 339,
+                "discountPercent": 0
             },
             "JPY": {
-                "initial": 277000,
-                "final": 41500,
-                "discountPercent": 85
+                "initial": 47000,
+                "final": 47000,
+                "discountPercent": 0
             },
             "AUD": {
-                "initial": 2995,
-                "final": 449,
-                "discountPercent": 85
+                "initial": 595,
+                "final": 595,
+                "discountPercent": 0
             },
             "CAD": {
-                "initial": 2599,
-                "final": 389,
-                "discountPercent": 85
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Human at the Core [10\u2605]\nSeparately, beat the game without triggering any alarms, without killing anyone and on the hardest difficulty. / Complete \"Hei Zhen Zhu\" and \"Rifleman Bank Station\" using no Praxis kits, weapons, grenades or mines.",
+            "primary": 2,
+            "primaryText": "Hopping Mad [170\u2605]\nGet a diamond trophy on every map in the \"Extremely difficult\" section.\n\nLeaps and Bounds [50\u2605]\nGet a diamond trophy on every map in all other sections.",
             "community": 0,
             "communityText": "",
-            "achievements": 59
+            "achievements": 20
         }
     }
 }
-var gameOfDayDate = '2025-09-18'
+var gameOfDayDate = '2025-09-19'
