7ae9bcb3341de26494f5474a594dfc2aac98cfad
Icondor <ivanfigus@gmail.com>
2025-09-20 08:55:58 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1841070,
-        "CEId": "60163c66-c6f4-488c-b66d-5c94697c1db6",
-        "name": "Bhop Craze",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1841070/header.jpg?t=1661906806",
-        "genre": "First-Person",
-        "tier": 5,
-        "points": 220,
-        "createdAt": "2022-09-19T08:44:29.000Z",
+        "platformId": 1990940,
+        "CEId": "b8d3fcbe-2ce7-44db-88e9-71dbb1eab543",
+        "name": "The Impossible Game 2",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1990940/header.jpg?t=1694452571",
+        "genre": "Platformer",
+        "tier": 3,
+        "points": 70,
+        "createdAt": "2024-09-20T14:14:59.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
         "playersCompleted": 2,
-        "playersStarted": 3,
-        "playersTotal": 55,
+        "playersStarted": 2,
+        "playersTotal": 10,
         "priceData": {
             "USD": {
-                "initial": 399,
-                "final": 399,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "GBP": {
                 "initial": 399,
                 "final": 399,
                 "discountPercent": 0
             },
-            "GBP": {
-                "initial": 339,
-                "final": 339,
-                "discountPercent": 0
-            },
             "JPY": {
-                "initial": 47000,
-                "final": 47000,
+                "initial": 58000,
+                "final": 58000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 595,
-                "final": 595,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 649,
+                "final": 649,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Hopping Mad [170\u2605]\nGet a diamond trophy on every map in the \"Extremely difficult\" section.\n\nLeaps and Bounds [50\u2605]\nGet a diamond trophy on every map in all other sections.",
+            "primaryText": "Impossibler [60\u2605]\nComplete all levels without using checkpoints.\n\nElectric Boogaloo [10\u2605]\nSeparately; Complete all levels using checkpoints, complete Cloud 9 with over 1 million score and complete Nacreous Snowmelt with over 600k score.",
             "community": 0,
             "communityText": "",
-            "achievements": 20
+            "achievements": 0
         }
     }
 }
-var gameOfDayDate = '2025-09-19'
+var gameOfDayDate = '2025-09-20'
