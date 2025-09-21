da341b14fed1e0a505bec47f9287bff5b648c9ce
Icondor <ivanfigus@gmail.com>
2025-09-21 09:13:03 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1990940,
-        "CEId": "b8d3fcbe-2ce7-44db-88e9-71dbb1eab543",
-        "name": "The Impossible Game 2",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1990940/header.jpg?t=1694452571",
-        "genre": "Platformer",
-        "tier": 3,
-        "points": 70,
-        "createdAt": "2024-09-20T14:14:59.000Z",
+        "gameType": 3,
+        "platformId": 2304,
+        "CEId": "14c8d474-a366-4b1a-937b-aef8f99496b1",
+        "name": "The Legendary Axe II",
+        "header": "https://i.postimg.cc/CLTxXZd9/legendaryaxe2-1692388445898.png",
+        "genre": "Action",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2024-10-05T20:12:26.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 2,
-        "playersTotal": 10,
-        "priceData": {
-            "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 58000,
-                "final": 58000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 649,
-                "final": 649,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 1,
+        "playersStarted": 0,
+        "playersTotal": 1,
         "milestones": {
-            "primary": 2,
-            "primaryText": "Impossibler [60\u2605]\nComplete all levels without using checkpoints.\n\nElectric Boogaloo [10\u2605]\nSeparately; Complete all levels using checkpoints, complete Cloud 9 with over 1 million score and complete Nacreous Snowmelt with over 600k score.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 0
+            "primary": 1,
+            "primaryText": "You Axed For It! [15\u2605]\nFinish the game.",
+            "community": 1,
+            "communityText": "Legendary Chap\nFinish the game without dying.",
+            "achievements": 18
         }
     }
 }
-var gameOfDayDate = '2025-09-20'
+var gameOfDayDate = '2025-09-21'
