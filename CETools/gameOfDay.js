db952b6c0ce703aeca87b91f960552351fd702a5
Icondor <ivanfigus@gmail.com>
2025-09-23 09:03:42 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2193690,
-        "CEId": "39096080-461e-4641-a0b2-21b02b5bb37d",
-        "name": "SPEEDRUNNER",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2193690/header.jpg?t=1733623656",
-        "genre": "First-Person",
+        "platformId": 207140,
+        "CEId": "f07bc03c-472d-4042-addf-03de29dbf080",
+        "name": "SpeedRunners",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/207140/header.jpg?t=1631522005",
+        "genre": "Platformer",
         "tier": 1,
-        "points": 10,
-        "createdAt": "2023-09-22T01:28:22.000Z",
+        "points": 15,
+        "createdAt": "2022-03-30T01:33:52.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 7,
+        "playersCompleted": 65,
         "playersStarted": 0,
-        "playersTotal": 16,
+        "playersTotal": 626,
         "priceData": {
             "USD": {
-                "initial": 299,
-                "final": 299,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 299,
-                "final": 299,
+                "initial": 1479,
+                "final": 1479,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 249,
-                "final": 249,
+                "initial": 1279,
+                "final": 1279,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 35000,
-                "final": 35000,
+                "initial": 170000,
+                "final": 170000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 450,
-                "final": 450,
+                "initial": 2195,
+                "final": 2195,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 389,
-                "final": 389,
+                "initial": 1949,
+                "final": 1949,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "STRAFERUNNER [10\u2605]\nProve yourself.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 20
+            "primaryText": "Live Fast [15\u2605]\nBeat all chapters on \"Unfair\" difficulty.",
+            "community": 1,
+            "communityText": "Die Young\nBe promoted to the Bronze League.",
+            "achievements": 29
         }
     }
 }
-var gameOfDayDate = '2025-09-22'
+var gameOfDayDate = '2025-09-23'
