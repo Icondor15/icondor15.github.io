c3fc02ae86b1212d7714cd5078cef73d62dc771a
Icondor <ivanfigus@gmail.com>
2025-09-26 08:58:43 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1520330,
-        "CEId": "1b2a5997-9812-4099-bcb7-b215e17de447",
-        "name": "TechBeat Heart",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1520330/header.jpg?t=1654351385",
-        "genre": "Bullet Hell",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2021-09-25T21:35:44.000Z",
+        "platformId": 373990,
+        "CEId": "fe826612-9685-41e5-b193-f30e15107394",
+        "name": "Metrico+",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/373990/header.jpg?t=1560863288",
+        "genre": "Platformer",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2022-09-26T07:57:31.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 4,
+        "playersCompleted": 5,
         "playersStarted": 0,
-        "playersTotal": 86,
+        "playersTotal": 82,
         "priceData": {
             "USD": {
-                "initial": 199,
-                "final": 199,
+                "initial": 1399,
+                "final": 1399,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 199,
-                "final": 199,
+                "initial": 1399,
+                "final": 1399,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 169,
-                "final": 169,
+                "initial": 1059,
+                "final": 1059,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 23500,
-                "final": 23500,
+                "initial": 139900,
+                "final": 139900,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 295,
-                "final": 295,
+                "initial": 1995,
+                "final": 1995,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 259,
-                "final": 259,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "With Love [30\u2605]\nProve yourself!",
+            "primaryText": "Abstraction [5\u2605]\n100% the game.",
             "community": 0,
             "communityText": "",
-            "achievements": 55
+            "achievements": 18
         }
     }
 }
-var gameOfDayDate = '2025-09-25'
+var gameOfDayDate = '2025-09-26'
