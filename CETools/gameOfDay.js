1c4d7766b174db344b61d4c39faa95b2e9cec13a
Icondor <ivanfigus@gmail.com>
2025-09-27 09:13:00 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 373990,
-        "CEId": "fe826612-9685-41e5-b193-f30e15107394",
-        "name": "Metrico+",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/373990/header.jpg?t=1560863288",
+        "platformId": 1074500,
+        "CEId": "56fe730d-ffe1-4e58-9181-3b9e59786335",
+        "name": "Cranked Up",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1074500/header.jpg?t=1626847262",
         "genre": "Platformer",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2022-09-26T07:57:31.000Z",
+        "points": 15,
+        "createdAt": "2022-09-27T02:41:27.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 5,
-        "playersStarted": 0,
-        "playersTotal": 82,
+        "playersCompleted": 2,
+        "playersStarted": 2,
+        "playersTotal": 11,
         "priceData": {
             "USD": {
-                "initial": 1399,
-                "final": 1399,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1399,
-                "final": 1399,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1059,
-                "final": 1059,
+                "initial": 719,
+                "final": 719,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 139900,
-                "final": 139900,
+                "initial": 101000,
+                "final": 101000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1995,
-                "final": 1995,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 1149,
+                "final": 1149,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Abstraction [5\u2605]\n100% the game.",
+            "primary": 2,
+            "primaryText": "True Golden Challenger [10\u2605]\nGet all gold medals in challenge mode.\n\nGilded Doughnut [5\u2605]\nGet all gold time medals and finish all levels in challenge mode.",
             "community": 0,
             "communityText": "",
-            "achievements": 18
+            "achievements": 46
         }
     }
 }
-var gameOfDayDate = '2025-09-26'
+var gameOfDayDate = '2025-09-27'
