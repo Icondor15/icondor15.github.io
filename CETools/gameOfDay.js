55178dbe79df8017df1a42c7341a116735b6645b
Icondor <ivanfigus@gmail.com>
2025-09-28 09:10:41 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1074500,
-        "CEId": "56fe730d-ffe1-4e58-9181-3b9e59786335",
-        "name": "Cranked Up",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1074500/header.jpg?t=1626847262",
-        "genre": "Platformer",
+        "gameType": 3,
+        "platformId": 16052,
+        "CEId": "939aac4b-2801-488f-8638-0b7a9646621d",
+        "name": "Robin Hood",
+        "header": "https://i.postimg.cc/8P53YP4X/robin-hood.png",
+        "genre": "Action",
         "tier": 1,
-        "points": 15,
-        "createdAt": "2022-09-27T02:41:27.000Z",
+        "points": 5,
+        "createdAt": "2024-09-28T22:55:28.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 2,
-        "playersTotal": 11,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 719,
-                "final": 719,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 101000,
-                "final": 101000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1450,
-                "final": 1450,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1149,
-                "final": 1149,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 1,
+        "playersStarted": 0,
+        "playersTotal": 2,
         "milestones": {
-            "primary": 2,
-            "primaryText": "True Golden Challenger [10\u2605]\nGet all gold medals in challenge mode.\n\nGilded Doughnut [5\u2605]\nGet all gold time medals and finish all levels in challenge mode.",
+            "primary": 1,
+            "primaryText": "I Feel like Robin Hood! [5\u2605]\nComplete difficulty 4 after looping difficulty 3.",
             "community": 0,
             "communityText": "",
-            "achievements": 46
+            "achievements": 15
         }
     }
 }
-var gameOfDayDate = '2025-09-27'
+var gameOfDayDate = '2025-09-28'
