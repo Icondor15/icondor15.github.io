6c86ef6ac51e3dd81fb0d9415ec794b5a199f281
Icondor <ivanfigus@gmail.com>
2025-09-29 09:12:37 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,59 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 16052,
-        "CEId": "939aac4b-2801-488f-8638-0b7a9646621d",
-        "name": "Robin Hood",
-        "header": "https://i.postimg.cc/8P53YP4X/robin-hood.png",
+        "gameType": 0,
+        "platformId": 1175460,
+        "CEId": "208f2524-e870-4161-980d-6f81e431ab7e",
+        "name": "Revita",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1175460/header.jpg?t=1722526793",
         "genre": "Action",
-        "tier": 1,
-        "points": 5,
-        "createdAt": "2024-09-28T22:55:28.000Z",
+        "tier": 4,
+        "points": 95,
+        "createdAt": "2024-09-29T19:12:53.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 0,
-        "playersTotal": 2,
+        "playersCompleted": 3,
+        "playersStarted": 9,
+        "playersTotal": 132,
+        "priceData": {
+            "USD": {
+                "initial": 1699,
+                "final": 1699,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1699,
+                "final": 1699,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1349,
+                "final": 1349,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 173000,
+                "final": 173000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2395,
+                "final": 2395,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1949,
+                "final": 1949,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "I Feel like Robin Hood! [5\u2605]\nComplete difficulty 4 after looping difficulty 3.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 15
-        }
+            "primary": 5,
+            "primaryText": "Finale of Acceptance   [40\u2605]\nSeparately; Hitless Last Refuge, complete the game without getting hit and complete the game on loop 5.\n\nCrux of Depression  [20\u2605]\nProve yourself.\n\nAxis of Bargaining  [15\u2605]\nProve yourself.\n\nDawn of Anger  [10\u2605]\nProve yourself.\n\nInception of Denial  [10\u2605]\nSeparately; Beat the Last Refuge and complete the first two areas without getting hit.",
+            "community": 1,
+            "communityText": "Stages of Grief (SO)\nBeat the game on shard 100.",
+            "achievements": 127
+        },
+        "information": "Balanced around stable version. Objectives should be done there instead of beta, specially those with video proof.\n\nGuide for all hidden achievements: [https://steamcommunity.com/sharedfiles/filedetails/?id=2971250949](https://steamcommunity.com/sharedfiles/filedetails/?id=2971250949)\n\nNote: Shard 100 may move to PO in the future if it is proven possible. Currently CO as a placeholder."
     }
 }
-var gameOfDayDate = '2025-09-28'
+var gameOfDayDate = '2025-09-29'
