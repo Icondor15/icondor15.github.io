10f8130612a482cc1b7d41b9635b01fb503b7d51
Icondor <ivanfigus@gmail.com>
2025-10-01 09:13:02 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,58 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 5535,
-        "CEId": "050fc36e-e692-483b-9863-b4be5640c238",
-        "name": "Crash Bandicoot 2: N-Tranced",
-        "header": "https://i.postimg.cc/Hnbvz9Fm/crash-n-traced.png",
+        "gameType": 0,
+        "platformId": 2815150,
+        "CEId": "82b12c4c-6142-4a6a-a534-29f3f1aaf980",
+        "name": "Hollow Floor",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2815150/header.jpg?t=1726736391",
         "genre": "Platformer",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2024-09-30T01:43:07.000Z",
+        "points": 10,
+        "createdAt": "2024-10-01T01:36:00.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 0,
-        "playersTotal": 2,
+        "playersCompleted": 3,
+        "playersStarted": 1,
+        "playersTotal": 10,
+        "priceData": {
+            "USD": {
+                "initial": 299,
+                "final": 299,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 299,
+                "final": 299,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 249,
+                "final": 249,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 35000,
+                "final": 35000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 450,
+                "final": 450,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 389,
+                "final": 389,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "N-Sane [5\u2605]\nGet all Gems, Crystals, and Platinum Relics",
+            "primary": 2,
+            "primaryText": "Coinllector [5\u2605]\nCollect all coins in Coin mode without dying.\n\nCollectorb [5\u2605]\nCollect all three orbs and finish the game without dying.",
             "community": 0,
             "communityText": "",
-            "achievements": 64
+            "achievements": 14
         }
     }
 }
-var gameOfDayDate = '2025-09-30'
+var gameOfDayDate = '2025-10-01'
