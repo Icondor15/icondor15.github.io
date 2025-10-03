228e66399b85c23a2d0494fc92735e3663129cd4
Icondor <ivanfigus@gmail.com>
2025-10-03 08:42:46 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,58 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1442170,
-        "CEId": "2f0c11dd-0339-49c9-ad57-fa9df01250a4",
-        "name": "KILLRUN",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1442170/header.jpg?t=1662651244",
-        "genre": "First-Person",
-        "tier": 4,
-        "points": 90,
-        "createdAt": "2022-10-02T10:22:54.000Z",
-        "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 10,
-        "playersStarted": 13,
-        "playersTotal": 107,
+        "platformId": 1627720,
+        "CEId": "a2f42487-5b7f-4427-bbb3-cd7a047d6031",
+        "name": "Lies of P",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1627720/header.jpg?t=1754552654",
+        "genre": "Action",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2023-10-03T23:01:37.000Z",
+        "updatedAt": "2025-09-13T16:28:58.000Z",
+        "playersCompleted": 2,
+        "playersStarted": 67,
+        "playersTotal": 131,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
+                "initial": 5999,
+                "final": 2999,
+                "discountPercent": 50
             },
             "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
+                "initial": 5999,
+                "final": 2999,
+                "discountPercent": 50
             },
             "GBP": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
+                "initial": 4999,
+                "final": 2499,
+                "discountPercent": 50
             },
             "JPY": {
-                "initial": 52000,
-                "final": 52000,
-                "discountPercent": 0
+                "initial": 836000,
+                "final": 418000,
+                "discountPercent": 50
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
+                "initial": 8995,
+                "final": 4497,
+                "discountPercent": 50
             },
             "CAD": {
-                "initial": 569,
-                "final": 569,
-                "discountPercent": 0
+                "initial": 7999,
+                "final": 3999,
+                "discountPercent": 50
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Pacifist Pro [40\u2605]\nSet a pro time on every Momentum Jumper map.\n\n360 No-Scope[20\u2605 (Partial: 5\u2605)]\nSet a pro time on every Killrun map. *Partial points are awarded as a progress milestone.\n\nJump Around[15\u2605 (Partial: 5\u2605)]\nComplete both map packs in Jumper mode. *Partial points are awarded as a progress milestone.\n\nPacifist Adept [15\u2605]\nSet a pro time on every Original Jumper map.",
+            "primary": 3,
+            "primaryText": "Weight of Your Picks [20\u2605]\nDefeat all battle memories on Difficulty 5.\n\nPainted in Blood [10\u2605]\nComplete the DLC on Legendary Stalker difficulty.\n\nNo Mere Puppet [10\u2605]\nDefeat the Nameless Puppet.",
             "community": 0,
             "communityText": "",
-            "achievements": 22
-        },
-        "information": "For Pacifist Adept:\nhttps://upload.cedb.me/hElePV.png"
+            "achievements": 53
+        }
     }
 }
-var gameOfDayDate = '2025-10-02'
+var gameOfDayDate = '2025-10-03'
