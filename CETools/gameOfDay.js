b216c958ddceb2a8bfd22951c2c519e18c38d1b9
Icondor <ivanfigus@gmail.com>
2025-10-05 09:12:45 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,58 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 1294170,
-        "CEId": "ac61194a-354d-4cdd-abcc-5d1bcbefba2f",
-        "name": "TCSTRIKERS3",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1294170/header.jpg?t=1713051474",
-        "genre": "Bullet Hell",
+        "gameType": 0,
+        "platformId": 1573090,
+        "CEId": "2a13022c-7098-4ecd-8459-3c93b1ab01cd",
+        "name": "Impaler Gold",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1573090/header.jpg?t=1726503502",
+        "genre": "First-Person",
         "tier": 1,
         "points": 5,
-        "createdAt": "2024-10-04T08:12:29.000Z",
+        "createdAt": "2024-10-05T20:23:34.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
         "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 52,
+        "playersTotal": 22,
+        "priceData": {
+            "USD": {
+                "initial": 499,
+                "final": 224,
+                "discountPercent": 55
+            },
+            "EUR": {
+                "initial": 499,
+                "final": 224,
+                "discountPercent": 55
+            },
+            "GBP": {
+                "initial": 429,
+                "final": 193,
+                "discountPercent": 55
+            },
+            "JPY": {
+                "initial": 58000,
+                "final": 26100,
+                "discountPercent": 55
+            },
+            "AUD": {
+                "initial": 750,
+                "final": 337,
+                "discountPercent": 55
+            },
+            "CAD": {
+                "initial": 649,
+                "final": 292,
+                "discountPercent": 55
+            }
+        },
         "milestones": {
             "primary": 1,
-            "primaryText": "Techno Comeback [5\u2605]\n1CC the game on Original mode and beat the extra stage.",
+            "primaryText": "Danger Zone [5\u2605]\nSeparately; complete the game on Nightmare difficulty and complete the collectathon mode in under 13 minutes.",
             "community": 0,
             "communityText": "",
-            "achievements": 0
+            "achievements": 23
         }
     }
 }
-var gameOfDayDate = '2025-10-04'
+var gameOfDayDate = '2025-10-05'
