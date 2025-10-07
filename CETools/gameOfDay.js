d23b9789424a0eb7b62a393d02d4992576b8a958
Icondor <ivanfigus@gmail.com>
2025-10-07 08:16:17 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1076200,
-        "CEId": "d39bc0c4-b2c6-4af4-b811-23dd33d84b67",
-        "name": "Roguebook",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1076200/header.jpg?t=1685103732",
-        "genre": "Strategy",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2023-10-06T12:29:29.000Z",
+        "platformId": 378540,
+        "CEId": "9885e972-2a91-4391-88dc-13c67f0a3c01",
+        "name": "The Surge",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/378540/header.jpg?t=1649842572",
+        "genre": "Action",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2022-04-17T01:51:54.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 5,
+        "playersCompleted": 16,
         "playersStarted": 0,
-        "playersTotal": 121,
+        "playersTotal": 273,
         "priceData": {
             "USD": {
-                "initial": 2499,
-                "final": 374,
-                "discountPercent": 85
+                "initial": 1499,
+                "final": 194,
+                "discountPercent": 87
             },
             "EUR": {
-                "initial": 2499,
-                "final": 374,
-                "discountPercent": 85
+                "initial": 1499,
+                "final": 194,
+                "discountPercent": 87
             },
             "GBP": {
-                "initial": 1999,
-                "final": 299,
-                "discountPercent": 85
+                "initial": 1299,
+                "final": 168,
+                "discountPercent": 87
             },
             "JPY": {
-                "initial": 257000,
-                "final": 38500,
-                "discountPercent": 85
+                "initial": 248000,
+                "final": 32200,
+                "discountPercent": 87
             },
             "AUD": {
-                "initial": 3595,
-                "final": 539,
-                "discountPercent": 85
+                "initial": 2295,
+                "final": 298,
+                "discountPercent": 87
             },
             "CAD": {
-                "initial": 2899,
-                "final": 434,
-                "discountPercent": 85
+                "initial": 1999,
+                "final": 259,
+                "discountPercent": 87
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Slay the Book [20\u2605]\nComplete a run at max Epilogue level.",
+            "primaryText": "Surge Into Action [10\u2605]\nProve yourself.",
             "community": 0,
             "communityText": "",
-            "achievements": 61
+            "achievements": 67
         }
     }
 }
-var gameOfDayDate = '2025-10-06'
+var gameOfDayDate = '2025-10-07'
