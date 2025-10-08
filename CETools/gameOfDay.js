04a8afe9b2a399d019d4c1524a034e4de0deb322
Icondor <ivanfigus@gmail.com>
2025-10-08 09:06:21 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 378540,
-        "CEId": "9885e972-2a91-4391-88dc-13c67f0a3c01",
-        "name": "The Surge",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/378540/header.jpg?t=1649842572",
-        "genre": "Action",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2022-04-17T01:51:54.000Z",
+        "platformId": 1265820,
+        "CEId": "37770c9d-2600-44f9-aecf-436958019de8",
+        "name": "Fights in Tight Spaces",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1265820/header.jpg?t=1646674466",
+        "genre": "Strategy",
+        "tier": 2,
+        "points": 22,
+        "createdAt": "2022-10-08T03:23:56.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 16,
-        "playersStarted": 0,
-        "playersTotal": 273,
+        "playersCompleted": 0,
+        "playersStarted": 5,
+        "playersTotal": 76,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 194,
-                "discountPercent": 87
+                "initial": 2499,
+                "final": 2499,
+                "discountPercent": 0
             },
             "EUR": {
-                "initial": 1499,
-                "final": 194,
-                "discountPercent": 87
+                "initial": 2099,
+                "final": 2099,
+                "discountPercent": 0
             },
             "GBP": {
-                "initial": 1299,
-                "final": 168,
-                "discountPercent": 87
+                "initial": 1949,
+                "final": 1949,
+                "discountPercent": 0
             },
             "JPY": {
-                "initial": 248000,
-                "final": 32200,
-                "discountPercent": 87
+                "initial": 257000,
+                "final": 257000,
+                "discountPercent": 0
             },
             "AUD": {
-                "initial": 2295,
-                "final": 298,
-                "discountPercent": 87
+                "initial": 3595,
+                "final": 3595,
+                "discountPercent": 0
             },
             "CAD": {
-                "initial": 1999,
-                "final": 259,
-                "discountPercent": 87
+                "initial": 2899,
+                "final": 2899,
+                "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Surge Into Action [10\u2605]\nProve yourself.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 67
+            "primary": 3,
+            "primaryText": "The Illusion of Choice (UNCLEARED) [1\u2605]\nComplete a full run in the Weapon of Choice mode on Brutal difficulty.\n\nBattles in Cruel Spots (UNCLEARED) [1\u2605]\nComplete a full run on Brutal difficulty.\n\nAgent 011 [20\u2605]\nComplete a full run on Purist difficulty. / Win a run in under an hour.",
+            "community": 1,
+            "communityText": "MI6 Deck Dominator\nComplete a full run on Purist difficulty with every starting deck.",
+            "achievements": 40
         }
     }
 }
-var gameOfDayDate = '2025-10-07'
+var gameOfDayDate = '2025-10-08'
