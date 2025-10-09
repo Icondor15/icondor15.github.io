0aa56fd33e3fdc56dc74ccf1ee55b67cd1132465
Icondor <ivanfigus@gmail.com>
2025-10-09 09:13:05 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1265820,
-        "CEId": "37770c9d-2600-44f9-aecf-436958019de8",
-        "name": "Fights in Tight Spaces",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1265820/header.jpg?t=1646674466",
-        "genre": "Strategy",
-        "tier": 2,
-        "points": 22,
-        "createdAt": "2022-10-08T03:23:56.000Z",
+        "platformId": 1710170,
+        "CEId": "f2fc5552-36af-48fa-b026-cf5182685d34",
+        "name": "Blade of Darkness",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1710170/header.jpg?t=1683621796",
+        "genre": "Action",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2023-10-09T00:41:31.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 5,
-        "playersTotal": 76,
+        "playersCompleted": 2,
+        "playersStarted": 0,
+        "playersTotal": 43,
         "priceData": {
             "USD": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2099,
-                "final": 2099,
+                "initial": 1249,
+                "final": 1249,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1949,
-                "final": 1949,
+                "initial": 1139,
+                "final": 1139,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 257000,
-                "final": 257000,
+                "initial": 152000,
+                "final": 152000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 3595,
-                "final": 3595,
+                "initial": 2150,
+                "final": 2150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2899,
-                "final": 2899,
+                "initial": 1749,
+                "final": 1749,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "The Illusion of Choice (UNCLEARED) [1\u2605]\nComplete a full run in the Weapon of Choice mode on Brutal difficulty.\n\nBattles in Cruel Spots (UNCLEARED) [1\u2605]\nComplete a full run on Brutal difficulty.\n\nAgent 011 [20\u2605]\nComplete a full run on Purist difficulty. / Win a run in under an hour.",
-            "community": 1,
-            "communityText": "MI6 Deck Dominator\nComplete a full run on Purist difficulty with every starting deck.",
-            "achievements": 40
+            "primary": 1,
+            "primaryText": "Edge of the Blade [15\u2605]\nComplete the game in one run, in 8 hours or less without using saves a single time.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 56
         }
     }
 }
-var gameOfDayDate = '2025-10-08'
+var gameOfDayDate = '2025-10-09'
