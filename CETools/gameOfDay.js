7c5fd50b08dcbef042349efd467548742ce6f25c
Icondor <ivanfigus@gmail.com>
2025-10-10 09:13:06 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1710170,
-        "CEId": "f2fc5552-36af-48fa-b026-cf5182685d34",
-        "name": "Blade of Darkness",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1710170/header.jpg?t=1683621796",
-        "genre": "Action",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2023-10-09T00:41:31.000Z",
+        "platformId": 681530,
+        "CEId": "39f6d210-1320-49ca-8038-964cc4fe4ad8",
+        "name": "NOISZ",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/681530/header.jpg?t=1693514208",
+        "genre": "Bullet Hell",
+        "tier": 6,
+        "points": 505,
+        "createdAt": "2021-10-10T16:32:22.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 0,
-        "playersTotal": 43,
+        "playersCompleted": 3,
+        "playersStarted": 26,
+        "playersTotal": 99,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 1679,
+                "final": 1679,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 1549,
+                "final": 1549,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 152000,
-                "final": 152000,
+                "initial": 154000,
+                "final": 154000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 2895,
+                "final": 2895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 2279,
+                "final": 2279,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Edge of the Blade [15\u2605]\nComplete the game in one run, in 8 hours or less without using saves a single time.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 56
+            "primary": 12,
+            "primaryText": "Overlord [100\u2605]\nGet SS rank on all re:||VERSE songs on Overload.\n\nThe Last Piece of Music [55\u2605]\nGet SS rank on all Episode G and extra pack songs on Overload.\n\nVoid Vanquisher[100\u2605 (Partial: 50\u2605)]\nMaster the re:||VERSE DLC achievements.\n\nGaiusified [35\u2605]\nMaster the Episode G DLC achievements.\n\nGuiding Light [40\u2605]\nGet AA rank or higher on all Stellar Linkage pack songs on Overload.\n\nFly Beyond [40\u2605]\nGet SS rank on all base game songs on Overload.\n\nEchoes of Two Souls [40\u2605]\nGet A on all re:||VERSE and Episode G songs on Hard. / Complete all re:||VERSE extra stories on Hard or above.\n\nSound Dodger [35\u2605]\nMaster the base game achievements.\n\ngalaXXy brain [25\u2605]\nGet AA rank or higher on all DM Ashura and HyuN artist pack songs on Overload.\n\nNew Challenger [10\u2605]\nBeat the early courses. (RE:||VERSE DLC required)\n\nIgnite your Heart [20\u2605]\nGet A on all base game songs on Hard. / Complete all Extra stories on Hard or above.\n\nLight your Spark [5\u2605]\nPlay around with the base game.",
+            "community": 4,
+            "communityText": "Beast Rush\nGet an SS on the re:||VERSE credits and the Episode G credits on Overload.\n\nMusical Course Through Void\nBeat Course ULTIMATE with an SS total grade.\n\nQuick Maths\nGet SS rank on \u03c0\u2022\u03c1\u2022maniac on Overload while wearing the \"Math Genius\" title.\n\nSuper Hero Adventure (SO)\nGet SS rank on \"Resurrection\" on Overload.",
+            "achievements": 94
         }
     }
 }
-var gameOfDayDate = '2025-10-09'
+var gameOfDayDate = '2025-10-10'
