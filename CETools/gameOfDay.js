39d3c38bf1b3f7dbb176d46c52940d06c0319abb
Icondor <ivanfigus@gmail.com>
2025-10-11 08:31:33 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 681530,
-        "CEId": "39f6d210-1320-49ca-8038-964cc4fe4ad8",
-        "name": "NOISZ",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/681530/header.jpg?t=1693514208",
-        "genre": "Bullet Hell",
-        "tier": 6,
-        "points": 505,
-        "createdAt": "2021-10-10T16:32:22.000Z",
+        "platformId": 512680,
+        "CEId": "7bc4d6c1-6f50-48ab-b6d2-ce5d3fb834c3",
+        "name": "Pan-Dimensional Conga Combat",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/512680/header.jpg?t=1518774334",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2022-10-11T23:49:38.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 26,
-        "playersTotal": 99,
+        "playersCompleted": 5,
+        "playersStarted": 0,
+        "playersTotal": 10,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1679,
-                "final": 1679,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1549,
-                "final": 1549,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 154000,
-                "final": 154000,
+                "initial": 52000,
+                "final": 52000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2895,
-                "final": 2895,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2279,
-                "final": 2279,
+                "initial": 569,
+                "final": 569,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 12,
-            "primaryText": "Overlord [100\u2605]\nGet SS rank on all re:||VERSE songs on Overload.\n\nThe Last Piece of Music [55\u2605]\nGet SS rank on all Episode G and extra pack songs on Overload.\n\nVoid Vanquisher[100\u2605 (Partial: 50\u2605)]\nMaster the re:||VERSE DLC achievements.\n\nGaiusified [35\u2605]\nMaster the Episode G DLC achievements.\n\nGuiding Light [40\u2605]\nGet AA rank or higher on all Stellar Linkage pack songs on Overload.\n\nFly Beyond [40\u2605]\nGet SS rank on all base game songs on Overload.\n\nEchoes of Two Souls [40\u2605]\nGet A on all re:||VERSE and Episode G songs on Hard. / Complete all re:||VERSE extra stories on Hard or above.\n\nSound Dodger [35\u2605]\nMaster the base game achievements.\n\ngalaXXy brain [25\u2605]\nGet AA rank or higher on all DM Ashura and HyuN artist pack songs on Overload.\n\nNew Challenger [10\u2605]\nBeat the early courses. (RE:||VERSE DLC required)\n\nIgnite your Heart [20\u2605]\nGet A on all base game songs on Hard. / Complete all Extra stories on Hard or above.\n\nLight your Spark [5\u2605]\nPlay around with the base game.",
-            "community": 4,
-            "communityText": "Beast Rush\nGet an SS on the re:||VERSE credits and the Episode G credits on Overload.\n\nMusical Course Through Void\nBeat Course ULTIMATE with an SS total grade.\n\nQuick Maths\nGet SS rank on \u03c0\u2022\u03c1\u2022maniac on Overload while wearing the \"Math Genius\" title.\n\nSuper Hero Adventure (SO)\nGet SS rank on \"Resurrection\" on Overload.",
-            "achievements": 94
+            "primary": 1,
+            "primaryText": "Immortal Conganoid [5\u2605]\nLoop the game 3 times.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 14
         }
     }
 }
-var gameOfDayDate = '2025-10-10'
+var gameOfDayDate = '2025-10-11'
