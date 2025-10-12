e66cfd792e13ab83254c58ad9d258acba0b7df41
Icondor <ivanfigus@gmail.com>
2025-10-12 08:09:49 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 512680,
-        "CEId": "7bc4d6c1-6f50-48ab-b6d2-ce5d3fb834c3",
-        "name": "Pan-Dimensional Conga Combat",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/512680/header.jpg?t=1518774334",
-        "genre": "Arcade",
+        "platformId": 1372210,
+        "CEId": "7938a2f0-a471-402f-a02c-f7d17a03155a",
+        "name": "Dap",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1372210/header.jpg?t=1635209446",
+        "genre": "Action",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2022-10-11T23:49:38.000Z",
-        "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 5,
+        "points": 15,
+        "createdAt": "2022-10-12T00:17:10.000Z",
+        "updatedAt": "2025-10-03T17:01:24.000Z",
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 10,
+        "playersTotal": 35,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1399,
+                "final": 1399,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1159,
+                "final": 1159,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 399,
-                "final": 399,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 52000,
-                "final": 52000,
+                "initial": 142000,
+                "final": 142000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 1995,
+                "final": 1995,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 569,
-                "final": 569,
+                "initial": 1549,
+                "final": 1549,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Immortal Conganoid [5\u2605]\nLoop the game 3 times.",
+            "primaryText": "Depressing Alternative of Pikmin [15\u2605]\nBeat the game without dying.",
             "community": 0,
             "communityText": "",
-            "achievements": 14
+            "achievements": 17
         }
     }
 }
-var gameOfDayDate = '2025-10-11'
+var gameOfDayDate = '2025-10-12'
