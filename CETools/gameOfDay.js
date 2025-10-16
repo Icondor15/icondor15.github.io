4a8af6c1ba822cf264b737588b94f44f998fd0b3
Icondor <ivanfigus@gmail.com>
2025-10-16 11:52:49 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1834950,
-        "CEId": "819714fa-c27d-4cd7-b55e-466e97f6dd9d",
-        "name": "Salaryman Shi",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1834950/header.jpg?t=1656605746",
-        "genre": "Platformer",
-        "tier": 2,
-        "points": 35,
-        "createdAt": "2022-10-15T03:34:19.000Z",
+        "gameType": 1,
+        "platformId": 2181800,
+        "CEId": "9a2bf4db-adb5-4419-9423-74992b0467d1",
+        "name": "Just Dodge, LOL",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2181800/header.jpg?t=1706731389",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 16,
+        "createdAt": "2024-10-16T01:52:32.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 0,
-        "playersTotal": 13,
-        "priceData": {
-            "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 52000,
-                "final": 52000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 569,
-                "final": 569,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 0,
+        "playersStarted": 2,
+        "playersTotal": 28,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Cog in the Machine[35\u2605 (Partial: 25\u2605)]\nComplete all missions and obtain all platinum medals on all the main and bonus worlds.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 20
+            "primary": 3,
+            "primaryText": "You Can Stop Now (UNCLEARED) [1\u2605]\nReach the true ending in ultra hard difficulty.\n\nYou Win [10\u2605]\nReach the true ending.\n\nDodge Harder [5\u2605]\nComplete every level with all ratings.",
+            "community": 1,
+            "communityText": "STOP DODGING HOLY\nReach the true ending in ultra hard difficulty without equipping shields or items.",
+            "achievements": 21
         }
     }
 }
-var gameOfDayDate = '2025-10-15'
+var gameOfDayDate = '2025-10-16'
