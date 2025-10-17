359319f3410b0665c026ec459cd1d717bbecf2dd
Icondor <ivanfigus@gmail.com>
2025-10-17 08:18:19 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 2181800,
-        "CEId": "9a2bf4db-adb5-4419-9423-74992b0467d1",
-        "name": "Just Dodge, LOL",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2181800/header.jpg?t=1706731389",
-        "genre": "Arcade",
+        "gameType": 3,
+        "platformId": 16955,
+        "CEId": "396c7fd1-a703-413d-b542-2e2ef49851fe",
+        "name": "Tower Toppler",
+        "header": "https://i.postimg.cc/k4t1CfD2/tower-toppler-atari-7800-v0-xqdo5spi7t8d1.png",
+        "genre": "Platformer",
         "tier": 1,
-        "points": 16,
-        "createdAt": "2024-10-16T01:52:32.000Z",
+        "points": 5,
+        "createdAt": "2024-10-17T23:03:36.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 2,
-        "playersTotal": 28,
+        "playersCompleted": 1,
+        "playersStarted": 0,
+        "playersTotal": 3,
         "milestones": {
-            "primary": 3,
-            "primaryText": "You Can Stop Now (UNCLEARED) [1\u2605]\nReach the true ending in ultra hard difficulty.\n\nYou Win [10\u2605]\nReach the true ending.\n\nDodge Harder [5\u2605]\nComplete every level with all ratings.",
-            "community": 1,
-            "communityText": "STOP DODGING HOLY\nReach the true ending in ultra hard difficulty without equipping shields or items.",
-            "achievements": 21
+            "primary": 1,
+            "primaryText": "Top Toppler [5\u2605]\nTopple all eight towers in a single game.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 11
         }
     }
 }
-var gameOfDayDate = '2025-10-16'
+var gameOfDayDate = '2025-10-17'
