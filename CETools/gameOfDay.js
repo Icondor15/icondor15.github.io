5a8431042b4b3b1b2469a51d78e11e0d16a0b4ac
Icondor <ivanfigus@gmail.com>
2025-10-23 09:14:07 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,58 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 18333,
-        "CEId": "db6428e4-780d-4348-8ca7-78e6ffca8c3a",
-        "name": "TrackMania DS",
-        "header": "https://upload.cedb.me/db6428e4-780d-4348-8ca7-78e6ffca8c3a.png",
-        "genre": "Arcade",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2024-10-22T18:59:11.000Z",
-        "updatedAt": "2025-10-21T04:41:00.000Z",
-        "playersCompleted": 5,
+        "gameType": 0,
+        "platformId": 1972440,
+        "CEId": "651632fe-874a-41f6-a379-f3896feb69ef",
+        "name": "Shotgun King: The Final Checkmate",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1972440/header.jpg?t=1668450783",
+        "genre": "Strategy",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2022-10-23T21:12:14.000Z",
+        "updatedAt": "2025-09-08T23:40:11.000Z",
+        "playersCompleted": 10,
         "playersStarted": 0,
-        "playersTotal": 11,
+        "playersTotal": 274,
+        "priceData": {
+            "USD": {
+                "initial": 999,
+                "final": 999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 999,
+                "final": 999,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 899,
+                "final": 899,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 140000,
+                "final": 140000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 1499,
+                "final": 1499,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1299,
+                "final": 1299,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
             "primary": 1,
-            "primaryText": "Janky Joyride [25\u2605]\nCollect all author medals within race mode.",
+            "primaryText": "Regicide [15\u2605]\nFinish Throne Mode at Rank 15. / Beat Throne Mode in less than 2 minutes.",
             "community": 0,
             "communityText": "",
-            "achievements": 230
+            "achievements": 84
         }
     }
 }
-var gameOfDayDate = '2025-10-22'
+var gameOfDayDate = '2025-10-23'
