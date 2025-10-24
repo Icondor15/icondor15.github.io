290ef4b2621645dee886b5a479a67a76d6f84b8f
Icondor <ivanfigus@gmail.com>
2025-10-24 09:02:26 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1972440,
-        "CEId": "651632fe-874a-41f6-a379-f3896feb69ef",
-        "name": "Shotgun King: The Final Checkmate",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1972440/header.jpg?t=1668450783",
-        "genre": "Strategy",
+        "platformId": 1083310,
+        "CEId": "7c1920f6-1703-42f8-99fc-ef7181014a39",
+        "name": "OUTBUDDIES DX",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1083310/header.jpg?t=1594997233",
+        "genre": "Action",
         "tier": 1,
         "points": 15,
-        "createdAt": "2022-10-23T21:12:14.000Z",
+        "createdAt": "2022-10-24T01:16:40.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 10,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 274,
+        "playersTotal": 103,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
+                "initial": 1799,
+                "final": 125,
+                "discountPercent": 93
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
+                "initial": 1799,
+                "final": 125,
+                "discountPercent": 93
             },
             "GBP": {
-                "initial": 899,
-                "final": 899,
-                "discountPercent": 0
+                "initial": 1499,
+                "final": 104,
+                "discountPercent": 93
             },
             "JPY": {
-                "initial": 140000,
-                "final": 140000,
-                "discountPercent": 0
+                "initial": 200000,
+                "final": 14000,
+                "discountPercent": 93
             },
             "AUD": {
-                "initial": 1499,
-                "final": 1499,
-                "discountPercent": 0
+                "initial": 2650,
+                "final": 185,
+                "discountPercent": 93
             },
             "CAD": {
-                "initial": 1299,
-                "final": 1299,
-                "discountPercent": 0
+                "initial": 2349,
+                "final": 164,
+                "discountPercent": 93
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Regicide [15\u2605]\nFinish Throne Mode at Rank 15. / Beat Throne Mode in less than 2 minutes.",
+            "primaryText": "King of Monsters [15\u2605]\nComplete the Boss Rush mode.",
             "community": 0,
             "communityText": "",
-            "achievements": 84
+            "achievements": 38
         }
     }
 }
-var gameOfDayDate = '2025-10-23'
+var gameOfDayDate = '2025-10-24'
