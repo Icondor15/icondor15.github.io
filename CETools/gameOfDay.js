5d9532c6fb5bc76c743d5baebfabedf844a8b198
Icondor <ivanfigus@gmail.com>
2025-10-26 08:04:22 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 248330,
-        "CEId": "3cea3a49-ab0e-41b5-ab57-c1ffb999b5e9",
-        "name": "Dino Run DX",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/248330/header.jpg?t=1588695602",
+        "platformId": 792710,
+        "CEId": "3fa0356b-f027-48e6-8923-d279b6e65b7b",
+        "name": "Levelhead: Platformer Maker",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/792710/header.jpg?t=1687544673",
         "genre": "Platformer",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2021-10-25T00:06:23.000Z",
+        "points": 10,
+        "createdAt": "2023-10-26T19:07:09.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 23,
+        "playersCompleted": 9,
         "playersStarted": 0,
-        "playersTotal": 220,
+        "playersTotal": 205,
         "priceData": {
             "USD": {
-                "initial": 599,
-                "final": 599,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 589,
-                "final": 589,
-                "discountPercent": 0
-            },
-            "GBP": {
                 "initial": 499,
                 "final": 499,
                 "discountPercent": 0
             },
+            "EUR": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 429,
+                "final": 429,
+                "discountPercent": 0
+            },
             "JPY": {
-                "initial": 70000,
-                "final": 70000,
+                "initial": 58000,
+                "final": 58000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 879,
-                "final": 879,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 779,
-                "final": 779,
+                "initial": 649,
+                "final": 649,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Meteor Dodger [5\u2605]\nComplete the Insane Challenge. / Complete the Insane Halloween Hellrun Challenge. / Complete the Insane Planet D Challenge. / Complete All Insane Speedruns. / Doomsurf for 60 Seconds.",
+            "primaryText": "Zipper Flipper [10\u2605]\nDeliver every package under the On-Time benchmark in the Bureau's training program.",
             "community": 0,
             "communityText": "",
-            "achievements": 49
+            "achievements": 44
         }
     }
 }
-var gameOfDayDate = '2025-10-25'
+var gameOfDayDate = '2025-10-26'
