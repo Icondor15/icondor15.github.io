36ce43acb6e0f4859fe8d55b76c7ed1fc46a2983
Icondor <ivanfigus@gmail.com>
2025-10-28 07:59:31 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 718590,
-        "CEId": "33919e7d-e394-4b2a-9f5c-c4934e94555c",
-        "name": "Dark Devotion",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/718590/header.jpg?t=1574418537",
+        "platformId": 1705150,
+        "CEId": "709839ad-9cd2-43ae-b880-9a75f049a0c8",
+        "name": "SKULL CHAINZ",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1705150/header.jpg?t=1667188864",
         "genre": "Action",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2020-01-28T08:27:58.000Z",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2023-10-28T03:18:43.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 5,
+        "playersCompleted": 1,
         "playersStarted": 0,
-        "playersTotal": 124,
+        "playersTotal": 7,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 819,
+                "final": 819,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 719,
+                "final": 719,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 205000,
-                "final": 205000,
+                "initial": 101000,
+                "final": 101000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2895,
-                "final": 2895,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2279,
-                "final": 2279,
+                "initial": 1149,
+                "final": 1149,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Faithful Templar [20\u2605]\nReach the end of the Temple while wearing the Redeemer\u2019s curse. Be an example, show the path.",
+            "primaryText": "7 Deadly Sins [5\u2605]\nComplete all levels without throwing or getting hit. ",
             "community": 0,
             "communityText": "",
-            "achievements": 45
+            "achievements": 23
         }
     }
 }
-var gameOfDayDate = '2025-10-27'
+var gameOfDayDate = '2025-10-28'
