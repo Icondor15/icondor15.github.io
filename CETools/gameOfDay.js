08707bfa3143ef5197076cf09b02e20f49e65713
Icondor <ivanfigus@gmail.com>
2025-10-30 08:59:57 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 377870,
-        "CEId": "1ae64844-3c2d-485e-8fbe-3cbb2afa1428",
-        "name": "DARIUSBURST Chronicle Saviours",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/377870/header.jpg?t=1681233055",
-        "genre": "Bullet Hell",
-        "tier": 3,
-        "points": 45,
-        "createdAt": "2023-10-29T17:56:10.000Z",
+        "platformId": 1921470,
+        "CEId": "02d4e307-8ef7-4e3c-b729-8ec59caf564c",
+        "name": "TAGLINE",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1921470/header.jpg?t=1697754352",
+        "genre": "First-Person",
+        "tier": 4,
+        "points": 110,
+        "createdAt": "2023-10-30T00:49:45.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
+        "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 56,
+        "playersTotal": 26,
         "priceData": {
             "USD": {
-                "initial": 4999,
-                "final": 4999,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 4899,
-                "final": 4899,
+                "initial": 975,
+                "final": 975,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 4199,
-                "final": 4199,
+                "initial": 850,
+                "final": 850,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 515000,
-                "final": 515000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 7350,
-                "final": 7350,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 6499,
-                "final": 6499,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Flawless Space Sailor [45\u2605]\nComplete the QUZ route in Original EX Mode without getting hit.",
-            "community": 2,
-            "communityText": "Chronicle Specialist\nComplete CS mode using the default ship on every mission.\n\nSaviour of the Cosmos\nGet all achievements.",
-            "achievements": 32
+            "primary": 2,
+            "primaryText": "Speedy Graffiti [100\u2605]\nBeat the developer score on all agent challenges.\n\nTag, you're it [10\u2605]\nObtain all platinum discs, and complete all terminals. / Survive 6 minutes in an ENDURANCE challenge. / Chain all vents in SITE without touching the floor.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 50
         }
     }
 }
-var gameOfDayDate = '2025-10-29'
+var gameOfDayDate = '2025-10-30'
