c3172c2dd5cec011164e56834c66fe4d8103865a
Icondor <ivanfigus@gmail.com>
2025-10-31 08:25:46 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,58 +1,26 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1921470,
-        "CEId": "02d4e307-8ef7-4e3c-b729-8ec59caf564c",
-        "name": "TAGLINE",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1921470/header.jpg?t=1697754352",
+        "gameType": 2,
+        "platformId": 1938090,
+        "CEId": "f5911e24-699d-4cc1-a1cf-735d0f362e59",
+        "name": "Call of Duty\u00ae: Modern Warfare\u00ae II",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1938090/header.jpg?t=1666987393",
         "genre": "First-Person",
-        "tier": 4,
-        "points": 110,
-        "createdAt": "2023-10-30T00:49:45.000Z",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2022-10-31T03:50:54.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 3,
+        "playersCompleted": 44,
         "playersStarted": 0,
-        "playersTotal": 26,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 975,
-                "final": 975,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 850,
-                "final": 850,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 120000,
-                "final": 120000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1450,
-                "final": 1450,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1299,
-                "final": 1299,
-                "discountPercent": 0
-            }
-        },
+        "playersTotal": 539,
         "milestones": {
-            "primary": 2,
-            "primaryText": "Speedy Graffiti [100\u2605]\nBeat the developer score on all agent challenges.\n\nTag, you're it [10\u2605]\nObtain all platinum discs, and complete all terminals. / Survive 6 minutes in an ENDURANCE challenge. / Chain all vents in SITE without touching the floor.",
+            "primary": 1,
+            "primaryText": "Ghost [5\u2605]\nFinish all campaign missions on \"Veteran\" or \"Realism\" difficulty.",
             "community": 0,
             "communityText": "",
-            "achievements": 50
+            "achievements": 24
         }
     }
 }
-var gameOfDayDate = '2025-10-30'
+var gameOfDayDate = '2025-10-31'
