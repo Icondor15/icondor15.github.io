16f1702d91f747abd333b453bb4efec0287ce17b
Icondor <ivanfigus@gmail.com>
2025-11-02 09:10:08 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1268960,
-        "CEId": "48be3857-aa89-4744-a91c-84cb1457ad71",
-        "name": "pureya",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1268960/header.jpg?t=1635068832",
+        "platformId": 243360,
+        "CEId": "ba80ac2a-186d-4b55-8981-d4bce6a34663",
+        "name": "TrackMania\u00b2 Valley",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/243360/header.jpg?t=1588603133",
         "genre": "Arcade",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2021-11-01T01:01:33.000Z",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2022-08-29T07:21:24.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 74,
-        "playersStarted": 0,
-        "playersTotal": 207,
+        "playersCompleted": 8,
+        "playersStarted": 5,
+        "playersTotal": 84,
         "priceData": {
             "USD": {
-                "initial": 599,
-                "final": 599,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 599,
-                "final": 599,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1699,
+                "final": 1699,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 70000,
-                "final": 70000,
+                "initial": 264000,
+                "final": 264000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 879,
-                "final": 879,
+                "initial": 2995,
+                "final": 2995,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 779,
-                "final": 779,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Lost Your Marbles [25\u2605]\nObtain all survival medals.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 9
+            "primary": 2,
+            "primaryText": "Well-Stacked in Tracks [30\u2605]\nObtain 65 Author Medals.\n\nRoad Rager [10\u2605]\nObtain 65 Gold Medals.",
+            "community": 3,
+            "communityText": "Alpine All-Star\nGet all Author Medals in the TM1 Alpine Title Pack.\n\nSpeed All-Star\nGet all Author Medals in the TM1 Speed Title Pack.\n\nValley All-Star\nGet all Author Medals in the StarTrack Title Pack.",
+            "achievements": 0
         }
     }
 }
-var gameOfDayDate = '2025-11-01'
+var gameOfDayDate = '2025-11-02'
