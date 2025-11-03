0050444c1b6c34409a93d661040acd2a575773d8
Icondor <ivanfigus@gmail.com>
2025-11-03 09:13:13 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,22 +2,22 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 243360,
-        "CEId": "ba80ac2a-186d-4b55-8981-d4bce6a34663",
-        "name": "TrackMania\u00b2 Valley",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/243360/header.jpg?t=1588603133",
+        "platformId": 848350,
+        "CEId": "2d59032b-6074-4fab-87de-fdd0e31b7b4d",
+        "name": "Katamari Damacy REROLL",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/848350/header.jpg?t=1630580881",
         "genre": "Arcade",
-        "tier": 3,
-        "points": 40,
-        "createdAt": "2022-08-29T07:21:24.000Z",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2022-11-03T20:40:23.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 8,
-        "playersStarted": 5,
-        "playersTotal": 84,
+        "playersCompleted": 9,
+        "playersStarted": 0,
+        "playersTotal": 324,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "EUR": {
@@ -26,33 +26,34 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 1599,
+                "final": 1599,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 264000,
-                "final": 264000,
+                "initial": 352000,
+                "final": 352000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2995,
-                "final": 2995,
+                "initial": 2895,
+                "final": 2895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 3999,
+                "final": 3999,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Well-Stacked in Tracks [30\u2605]\nObtain 65 Author Medals.\n\nRoad Rager [10\u2605]\nObtain 65 Gold Medals.",
-            "community": 3,
-            "communityText": "Alpine All-Star\nGet all Author Medals in the TM1 Alpine Title Pack.\n\nSpeed All-Star\nGet all Author Medals in the TM1 Speed Title Pack.\n\nValley All-Star\nGet all Author Medals in the StarTrack Title Pack.",
-            "achievements": 0
-        }
+            "primary": 1,
+            "primaryText": "Prince of All Cosmos [10\u2605]\nAchieve a Super Clear on all Constellations and unlock all Shooting Stars/Meteors.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 21
+        },
+        "information": "To obtain a Super Clear on a Constellation level, you need to reach a specific score for that level, and if successful, all the stars within the related Constellation will brighten up and shine (You can see what that specific score is for every level on this page: [https://katamari.fandom.com/wiki/Super_Clear](https://katamari.fandom.com/wiki/Super_Clear)).\n\nTo obtain all Shooting Stars, or Meteors (also called Comets), you need to complete the main levels (Make a Star 1 to Make a Star 9, and Make a Moon) within a specific time limit (You can see what the time is for every level on this page: [https://katamari.fandom.com/wiki/Shooting_Stars](https://katamari.fandom.com/wiki/Shooting_Stars)).\n\nWARNING: When you finish a level, you'll have to choose between \"Make a Star\" or \"Make a Stardust\" (If you are playing a Constellation level, the options will be \"Make a Constellation\" and \"Make a Stardust\").\nMaking a Star/Constellation will rewrite your previous score, and save your current one, no matter if it's worse or better. Making a Stardust will not save your current score. When going for the PO, be sure to pick the \"Make a Star/Constellation\" option, otherwise your score won't be saved. Be aware that when Making a Star/Constellation your previous score will be deleted and you may lose progress for the PO. For example, if you obtained a Super Clear on the Cancer Constellation, and then you replay the level and finish it with a worse score, if you pick \"Make a Constellation\" then your current score will be saved, and you'll lose the Super Clear on that Constellation, forcing you to replay the level again for the Super Clear."
     }
 }
-var gameOfDayDate = '2025-11-02'
+var gameOfDayDate = '2025-11-03'
