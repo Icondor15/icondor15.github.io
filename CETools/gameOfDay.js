718bf3245359842cfd7cb014778bc87f26f0a42b
Icondor <ivanfigus@gmail.com>
2025-11-05 09:10:05 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,58 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 2099440,
-        "CEId": "19903188-b0cc-4dc0-855f-3d048d8a5845",
-        "name": "Bananner Nababber",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2099440/header.jpg?t=1660484407",
-        "genre": "Action",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2022-12-28T23:42:41.000Z",
-        "updatedAt": "2025-10-21T04:17:51.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 26,
-        "playersTotal": 158,
+        "gameType": 0,
+        "platformId": 1696720,
+        "CEId": "68de8163-a070-4fdc-a845-9f06ffecb82f",
+        "name": "PsiloSybil",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1696720/header.jpg?t=1727872823",
+        "genre": "Platformer",
+        "tier": 3,
+        "points": 70,
+        "createdAt": "2024-11-05T02:12:24.000Z",
+        "updatedAt": "2025-09-08T23:40:11.000Z",
+        "playersCompleted": 1,
+        "playersStarted": 1,
+        "playersTotal": 8,
+        "priceData": {
+            "USD": {
+                "initial": 1299,
+                "final": 1299,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1279,
+                "final": 1279,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1099,
+                "final": 1099,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 150000,
+                "final": 150000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 1895,
+                "final": 1895,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1699,
+                "final": 1699,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Pump and Dump [5\u2605]\nBeat all bosses on Struggle difficulty with every character.\n\nRug Pull [5\u2605]\nBeat all bosses on Struggle difficulty.",
-            "community": 2,
-            "communityText": "All or None\nBeat all bosses on Struggle difficulty with any character without dying and without quitting to level select.\n\nBear Market\nBeat all bosses on Struggle difficulty with the One Hitpoint modifier enabled.",
-            "achievements": 38
+            "primary": 3,
+            "primaryText": "Not One to Truffle With[35\u2605 (Partial: 15\u2605)]\nBeat all non-boss levels with 100% collectibles deathless. Finish arcade mode.\n\nFun Gal Friendsy [25\u2605]\nAchieve 100% completion and beat the secret boss.\n\nAT THIS TIME OF YEAR, AT THIS TIME OF DAY, IN THIS PART OF THE COUNTRY, LOCALIZED ENTIRELY WITHIN YOUR KITCHEN!? [10\u2605]\nDefeat Lord Nutmeg.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 11
         }
     }
 }
-var gameOfDayDate = '2025-11-04'
+var gameOfDayDate = '2025-11-05'
