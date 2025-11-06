0cad01ba0f1e1661a0f399b84c0de69f47d16c93
Icondor <ivanfigus@gmail.com>
2025-11-06 09:10:05 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,58 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1696720,
-        "CEId": "68de8163-a070-4fdc-a845-9f06ffecb82f",
-        "name": "PsiloSybil",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1696720/header.jpg?t=1727872823",
-        "genre": "Platformer",
+        "platformId": 891790,
+        "CEId": "163eec0f-9799-42b8-9830-5bf2de116994",
+        "name": "Devil Engine",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/891790/header.jpg?t=1698088758",
+        "genre": "Bullet Hell",
         "tier": 3,
-        "points": 70,
-        "createdAt": "2024-11-05T02:12:24.000Z",
+        "points": 73,
+        "createdAt": "2023-12-29T18:14:42.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 1,
-        "playersTotal": 8,
+        "playersCompleted": 0,
+        "playersStarted": 2,
+        "playersTotal": 24,
         "priceData": {
             "USD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1279,
-                "final": 1279,
+                "initial": 1950,
+                "final": 1950,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 1675,
+                "final": 1675,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 150000,
-                "final": 150000,
+                "initial": 230000,
+                "final": 230000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1895,
-                "final": 1895,
+                "initial": 2950,
+                "final": 2950,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 2599,
+                "final": 2599,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Not One to Truffle With[35\u2605 (Partial: 15\u2605)]\nBeat all non-boss levels with 100% collectibles deathless. Finish arcade mode.\n\nFun Gal Friendsy [25\u2605]\nAchieve 100% completion and beat the secret boss.\n\nAT THIS TIME OF YEAR, AT THIS TIME OF DAY, IN THIS PART OF THE COUNTRY, LOCALIZED ENTIRELY WITHIN YOUR KITCHEN!? [10\u2605]\nDefeat Lord Nutmeg.",
+            "primary": 7,
+            "primaryText": "Devil Engines (UNCLEARED) [1\u2605]\nClear Expert with all three ships, in one session, without using continues or quitting prematurely.\n\nMisotheism (UNCLEARED) [1\u2605]\nClear Expert with each ship without using continues.\n\nUltimate Ignition (UNCLEARED) [1\u2605]\nClear Ignition Mode with all ships without using continues.\n\nDevil's Rush [25\u2605]\nClear the Boss Rush with each ship.\n\nArtificial Martyr [30\u2605]\nClear Very Hard with each ship without using continues.\n\nPocket Engine [5\u2605]\nClear the Extra Stage with each ship without using continues.\n\nBootcamp [10\u2605]\nClear all challenges.",
             "community": 0,
             "communityText": "",
-            "achievements": 11
-        }
+            "achievements": 29
+        },
+        "information": "DLC required for all POs except \"Bootcamp\"."
     }
 }
-var gameOfDayDate = '2025-11-05'
+var gameOfDayDate = '2025-11-06'
