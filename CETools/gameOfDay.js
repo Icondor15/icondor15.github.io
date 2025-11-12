d7aa92e25be03a80c480d133b5352a4e981949bd
Icondor <ivanfigus@gmail.com>
2025-11-12 08:39:40 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,26 +1,58 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 1739800,
-        "CEId": "5ea55abb-cbf0-4051-b29b-87ba78bc85de",
-        "name": "Damn Ropes",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1739800/header.jpg?t=1694091473",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2024-08-26T15:15:57.000Z",
-        "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 0,
-        "playersTotal": 54,
+        "gameType": 0,
+        "platformId": 3480,
+        "CEId": "fc2a85fc-690c-4e44-8c07-3e60137b8a58",
+        "name": "Peggle Deluxe",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/3480/header.jpg?t=1593276392",
+        "genre": "Strategy",
+        "tier": 2,
+        "points": 30,
+        "createdAt": "2022-11-12T17:36:19.000Z",
+        "updatedAt": "2025-10-21T04:22:00.000Z",
+        "playersCompleted": 27,
+        "playersStarted": 9,
+        "playersTotal": 467,
+        "priceData": {
+            "USD": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 70000,
+                "final": 70000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 795,
+                "final": 795,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 699,
+                "final": 699,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Ai? More Like Au [10\u2605]\nEarn the Extra-Gold medal in every level.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 35
+            "primary": 2,
+            "primaryText": "Peggle Grandmaster [10\u2605]\nEarn the trophy for clearing all of the pegs in every level.\n\nPeggle Master [20\u2605]\nEarn the trophy for completing all of the challenges.",
+            "community": 1,
+            "communityText": "EXTREM PEGGING\nClear all of the pegs in every level without the use of the Claude's flippers cheat.",
+            "achievements": 0
         }
     }
 }
-var gameOfDayDate = '2025-11-11'
+var gameOfDayDate = '2025-11-12'
