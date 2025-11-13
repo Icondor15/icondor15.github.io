1853d2e3127286f1ddeda714434683b878b85a57
Icondor <ivanfigus@gmail.com>
2025-11-13 08:39:39 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 3480,
-        "CEId": "fc2a85fc-690c-4e44-8c07-3e60137b8a58",
-        "name": "Peggle Deluxe",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/3480/header.jpg?t=1593276392",
+        "platformId": 576500,
+        "CEId": "0bfdd573-7819-4a3a-9a8b-ed1a42b261fc",
+        "name": "Evergarden",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/576500/header.jpg?t=1610642100",
         "genre": "Strategy",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2022-11-12T17:36:19.000Z",
-        "updatedAt": "2025-10-21T04:22:00.000Z",
-        "playersCompleted": 27,
-        "playersStarted": 9,
-        "playersTotal": 467,
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2022-11-13T21:34:59.000Z",
+        "updatedAt": "2025-09-08T23:40:11.000Z",
+        "playersCompleted": 26,
+        "playersStarted": 0,
+        "playersTotal": 241,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
+                "initial": 975,
+                "final": 975,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 499,
-                "final": 499,
+                "initial": 850,
+                "final": 850,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 70000,
-                "final": 70000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 795,
-                "final": 795,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 699,
-                "final": 699,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Peggle Grandmaster [10\u2605]\nEarn the trophy for clearing all of the pegs in every level.\n\nPeggle Master [20\u2605]\nEarn the trophy for completing all of the challenges.",
+            "primary": 1,
+            "primaryText": "Flora and Fauna [5\u2605]\nScore 80,000 points in a single game.",
             "community": 1,
-            "communityText": "EXTREM PEGGING\nClear all of the pegs in every level without the use of the Claude's flippers cheat.",
-            "achievements": 0
+            "communityText": "Stonemason\nCreate a tier three monolith.",
+            "achievements": 21
         }
     }
 }
-var gameOfDayDate = '2025-11-12'
+var gameOfDayDate = '2025-11-13'
