2ad551d00671387a0afc6af65caa8d077aec0dcd
Icondor <ivanfigus@gmail.com>
2025-11-16 08:46:39 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,28 +1,69 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 2,
-        "platformId": 1297720,
-        "CEId": "8194c133-1305-400c-a71a-7621c96ac9a5",
-        "name": "Starstruck: Prologue",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1297720/header.jpg?t=1666057889",
-        "genre": "Arcade",
-        "tier": 3,
-        "points": 55,
-        "createdAt": "2022-11-15T16:33:27.000Z",
+        "gameType": 0,
+        "platformId": 264220,
+        "CEId": "1d10e32a-918d-4c02-9807-b1ab7de03016",
+        "name": "Mr. Bree+",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/264220/header.jpg?t=1447359435",
+        "genre": "Platformer",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2022-11-16T04:53:51.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 9,
-        "playersStarted": 15,
-        "playersTotal": 149,
+        "playersCompleted": 12,
+        "playersStarted": 0,
+        "playersTotal": 59,
+        "priceData": {
+            "USD": {
+                "initial": 299,
+                "final": 299,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 299,
+                "final": 299,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 199,
+                "final": 199,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 29800,
+                "final": 29800,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 329,
+                "final": 329,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Guitar Star [35\u2605]\nObtain S rank on every song in the \"More\" tab on Master difficulty.\n\nChord Strummer [5\u2605]\nObtain A rank on every song in the \"More\" tab on Master difficulty.\n\nFretboard Finesse[15\u2605 (Partial: 5\u2605)]\nObtain S rank on every song in the \"20XX\" tab on Master difficulty. *Partial points are awarded as a progress milestone.",
+            "primary": 1,
+            "primaryText": "This Piggy Went Home Alive [10\u2605]\nBeat all levels without dying.",
             "community": 0,
             "communityText": "",
-            "achievements": 12
+            "achievements": 43
         },
-        "gameTags": [],
-        "information": "Delisted from Steam."
+        "gameTags": [
+            "Indie",
+            "Adventure",
+            "Action",
+            "Platformer",
+            "2D",
+            "Singleplayer"
+        ],
+        "languages": {
+            "english": "IS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "spanish": "IS",
+            "brazilian": "IS"
+        }
     }
 }
-var gameOfDayDate = '2025-11-15'
+var gameOfDayDate = '2025-11-16'
