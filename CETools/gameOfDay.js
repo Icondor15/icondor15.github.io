ce49ac41bb4c4c2295709168822da7f5a69a32f4
Icondor <ivanfigus@gmail.com>
2025-11-17 08:50:00 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,68 +2,93 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 264220,
-        "CEId": "1d10e32a-918d-4c02-9807-b1ab7de03016",
-        "name": "Mr. Bree+",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/264220/header.jpg?t=1447359435",
-        "genre": "Platformer",
+        "platformId": 1370050,
+        "CEId": "e3f715a2-b645-4e6f-8d3c-df8c92d4672d",
+        "name": "Trek to Yomi",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1370050/header.jpg?t=1728950386",
+        "genre": "Action",
         "tier": 1,
-        "points": 10,
-        "createdAt": "2022-11-16T04:53:51.000Z",
+        "points": 5,
+        "createdAt": "2024-11-17T10:48:22.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 12,
+        "playersCompleted": 2,
         "playersStarted": 0,
-        "playersTotal": 59,
+        "playersTotal": 81,
         "priceData": {
             "USD": {
-                "initial": 299,
-                "final": 299,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 299,
-                "final": 299,
+                "initial": 1950,
+                "final": 1950,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 199,
-                "final": 199,
+                "initial": 1675,
+                "final": 1675,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 29800,
-                "final": 29800,
+                "initial": 230000,
+                "final": 230000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2950,
+                "final": 2950,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 329,
-                "final": 329,
+                "initial": 2599,
+                "final": 2599,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "This Piggy Went Home Alive [10\u2605]\nBeat all levels without dying.",
+            "primaryText": "Unstoppable [5\u2605]\nComplete the game in Ronin Mode.",
             "community": 0,
             "communityText": "",
-            "achievements": 43
+            "achievements": 28
         },
         "gameTags": [
-            "Indie",
-            "Adventure",
             "Action",
-            "Platformer",
-            "2D",
-            "Singleplayer"
+            "Adventure",
+            "Side Scroller",
+            "Hack and Slash",
+            "Action-Adventure",
+            "Cinematic",
+            "Swordplay",
+            "Atmospheric",
+            "Stylized",
+            "Third Person",
+            "Ninja",
+            "Historical",
+            "Demons",
+            "Combat",
+            "Singleplayer",
+            "Story Rich",
+            "Narration",
+            "Violent",
+            "Drama",
+            "Martial Arts"
         ],
         "languages": {
             "english": "IS",
             "french": "IS",
             "german": "IS",
             "italian": "IS",
+            "japanese": "IAS",
             "spanish": "IS",
-            "brazilian": "IS"
+            "brazilian": "IS",
+            "koreana": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2025-11-16'
+var gameOfDayDate = '2025-11-17'
