415f3da61701552e4eca79cd54b43a9d5ff7e53d
Icondor <ivanfigus@gmail.com>
2025-11-19 07:31:27 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,73 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 255340,
-        "CEId": "e5c143b7-7d85-4e59-a0e0-5f7ccaf4922c",
-        "name": "Escape Goat 2",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/255340/header.jpg?t=1667944341",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 5,
-        "createdAt": "2023-11-18T18:46:06.000Z",
+        "platformId": 2002850,
+        "CEId": "f52f156d-4ca1-41ba-b256-a028da59d58c",
+        "name": "Raiden IV x MIKADO remix",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2002850/header.jpg?t=1675188244",
+        "genre": "Bullet Hell",
+        "tier": 4,
+        "points": 150,
+        "createdAt": "2023-11-19T17:40:31.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 7,
-        "playersStarted": 0,
-        "playersTotal": 86,
+        "playersCompleted": 1,
+        "playersStarted": 1,
+        "playersTotal": 5,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 699,
-                "final": 699,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 98000,
-                "final": 98000,
+                "initial": 418000,
+                "final": 418000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 4495,
+                "final": 4495,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 3999,
+                "final": 3999,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "You're Goated [5\u2605]\nBeat the game in under 30 minutes without loading from saves. / Beat all secret rooms.",
+            "primary": 4,
+            "primaryText": "Ultimate OverKill [60\u2605]\n1CC OverKill mode on \"ULTIMATE\" difficulty(changing lives/bombs is allowed).\n\nRaiden Ace [35\u2605]\n1CC OverKill Mode on \"Normal\" difficulty or higher (changing lives/bombs is allowed).\n\nArcade Legend [40\u2605]\n1CC Arcade Mode Original on \"Original\" difficulty or higher on default settings.\n\nBoss Rusher [15\u2605]\n1CC both loops on Boss Rush Mode (changing lives/bombs is allowed).",
             "community": 0,
             "communityText": "",
-            "achievements": 16
+            "achievements": 47
         },
         "gameTags": [
-            "Indie",
-            "Platformer",
-            "Puzzle",
-            "Casual",
             "Action",
-            "Great Soundtrack",
-            "2D",
-            "Singleplayer",
-            "Puzzle Platformer",
-            "Controller",
-            "Pixel Graphics"
+            "Adventure",
+            "Arcade",
+            "Shooter",
+            "Bullet Hell",
+            "Shoot 'Em Up",
+            "Flight",
+            "Top-Down Shooter",
+            "Colorful",
+            "Pixel Graphics",
+            "Top-Down",
+            "Aliens",
+            "Jet",
+            "Sci-fi",
+            "Space",
+            "Score Attack",
+            "Co-op Campaign",
+            "Local Co-Op",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "I"
+            "english": "IS",
+            "japanese": "IS"
         }
     }
 }
-var gameOfDayDate = '2025-11-18'
+var gameOfDayDate = '2025-11-19'
