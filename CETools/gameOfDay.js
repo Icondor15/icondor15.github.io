fee0e0cf8078b5aa877a63973f0c4974387bb18f
Icondor <ivanfigus@gmail.com>
2025-11-26 08:47:46 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,81 +2,87 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2581050,
-        "CEId": "79247a42-1309-495a-bf19-7028940bbace",
-        "name": "KAMITSUBAKI CITY ENSEMBLE",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2581050/header.jpg?t=1727276930",
-        "genre": "Arcade",
-        "tier": 3,
-        "points": 51,
-        "createdAt": "2024-11-25T19:31:29.000Z",
+        "platformId": 460790,
+        "CEId": "97594e7f-bde8-4f71-b4eb-32cd81675b66",
+        "name": "Bayonetta",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/460790/header.jpg?t=1670260448",
+        "genre": "Action",
+        "tier": 4,
+        "points": 80,
+        "createdAt": "2023-11-26T21:21:39.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 6,
-        "playersTotal": 53,
+        "playersCompleted": 3,
+        "playersStarted": 18,
+        "playersTotal": 396,
         "priceData": {
             "USD": {
-                "initial": 399,
-                "final": 199,
-                "discountPercent": 50
+                "initial": 1999,
+                "final": 599,
+                "discountPercent": 70
             },
             "EUR": {
-                "initial": 399,
-                "final": 199,
-                "discountPercent": 50
+                "initial": 1999,
+                "final": 599,
+                "discountPercent": 70
             },
             "GBP": {
-                "initial": 339,
-                "final": 169,
-                "discountPercent": 50
+                "initial": 1599,
+                "final": 479,
+                "discountPercent": 70
             },
             "JPY": {
-                "initial": 44000,
-                "final": 22000,
-                "discountPercent": 50
+                "initial": 218000,
+                "final": 65400,
+                "discountPercent": 70
             },
             "AUD": {
-                "initial": 595,
-                "final": 297,
-                "discountPercent": 50
+                "initial": 2495,
+                "final": 748,
+                "discountPercent": 70
             },
             "CAD": {
-                "initial": 499,
-                "final": 249,
-                "discountPercent": 50
+                "initial": 2999,
+                "final": 899,
+                "discountPercent": 70
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "KAMITSUBAKI Regenerated (UNVALUED) [1\u2605]\nAchieve a full combo on all Pro difficulty songs from the base game.\n\nThe Voice of AI [50\u2605]\nAchieve a full combo on all Hard difficulty songs from the base game.",
+            "primaryText": "Angel Slayer [50\u2605]\n Earn a Pure Platinum medal on \"Lost Chapter: Angel Slayer\".\n\nLegendary Dark Witch[30\u2605 (Partial: 5\u2605)]\nEarn a Pure Platinum medal on all main chapters on \u221e Climax difficulty.",
             "community": 0,
             "communityText": "",
             "achievements": 50
         },
         "gameTags": [
-            "Music",
-            "Adventure",
-            "Rhythm",
-            "Anime",
-            "Casual",
             "Action",
             "Female Protagonist",
-            "Emotional",
-            "Atmospheric",
-            "Story Rich",
-            "Indie",
-            "Sci-fi",
+            "Hack and Slash",
+            "Sexual Content",
+            "Nudity",
             "Singleplayer",
-            "Sports",
-            "Cute"
+            "Spectacle fighter",
+            "Great Soundtrack",
+            "Mature",
+            "Character Action Game",
+            "Adventure",
+            "Fast-Paced",
+            "Third Person",
+            "Fantasy",
+            "Beat 'em up",
+            "Comedy",
+            "Gore",
+            "Classic",
+            "Anime",
+            "Atmospheric"
         ],
         "languages": {
-            "english": "IS",
-            "japanese": "IS",
-            "koreana": "IS",
-            "schinese": "IS",
-            "tchinese": "IS"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IAS",
+            "spanish": "IS"
         }
     }
 }
-var gameOfDayDate = '2025-11-25'
+var gameOfDayDate = '2025-11-26'
