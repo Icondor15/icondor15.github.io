1d19dcceb14f741a30498efd3b6489fd8085a6e1
Icondor <ivanfigus@gmail.com>
2025-11-27 08:47:50 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,87 +2,90 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 460790,
-        "CEId": "97594e7f-bde8-4f71-b4eb-32cd81675b66",
-        "name": "Bayonetta",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/460790/header.jpg?t=1670260448",
-        "genre": "Action",
-        "tier": 4,
-        "points": 80,
-        "createdAt": "2023-11-26T21:21:39.000Z",
+        "platformId": 461230,
+        "CEId": "26479229-b44d-4035-92bc-1549c526a13a",
+        "name": "Dashy Square",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/461230/header.jpg?t=1530132030",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2023-11-27T03:10:26.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 18,
-        "playersTotal": 396,
+        "playersCompleted": 5,
+        "playersStarted": 0,
+        "playersTotal": 64,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 599,
-                "discountPercent": 70
+                "initial": 699,
+                "final": 76,
+                "discountPercent": 89
             },
             "EUR": {
-                "initial": 1999,
-                "final": 599,
-                "discountPercent": 70
+                "initial": 689,
+                "final": 75,
+                "discountPercent": 89
             },
             "GBP": {
-                "initial": 1599,
-                "final": 479,
-                "discountPercent": 70
+                "initial": 589,
+                "final": 64,
+                "discountPercent": 89
             },
             "JPY": {
-                "initial": 218000,
-                "final": 65400,
-                "discountPercent": 70
+                "initial": 80000,
+                "final": 8800,
+                "discountPercent": 89
             },
             "AUD": {
-                "initial": 2495,
-                "final": 748,
-                "discountPercent": 70
+                "initial": 1025,
+                "final": 112,
+                "discountPercent": 89
             },
             "CAD": {
-                "initial": 2999,
-                "final": 899,
-                "discountPercent": 70
+                "initial": 899,
+                "final": 98,
+                "discountPercent": 89
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Angel Slayer [50\u2605]\n Earn a Pure Platinum medal on \"Lost Chapter: Angel Slayer\".\n\nLegendary Dark Witch[30\u2605 (Partial: 5\u2605)]\nEarn a Pure Platinum medal on all main chapters on \u221e Climax difficulty.",
+            "primary": 1,
+            "primaryText": "Ghost Demon [15\u2605]\nBeat all levels. / Reach Wave 15 on all endless levels.",
             "community": 0,
             "communityText": "",
-            "achievements": 50
+            "achievements": 124
         },
         "gameTags": [
+            "Indie",
             "Action",
-            "Female Protagonist",
-            "Hack and Slash",
-            "Sexual Content",
-            "Nudity",
+            "Music",
+            "Difficult",
+            "Platformer",
+            "Rhythm",
+            "2D",
+            "Level Editor",
+            "Side Scroller",
             "Singleplayer",
-            "Spectacle fighter",
+            "Arcade",
+            "Precision Platformer",
+            "Casual",
+            "Character Customization",
+            "Colorful",
             "Great Soundtrack",
-            "Mature",
-            "Character Action Game",
-            "Adventure",
-            "Fast-Paced",
-            "Third Person",
-            "Fantasy",
-            "Beat 'em up",
-            "Comedy",
-            "Gore",
-            "Classic",
-            "Anime",
-            "Atmospheric"
+            "Racing",
+            "JRPG",
+            "Quick-Time Events",
+            "Cute"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IAS",
-            "spanish": "IS"
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "koreana": "I",
+            "russian": "I",
+            "schinese": "I"
         }
     }
 }
-var gameOfDayDate = '2025-11-26'
+var gameOfDayDate = '2025-11-27'
