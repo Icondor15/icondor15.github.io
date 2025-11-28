c94986755dcfb8d0bbd32ed38ba85562afe528de
Icondor <ivanfigus@gmail.com>
2025-11-28 08:49:55 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,91 +1,52 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 461230,
-        "CEId": "26479229-b44d-4035-92bc-1549c526a13a",
-        "name": "Dashy Square",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/461230/header.jpg?t=1530132030",
-        "genre": "Arcade",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2023-11-27T03:10:26.000Z",
+        "gameType": 1,
+        "platformId": 1954100,
+        "CEId": "d8226935-eb6c-491f-aac2-d6db16691de7",
+        "name": "Bunny Hop : Eggstreme",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1954100/header.jpg?t=1668042474",
+        "genre": "First-Person",
+        "tier": 5,
+        "points": 200,
+        "createdAt": "2022-11-28T01:59:15.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 5,
-        "playersStarted": 0,
-        "playersTotal": 64,
-        "priceData": {
-            "USD": {
-                "initial": 699,
-                "final": 76,
-                "discountPercent": 89
-            },
-            "EUR": {
-                "initial": 689,
-                "final": 75,
-                "discountPercent": 89
-            },
-            "GBP": {
-                "initial": 589,
-                "final": 64,
-                "discountPercent": 89
-            },
-            "JPY": {
-                "initial": 80000,
-                "final": 8800,
-                "discountPercent": 89
-            },
-            "AUD": {
-                "initial": 1025,
-                "final": 112,
-                "discountPercent": 89
-            },
-            "CAD": {
-                "initial": 899,
-                "final": 98,
-                "discountPercent": 89
-            }
-        },
+        "playersCompleted": 7,
+        "playersStarted": 6,
+        "playersTotal": 154,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Ghost Demon [15\u2605]\nBeat all levels. / Reach Wave 15 on all endless levels.",
+            "primary": 3,
+            "primaryText": "Mad Sprint [100\u2605]\nBeat the gold time on all extreme maps.\n\nModerate Run [60\u2605]\nBeat the gold time on all hard maps.\n\nLight Jog [40\u2605]\nBeat the gold time on all easy and medium maps.",
             "community": 0,
             "communityText": "",
-            "achievements": 124
+            "achievements": 83
         },
         "gameTags": [
-            "Indie",
-            "Action",
-            "Music",
-            "Difficult",
-            "Platformer",
-            "Rhythm",
-            "2D",
-            "Level Editor",
-            "Side Scroller",
+            "Score Attack",
+            "3D Platformer",
+            "First-Person",
+            "Parkour",
+            "Hidden Object",
             "Singleplayer",
-            "Arcade",
-            "Precision Platformer",
-            "Casual",
-            "Character Customization",
-            "Colorful",
-            "Great Soundtrack",
+            "Multiplayer",
             "Racing",
-            "JRPG",
-            "Quick-Time Events",
-            "Cute"
+            "FPS",
+            "Platformer",
+            "3D",
+            "Colorful",
+            "Stylized",
+            "Relaxing",
+            "Action",
+            "Sports",
+            "Nature",
+            "Indie",
+            "Free to Play"
         ],
         "languages": {
             "english": "I",
             "french": "I",
-            "german": "I",
-            "italian": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "koreana": "I",
-            "russian": "I",
-            "schinese": "I"
+            "russian": "I"
         }
     }
 }
-var gameOfDayDate = '2025-11-27'
+var gameOfDayDate = '2025-11-28'
