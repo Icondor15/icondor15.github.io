fa09c4d30f8a34a0c98f30702ec0caf7cfa44bdd
Icondor <ivanfigus@gmail.com>
2025-11-30 09:15:17 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,85 +2,72 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1225370,
-        "CEId": "f87e6829-8820-4a6e-8f14-f329a71cde1e",
-        "name": "Blast Rush Classic",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1225370/header.jpg?t=1588620117",
-        "genre": "Bullet Hell",
-        "tier": 1,
-        "points": 5,
-        "createdAt": "2021-11-29T06:52:14.000Z",
+        "platformId": 640340,
+        "CEId": "54aea550-c367-4cf0-9ad2-0b5dea2aed62",
+        "name": "Serious Sam: Tormental",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/640340/header.jpg?t=1652866132",
+        "genre": "Action",
+        "tier": 2,
+        "points": 25,
+        "createdAt": "2022-11-30T01:54:01.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 12,
-        "playersStarted": 0,
-        "playersTotal": 31,
+        "playersCompleted": 5,
+        "playersStarted": 3,
+        "playersTotal": 44,
         "priceData": {
             "USD": {
-                "initial": 99,
-                "final": 99,
+                "initial": 1199,
+                "final": 1199,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 99,
-                "final": 99,
+                "initial": 1179,
+                "final": 1179,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 89,
-                "final": 89,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 12000,
-                "final": 12000,
+                "initial": 140000,
+                "final": 140000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 150,
-                "final": 150,
+                "initial": 1775,
+                "final": 1775,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 129,
-                "final": 129,
+                "initial": 1549,
+                "final": 1549,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Endurance Emperor [5\u2605]\nComplete 200 waves in \"Endurance\" mode. / Clear \"Standard\" mode without restarts.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 14
+            "primary": 2,
+            "primaryText": "Mental [15\u2605]\nBeat Mental. / Beat Iteration V3 with 9 active moods. / Beat Iteration V2 with the Sadistic Mood. / Complete the \"Frazzled\" and \"Death from Above\" quests.\n\nHypnic Jerk [10\u2605]\nReach the Vault in under 12 minutes. / Reach the Vault after looping twice. / Reach the high score with Bomb.",
+            "community": 2,
+            "communityText": "Psychosis\nBeat Iteration V3 in under 25 minutes without using torments.\n\nQuests and Qualms\nComplete every quest (excluding Moody Mental).",
+            "achievements": 49
         },
         "gameTags": [
-            "Shoot 'Em Up",
-            "Bullet Hell",
-            "Pixel Graphics",
-            "Casual",
-            "Indie",
             "Action",
-            "Destruction",
-            "Experimental",
-            "2D",
-            "Retro",
-            "Singleplayer",
-            "Controller",
-            "Difficult",
-            "Great Soundtrack",
-            "Replay Value",
-            "Short",
-            "Arcade",
+            "Action Roguelike",
+            "Indie",
+            "Bullet Hell",
+            "Roguelite",
             "Top-Down Shooter",
-            "Top-Down",
-            "1990's"
+            "Twin Stick Shooter",
+            "Narrative",
+            "Co-op",
+            "Roguelike"
         ],
         "languages": {
-            "english": "I",
-            "japanese": "I",
-            "schinese": "I",
-            "vietnamese": "I"
+            "english": "IA"
         }
     }
 }
-var gameOfDayDate = '2025-11-29'
+var gameOfDayDate = '2025-11-30'
