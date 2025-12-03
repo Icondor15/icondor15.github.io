73edacdcae6c4d3d720424010cef39253b047690
Icondor <ivanfigus@gmail.com>
2025-12-03 08:47:52 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,50 +1,71 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 1058640,
-        "CEId": "6f60a578-2524-4d07-8db6-2f7a85d63f6d",
-        "name": "RED HOT VENGEANCE",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1058640/header.jpg?t=1572210555",
-        "genre": "Action",
+        "gameType": 0,
+        "platformId": 1988540,
+        "CEId": "92b8ec28-1282-4113-9fb7-1e078996fa00",
+        "name": "The Zachtronics Solitaire Collection",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1988540/header.jpg?t=1663901533",
+        "genre": "Strategy",
         "tier": 1,
         "points": 10,
-        "createdAt": "2023-12-02T01:12:46.000Z",
+        "createdAt": "2022-12-03T23:55:40.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 14,
-        "playersStarted": 0,
-        "playersTotal": 132,
+        "playersCompleted": 28,
+        "playersStarted": 3,
+        "playersTotal": 63,
+        "priceData": {
+            "USD": {
+                "initial": 999,
+                "final": 999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 975,
+                "final": 975,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 850,
+                "final": 850,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 120000,
+                "final": 120000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 1450,
+                "final": 1450,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1299,
+                "final": 1299,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Vengeance Delivered [10\u2605]\nGet all medals on new game plus. / Complete all challenges. ",
-            "community": 0,
-            "communityText": "",
-            "achievements": 15
+            "primary": 2,
+            "primaryText": "Fortune Teller [5\u2605]\nWin 10 games of Fortune's Foundation.\n\nHardly Working [5\u2605]\nWin 10 games of all other solitaire variants.",
+            "community": 3,
+            "communityText": "Working Hard\nWin each solitaire variant in order and in a row without failing, restarting, or quitting at any point.\n\nFortune's Favored\nWin a game of Fortune's Foundation as The Fool or The World without ever covering the stack and without any undoes all in the same run.\n\nSolitairy Refinement\nWin a game of each solitaire variant under specific constraints.",
+            "achievements": 24
         },
         "gameTags": [
-            "Free to Play",
-            "Violent",
-            "Gore",
-            "Indie",
-            "Action",
+            "Solitaire",
+            "Casual",
+            "Card Game",
             "Singleplayer",
-            "Shooter",
-            "Top-Down",
-            "Top-Down Shooter",
-            "Level Editor",
-            "Twin Stick Shooter",
-            "Isometric",
-            "GameMaker",
-            "Difficult",
-            "Great Soundtrack",
-            "Co-op",
-            "Multiplayer"
+            "Choices Matter",
+            "2D",
+            "Indie",
+            "Top-Down"
         ],
         "languages": {
-            "english": "IAS",
-            "spanish": "IS",
-            "russian": "IS"
+            "english": "I"
         }
     }
 }
-var gameOfDayDate = '2025-12-02'
+var gameOfDayDate = '2025-12-03'
