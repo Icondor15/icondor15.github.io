591d5fe2777ffb9cb8e11756ea4b6db059519bd3
Icondor <ivanfigus@gmail.com>
2025-12-04 11:08:17 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,70 +2,73 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1988540,
-        "CEId": "92b8ec28-1282-4113-9fb7-1e078996fa00",
-        "name": "The Zachtronics Solitaire Collection",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1988540/header.jpg?t=1663901533",
-        "genre": "Strategy",
+        "platformId": 288020,
+        "CEId": "c3f9646b-c301-44e8-9455-e8bcbf3114a5",
+        "name": "Rex Rocket",
+        "header": "https://upload.cedb.me/c3f9646b-c301-44e8-9455-e8bcbf3114a5.jpeg",
+        "genre": "Platformer",
         "tier": 1,
-        "points": 10,
-        "createdAt": "2022-12-03T23:55:40.000Z",
-        "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 28,
+        "points": 15,
+        "createdAt": "2024-12-04T15:36:37.000Z",
+        "updatedAt": "2025-10-21T05:40:43.000Z",
+        "playersCompleted": 1,
         "playersStarted": 3,
-        "playersTotal": 63,
+        "playersTotal": 25,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 99,
+                "final": 99,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 975,
-                "final": 975,
+                "initial": 79,
+                "final": 79,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 850,
-                "final": 850,
+                "initial": 79,
+                "final": 79,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 120000,
-                "final": 120000,
+                "initial": 10000,
+                "final": 10000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 150,
+                "final": 150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 119,
+                "final": 119,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Fortune Teller [5\u2605]\nWin 10 games of Fortune's Foundation.\n\nHardly Working [5\u2605]\nWin 10 games of all other solitaire variants.",
-            "community": 3,
-            "communityText": "Working Hard\nWin each solitaire variant in order and in a row without failing, restarting, or quitting at any point.\n\nFortune's Favored\nWin a game of Fortune's Foundation as The Fool or The World without ever covering the stack and without any undoes all in the same run.\n\nSolitairy Refinement\nWin a game of each solitaire variant under specific constraints.",
-            "achievements": 24
+            "primaryText": "Rex Rampage [10\u2605]\nBeat the game without losing 10 or more lives.\n\nRocketeer [5\u2605]\nBeat the game, and collect both the Air-Talarias and Grenade Launcher.",
+            "community": 2,
+            "communityText": "Rex's Royal Rampage\nBeat the game without losing a life.\n\nRocket Rush\nBeat all bosses without taking damage.",
+            "achievements": 40
         },
         "gameTags": [
-            "Solitaire",
-            "Casual",
-            "Card Game",
-            "Singleplayer",
-            "Choices Matter",
-            "2D",
+            "Action",
+            "Adventure",
             "Indie",
-            "Top-Down"
+            "Metroidvania",
+            "Platformer",
+            "Retro",
+            "Pixel Graphics",
+            "Difficult",
+            "Great Soundtrack",
+            "2D"
         ],
         "languages": {
-            "english": "I"
-        }
+            "english": "IA"
+        },
+        "information": "To launch the game on windows, use this guide:\n\nhttps://steamcommunity.com/sharedfiles/filedetails/?id=3375677826"
     }
 }
-var gameOfDayDate = '2025-12-03'
+var gameOfDayDate = '2025-12-04'
