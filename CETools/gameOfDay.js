038f1cbbad8e59b734bd90ef67f1b5c5013f817d
Icondor <ivanfigus@gmail.com>
2025-12-06 08:46:51 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,85 +1,48 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 249050,
-        "CEId": "fc7fbe60-d534-41c6-a566-76eadc6767e1",
-        "name": "Dungeon of the ENDLESS\u2122",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/249050/header.jpg?t=1689949161",
-        "genre": "Strategy",
+        "gameType": 1,
+        "platformId": 1992430,
+        "CEId": "38fe51b2-1a16-4ae6-a2df-42a89115b6b5",
+        "name": "Cheese Game",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1992430/header.jpg?t=1653917385",
+        "genre": "Bullet Hell",
         "tier": 2,
         "points": 20,
-        "createdAt": "2023-12-05T06:36:15.000Z",
+        "createdAt": "2022-12-06T06:06:01.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 9,
-        "playersTotal": 578,
-        "priceData": {
-            "USD": {
-                "initial": 1199,
-                "final": 299,
-                "discountPercent": 75
-            },
-            "EUR": {
-                "initial": 1199,
-                "final": 299,
-                "discountPercent": 75
-            },
-            "GBP": {
-                "initial": 899,
-                "final": 224,
-                "discountPercent": 75
-            },
-            "JPY": {
-                "initial": 118000,
-                "final": 29500,
-                "discountPercent": 75
-            },
-            "AUD": {
-                "initial": 1399,
-                "final": 349,
-                "discountPercent": 75
-            },
-            "CAD": {
-                "initial": 1299,
-                "final": 324,
-                "discountPercent": 75
-            }
-        },
+        "playersCompleted": 10,
+        "playersStarted": 0,
+        "playersTotal": 137,
         "milestones": {
-            "primary": 3,
-            "primaryText": "Cleaning Out the Fridge [10\u2605]\nBeat the game on Easy difficulty with the Refreezerator and Sanitary Pods.\n\nEndlessly Easy [5\u2605]\nBeat the game on Easy difficulty.\n\nNow THIS is Podraiding! [5\u2605]\nBeat the game with the Refreezerator and Sanitary Pods.",
+            "primary": 1,
+            "primaryText": "Cheesed [20\u2605]\nDefeat each boss on Hard Mode without getting hit.",
             "community": 0,
             "communityText": "",
-            "achievements": 33
+            "achievements": 16
         },
         "gameTags": [
-            "Roguelike",
-            "Strategy",
-            "Tower Defense",
-            "Pixel Graphics",
+            "Action",
+            "Casual",
+            "Arcade",
+            "3D",
+            "Third Person",
+            "Comedy",
+            "Funny",
+            "Family Friendly",
+            "Memes",
+            "Free to Play",
             "Indie",
-            "RPG",
-            "Dungeon Crawler",
-            "Sci-fi",
-            "Adventure",
-            "Perma Death",
-            "Procedural Generation",
-            "Co-op",
-            "Difficult",
-            "Resource Management",
-            "Survival",
-            "Roguelite",
-            "Replay Value",
-            "Great Soundtrack",
-            "Singleplayer",
-            "2D"
+            "Combat",
+            "Physics",
+            "PvE",
+            "Bullet Hell",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "IS",
-            "french": "IS",
-            "german": "IS"
-        }
+            "english": "I"
+        },
+        "information": "\n"
     }
 }
-var gameOfDayDate = '2025-12-05'
+var gameOfDayDate = '2025-12-06'
