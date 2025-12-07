8abbd2d8cef2ba7c069119bcc101a32fcf8c1a17
Icondor <ivanfigus@gmail.com>
2025-12-07 08:47:26 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,48 +1,80 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 1992430,
-        "CEId": "38fe51b2-1a16-4ae6-a2df-42a89115b6b5",
-        "name": "Cheese Game",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1992430/header.jpg?t=1653917385",
-        "genre": "Bullet Hell",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2022-12-06T06:06:01.000Z",
+        "gameType": 0,
+        "platformId": 2131920,
+        "CEId": "3c10ba08-a710-4762-abbe-693e58444c27",
+        "name": "Super Hamster Ball",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2131920/header.jpg?t=1669354143",
+        "genre": "Platformer",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2022-12-07T22:16:21.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 10,
+        "playersCompleted": 11,
         "playersStarted": 0,
-        "playersTotal": 137,
+        "playersTotal": 25,
+        "priceData": {
+            "USD": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 339,
+                "final": 339,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 47000,
+                "final": 47000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 595,
+                "final": 595,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
             "primary": 1,
-            "primaryText": "Cheesed [20\u2605]\nDefeat each boss on Hard Mode without getting hit.",
+            "primaryText": "Hamtaro [10\u2605]\nCollect all stars in pathfinder mode and fully complete adventure mode.",
             "community": 0,
             "communityText": "",
-            "achievements": 16
+            "achievements": 17
         },
         "gameTags": [
-            "Action",
             "Casual",
             "Arcade",
-            "3D",
-            "Third Person",
-            "Comedy",
-            "Funny",
-            "Family Friendly",
-            "Memes",
-            "Free to Play",
-            "Indie",
-            "Combat",
-            "Physics",
-            "PvE",
-            "Bullet Hell",
+            "Puzzle",
+            "2D Platformer",
+            "Puzzle Platformer",
+            "2D",
+            "Cute",
+            "Pixel Graphics",
+            "1990's",
+            "Retro",
+            "Controller",
             "Singleplayer"
         ],
         "languages": {
-            "english": "I"
-        },
-        "information": "\n"
+            "english": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "koreana": "I",
+            "schinese": "I",
+            "tchinese": "I"
+        }
     }
 }
-var gameOfDayDate = '2025-12-06'
+var gameOfDayDate = '2025-12-07'
