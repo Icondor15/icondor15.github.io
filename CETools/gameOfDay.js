1876c75d025c3ca069c745c53abcfa00e39cecb2
Icondor <ivanfigus@gmail.com>
2025-12-09 08:47:12 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,85 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1165470,
-        "CEId": "a7bffd37-301b-446f-ba40-c45805277cb2",
-        "name": "TombStar",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1165470/header.jpg?t=1662081328",
-        "genre": "Action",
+        "platformId": 1996470,
+        "CEId": "27231b8f-889d-4b78-9286-ff0cf434d97a",
+        "name": "Skator Gator 3D",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1996470/header.jpg?t=1694548750",
+        "genre": "Platformer",
         "tier": 1,
         "points": 5,
-        "createdAt": "2023-12-08T00:23:24.000Z",
+        "createdAt": "2023-09-17T15:05:31.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 7,
+        "playersTotal": 23,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 429,
+                "final": 429,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 152000,
-                "final": 152000,
+                "initial": 58000,
+                "final": 58000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 649,
+                "final": 649,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Octace?! [5\u2605]\nWin with the \"Only Need One\" and \"High Octane\" badges equipped.",
+            "primaryText": "Later, Speed Gator [5\u2605]\nGet all staff times.",
             "community": 0,
             "communityText": "",
-            "achievements": 33
+            "achievements": 24
         },
         "gameTags": [
-            "Perma Death",
-            "Action Roguelike",
-            "Twin Stick Shooter",
-            "Bullet Hell",
-            "Dungeon Crawler",
-            "Roguelite",
-            "Procedural Generation",
-            "Roguelike",
+            "3D Platformer",
+            "Collectathon",
+            "Short",
             "Action",
-            "Arcade",
+            "Family Friendly",
+            "Skateboarding",
+            "Singleplayer",
+            "Cartoon",
+            "Action-Adventure",
+            "Platformer",
             "3D",
-            "Funny",
-            "Cartoony",
+            "Cute",
             "Colorful",
-            "Fantasy",
-            "Space",
-            "Sci-fi",
-            "Class-Based",
-            "Loot",
-            "Western"
+            "Skating",
+            "Cartoony",
+            "Adventure",
+            "Sports",
+            "Linear",
+            "Precision Platformer",
+            "Arcade"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "schinese": "IS"
+            "english": "I"
         }
     }
 }
-var gameOfDayDate = '2025-12-08'
+var gameOfDayDate = '2025-12-09'
