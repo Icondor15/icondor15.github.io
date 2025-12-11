a00f390ce5f1b30c3c06e5e5b9418f590a820fc2
Icondor <ivanfigus@gmail.com>
2025-12-11 10:52:43 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,82 +1,28 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 78000,
-        "CEId": "f1eab776-645c-42d1-b640-244cbf4a590d",
-        "name": "Bejeweled\u00ae 3",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/78000/header.jpg?t=1584804978",
+        "gameType": 2,
+        "platformId": 1604600,
+        "CEId": "aa9a3ad8-a339-4072-b804-da39cacb3e63",
+        "name": "DROPSHIP",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1604600/header.jpg?t=1669822218",
         "genre": "Arcade",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2020-08-25T17:35:19.000Z",
+        "tier": 2,
+        "points": 20,
+        "createdAt": "2022-12-11T01:08:48.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 59,
+        "playersCompleted": 5,
         "playersStarted": 0,
-        "playersTotal": 367,
-        "priceData": {
-            "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 425,
-                "final": 425,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 49800,
-                "final": 49800,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            }
-        },
+        "playersTotal": 27,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Bedazzling [15\u2605]\nProve yourself.",
+            "primary": 2,
+            "primaryText": "Drop to the Top [10\u2605]\nEarn the star for beating the developer time on every level.\n\nDroppapalooza [10\u2605]\nFinish the game in Hardcore Mode.",
             "community": 0,
             "communityText": "",
-            "achievements": 65
+            "achievements": 18
         },
-        "gameTags": [
-            "Casual",
-            "Puzzle",
-            "Match 3",
-            "Singleplayer",
-            "Strategy",
-            "Addictive",
-            "Relaxing",
-            "Classic",
-            "Great Soundtrack",
-            "Touch-Friendly",
-            "Colorful",
-            "Family Friendly",
-            "Arcade",
-            "Indie",
-            "2D"
-        ],
-        "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "spanish": "I"
-        }
+        "gameTags": [],
+        "languages": {}
     }
 }
-var gameOfDayDate = '2025-12-10'
+var gameOfDayDate = '2025-12-11'
