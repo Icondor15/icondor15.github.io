4581097dfd8dc7d7138a6e277cd33d103bcbd1f4
Icondor <ivanfigus@gmail.com>
2025-12-12 08:47:48 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,28 +1,83 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 2,
-        "platformId": 1604600,
-        "CEId": "aa9a3ad8-a339-4072-b804-da39cacb3e63",
-        "name": "DROPSHIP",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1604600/header.jpg?t=1669822218",
-        "genre": "Arcade",
+        "gameType": 0,
+        "platformId": 1271280,
+        "CEId": "e9bf3920-0816-44e6-80b0-63db3022125a",
+        "name": "Rift Wizard",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1271280/header.jpg?t=1669170708",
+        "genre": "Strategy",
         "tier": 2,
-        "points": 20,
-        "createdAt": "2022-12-11T01:08:48.000Z",
+        "points": 30,
+        "createdAt": "2022-12-12T05:50:10.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 5,
+        "playersCompleted": 1,
         "playersStarted": 0,
-        "playersTotal": 27,
+        "playersTotal": 39,
+        "priceData": {
+            "USD": {
+                "initial": 1499,
+                "final": 1499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1249,
+                "final": 1249,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1139,
+                "final": 1139,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 152000,
+                "final": 152000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2150,
+                "final": 2150,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1749,
+                "final": 1749,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
             "primary": 2,
-            "primaryText": "Drop to the Top [10\u2605]\nEarn the star for beating the developer time on every level.\n\nDroppapalooza [10\u2605]\nFinish the game in Hardcore Mode.",
+            "primaryText": "Trials and Tribulations [20\u2605]\nComplete all trials.\n\nSpellslave [10\u2605]\nWin five consecutive games.",
             "community": 0,
             "communityText": "",
-            "achievements": 18
+            "achievements": 21
         },
-        "gameTags": [],
-        "languages": {}
+        "gameTags": [
+            "Character Customization",
+            "Roguelike",
+            "Dungeon Crawler",
+            "Perma Death",
+            "Turn-Based",
+            "Magic",
+            "2D",
+            "Turn-Based Combat",
+            "Procedural Generation",
+            "Pixel Graphics",
+            "Minimalist",
+            "Fantasy",
+            "Strategy",
+            "Top-Down",
+            "Traditional Roguelike",
+            "Old School",
+            "Retro",
+            "Singleplayer",
+            "RPG",
+            "Indie"
+        ],
+        "languages": {
+            "english": "IAS"
+        }
     }
 }
-var gameOfDayDate = '2025-12-11'
+var gameOfDayDate = '2025-12-12'
