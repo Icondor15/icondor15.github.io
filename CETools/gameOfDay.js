f7b58149910573fcc83d613462c8d5f52f66d1b1
Icondor <ivanfigus@gmail.com>
2025-12-17 10:08:17 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,55 +1,89 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 2009720,
-        "CEId": "9afdf642-4ea4-4bca-9207-2d1b08b72c22",
-        "name": "Mega Serval",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2009720/header.jpg?t=1669818292",
-        "genre": "Action",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2022-12-16T23:07:37.000Z",
-        "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 17,
-        "playersStarted": 0,
-        "playersTotal": 161,
+        "gameType": 0,
+        "platformId": 1147860,
+        "CEId": "3dd5dd44-94e6-4a79-b067-5890cd6f4d73",
+        "name": "UFO 50",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1147860/header.jpg?t=1727302239",
+        "genre": "Arcade",
+        "tier": 3,
+        "points": 70,
+        "createdAt": "2024-12-17T08:51:03.000Z",
+        "updatedAt": "2025-10-21T04:29:33.000Z",
+        "playersCompleted": 3,
+        "playersStarted": 4,
+        "playersTotal": 142,
+        "priceData": {
+            "USD": {
+                "initial": 2499,
+                "final": 2499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 2450,
+                "final": 2450,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 2099,
+                "final": 2099,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 280000,
+                "final": 280000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 3650,
+                "final": 3650,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 3250,
+                "final": 3250,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Japari Safari [10\u2605]\nObtain the blue hitless badge on every stage.",
+            "primary": 5,
+            "primaryText": "Really Mixing Cherries [20\u2605]\nObtain all relevant cherry disks from the Arcade category. See information.\n\nReally Hitting Cherries [15\u2605]\nObtain all relevant cherry disks from the Action category. See information section.\n\nReally Thinking About Cherries [15\u2605]\nObtain all relevant cherry disks from the Strategy category. See information section.\n\nReally Dodging Cherries [10\u2605]\nObtain all relevant cherry disks from the Bullet Hell category. See information section.\n\nReally Jumping On Cherries [10\u2605]\nObtain all relevant cherry disks from the Platformer category. See information section.",
             "community": 0,
             "communityText": "",
-            "achievements": 16
+            "achievements": 30
         },
         "gameTags": [
-            "Action",
-            "Singleplayer",
-            "Adventure",
-            "Female Protagonist",
-            "1980s",
-            "Platformer",
-            "Post-apocalyptic",
-            "Dystopian ",
-            "Nature",
-            "2D Platformer",
-            "2D",
+            "Indie",
             "Pixel Graphics",
-            "Third Person",
-            "Cartoon",
-            "Colorful",
-            "Cute",
-            "Family Friendly",
-            "Cats",
-            "Score Attack",
-            "Free to Play"
+            "Retro",
+            "Action",
+            "Strategy",
+            "2D",
+            "RPG",
+            "Arcade",
+            "Adventure",
+            "Difficult",
+            "1980s",
+            "Local Multiplayer",
+            "Casual",
+            "Minigames",
+            "Great Soundtrack",
+            "Puzzle",
+            "2D Platformer",
+            "Platformer",
+            "Roguelike",
+            "Sports"
         ],
         "languages": {
             "english": "I",
+            "french": "I",
             "german": "I",
-            "japanese": "I",
-            "latam": "I",
-            "russian": "I"
-        }
+            "italian": "I",
+            "spanish": "I",
+            "brazilian": "I"
+        },
+        "information": "The relevant games by category are as follows:\n\n__Arcade:__ Magic Garden, Pingolf, Paint Chase, Bushido Ball, Planet Zoldath, Hot Foot, Onion Delivery, Campanella.\n\n__Action:__ Kick Club, Elfazar's Hat, Overbold, Fist Hell, Campanella 2, Cyber Owls.\n\n__Bullet Hell:__ Star Waspir, Caramel Caramel, Seaside Drive, Campanella 3.\n\n__Platformer:__ Ninpek, Waldorf's journey, Velgress, Rakshasa.\n\n__Strategy:__ Lords of Diskonia, Party House, Bug Hunter, Devilition."
     }
 }
-var gameOfDayDate = '2025-12-16'
+var gameOfDayDate = '2025-12-17'
