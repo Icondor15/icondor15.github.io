76ad8db339d856e55bc0721a8f97f8e18129eef3
Icondor <ivanfigus@gmail.com>
2025-12-20 09:17:21 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,82 +2,65 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1000030,
-        "CEId": "9241fe50-3061-47c7-8a4d-4969e55e8398",
-        "name": "Cook, Serve, Delicious! 3?!",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1000030/header.jpg?t=1660866300",
-        "genre": "Arcade",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2022-12-19T20:00:09.000Z",
+        "platformId": 756200,
+        "CEId": "c40c927c-99a8-493b-a837-4a524a9b344b",
+        "name": "Impossibox",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/756200/header.jpg?t=1607007871",
+        "genre": "Platformer",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2024-07-18T04:48:55.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 6,
+        "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 300,
+        "playersTotal": 18,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 499,
-                "discountPercent": 75
+                "initial": 99,
+                "final": 99,
+                "discountPercent": 0
             },
             "EUR": {
-                "initial": 1679,
-                "final": 419,
-                "discountPercent": 75
+                "initial": 99,
+                "final": 99,
+                "discountPercent": 0
             },
             "GBP": {
-                "initial": 1549,
-                "final": 387,
-                "discountPercent": 75
+                "initial": 89,
+                "final": 89,
+                "discountPercent": 0
             },
             "JPY": {
-                "initial": 205000,
-                "final": 51200,
-                "discountPercent": 75
+                "initial": 12000,
+                "final": 12000,
+                "discountPercent": 0
             },
             "AUD": {
-                "initial": 2895,
-                "final": 723,
-                "discountPercent": 75
+                "initial": 150,
+                "final": 150,
+                "discountPercent": 0
             },
             "CAD": {
-                "initial": 2279,
-                "final": 569,
-                "discountPercent": 75
+                "initial": 129,
+                "final": 129,
+                "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Golden Roadtrip [15\u2605]\nObtain all gold on every route.\n\nMake Gordon Proud [15\u2605]\nObtain all \"perfect day using n-point menu\" achievements.",
+            "primary": 1,
+            "primaryText": "Don't Go Home in a Box [40\u2605]\nBeat the game without dying.",
             "community": 0,
             "communityText": "",
-            "achievements": 57
+            "achievements": 9
         },
         "gameTags": [
-            "Typing",
-            "Management",
-            "Difficult",
-            "Casual",
-            "Arcade",
-            "Strategy",
-            "2D",
-            "Funny",
-            "Simulation",
-            "Co-op",
-            "Comedy",
-            "Family Friendly",
             "Action",
-            "Local Co-Op",
-            "Multiplayer",
-            "Local Multiplayer",
-            "Cooking",
-            "Singleplayer",
-            "Co-op Campaign",
-            "Fast-Paced"
+            "Indie",
+            "Platformer"
         ],
         "languages": {
             "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2025-12-19'
+var gameOfDayDate = '2025-12-20'
