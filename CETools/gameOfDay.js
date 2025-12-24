78936307e0f2ea20287b925b1fa565dd5476c005
Icondor <ivanfigus@gmail.com>
2025-12-24 08:47:31 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,32 +1,66 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 2830,
-        "CEId": "548e327c-ef47-41cf-b94a-00d873efad1d",
-        "name": "Gran Turismo 3: A-Spec",
-        "header": "https://upload.cedb.me/548e327c-ef47-41cf-b94a-00d873efad1d.jpg",
+        "gameType": 0,
+        "platformId": 669340,
+        "CEId": "8d60d2db-3780-49c5-bb5c-cd9e5bb8a70b",
+        "name": "The Royal Cosmonautical Society",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/669340/header.jpg?t=1502379134",
         "genre": "Arcade",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2024-12-23T17:18:30.000Z",
-        "updatedAt": "2025-10-21T04:35:12.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 1,
-        "playersTotal": 6,
-        "gameTags": [],
-        "consoleData": {
-            "name": "PlayStation 2",
-            "family": "Sony",
-            "icon": "https://static.retroachievements.org/assets/images/system/ps2.png"
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2022-12-24T14:43:54.000Z",
+        "updatedAt": "2025-09-08T23:40:11.000Z",
+        "playersCompleted": 10,
+        "playersStarted": 0,
+        "playersTotal": 19,
+        "priceData": {
+            "USD": {
+                "initial": 699,
+                "final": 349,
+                "discountPercent": 50
+            },
+            "EUR": {
+                "initial": 499,
+                "final": 249,
+                "discountPercent": 50
+            },
+            "GBP": {
+                "initial": 499,
+                "final": 249,
+                "discountPercent": 50
+            },
+            "JPY": {
+                "initial": 69800,
+                "final": 34900,
+                "discountPercent": 50
+            },
+            "AUD": {
+                "initial": 995,
+                "final": 497,
+                "discountPercent": 50
+            },
+            "CAD": {
+                "initial": 779,
+                "final": 389,
+                "discountPercent": 50
+            }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "The Licenser [25\u2605]\nEarn the gold medal in all License Tests.\n\nComplex Touristing [5\u2605]\nBeat all Arcade Time Trials.",
+            "primary": 1,
+            "primaryText": "Cosmonautical Master [15\u2605]\nAchieve a 3 Star time in every level.",
             "community": 0,
             "communityText": "",
-            "achievements": 321
+            "achievements": 23
+        },
+        "gameTags": [
+            "Indie",
+            "Action",
+            "Adventure"
+        ],
+        "languages": {
+            "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2025-12-23'
+var gameOfDayDate = '2025-12-24'
