c52fc71a0995580490ffb7baeefb3edbeaa0282a
Icondor <ivanfigus@gmail.com>
2025-12-28 08:46:46 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,89 +2,90 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1072750,
-        "CEId": "9bab3269-84b0-4638-ab62-6602b3238552",
-        "name": "That Flipping Mountain",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1072750/header.jpg?t=1639047779",
-        "genre": "Platformer",
-        "tier": 2,
-        "points": 35,
-        "createdAt": "2021-12-27T00:34:41.000Z",
+        "platformId": 1285020,
+        "CEId": "f7952e04-0c79-4b94-9586-3e617dad45fb",
+        "name": "Flipon",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/1285020/header.jpg?t=1602160499",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2020-12-28T16:03:27.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 10,
+        "playersCompleted": 54,
         "playersStarted": 0,
-        "playersTotal": 78,
+        "playersTotal": 95,
         "priceData": {
             "USD": {
-                "initial": 699,
-                "final": 699,
-                "discountPercent": 0
+                "initial": 399,
+                "final": 119,
+                "discountPercent": 70
             },
             "EUR": {
-                "initial": 569,
-                "final": 569,
-                "discountPercent": 0
+                "initial": 399,
+                "final": 119,
+                "discountPercent": 70
             },
             "GBP": {
-                "initial": 519,
-                "final": 519,
-                "discountPercent": 0
+                "initial": 339,
+                "final": 101,
+                "discountPercent": 70
             },
             "JPY": {
-                "initial": 72000,
-                "final": 72000,
-                "discountPercent": 0
+                "initial": 47000,
+                "final": 14100,
+                "discountPercent": 70
             },
             "AUD": {
-                "initial": 995,
-                "final": 995,
-                "discountPercent": 0
+                "initial": 595,
+                "final": 178,
+                "discountPercent": 70
             },
             "CAD": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
+                "initial": 499,
+                "final": 149,
+                "discountPercent": 70
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Flipping Out [35\u2605]\nSeparately, climb the mountain with 0 fails, and under 15 minutes.",
+            "primaryText": "Panel de Pon [5\u2605]\nComplete the bonus levels.",
             "community": 0,
             "communityText": "",
-            "achievements": 10
+            "achievements": 16
         },
         "gameTags": [
-            "Difficult",
+            "Indie",
+            "Arcade",
+            "Puzzle",
+            "Match 3",
+            "Anime",
+            "Local Multiplayer",
             "Funny",
-            "Psychological Horror",
-            "Precision Platformer",
-            "Platformer",
-            "Action",
-            "Physics",
-            "Side Scroller",
-            "Philosophical",
-            "Snowboarding",
-            "Singleplayer",
-            "Skiing",
-            "Dark Humor",
-            "Parkour",
-            "Adventure",
-            "Comedy",
-            "2D",
+            "4 Player Local",
             "Casual",
-            "Simulation",
-            "Sports"
+            "Controller",
+            "Remake",
+            "Cute",
+            "PvP",
+            "Colorful",
+            "Female Protagonist",
+            "2D",
+            "Split Screen",
+            "Linear",
+            "Procedural Generation",
+            "Multiplayer"
         ],
         "languages": {
             "english": "I",
+            "french": "I",
             "german": "I",
+            "italian": "I",
+            "japanese": "I",
             "spanish": "I",
             "brazilian": "I",
-            "czech": "I",
-            "russian": "I",
-            "schinese": "I",
-            "tchinese": "I"
+            "koreana": "I",
+            "schinese": "I"
         }
     }
 }
-var gameOfDayDate = '2025-12-27'
+var gameOfDayDate = '2025-12-28'
