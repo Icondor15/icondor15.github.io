9ebb283ded4d04cdb7489d961cbbe79c3667593e
Icondor <ivanfigus@gmail.com>
2025-12-29 08:51:53 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,90 +2,85 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1285020,
-        "CEId": "f7952e04-0c79-4b94-9586-3e617dad45fb",
-        "name": "Flipon",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/1285020/header.jpg?t=1602160499",
-        "genre": "Arcade",
+        "platformId": 1483100,
+        "CEId": "d1ffc786-4077-4deb-9a3f-a7ef8a119165",
+        "name": "One More Dungeon 2",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1483100/header.jpg?t=1699005093",
+        "genre": "First-Person",
         "tier": 1,
         "points": 5,
-        "createdAt": "2020-12-28T16:03:27.000Z",
+        "createdAt": "2023-12-29T18:07:23.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 54,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 95,
+        "playersTotal": 24,
         "priceData": {
             "USD": {
-                "initial": 399,
-                "final": 119,
+                "initial": 1499,
+                "final": 449,
                 "discountPercent": 70
             },
             "EUR": {
-                "initial": 399,
-                "final": 119,
+                "initial": 1479,
+                "final": 443,
                 "discountPercent": 70
             },
             "GBP": {
-                "initial": 339,
-                "final": 101,
+                "initial": 1279,
+                "final": 383,
                 "discountPercent": 70
             },
             "JPY": {
-                "initial": 47000,
-                "final": 14100,
+                "initial": 170000,
+                "final": 51000,
                 "discountPercent": 70
             },
             "AUD": {
-                "initial": 595,
-                "final": 178,
+                "initial": 2195,
+                "final": 658,
                 "discountPercent": 70
             },
             "CAD": {
-                "initial": 499,
-                "final": 149,
+                "initial": 1949,
+                "final": 584,
                 "discountPercent": 70
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Panel de Pon [5\u2605]\nComplete the bonus levels.",
+            "primaryText": "Snailarchy [5\u2605]\nOverthrow the Snail King.",
             "community": 0,
             "communityText": "",
-            "achievements": 16
+            "achievements": 20
         },
         "gameTags": [
-            "Indie",
-            "Arcade",
-            "Puzzle",
-            "Match 3",
-            "Anime",
-            "Local Multiplayer",
-            "Funny",
-            "4 Player Local",
-            "Casual",
-            "Controller",
-            "Remake",
-            "Cute",
-            "PvP",
-            "Colorful",
-            "Female Protagonist",
-            "2D",
-            "Split Screen",
-            "Linear",
+            "Dungeon Crawler",
+            "Difficult",
+            "Mystery Dungeon",
             "Procedural Generation",
-            "Multiplayer"
+            "Action Roguelike",
+            "First-Person",
+            "Roguelite",
+            "Colorful",
+            "Perma Death",
+            "Looter Shooter",
+            "Fantasy",
+            "FPS",
+            "Magic",
+            "Action",
+            "Stylized",
+            "Shooter",
+            "Loot",
+            "Resource Management",
+            "Singleplayer",
+            "Old School"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "koreana": "I",
-            "schinese": "I"
+            "english": "IS",
+            "german": "IS",
+            "russian": "IS",
+            "schinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2025-12-28'
+var gameOfDayDate = '2025-12-29'
