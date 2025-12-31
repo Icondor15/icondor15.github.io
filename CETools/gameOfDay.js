f558d1ff9f386b328282b4d033661e988079bcc0
Icondor <ivanfigus@gmail.com>
2025-12-31 08:46:58 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,83 +2,88 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 317250,
-        "CEId": "8575702b-cd01-4139-a381-9eca9f2f9cbc",
-        "name": "Airscape - The Fall of Gravity",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/317250/header.jpg?t=1574484005",
-        "genre": "Platformer",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2021-12-30T19:53:54.000Z",
+        "platformId": 762610,
+        "CEId": "28bc683e-8ead-4eff-acc8-5f0cef84e27b",
+        "name": "Russian Subway Dogs",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/762610/header.jpg?t=1632861952",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2021-12-31T18:22:54.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 17,
-        "playersTotal": 235,
+        "playersCompleted": 8,
+        "playersStarted": 0,
+        "playersTotal": 28,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
+                "initial": 1499,
+                "final": 449,
+                "discountPercent": 70
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
+                "initial": 1479,
+                "final": 443,
+                "discountPercent": 70
             },
             "GBP": {
-                "initial": 429,
-                "final": 429,
-                "discountPercent": 0
+                "initial": 1279,
+                "final": 383,
+                "discountPercent": 70
             },
             "JPY": {
-                "initial": 58000,
-                "final": 58000,
-                "discountPercent": 0
+                "initial": 170000,
+                "final": 51000,
+                "discountPercent": 70
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
+                "initial": 2195,
+                "final": 658,
+                "discountPercent": 70
             },
             "CAD": {
-                "initial": 649,
-                "final": 649,
-                "discountPercent": 0
+                "initial": 1949,
+                "final": 584,
+                "discountPercent": 70
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Clocktopus [10\u2605]\nBeat the game in 60 minutes or less.\n\nCephaloGod [10\u2605]\nReach 101% in-game completion.",
+            "primary": 1,
+            "primaryText": "Proletariat Puprising [15\u2605]\nProve yourself.",
             "community": 0,
             "communityText": "",
-            "achievements": 23
+            "achievements": 37
         },
         "gameTags": [
-            "Shoot 'Em Up",
-            "Exploration",
-            "Difficult",
-            "Casual",
-            "Platformer",
-            "2D",
-            "Arcade",
+            "Dog",
+            "Cats",
             "Cute",
-            "2.5D",
+            "Score Attack",
+            "2D",
+            "Pixel Graphics",
+            "Funny",
             "Action",
-            "Puzzle",
-            "Colorful",
-            "Physics",
-            "Adventure",
+            "Arcade",
+            "Retro",
+            "Old School",
+            "Trains",
+            "Memes",
+            "Electronic Music",
             "Singleplayer",
+            "Side Scroller",
             "Indie",
-            "Great Soundtrack",
-            "Sci-fi",
-            "Family Friendly",
-            "Atmospheric"
+            "Soundtrack",
+            "Experimental",
+            "Controller"
         ],
         "languages": {
-            "english": "IAS",
-            "finnish": "I"
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "japanese": "I",
+            "brazilian": "I",
+            "latam": "I",
+            "russian": "I"
         }
     }
 }
-var gameOfDayDate = '2025-12-30'
+var gameOfDayDate = '2025-12-31'
