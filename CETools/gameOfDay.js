bc252ff2fb306493035318818ae77a11950e3758
Icondor <ivanfigus@gmail.com>
2026-01-02 08:48:52 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,91 +2,85 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 465200,
-        "CEId": "d41ad53d-5994-4092-80ea-6172ab8fed8f",
-        "name": "Fury Unleashed",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/465200/header.jpg?t=1637835354",
+        "platformId": 1489410,
+        "CEId": "4f672da8-31d4-4825-b12b-6c62872c7920",
+        "name": "Alice Escaped!",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1489410/header.jpg?t=1728605119",
         "genre": "Action",
-        "tier": 3,
-        "points": 50,
-        "createdAt": "2022-01-01T20:04:43.000Z",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2025-01-02T07:10:48.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 12,
+        "playersCompleted": 10,
         "playersStarted": 0,
-        "playersTotal": 370,
+        "playersTotal": 22,
         "priceData": {
             "USD": {
                 "initial": 1999,
-                "final": 199,
-                "discountPercent": 90
+                "final": 1199,
+                "discountPercent": 40
             },
             "EUR": {
                 "initial": 1950,
-                "final": 195,
-                "discountPercent": 90
+                "final": 1170,
+                "discountPercent": 40
             },
             "GBP": {
                 "initial": 1675,
-                "final": 167,
-                "discountPercent": 90
+                "final": 1005,
+                "discountPercent": 40
             },
             "JPY": {
-                "initial": 230000,
-                "final": 23000,
-                "discountPercent": 90
+                "initial": 198000,
+                "final": 118800,
+                "discountPercent": 40
             },
             "AUD": {
                 "initial": 2950,
-                "final": 295,
-                "discountPercent": 90
+                "final": 1770,
+                "discountPercent": 40
             },
             "CAD": {
                 "initial": 2599,
-                "final": 259,
-                "discountPercent": 90
+                "final": 1559,
+                "discountPercent": 40
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Comically Good [50\u2605]\nSeparately, clear Legendary mode and clear the game without breaking your combo.",
+            "primaryText": "Mini-Ribi [5\u2605]\nDefeat Alice Within 40 Minutes / Defeat Grimoire Cheshire Cat Without Obtaining Guard Master",
             "community": 0,
             "communityText": "",
-            "achievements": 47
+            "achievements": 29
         },
         "gameTags": [
-            "Local Co-Op",
-            "Combat",
-            "Multiplayer",
-            "Action",
-            "Replay Value",
-            "Roguelike",
-            "Fast-Paced",
-            "Difficult",
-            "Online Co-Op",
-            "Old School",
+            "Atmospheric",
+            "Adventure",
             "Platformer",
-            "Shooter",
-            "2D Platformer",
-            "Local Multiplayer",
+            "Metroidvania",
+            "Cute",
+            "Story Rich",
             "2D",
-            "Perma Death",
-            "Roguelite",
-            "Co-op",
-            "Side Scroller",
-            "Adventure"
+            "Fantasy",
+            "Spectacle fighter",
+            "Female Protagonist",
+            "Nature",
+            "Action-Adventure",
+            "Difficult",
+            "Hack and Slash",
+            "Anime",
+            "Action",
+            "Colorful",
+            "Dark",
+            "Combat",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IAS",
-            "german": "IAS",
-            "japanese": "IAS",
-            "spanish": "IAS",
-            "brazilian": "IAS",
-            "koreana": "IAS",
-            "polish": "IAS",
-            "russian": "IAS",
-            "schinese": "IAS"
+            "english": "IS",
+            "japanese": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-01-01'
+var gameOfDayDate = '2026-01-02'
