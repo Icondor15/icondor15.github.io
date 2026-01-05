b7e194f028407f85fcb32306d59f7b8649c084ab
Icondor <ivanfigus@gmail.com>
2026-01-05 08:51:11 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,82 +2,91 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 603530,
-        "CEId": "551f7196-16a5-4d4d-bd2a-c0afbeccbf16",
-        "name": "A Robot Named Fight!",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/603530/header.jpg?t=1666986913",
+        "platformId": 335300,
+        "CEId": "0a7ffb40-c98b-4478-a42e-2fce0cd90d95",
+        "name": "DARK SOULS\u2122 II: Scholar of the First Sin",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/335300/header.jpg?t=1666717194",
         "genre": "Action",
         "tier": 2,
-        "points": 20,
-        "createdAt": "2023-01-04T08:59:22.000Z",
+        "points": 30,
+        "createdAt": "2023-01-05T08:39:03.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 5,
-        "playersStarted": 0,
-        "playersTotal": 86,
+        "playersCompleted": 9,
+        "playersStarted": 258,
+        "playersTotal": 600,
         "priceData": {
             "USD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 3999,
+                "final": 3999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1279,
-                "final": 1279,
+                "initial": 3999,
+                "final": 3999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 150000,
-                "final": 150000,
+                "initial": 572000,
+                "final": 572000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1895,
-                "final": 1895,
+                "initial": 5695,
+                "final": 5695,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 5349,
+                "final": 5349,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Super Meatroid [15\u2605]\nComplete the game with a completion rate of less than 15%. / Defeat the MegaBeast Core with a completion rate of 100% in under 60 minutes.\n\nSuper Fighting Robot [5\u2605]\nComplete Mega Map. / Complete the boss rush. / Complete Exterminator.",
-            "community": 2,
-            "communityText": "Hoover Flags\nComplete the game with a completion rate of 0% without using The Thief.\n\nPoorly Prepared\nComplete the game with a completion rate of less than 15%, without using The Thief.",
-            "achievements": 84
+            "primary": 4,
+            "primaryText": "Dark Conqueror [10\u2605]\nBeat the game without dying.\n\nExalted Soul [5\u2605]\nBeat the game without resting at a bonfire.\n\nCrown of the Fallen [5\u2605]\nDefeat all bosses in all of the three DLCs.\n\nBearer of the Curse [10\u2605]\nBeat the game.",
+            "community": 4,
+            "communityText": "Dark Child of the Outlands\nBeat the game on NG+7.\n\nDeprived For Life\nBeat the game at soul level 1.\n\nPop No Hits, Vol. 2\nBeat the game without getting hit (see the proof requirement below).\n\nRestless Soul \nBeat the game without dying or resting at a bonfire on a solo NG savefile.",
+            "achievements": 38
         },
         "gameTags": [
-            "Metroidvania",
-            "Roguevania",
-            "Action Roguelike",
+            "Souls-like",
+            "Dark Fantasy",
+            "RPG",
+            "Difficult",
             "Action",
-            "Roguelite",
-            "Indie",
-            "Gore",
-            "Violent",
-            "Platformer",
-            "Pixel Graphics",
-            "Procedural Generation",
-            "2D",
-            "Retro",
-            "Robots",
-            "Roguelike",
-            "Local Co-Op",
-            "Local Multiplayer",
-            "4 Player Local",
-            "Nonlinear",
-            "Split Screen"
+            "Third Person",
+            "Atmospheric",
+            "Action RPG",
+            "Character Customization",
+            "Co-op",
+            "Multiplayer",
+            "Adventure",
+            "Fantasy",
+            "Medieval",
+            "Dark",
+            "Singleplayer",
+            "Lore-Rich",
+            "Story Rich",
+            "PvP",
+            "Remake"
         ],
         "languages": {
-            "english": "IAS"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "koreana": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-01-04'
+var gameOfDayDate = '2026-01-05'
