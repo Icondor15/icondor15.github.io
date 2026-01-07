a307663f5175b8e5ae6d2259e62ac8f826f54c24
Icondor <ivanfigus@gmail.com>
2026-01-07 08:47:07 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,91 +2,88 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1256060,
-        "CEId": "d123c8a9-a9bb-4360-8666-1ce84c046f4a",
-        "name": "Cosmodread",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1256060/header.jpg?t=1620726395",
-        "genre": "First-Person",
+        "platformId": 1781260,
+        "CEId": "b7ff0ee7-713d-41bb-ad03-c4956e50eff6",
+        "name": "Promenade",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1781260/header.jpg?t=1715691133",
+        "genre": "Platformer",
         "tier": 1,
-        "points": 5,
-        "createdAt": "2024-01-06T17:54:27.000Z",
+        "points": 10,
+        "createdAt": "2025-01-07T22:10:47.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
         "playersCompleted": 1,
         "playersStarted": 0,
-        "playersTotal": 2,
+        "playersTotal": 9,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 1950,
+                "final": 1950,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 1675,
+                "final": 1675,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 152000,
-                "final": 152000,
+                "initial": 230000,
+                "final": 230000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 2950,
+                "final": 2950,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 2599,
+                "final": 2599,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Astrophobia [5\u2605]\nEscape the spaceship and beat the game.",
+            "primaryText": "Not Cogged Down [10\u2605]\nBeat game in 2h30m or less. / Beat ALL the developers times in time trials. / Climb the mountain without falling. / Beat the Comedian's Lair without taking damage. / Collect at least 26 cog fragments without acquiring the Omnihook. / Enter the spaceship before acquiring the Omnihook.",
             "community": 0,
             "communityText": "",
-            "achievements": 0
+            "achievements": 43
         },
         "gameTags": [
-            "VR",
-            "Survival Horror",
-            "Sci-fi",
-            "Atmospheric",
-            "Perma Death",
-            "Survival",
-            "Horror",
-            "Action",
             "Adventure",
-            "Roguelike",
-            "Procedural Generation",
-            "Gore",
-            "Space",
+            "Cute",
+            "Platformer",
+            "Exploration",
+            "Collectathon",
             "Singleplayer",
-            "Immersive",
-            "Roguelite",
-            "Difficult",
-            "Indie",
-            "Strategy",
-            "Real-Time"
+            "Family Friendly",
+            "Action",
+            "Colorful",
+            "Casual",
+            "Puzzle Platformer",
+            "Funny",
+            "Puzzle",
+            "Action-Adventure",
+            "Atmospheric",
+            "Cartoony",
+            "Controller",
+            "Retro",
+            "Metroidvania",
+            "2D Platformer"
         ],
         "languages": {
             "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "koreana": "IS",
-            "russian": "IS",
-            "schinese": "IS"
-        },
-        "information": "This game requires a VR headset in order to play."
+            "french": "IAS",
+            "german": "IAS",
+            "italian": "IAS",
+            "japanese": "IAS",
+            "spanish": "IAS",
+            "schinese": "IAS"
+        }
     }
 }
-var gameOfDayDate = '2026-01-06'
+var gameOfDayDate = '2026-01-07'
