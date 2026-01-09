2180b8207621095007aecd87ec89a9fd6d78be71
Icondor <ivanfigus@gmail.com>
2026-01-09 08:52:27 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,79 +2,94 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1565880,
-        "CEId": "ab92f2e1-1a6a-4c0c-91f4-e53079c36c41",
-        "name": "Turbo Shell",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1565880/header.jpg?t=1684854029",
-        "genre": "Platformer",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2024-01-08T13:03:00.000Z",
+        "platformId": 644830,
+        "CEId": "40849373-5f26-4115-93eb-e2645185480f",
+        "name": "The Surge 2",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/644830/header.jpg?t=1663148861",
+        "genre": "Action",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2023-01-09T05:37:27.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 0,
+        "playersCompleted": 17,
         "playersStarted": 0,
-        "playersTotal": 12,
+        "playersTotal": 229,
         "priceData": {
             "USD": {
-                "initial": 1199,
-                "final": 179,
-                "discountPercent": 85
+                "initial": 2999,
+                "final": 299,
+                "discountPercent": 90
             },
             "EUR": {
-                "initial": 1199,
-                "final": 179,
-                "discountPercent": 85
+                "initial": 2999,
+                "final": 299,
+                "discountPercent": 90
             },
             "GBP": {
-                "initial": 999,
-                "final": 149,
-                "discountPercent": 85
+                "initial": 2499,
+                "final": 249,
+                "discountPercent": 90
             },
             "JPY": {
-                "initial": 140000,
-                "final": 21000,
-                "discountPercent": 85
+                "initial": 498000,
+                "final": 49800,
+                "discountPercent": 90
             },
             "AUD": {
-                "initial": 1775,
-                "final": 266,
-                "discountPercent": 85
+                "initial": 4495,
+                "final": 449,
+                "discountPercent": 90
             },
             "CAD": {
-                "initial": 1549,
-                "final": 232,
-                "discountPercent": 85
+                "initial": 3999,
+                "final": 399,
+                "discountPercent": 90
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Cracked [15\u2605]\nObtain all S ranks on Nightmare difficulty.\n\nShell Shocked [10\u2605]\nBeat all levels on Hard difficulty.",
-            "community": 1,
-            "communityText": "Fried to a Crisp\nObtain an S rank on all difficulties. ",
-            "achievements": 21
+            "primary": 1,
+            "primaryText": "Surge Harder [5\u2605]\nBeat the final boss of the base game.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 57
         },
         "gameTags": [
-            "Precision Platformer",
-            "2D Platformer",
+            "Souls-like",
+            "RPG",
             "Action",
-            "Pixel Graphics",
-            "Space",
+            "Open World",
             "Sci-fi",
+            "Violent",
+            "Difficult",
             "Singleplayer",
-            "2D",
-            "Platformer",
-            "Indie",
-            "Fast-Paced",
-            "Difficult"
+            "Third Person",
+            "Cyberpunk",
+            "Character Customization",
+            "Multiplayer",
+            "Gore",
+            "Mechs",
+            "Hack and Slash",
+            "Action RPG",
+            "Robots",
+            "Female Protagonist",
+            "Story Rich",
+            "Co-op"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "spanish": "I",
-            "ukrainian": "I"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IAS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "czech": "IS",
+            "koreana": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-01-08'
+var gameOfDayDate = '2026-01-09'
