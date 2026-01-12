531f6b2e28de2a2d418bf44ed76ffa33be445249
Icondor <ivanfigus@gmail.com>
2026-01-12 08:51:14 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,92 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 673750,
-        "CEId": "05d31dda-14ce-4b42-85d0-ce260a8379f0",
-        "name": "Super Bunny Man",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/673750/header.jpg?t=1684270840",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2023-06-21T17:25:22.000Z",
+        "platformId": 937580,
+        "CEId": "c4e82c1c-0ba0-4a84-9e95-86cb6db7e171",
+        "name": "Touhou Kanjuden ~ Legacy of Lunatic Kingdom.",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/937580/header.jpg?t=1667959178",
+        "genre": "Bullet Hell",
+        "tier": 4,
+        "points": 150,
+        "createdAt": "2023-01-12T07:04:06.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 6,
-        "playersStarted": 0,
-        "playersTotal": 124,
+        "playersCompleted": 1,
+        "playersStarted": 12,
+        "playersTotal": 79,
         "priceData": {
             "USD": {
                 "initial": 1499,
-                "final": 899,
-                "discountPercent": 40
+                "final": 1499,
+                "discountPercent": 0
             },
             "EUR": {
-                "initial": 1499,
-                "final": 899,
-                "discountPercent": 40
+                "initial": 1249,
+                "final": 1249,
+                "discountPercent": 0
             },
             "GBP": {
-                "initial": 1199,
-                "final": 719,
-                "discountPercent": 40
+                "initial": 1139,
+                "final": 1139,
+                "discountPercent": 0
             },
             "JPY": {
-                "initial": 170000,
-                "final": 102000,
-                "discountPercent": 40
+                "initial": 154000,
+                "final": 154000,
+                "discountPercent": 0
             },
             "AUD": {
-                "initial": 2249,
-                "final": 1349,
-                "discountPercent": 40
+                "initial": 2150,
+                "final": 2150,
+                "discountPercent": 0
             },
             "CAD": {
-                "initial": 1799,
-                "final": 1079,
-                "discountPercent": 40
+                "initial": 1749,
+                "final": 1749,
+                "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Chubby Bunny [15\u2605]\nObtain all 150 medals.",
-            "community": 0,
-            "communityText": "",
+            "primary": 4,
+            "primaryText": "Brilliant Pristine Madness [100\u2605]\nBeat the game on Legacy Lunatic without dying.\n\nCrimson Impurity [30\u2605]\nBeat the game on Legacy Lunatic without using continues.\n\nWelcome \u2764 Hell [15\u2605]\nBeat the Extra mode.\n\nA Sip of Lunacy [5\u2605]\nBeat the game on Pointdevice Lunatic.",
+            "community": 2,
+            "communityText": "Chang'e Incarnate\nBeat the game on Legacy Lunatic without dying and bombing.\n\nSpace Juggler\nBeat the Extra mode without dying and bombing.",
             "achievements": 0
         },
         "gameTags": [
-            "Platformer",
-            "Sandbox",
-            "Multiplayer",
-            "Co-op",
-            "Singleplayer",
-            "Funny",
-            "Online Co-Op",
-            "Physics",
-            "Local Co-Op",
-            "Party",
-            "Comedy",
-            "Casual",
-            "2D Platformer",
-            "Puzzle Platformer",
-            "Memes",
+            "Bullet Hell",
+            "Difficult",
+            "Great Soundtrack",
+            "Faith",
+            "Indie",
+            "Psychological Horror",
+            "Female Protagonist",
+            "Shoot 'Em Up",
             "Action",
-            "Adventure",
-            "PvP",
-            "Local Multiplayer",
-            "Party Game"
+            "Singleplayer",
+            "Anime",
+            "Cute",
+            "2D",
+            "Arcade",
+            "Shooter",
+            "Fantasy",
+            "Lore-Rich",
+            "Story Rich",
+            "Souls-like",
+            "Adventure"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "koreana": "I",
-            "russian": "I",
-            "schinese": "I",
-            "tchinese": "I"
+            "japanese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-01-11'
+var gameOfDayDate = '2026-01-12'
