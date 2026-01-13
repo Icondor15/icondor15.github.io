6f2af3b340b7bd6f4a33c2a3aaedb6fe9d52d27f
Icondor <ivanfigus@gmail.com>
2026-01-13 08:47:39 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,82 +2,83 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 937580,
-        "CEId": "c4e82c1c-0ba0-4a84-9e95-86cb6db7e171",
-        "name": "Touhou Kanjuden ~ Legacy of Lunatic Kingdom.",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/937580/header.jpg?t=1667959178",
-        "genre": "Bullet Hell",
+        "platformId": 1860350,
+        "CEId": "72a90222-f641-417d-845e-6cf7c040ee1f",
+        "name": "Dream Swing",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1860350/header.jpg?t=1689872693",
+        "genre": "First-Person",
         "tier": 4,
-        "points": 150,
-        "createdAt": "2023-01-12T07:04:06.000Z",
+        "points": 130,
+        "createdAt": "2024-01-13T20:56:43.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 12,
-        "playersTotal": 79,
+        "playersCompleted": 4,
+        "playersStarted": 15,
+        "playersTotal": 98,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 299,
+                "final": 299,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 299,
+                "final": 299,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 249,
+                "final": 249,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 154000,
-                "final": 154000,
+                "initial": 35000,
+                "final": 35000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 450,
+                "final": 450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 389,
+                "final": 389,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Brilliant Pristine Madness [100\u2605]\nBeat the game on Legacy Lunatic without dying.\n\nCrimson Impurity [30\u2605]\nBeat the game on Legacy Lunatic without using continues.\n\nWelcome \u2764 Hell [15\u2605]\nBeat the Extra mode.\n\nA Sip of Lunacy [5\u2605]\nBeat the game on Pointdevice Lunatic.",
-            "community": 2,
-            "communityText": "Chang'e Incarnate\nBeat the game on Legacy Lunatic without dying and bombing.\n\nSpace Juggler\nBeat the Extra mode without dying and bombing.",
-            "achievements": 0
+            "primary": 3,
+            "primaryText": "Lucid Dreamer[90\u2605 (Partial: 15\u2605)]\nGet 150 orbs in all modes except Weekly (Partial points awarded for obtaining 150 orbs in any mode)\n\nCosmic Swinger[35\u2605 (Partial: 5\u2605)]\nGet 130 orbs in all modes except Weekly (Partial points awarded for obtaining 130 orbs in any mode)\n\nA Chaotic Start [5\u2605]\nGet 120 orbs in any mode. (Chaos Original recommended)",
+            "community": 0,
+            "communityText": "",
+            "achievements": 40
         },
         "gameTags": [
-            "Bullet Hell",
-            "Difficult",
-            "Great Soundtrack",
-            "Faith",
+            "Precision Platformer",
             "Indie",
-            "Psychological Horror",
-            "Female Protagonist",
-            "Shoot 'Em Up",
+            "Relaxing",
+            "First-Person",
+            "FPS",
+            "3D Platformer",
+            "Racing",
+            "Sports",
+            "Difficult",
+            "Casual",
             "Action",
-            "Singleplayer",
-            "Anime",
-            "Cute",
-            "2D",
-            "Arcade",
+            "Parkour",
+            "Platformer",
+            "Surreal",
             "Shooter",
-            "Fantasy",
-            "Lore-Rich",
-            "Story Rich",
-            "Souls-like",
-            "Adventure"
+            "Arcade",
+            "Physics",
+            "Minimalist",
+            "Stylized",
+            "Singleplayer"
         ],
         "languages": {
-            "japanese": "IS"
+            "english": "IAS",
+            "turkish": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-01-12'
+var gameOfDayDate = '2026-01-13'
