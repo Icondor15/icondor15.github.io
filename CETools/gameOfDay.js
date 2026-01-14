e9d454fad3451b286bb9d0bdf13903c709d78e03
Icondor <ivanfigus@gmail.com>
2026-01-14 08:47:40 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,83 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1860350,
-        "CEId": "72a90222-f641-417d-845e-6cf7c040ee1f",
-        "name": "Dream Swing",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1860350/header.jpg?t=1689872693",
-        "genre": "First-Person",
-        "tier": 4,
-        "points": 130,
-        "createdAt": "2024-01-13T20:56:43.000Z",
+        "platformId": 200900,
+        "CEId": "d415208e-0b56-43e5-a4fc-f8d350953cb9",
+        "name": "Cave Story+",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/200900/header.jpg?t=1488657143",
+        "genre": "Action",
+        "tier": 3,
+        "points": 65,
+        "createdAt": "2020-01-28T09:31:36.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 4,
-        "playersStarted": 15,
-        "playersTotal": 98,
+        "playersCompleted": 39,
+        "playersStarted": 0,
+        "playersTotal": 529,
         "priceData": {
             "USD": {
-                "initial": 299,
-                "final": 299,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 299,
-                "final": 299,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 249,
-                "final": 249,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 35000,
-                "final": 35000,
+                "initial": 148000,
+                "final": 148000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 450,
-                "final": 450,
+                "initial": 2150,
+                "final": 2150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 389,
-                "final": 389,
+                "initial": 1699,
+                "final": 1699,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Lucid Dreamer[90\u2605 (Partial: 15\u2605)]\nGet 150 orbs in all modes except Weekly (Partial points awarded for obtaining 150 orbs in any mode)\n\nCosmic Swinger[35\u2605 (Partial: 5\u2605)]\nGet 130 orbs in all modes except Weekly (Partial points awarded for obtaining 130 orbs in any mode)\n\nA Chaotic Start [5\u2605]\nGet 120 orbs in any mode. (Chaos Original recommended)",
-            "community": 0,
-            "communityText": "",
-            "achievements": 40
+            "primary": 1,
+            "primaryText": "Pixel Legend [65\u2605]\nProve yourself.",
+            "community": 1,
+            "communityText": "Pixel Legend+\nObtain the \"best ending\" on Hard mode without saving.",
+            "achievements": 76
         },
         "gameTags": [
-            "Precision Platformer",
-            "Indie",
-            "Relaxing",
-            "First-Person",
-            "FPS",
-            "3D Platformer",
-            "Racing",
-            "Sports",
-            "Difficult",
-            "Casual",
-            "Action",
-            "Parkour",
             "Platformer",
-            "Surreal",
+            "Metroidvania",
+            "Indie",
+            "Great Soundtrack",
+            "Pixel Graphics",
+            "Action",
+            "Adventure",
+            "2D",
+            "Singleplayer",
+            "Retro",
+            "Classic",
+            "Exploration",
+            "Replay Value",
+            "Difficult",
+            "Story Rich",
             "Shooter",
-            "Arcade",
-            "Physics",
-            "Minimalist",
-            "Stylized",
-            "Singleplayer"
+            "Remake",
+            "Cute",
+            "Sci-fi",
+            "Anime"
         ],
         "languages": {
-            "english": "IAS",
-            "turkish": "IAS"
+            "english": "IA"
         }
     }
 }
-var gameOfDayDate = '2026-01-13'
+var gameOfDayDate = '2026-01-14'
