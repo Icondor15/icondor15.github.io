db9a46e1094542284a2de9396535825c529fa7f6
Icondor <ivanfigus@gmail.com>
2026-01-18 08:37:11 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,92 +2,86 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 881100,
-        "CEId": "0c10c492-c431-41fb-9fbf-89526130b3f4",
-        "name": "Noita",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/881100/header.jpg?t=1627905909",
-        "genre": "Action",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2021-01-17T02:29:53.000Z",
+        "platformId": 1956430,
+        "CEId": "c05bd580-ad8d-432c-b7a1-9b2930e7d128",
+        "name": "Terminal Velocity\u2122: Boosted Edition",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1956430/header.jpg?t=1678798828",
+        "genre": "Arcade",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2024-01-18T13:49:25.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 30,
-        "playersStarted": 229,
-        "playersTotal": 964,
+        "playersCompleted": 1,
+        "playersStarted": 0,
+        "playersTotal": 36,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 1199,
+                "final": 1199,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1899,
-                "final": 1899,
+                "initial": 1179,
+                "final": 1179,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1549,
-                "final": 1549,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 205000,
-                "final": 205000,
+                "initial": 140000,
+                "final": 140000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2895,
-                "final": 2895,
+                "initial": 1775,
+                "final": 1775,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2279,
-                "final": 2279,
+                "initial": 1549,
+                "final": 1549,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Noitmare [15\u2605]\nClear the game on \"Nightmare\" Mode.\n\nGive Me Sorcery [10\u2605]\nClear the game.",
-            "community": 3,
-            "communityText": "Mestarien Mestari\nComplete the sun quest, and get the peaceful ending.\n\nSauvojen Tuntija\nGet 100% progress. / Create an entity with infinite lifetime. / Do 1 million damage in one shot.\n\nThe End of Everything\nComplete all pillar achievements in game.",
-            "achievements": 14
+            "primary": 1,
+            "primaryText": "After Burner [5\u2605]\nSeparately; Complete the game on Terminal difficulty, and complete each episode under the specified time limit.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 32
         },
         "gameTags": [
-            "Physics",
-            "Roguelike",
-            "Pixel Graphics",
-            "Difficult",
-            "Dungeon Crawler",
-            "Indie",
-            "Sandbox",
-            "Open World",
-            "2D",
-            "Perma Death",
             "Action",
-            "Action Roguelike",
-            "Gun Customization",
-            "Roguelite",
-            "2D Platformer",
-            "RPG",
-            "Dark Humor",
-            "Mythology",
+            "Shooter",
+            "Old School",
+            "Sci-fi",
+            "Retro",
+            "Singleplayer",
+            "Adventure",
+            "FPS",
+            "Open World",
+            "Realistic",
+            "Space",
+            "Combat",
+            "War",
+            "Simulation",
+            "1990's",
+            "Flight",
+            "Space Sim",
             "Action-Adventure",
-            "Crafting"
+            "3D",
+            "Futuristic"
         ],
         "languages": {
-            "english": "I",
+            "english": "IA",
             "french": "I",
             "german": "I",
             "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "koreana": "I",
-            "polish": "I",
-            "russian": "I",
-            "schinese": "I"
+            "spanish": "I"
         }
     }
 }
-var gameOfDayDate = '2026-01-17'
+var gameOfDayDate = '2026-01-18'
