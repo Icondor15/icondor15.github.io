8774c3d3efe3d72921847f76ababb820e68f97f2
Icondor <ivanfigus@gmail.com>
2026-01-21 08:47:11 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,83 +1,49 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 445110,
-        "CEId": "0691dcc6-5d17-4223-80f0-a9c74da1c0f5",
-        "name": "Tasty Planet: Back for Seconds",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/445110/header.jpg?t=1678413164",
-        "genre": "Arcade",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2024-01-20T15:55:46.000Z",
-        "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 10,
-        "playersTotal": 50,
-        "priceData": {
-            "USD": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 659,
-                "final": 659,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 579,
-                "final": 579,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 82000,
-                "final": 82000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1150,
-                "final": 1150,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 899,
-                "final": 899,
-                "discountPercent": 0
-            }
-        },
+        "gameType": 1,
+        "platformId": 769560,
+        "CEId": "6e505741-d18c-41ef-afc2-4135e74cfeab",
+        "name": "\u6708\u5706\u4e4b\u591c (Night of Full Moon)",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/769560/header.jpg?t=1703806510",
+        "genre": "Strategy",
+        "tier": 1,
+        "points": 12,
+        "createdAt": "2024-01-21T16:43:20.000Z",
+        "updatedAt": "2025-09-19T20:46:24.000Z",
+        "playersCompleted": 0,
+        "playersStarted": 4,
+        "playersTotal": 129,
         "milestones": {
-            "primary": 2,
-            "primaryText": "Dinner for Two (S) [10\u2605]\nObtain all possible co-op Gold Medals.\n\nTasted Planet [10\u2605]\nObtain all possible single player Gold Medals. ",
+            "primary": 3,
+            "primaryText": "Scion of the Black Forest [10\u2605]\nSeparately; Clear Hard VII and Complete Nightmare with 3 moves.\n\nDignitary of the Night Fall (UNCLEARED) [1\u2605]\nComplete Hard VII with all characters.\n\nLunar Eclipse (UNVALUED) [1\u2605]\nClear difficulty 10 without losing HP.",
             "community": 0,
             "communityText": "",
-            "achievements": 25
+            "achievements": 152
         },
         "gameTags": [
-            "Casual",
-            "Adventure",
-            "Indie",
-            "Action",
-            "Funny",
-            "2D",
-            "Cute",
-            "Top-Down",
-            "Sci-fi",
-            "Singleplayer",
-            "Dinosaurs",
-            "Co-op",
-            "Multiplayer",
-            "Local Multiplayer",
+            "Card Game",
             "Free to Play",
-            "Local Co-Op",
-            "Survival",
-            "Space",
-            "Destruction",
-            "Comedy"
+            "Strategy",
+            "Deckbuilding",
+            "Indie",
+            "Card Battler",
+            "Adventure",
+            "Roguelite",
+            "Singleplayer",
+            "Roguelike Deckbuilder",
+            "Roguelike",
+            "Female Protagonist",
+            "Turn-Based",
+            "RPG"
         ],
         "languages": {
-            "english": "IAS"
+            "english": "IA",
+            "japanese": "IS",
+            "koreana": "IS",
+            "schinese": "IA",
+            "tchinese": "IA"
         }
     }
 }
-var gameOfDayDate = '2026-01-20'
+var gameOfDayDate = '2026-01-21'
