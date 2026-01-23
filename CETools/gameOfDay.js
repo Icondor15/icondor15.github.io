45fc992d58a876452525bb7fb2cee1372838e9b9
Icondor <ivanfigus@gmail.com>
2026-01-23 08:47:45 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,82 +2,78 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 946010,
-        "CEId": "104bd545-7ba7-4825-a618-f85586bfb8d3",
-        "name": "Tasty Planet Forever",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/946010/header.jpg?t=1694028162",
-        "genre": "Arcade",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2024-01-22T11:02:37.000Z",
+        "platformId": 1432970,
+        "CEId": "a65604a9-90f4-4dac-ab05-2469b3b481ff",
+        "name": "ASSAULT SHELL",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1432970/header.jpg?t=1616148066",
+        "genre": "Bullet Hell",
+        "tier": 2,
+        "points": 20,
+        "createdAt": "2023-01-23T17:17:37.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 10,
-        "playersStarted": 0,
-        "playersTotal": 33,
+        "playersCompleted": 2,
+        "playersStarted": 2,
+        "playersTotal": 15,
         "priceData": {
             "USD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1079,
-                "final": 1079,
+                "initial": 819,
+                "final": 819,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1029,
-                "final": 1029,
+                "initial": 719,
+                "final": 719,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 132000,
-                "final": 132000,
+                "initial": 101000,
+                "final": 101000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1850,
-                "final": 1850,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1449,
-                "final": 1449,
+                "initial": 1149,
+                "final": 1149,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Four Course Meal [15\u2605]\nEarn 3 stars on every level on Deadly difficulty. ",
+            "primary": 2,
+            "primaryText": "Ultimate Ace [15\u2605]\nClear the game without taking a hit.\n\nFull Impact [5\u2605]\nClear Original Full.",
             "community": 0,
             "communityText": "",
-            "achievements": 13
+            "achievements": 20
         },
         "gameTags": [
-            "Destruction",
+            "Bullet Hell",
+            "Action",
+            "Shoot 'Em Up",
+            "Shooter",
             "Arcade",
             "2D",
-            "Cute",
-            "Split Screen",
             "Top-Down",
-            "Funny",
-            "Post-apocalyptic",
-            "Side Scroller",
-            "Controller",
-            "Action",
-            "Casual",
-            "Local Co-Op",
-            "Cartoony",
-            "Singleplayer",
+            "Multiple Endings",
+            "Pixel Graphics",
+            "Indie",
+            "Sci-fi",
+            "Dystopian ",
             "Score Attack",
-            "2D Platformer",
-            "Hand-drawn",
-            "Platformer",
-            "Cats"
+            "Singleplayer",
+            "War"
         ],
         "languages": {
-            "english": "IAS"
+            "english": "IS",
+            "italian": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-01-22'
+var gameOfDayDate = '2026-01-23'
