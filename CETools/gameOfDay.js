b440511e3909cc4733e70a624837926dd556c627
Icondor <ivanfigus@gmail.com>
2026-01-30 08:47:41 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,92 +2,77 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 8870,
-        "CEId": "bf95781c-7d08-4afb-b3c1-8d1791832b97",
-        "name": "BioShock Infinite",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/8870/header.jpg?t=1602794480",
-        "genre": "First-Person",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2023-01-29T19:16:53.000Z",
+        "platformId": 2861740,
+        "CEId": "e88a8007-169d-4aba-8148-045ffa8902c3",
+        "name": "\u6771\u65b9\u8d44\u5fd7\u758f ~ Immortal Immanuel.",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2861740/header.jpg?t=1737085764",
+        "genre": "Bullet Hell",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2025-01-30T08:21:03.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 17,
-        "playersStarted": 39,
-        "playersTotal": 797,
+        "playersCompleted": 3,
+        "playersStarted": 0,
+        "playersTotal": 7,
         "priceData": {
             "USD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 975,
+                "final": 975,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 850,
+                "final": 850,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 298000,
-                "final": 298000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 3995,
-                "final": 3995,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 3299,
-                "final": 3299,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Blue Ribbon Champ[25\u2605 (Partial: 5\u2605)]\nComplete all Clash in the Clouds blue ribbon challenges.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 80
+            "primaryText": "Knowledge Linked [10\u2605]\nBeat the game on Lunatic without continuing with all characters. / Score 2.214B in a run. / Beat either the game or Extra mode without dying and bombing.",
+            "community": 2,
+            "communityText": "Joy Star Lord\nBeat the game on Lunatic without dying, bombing and using your hyper.\n\nLocked In in Heaven\nBeat the Extra mode without dying, bombing and using your hyper.",
+            "achievements": 24
         },
         "gameTags": [
             "Action",
+            "Bullet Hell",
+            "Shoot 'Em Up",
+            "Indie",
+            "2D",
             "Shooter",
-            "First-Person",
-            "FPS",
-            "RPG",
-            "Fantasy",
+            "Anime",
             "Singleplayer",
-            "Adventure",
-            "Sci-fi",
-            "Story Rich",
-            "Atmospheric",
-            "Gore",
-            "Alternate History",
-            "Dystopian ",
-            "Immersive",
-            "Time Travel",
-            "Steampunk",
-            "Linear",
-            "Combat",
-            "Dark"
+            "Fantasy",
+            "Magic",
+            "Female Protagonist",
+            "Music",
+            "Arcade",
+            "Flight",
+            "Faith"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IAS",
-            "german": "IAS",
-            "italian": "IAS",
-            "japanese": "IAS",
-            "spanish": "IAS",
-            "brazilian": "IS",
-            "koreana": "IS",
-            "polish": "IS",
-            "russian": "IS",
-            "tchinese": "IS"
+            "schinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-01-29'
+var gameOfDayDate = '2026-01-30'
