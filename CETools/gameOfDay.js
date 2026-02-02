355433225fd39a0d836c90d70c0c419e5b096cda
Icondor <ivanfigus@gmail.com>
2026-02-02 08:50:09 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,74 +2,78 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 720150,
-        "CEId": "29c0f245-0552-4aa5-84d8-280085ac69e1",
-        "name": "Shikhondo(\u98df\u9b42\u5f92) - Soul Eater",
-        "header": "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/720150/header.jpg?t=1731979117",
-        "genre": "Bullet Hell",
-        "tier": 2,
-        "points": 30,
-        "createdAt": "2025-02-01T04:39:20.000Z",
+        "platformId": 1178790,
+        "CEId": "d5fa9983-ebaa-4faf-8c5a-f537c15c0f77",
+        "name": "Shiren the Wanderer: The Tower of Fortune and the Dice of Fate | Shiren 5",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1178790/header.jpg?t=1635759919",
+        "genre": "Strategy",
+        "tier": 5,
+        "points": 260,
+        "createdAt": "2023-04-12T14:35:52.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 2,
-        "playersTotal": 21,
+        "playersCompleted": 8,
+        "playersStarted": 21,
+        "playersTotal": 171,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 719,
-                "final": 719,
+                "initial": 1799,
+                "final": 1799,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 101000,
-                "final": 101000,
+                "initial": 298000,
+                "final": 298000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 3000,
+                "final": 3000,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1149,
-                "final": 1149,
+                "initial": 2519,
+                "final": 2519,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Raising Hell [25\u2605]\nComplete Arcade Mode on Extreme without using continues.\n\nSoul Beater [5\u2605]\nComplete Arcade Mode on Normal or higher without using continues.",
+            "primary": 4,
+            "primaryText": "Fortunate Nomad [100\u2605]\nComplete Storm Forest and Rousing Paradise.\n\nWayfaring Wanderer [80\u2605]\nComplete Double Strike Trail, Perilous Rocks, Onigiri Hollow, and Master's Footprint.\n\nDestined Descent [50\u2605]\nComplete Destiny's Descent.\n\nMamel Savior[30\u2605 (Partial: 10\u2605)]\nComplete Primordial Chasm. *Partial points are awarded as a progress milestone.",
             "community": 1,
-            "communityText": "Reaper Feared by All\nComplete Hardcore Mode on Extreme without using continues.",
-            "achievements": 7
+            "communityText": "Countdown to Doomsday\nComplete Cloister of Certain Doom with the turn limit set to 10000.",
+            "achievements": 55
         },
         "gameTags": [
-            "Indie",
-            "Action",
-            "Shoot 'Em Up",
-            "Bullet Hell",
-            "Casual",
-            "Shooter",
+            "Mystery Dungeon",
+            "Roguelike",
+            "RPG",
+            "Dungeon Crawler",
+            "Pixel Graphics",
+            "Traditional Roguelike",
+            "2D",
+            "Turn-Based",
             "Anime",
-            "Female Protagonist",
-            "Arcade",
+            "JRPG",
+            "Difficult",
             "Singleplayer"
         ],
         "languages": {
-            "english": "IS",
-            "japanese": "S",
-            "schinese": "S"
-        }
+            "english": "I",
+            "japanese": "I",
+            "schinese": "I",
+            "tchinese": "I"
+        },
+        "information": "Curated on Steam: [https://store.steampowered.com/app/1178790/Shiren_the_Wanderer_The_Tower_of_Fortune_and_the_Dice_of_Fate/?curator_clanid=36185934](https://store.steampowered.com/app/1178790/Shiren_the_Wanderer_The_Tower_of_Fortune_and_the_Dice_of_Fate/?curator_clanid=36185934)"
     }
 }
-var gameOfDayDate = '2026-02-01'
+var gameOfDayDate = '2026-02-02'
