86b6123d3e257450420536c4a96d15de5ab6566e
Icondor <ivanfigus@gmail.com>
2026-02-03 08:47:00 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,78 +2,73 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1178790,
-        "CEId": "d5fa9983-ebaa-4faf-8c5a-f537c15c0f77",
-        "name": "Shiren the Wanderer: The Tower of Fortune and the Dice of Fate | Shiren 5",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1178790/header.jpg?t=1635759919",
-        "genre": "Strategy",
-        "tier": 5,
-        "points": 260,
-        "createdAt": "2023-04-12T14:35:52.000Z",
+        "platformId": 566320,
+        "CEId": "0f130d6a-d955-41b9-85ad-bb9fb26316cb",
+        "name": "An Octonaut Odyssey",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/566320/header.jpg?t=1488559633",
+        "genre": "Platformer",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2023-02-03T04:21:18.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 8,
-        "playersStarted": 21,
-        "playersTotal": 171,
+        "playersCompleted": 3,
+        "playersStarted": 0,
+        "playersTotal": 21,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 99,
+                "final": 99,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 79,
+                "final": 79,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1799,
-                "final": 1799,
+                "initial": 79,
+                "final": 79,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 298000,
-                "final": 298000,
+                "initial": 10000,
+                "final": 10000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 3000,
-                "final": 3000,
+                "initial": 150,
+                "final": 150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2519,
-                "final": 2519,
+                "initial": 119,
+                "final": 119,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Fortunate Nomad [100\u2605]\nComplete Storm Forest and Rousing Paradise.\n\nWayfaring Wanderer [80\u2605]\nComplete Double Strike Trail, Perilous Rocks, Onigiri Hollow, and Master's Footprint.\n\nDestined Descent [50\u2605]\nComplete Destiny's Descent.\n\nMamel Savior[30\u2605 (Partial: 10\u2605)]\nComplete Primordial Chasm. *Partial points are awarded as a progress milestone.",
-            "community": 1,
-            "communityText": "Countdown to Doomsday\nComplete Cloister of Certain Doom with the turn limit set to 10000.",
-            "achievements": 55
+            "primary": 2,
+            "primaryText": "Octopus Prime [10\u2605]\nComplete the game without dying.\n\nI'm Trippin' Balls [5\u2605]\nComplete Impossible Mode.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 14
         },
         "gameTags": [
-            "Mystery Dungeon",
-            "Roguelike",
-            "RPG",
-            "Dungeon Crawler",
-            "Pixel Graphics",
-            "Traditional Roguelike",
+            "Indie",
+            "Casual",
+            "Adventure",
+            "Platformer",
             "2D",
-            "Turn-Based",
-            "Anime",
-            "JRPG",
-            "Difficult",
-            "Singleplayer"
+            "Sci-fi",
+            "Psychedelic",
+            "Retro",
+            "Singleplayer",
+            "Puzzle Platformer"
         ],
         "languages": {
-            "english": "I",
-            "japanese": "I",
-            "schinese": "I",
-            "tchinese": "I"
-        },
-        "information": "Curated on Steam: [https://store.steampowered.com/app/1178790/Shiren_the_Wanderer_The_Tower_of_Fortune_and_the_Dice_of_Fate/?curator_clanid=36185934](https://store.steampowered.com/app/1178790/Shiren_the_Wanderer_The_Tower_of_Fortune_and_the_Dice_of_Fate/?curator_clanid=36185934)"
+            "english": "IAS",
+            "french": "IAS"
+        }
     }
 }
-var gameOfDayDate = '2026-02-02'
+var gameOfDayDate = '2026-02-03'
