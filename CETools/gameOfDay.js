844ec9258f199064d3e3b0f30f87f2ab39f5dda1
Icondor <ivanfigus@gmail.com>
2026-02-04 08:47:09 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,73 +2,77 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 566320,
-        "CEId": "0f130d6a-d955-41b9-85ad-bb9fb26316cb",
-        "name": "An Octonaut Odyssey",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/566320/header.jpg?t=1488559633",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 15,
-        "createdAt": "2023-02-03T04:21:18.000Z",
+        "platformId": 1037590,
+        "CEId": "aea1e6f9-a0fc-40e4-beb6-d962e65538b7",
+        "name": "Neptunia Shooter",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1037590/header.jpg?t=1614907670",
+        "genre": "Bullet Hell",
+        "tier": 2,
+        "points": 20,
+        "createdAt": "2022-02-04T20:29:18.000Z",
         "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 3,
+        "playersCompleted": 18,
         "playersStarted": 0,
-        "playersTotal": 21,
+        "playersTotal": 144,
         "priceData": {
             "USD": {
-                "initial": 99,
-                "final": 99,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 79,
-                "final": 79,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 79,
-                "final": 79,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 10000,
-                "final": 10000,
+                "initial": 52000,
+                "final": 52000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 150,
-                "final": 150,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 119,
-                "final": 119,
+                "initial": 569,
+                "final": 569,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Octopus Prime [10\u2605]\nComplete the game without dying.\n\nI'm Trippin' Balls [5\u2605]\nComplete Impossible Mode.",
+            "primary": 1,
+            "primaryText": "Neptunia Overlord [20\u2605]\nClear all stages with no damage.",
             "community": 0,
             "communityText": "",
-            "achievements": 14
+            "achievements": 13
         },
         "gameTags": [
-            "Indie",
-            "Casual",
-            "Adventure",
-            "Platformer",
-            "2D",
-            "Sci-fi",
-            "Psychedelic",
+            "Bullet Hell",
+            "Action",
+            "Shoot 'Em Up",
+            "Pixel Graphics",
+            "Anime",
+            "Cute",
             "Retro",
-            "Singleplayer",
-            "Puzzle Platformer"
+            "Female Protagonist",
+            "2D",
+            "Arcade",
+            "Shooter",
+            "Side Scroller",
+            "Difficult",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IAS"
+            "english": "IS",
+            "japanese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-02-03'
+var gameOfDayDate = '2026-02-04'
