b755cb0d7029092af745597a3f1bb2c0c17ba183
Icondor <ivanfigus@gmail.com>
2026-02-05 08:46:19 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,77 +2,86 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1037590,
-        "CEId": "aea1e6f9-a0fc-40e4-beb6-d962e65538b7",
-        "name": "Neptunia Shooter",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1037590/header.jpg?t=1614907670",
-        "genre": "Bullet Hell",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2022-02-04T20:29:18.000Z",
-        "updatedAt": "2025-09-08T23:40:11.000Z",
-        "playersCompleted": 18,
+        "platformId": 1488650,
+        "CEId": "451ec9df-8f4f-4be6-aa4d-ff1d50b6c829",
+        "name": "Velocity Noodle",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1488650/header.jpg?t=1644670741",
+        "genre": "Platformer",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2023-02-05T04:46:36.000Z",
+        "updatedAt": "2026-02-05T02:55:50.000Z",
+        "playersCompleted": 12,
         "playersStarted": 0,
-        "playersTotal": 144,
+        "playersTotal": 68,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 399,
-                "final": 399,
+                "initial": 1279,
+                "final": 1279,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 399,
-                "final": 399,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 52000,
-                "final": 52000,
+                "initial": 150000,
+                "final": 150000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 1895,
+                "final": 1895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 569,
-                "final": 569,
+                "initial": 1699,
+                "final": 1699,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Neptunia Overlord [20\u2605]\nClear all stages with no damage.",
+            "primaryText": "Ichiraku Allstar [10\u2605]\nBeat all gold medal times (including both hard level chapters) and get hitless Noodle Hearts on all hard levels.",
             "community": 0,
             "communityText": "",
-            "achievements": 13
+            "achievements": 14
         },
         "gameTags": [
-            "Bullet Hell",
-            "Action",
-            "Shoot 'Em Up",
-            "Pixel Graphics",
-            "Anime",
-            "Cute",
-            "Retro",
-            "Female Protagonist",
-            "2D",
-            "Arcade",
-            "Shooter",
-            "Side Scroller",
+            "Precision Platformer",
+            "Platformer",
             "Difficult",
-            "Singleplayer"
+            "Indie",
+            "Pixel Graphics",
+            "Side Scroller",
+            "2D Platformer",
+            "2D",
+            "Singleplayer",
+            "Adventure",
+            "Action",
+            "Retro",
+            "Linear",
+            "Cyberpunk",
+            "Score Attack",
+            "Atmospheric",
+            "Futuristic",
+            "Parkour",
+            "Swordplay",
+            "Female Protagonist"
         ],
         "languages": {
-            "english": "IS",
-            "japanese": "IS"
+            "english": "IAS",
+            "japanese": "IAS",
+            "brazilian": "IAS",
+            "latam": "IAS",
+            "schinese": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-02-04'
+var gameOfDayDate = '2026-02-05'
