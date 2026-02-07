4b3609f20c84dfb0a4cf33fc3d6dc5d79d72fcb0
Icondor <ivanfigus@gmail.com>
2026-02-07 08:47:10 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,52 +1,91 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 2,
-        "platformId": 463700,
-        "CEId": "f953f6bc-7d67-47f5-94d6-e7302da0fe1d",
-        "name": "Super Cloudbuilt",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/463700/header.jpg?t=1530000096",
-        "genre": "Platformer",
-        "tier": 4,
-        "points": 190,
-        "createdAt": "2020-02-06T03:13:43.000Z",
+        "gameType": 0,
+        "platformId": 1640320,
+        "CEId": "19d7538f-4447-40f4-a252-4bea822afdc0",
+        "name": "B.I.O.T.A.",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1640320/header.jpg?t=1706095104",
+        "genre": "Action",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2024-02-07T14:18:47.000Z",
         "updatedAt": "2026-02-05T02:55:50.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 7,
-        "playersTotal": 145,
+        "playersCompleted": 3,
+        "playersStarted": 0,
+        "playersTotal": 22,
+        "priceData": {
+            "USD": {
+                "initial": 999,
+                "final": 999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 899,
+                "final": 899,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 749,
+                "final": 749,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 99000,
+                "final": 99000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 1429,
+                "final": 1429,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1149,
+                "final": 1149,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Immortal [60\u2605]\nBeat the Full Game + Defiance Rush Mode without dying or restarting.\n\nGodspeed [110\u2605]\nObtain an S rank in all modes for all levels.\n\nBeyond The Clouds [20\u2605]\nProve yourself.",
+            "primary": 1,
+            "primaryText": "Raider [5\u2605]\nComplete Time Attack without dying.",
             "community": 0,
             "communityText": "",
-            "achievements": 38
+            "achievements": 24
         },
         "gameTags": [
-            "Action",
-            "Indie",
-            "Parkour",
-            "3D Platformer",
-            "Female Protagonist",
-            "Difficult",
-            "Fast-Paced",
-            "Great Soundtrack",
-            "Platformer"
+            "2D Platformer",
+            "Aliens",
+            "Retro",
+            "Shoot 'Em Up",
+            "Side Scroller",
+            "Metroidvania",
+            "Singleplayer",
+            "Pixel Graphics",
+            "Arcade",
+            "Platformer",
+            "1980s",
+            "Colorful",
+            "Mechs",
+            "Old School",
+            "Casual",
+            "Sci-fi",
+            "Combat",
+            "Precision Platformer",
+            "2D",
+            "Action"
         ],
         "languages": {
             "english": "IAS",
             "french": "IS",
             "german": "IS",
             "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
             "brazilian": "IS",
-            "dutch": "IS",
+            "latam": "IS",
+            "polish": "IS",
             "russian": "IS",
-            "schinese": "IS",
-            "swedish": "IS",
-            "tchinese": "IS"
-        },
-        "information": "Delisted from Steam."
+            "schinese": "IS"
+        }
     }
 }
-var gameOfDayDate = '2026-02-06'
+var gameOfDayDate = '2026-02-07'
