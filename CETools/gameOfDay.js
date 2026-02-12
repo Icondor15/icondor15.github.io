d11e2b8227712e2d90a2e23e51d1572befbed85f
Icondor <ivanfigus@gmail.com>
2026-02-12 08:44:06 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,69 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1225130,
-        "CEId": "41de5d7a-ceb7-45d8-87f5-29578dcad454",
-        "name": "The Adventures of Ten and Till",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1225130/header.jpg?t=1588823535",
-        "genre": "Bullet Hell",
+        "platformId": 1822710,
+        "CEId": "25903625-113c-46b0-a2ec-ef867b91cac9",
+        "name": "Automon",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1822710/header.jpg?t=1677757864",
+        "genre": "Strategy",
         "tier": 1,
         "points": 5,
-        "createdAt": "2023-02-11T05:52:08.000Z",
+        "createdAt": "2024-02-12T13:50:29.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 1,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 15,
+        "playersTotal": 14,
         "priceData": {
             "USD": {
-                "initial": 1199,
-                "final": 1199,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 659,
+                "final": 659,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 929,
-                "final": 929,
+                "initial": 579,
+                "final": 579,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 122000,
-                "final": 122000,
+                "initial": 82000,
+                "final": 82000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1695,
-                "final": 1695,
+                "initial": 1150,
+                "final": 1150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1349,
-                "final": 1349,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Waves of Devolution [5\u2605]\nGet the true ending.",
+            "primaryText": "Who's That Automon? [5\u2605]\nClear difficulty 10.",
             "community": 0,
             "communityText": "",
-            "achievements": 0
+            "achievements": 56
         },
         "gameTags": [
-            "Action",
-            "Shoot 'Em Up",
-            "Bullet Hell",
-            "Pixel Graphics",
+            "Strategy",
+            "Casual",
+            "Auto Battler",
+            "Roguelite",
             "2D",
-            "Arcade",
-            "Great Soundtrack"
+            "Cartoony",
+            "Colorful",
+            "Pixel Graphics",
+            "Comedy",
+            "Fantasy",
+            "Choices Matter",
+            "Combat",
+            "PvE",
+            "Singleplayer",
+            "Roguelike",
+            "Cartoon",
+            "Dark Humor",
+            "Funny",
+            "Cute",
+            "Creature Collector"
         ],
         "languages": {
-            "english": "IS"
+            "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-02-11'
+var gameOfDayDate = '2026-02-12'
