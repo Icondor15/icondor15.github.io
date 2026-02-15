14eddb52663d508ce9a1406a0cd38602515af17c
Icondor <ivanfigus@gmail.com>
2026-02-15 10:15:16 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,78 +2,89 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 435480,
-        "CEId": "dc95f7fb-c828-4b7b-89a8-f960b0c3f14d",
-        "name": "Trials of the Blood Dragon",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/435480/header.jpg?t=1507630460",
-        "genre": "Arcade",
-        "tier": 2,
-        "points": 20,
-        "createdAt": "2022-02-14T06:48:00.000Z",
+        "platformId": 1000750,
+        "CEId": "6cdc911e-4860-4c33-9ffb-04ca2289141b",
+        "name": "Rotund Rebound",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1000750/header.jpg?t=1648829248",
+        "genre": "Platformer",
+        "tier": 4,
+        "points": 100,
+        "createdAt": "2023-02-15T02:44:28.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 5,
+        "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 50,
+        "playersTotal": 28,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 1679,
+                "final": 1679,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 1549,
+                "final": 1549,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 198000,
-                "final": 198000,
+                "initial": 205000,
+                "final": 205000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2295,
-                "final": 2295,
+                "initial": 2895,
+                "final": 2895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 2279,
+                "final": 2279,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Scenic Route [10\u2605]\nObtain an A+ grade on all main story levels.\n\nVirtual Bootcamp Boss [10\u2605]\nObtain an A+ grade on all bonus levels.",
+            "primaryText": "Supreme Victory! [75\u2605]\nGet all 200 Elite Flags.\n\nLeaf on the Wind [25\u2605]\nGet all 200 Gold medals.",
             "community": 0,
             "communityText": "",
-            "achievements": 20
+            "achievements": 50
         },
         "gameTags": [
-            "Racing",
-            "Retro",
+            "Precision Platformer",
             "Platformer",
-            "1980s",
-            "Great Soundtrack",
-            "Cyberpunk",
-            "Action"
+            "Difficult",
+            "2D Platformer",
+            "Retro",
+            "Pixel Graphics",
+            "Adventure",
+            "2D",
+            "Colorful",
+            "Puzzle Platformer",
+            "Action",
+            "Time Attack",
+            "Side Scroller",
+            "Singleplayer",
+            "Surreal",
+            "Fast-Paced",
+            "Funny",
+            "Cute",
+            "Indie",
+            "Great Soundtrack"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "dutch": "IS",
-            "russian": "IS",
-            "schinese": "IS"
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "russian": "I"
         }
     }
 }
-var gameOfDayDate = '2026-02-14'
+var gameOfDayDate = '2026-02-15'
