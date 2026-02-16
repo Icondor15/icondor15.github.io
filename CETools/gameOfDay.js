6b003fc79c3d0c78ecbf35617491392ac0f6c09f
Icondor <ivanfigus@gmail.com>
2026-02-16 08:50:20 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,89 +2,83 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1000750,
-        "CEId": "6cdc911e-4860-4c33-9ffb-04ca2289141b",
-        "name": "Rotund Rebound",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1000750/header.jpg?t=1648829248",
-        "genre": "Platformer",
-        "tier": 4,
-        "points": 100,
-        "createdAt": "2023-02-15T02:44:28.000Z",
+        "platformId": 392030,
+        "CEId": "49138677-18a3-4238-9c44-f8bd35503fe4",
+        "name": "Murasaki",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/392030/header.jpg?t=1578972607",
+        "genre": "Bullet Hell",
+        "tier": 3,
+        "points": 50,
+        "createdAt": "2020-02-16T20:58:15.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 28,
+        "playersCompleted": 8,
+        "playersStarted": 4,
+        "playersTotal": 111,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1679,
-                "final": 1679,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1549,
-                "final": 1549,
+                "initial": 399,
+                "final": 399,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 205000,
-                "final": 205000,
+                "initial": 49800,
+                "final": 49800,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2895,
-                "final": 2895,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2279,
-                "final": 2279,
+                "initial": 549,
+                "final": 549,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Supreme Victory! [75\u2605]\nGet all 200 Elite Flags.\n\nLeaf on the Wind [25\u2605]\nGet all 200 Gold medals.",
+            "primaryText": "Lethal Concert [20\u2605]\nBeat all unprotected stages.\n\nRinko's Demise [30\u2605]\nWin all duels with Kairi and Satsuki and earn 3 million points in stage 6.",
             "community": 0,
             "communityText": "",
-            "achievements": 50
+            "achievements": 48
         },
         "gameTags": [
-            "Precision Platformer",
-            "Platformer",
-            "Difficult",
-            "2D Platformer",
-            "Retro",
-            "Pixel Graphics",
-            "Adventure",
-            "2D",
-            "Colorful",
-            "Puzzle Platformer",
             "Action",
-            "Time Attack",
-            "Side Scroller",
-            "Singleplayer",
-            "Surreal",
-            "Fast-Paced",
-            "Funny",
-            "Cute",
             "Indie",
-            "Great Soundtrack"
+            "Bullet Hell",
+            "Shoot 'Em Up",
+            "Anime",
+            "Puzzle",
+            "Great Soundtrack",
+            "Singleplayer",
+            "Arcade",
+            "2D",
+            "Casual",
+            "Female Protagonist",
+            "Fantasy",
+            "Difficult",
+            "Controller",
+            "Experimental",
+            "Retro",
+            "Atmospheric",
+            "Colorful",
+            "Cute"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "russian": "I"
+            "english": "IS",
+            "japanese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-02-15'
+var gameOfDayDate = '2026-02-16'
