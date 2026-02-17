2b2afe1c2af8c36f9d7dc5bc9384d697b494cb40
Icondor <ivanfigus@gmail.com>
2026-02-17 08:46:17 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,83 +2,93 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 392030,
-        "CEId": "49138677-18a3-4238-9c44-f8bd35503fe4",
-        "name": "Murasaki",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/392030/header.jpg?t=1578972607",
-        "genre": "Bullet Hell",
-        "tier": 3,
-        "points": 50,
-        "createdAt": "2020-02-16T20:58:15.000Z",
+        "platformId": 597170,
+        "CEId": "8fa87bf9-55df-48bd-8412-f9cb9094f7ec",
+        "name": "Clone Drone in the Danger Zone",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/597170/3c7ef2315d8bfac6f65284da1ac63ea313530abe/header_alt_assets_1.jpg?t=1765822442",
+        "genre": "Action",
+        "tier": 2,
+        "points": 25,
+        "createdAt": "2023-02-17T17:05:48.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 8,
+        "playersCompleted": 14,
         "playersStarted": 4,
-        "playersTotal": 111,
+        "playersTotal": 135,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1679,
+                "final": 1679,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 399,
-                "final": 399,
+                "initial": 1549,
+                "final": 1549,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 49800,
-                "final": 49800,
+                "initial": 205000,
+                "final": 205000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 2895,
+                "final": 2895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 549,
-                "final": 549,
+                "initial": 2279,
+                "final": 2279,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Lethal Concert [20\u2605]\nBeat all unprotected stages.\n\nRinko's Demise [30\u2605]\nWin all duels with Kairi and Satsuki and earn 3 million points in stage 6.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 48
+            "primaryText": "This Human is a Beast [15\u2605]\nComplete all solo challenges. / Reach Insanium in Endless Mode.\n\nClone Drone in the Danger Arena [10\u2605]\nComplete Story Mode on \"Insane\" difficulty.",
+            "community": 1,
+            "communityText": "Danger Clone in the Drone Arena\nComplete all Co-op Challenges. / Reach Insanium in a public co-op match.",
+            "achievements": 63
         },
         "gameTags": [
+            "Robots",
             "Action",
+            "3D Fighter",
+            "Swordplay",
+            "Multiplayer",
+            "Fighting",
             "Indie",
-            "Bullet Hell",
-            "Shoot 'Em Up",
-            "Anime",
-            "Puzzle",
-            "Great Soundtrack",
+            "Funny",
+            "Voxel",
+            "Co-op",
+            "Roguelike",
             "Singleplayer",
-            "Arcade",
-            "2D",
-            "Casual",
-            "Female Protagonist",
-            "Fantasy",
+            "Online Co-Op",
+            "Third Person",
+            "Pixel Graphics",
             "Difficult",
-            "Controller",
-            "Experimental",
-            "Retro",
-            "Atmospheric",
-            "Colorful",
-            "Cute"
+            "Futuristic",
+            "Comedy",
+            "Survival",
+            "Dark Humor"
         ],
         "languages": {
-            "english": "IS",
-            "japanese": "IS"
+            "english": "IAS",
+            "french": "IAS",
+            "german": "IAS",
+            "italian": "IS",
+            "japanese": "IAS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "koreana": "IS",
+            "latam": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-02-16'
+var gameOfDayDate = '2026-02-17'
