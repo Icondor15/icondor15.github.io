1f44d62765b837130a85b574513fca3ad308136e
Icondor <ivanfigus@gmail.com>
2026-02-18 08:46:50 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,94 +1,46 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 597170,
-        "CEId": "8fa87bf9-55df-48bd-8412-f9cb9094f7ec",
-        "name": "Clone Drone in the Danger Zone",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/597170/3c7ef2315d8bfac6f65284da1ac63ea313530abe/header_alt_assets_1.jpg?t=1765822442",
-        "genre": "Action",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2023-02-17T17:05:48.000Z",
+        "gameType": 1,
+        "platformId": 626680,
+        "CEId": "37e2dea6-b6ef-4799-86b0-a3906c17eb85",
+        "name": "Kreedz Climbing",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/626680/header.jpg?t=1581904648",
+        "genre": "First-Person",
+        "tier": 7,
+        "points": 1000,
+        "createdAt": "2022-02-18T00:44:55.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 14,
-        "playersStarted": 4,
-        "playersTotal": 135,
-        "priceData": {
-            "USD": {
-                "initial": 1999,
-                "final": 1999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 1679,
-                "final": 1679,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 1549,
-                "final": 1549,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 205000,
-                "final": 205000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 2895,
-                "final": 2895,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 2279,
-                "final": 2279,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 1,
+        "playersStarted": 60,
+        "playersTotal": 405,
         "milestones": {
-            "primary": 2,
-            "primaryText": "This Human is a Beast [15\u2605]\nComplete all solo challenges. / Reach Insanium in Endless Mode.\n\nClone Drone in the Danger Arena [10\u2605]\nComplete Story Mode on \"Insane\" difficulty.",
-            "community": 1,
-            "communityText": "Danger Clone in the Drone Arena\nComplete all Co-op Challenges. / Reach Insanium in a public co-op match.",
-            "achievements": 63
+            "primary": 10,
+            "primaryText": "Kreedz Climber [280\u2605]\nGet the zero checkpoint medal on the hardest Kreedz maps.\n\nMovement Overclocker [120\u2605]\nGet the zero checkpoint medal on a subset of hard Kreedz maps.\n\nLongjumper [80\u2605]\nComplete all Longjump maps.\n\nSpring in your Step [150\u2605]\nComplete all advanced KZ and BHOP maps. Check proof requirements for more information.\n\nMovement Perfectionist [130\u2605]\nBeat \"kz_afterdark\" and the hard course on \"kz_cloudwalker\" separately without using checkpoints.\n\nMovement Amateur [100\u2605]\nObtain all Climbing map achievements except \"Absolute Lunacy!\" and \"Nocturnal Conquerer\".\n\nShred the Gnar [80\u2605]\nComplete all advanced Surf maps.\n\nBoarding School[45\u2605 (Partial: 10\u2605)]\nObtain all Surf map achievements.\n\nPowerjump Pumper [10\u2605]\nComplete \"kz_powerjump\" and \"kz_powerjump[truthseeker]\".\n\nBungee Boss [5\u2605]\nObtain all Bungee map achievements.",
+            "community": 4,
+            "communityText": "Fleet-Footed\nClear \"kz_bhop_dydbhop\" in Normal Mode.\n\nLess is More\nClear \"kz_bhop_minimalism\" without the use of checkpoints.\n\nNear Death Experience\nClear \"kz_bhop_angkor[crypt]\" and \"kz_bhop_rainbowbhop\" with AWP.\n\nThe Silver Surfer\nClear \"kz_surf_lt_omnific\".",
+            "achievements": 40
         },
         "gameTags": [
-            "Robots",
-            "Action",
-            "3D Fighter",
-            "Swordplay",
-            "Multiplayer",
-            "Fighting",
+            "Free to Play",
+            "Racing",
             "Indie",
-            "Funny",
-            "Voxel",
-            "Co-op",
-            "Roguelike",
-            "Singleplayer",
-            "Online Co-Op",
-            "Third Person",
-            "Pixel Graphics",
-            "Difficult",
-            "Futuristic",
-            "Comedy",
-            "Survival",
-            "Dark Humor"
+            "Parkour",
+            "Sports",
+            "Multiplayer",
+            "FPS",
+            "First-Person",
+            "Action",
+            "3D Platformer",
+            "Singleplayer"
         ],
         "languages": {
             "english": "IAS",
-            "french": "IAS",
-            "german": "IAS",
-            "italian": "IS",
-            "japanese": "IAS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "tchinese": "IS"
-        }
+            "dutch": "I",
+            "russian": "I",
+            "schinese": "I"
+        },
+        "information": "All relevant Kreedz information can be found here: [Link](https://docs.google.com/document/d/1h_Pq00T5nlf8aFABE1ln-HtGcewx-zOgMBEcyZRLEto)"
     }
 }
-var gameOfDayDate = '2026-02-17'
+var gameOfDayDate = '2026-02-18'
