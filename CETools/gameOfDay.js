d9eaf41a9c26c5313a73c2b316d3a7fe4675bfac
Icondor <ivanfigus@gmail.com>
2026-02-19 08:47:28 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,46 +1,95 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 626680,
-        "CEId": "37e2dea6-b6ef-4799-86b0-a3906c17eb85",
-        "name": "Kreedz Climbing",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/626680/header.jpg?t=1581904648",
-        "genre": "First-Person",
-        "tier": 7,
-        "points": 1000,
-        "createdAt": "2022-02-18T00:44:55.000Z",
+        "gameType": 0,
+        "platformId": 957960,
+        "CEId": "f17c151a-241e-4e47-a228-0197849a996d",
+        "name": "Inkulinati",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/957960/header.jpg?t=1736356670",
+        "genre": "Strategy",
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2025-02-19T18:06:21.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 60,
-        "playersTotal": 405,
+        "playersCompleted": 2,
+        "playersStarted": 0,
+        "playersTotal": 107,
+        "priceData": {
+            "USD": {
+                "initial": 2499,
+                "final": 2499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 2499,
+                "final": 2499,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 2199,
+                "final": 2199,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 360000,
+                "final": 360000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 3795,
+                "final": 3795,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 3349,
+                "final": 3349,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 10,
-            "primaryText": "Kreedz Climber [280\u2605]\nGet the zero checkpoint medal on the hardest Kreedz maps.\n\nMovement Overclocker [120\u2605]\nGet the zero checkpoint medal on a subset of hard Kreedz maps.\n\nLongjumper [80\u2605]\nComplete all Longjump maps.\n\nSpring in your Step [150\u2605]\nComplete all advanced KZ and BHOP maps. Check proof requirements for more information.\n\nMovement Perfectionist [130\u2605]\nBeat \"kz_afterdark\" and the hard course on \"kz_cloudwalker\" separately without using checkpoints.\n\nMovement Amateur [100\u2605]\nObtain all Climbing map achievements except \"Absolute Lunacy!\" and \"Nocturnal Conquerer\".\n\nShred the Gnar [80\u2605]\nComplete all advanced Surf maps.\n\nBoarding School[45\u2605 (Partial: 10\u2605)]\nObtain all Surf map achievements.\n\nPowerjump Pumper [10\u2605]\nComplete \"kz_powerjump\" and \"kz_powerjump[truthseeker]\".\n\nBungee Boss [5\u2605]\nObtain all Bungee map achievements.",
-            "community": 4,
-            "communityText": "Fleet-Footed\nClear \"kz_bhop_dydbhop\" in Normal Mode.\n\nLess is More\nClear \"kz_bhop_minimalism\" without the use of checkpoints.\n\nNear Death Experience\nClear \"kz_bhop_angkor[crypt]\" and \"kz_bhop_rainbowbhop\" with AWP.\n\nThe Silver Surfer\nClear \"kz_surf_lt_omnific\".",
-            "achievements": 40
+            "primary": 1,
+            "primaryText": "Fraud of the Millenium [10\u2605]\nDefeat Death on Master Mode.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 26
         },
         "gameTags": [
-            "Free to Play",
-            "Racing",
-            "Indie",
-            "Parkour",
-            "Sports",
+            "Medieval",
+            "Turn-Based Strategy",
+            "Turn-Based Tactics",
+            "Turn-Based",
+            "Roguelite",
+            "Funny",
+            "Deckbuilding",
+            "Dark Humor",
+            "Hand-drawn",
+            "PvP",
+            "2D",
+            "Stylized",
+            "PvE",
+            "Strategy RPG",
+            "Historical",
+            "Turn-Based Combat",
+            "Strategy",
             "Multiplayer",
-            "FPS",
-            "First-Person",
-            "Action",
-            "3D Platformer",
-            "Singleplayer"
+            "Singleplayer",
+            "Local Multiplayer"
         ],
         "languages": {
             "english": "IAS",
-            "dutch": "I",
-            "russian": "I",
-            "schinese": "I"
-        },
-        "information": "All relevant Kreedz information can be found here: [Link](https://docs.google.com/document/d/1h_Pq00T5nlf8aFABE1ln-HtGcewx-zOgMBEcyZRLEto)"
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "koreana": "IS",
+            "latam": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
+        }
     }
 }
-var gameOfDayDate = '2026-02-18'
+var gameOfDayDate = '2026-02-19'
