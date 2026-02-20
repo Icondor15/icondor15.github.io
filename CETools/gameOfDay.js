ef11c1a237bdd00711960f0347d714d673b55cca
Icondor <ivanfigus@gmail.com>
2026-02-20 08:36:15 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,95 +1,66 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 957960,
-        "CEId": "f17c151a-241e-4e47-a228-0197849a996d",
-        "name": "Inkulinati",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/957960/header.jpg?t=1736356670",
-        "genre": "Strategy",
-        "tier": 1,
-        "points": 10,
-        "createdAt": "2025-02-19T18:06:21.000Z",
+        "gameType": 1,
+        "platformId": 11020,
+        "CEId": "66647036-1771-4f51-a233-ba6abf5aef32",
+        "name": "TrackMania Nations Forever",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/11020/header.jpg?t=1548090419",
+        "genre": "Arcade",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2023-02-20T00:40:11.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 0,
-        "playersTotal": 107,
-        "priceData": {
-            "USD": {
-                "initial": 2499,
-                "final": 2499,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 2499,
-                "final": 2499,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 2199,
-                "final": 2199,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 360000,
-                "final": 360000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 3795,
-                "final": 3795,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 3349,
-                "final": 3349,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 19,
+        "playersStarted": 12,
+        "playersTotal": 576,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Fraud of the Millenium [10\u2605]\nDefeat Death on Master Mode.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 26
+            "primary": 2,
+            "primaryText": "The Definition of Insanity [15\u2605]\nObtain all E track Author Medals.\n\nStadium Stan [25\u2605]\nObtain the Author Medal on all A through D tracks.",
+            "community": 2,
+            "communityText": "Trial by Tire\n Achieve T\u00b3 Recruitment.\n\nTrack Attack\nComplete 10 Trials maps, with at least one being Advanced level (9-10).",
+            "achievements": 0
         },
         "gameTags": [
-            "Medieval",
-            "Turn-Based Strategy",
-            "Turn-Based Tactics",
-            "Turn-Based",
-            "Roguelite",
-            "Funny",
-            "Deckbuilding",
-            "Dark Humor",
-            "Hand-drawn",
-            "PvP",
-            "2D",
-            "Stylized",
-            "PvE",
-            "Strategy RPG",
-            "Historical",
-            "Turn-Based Combat",
-            "Strategy",
+            "Racing",
+            "Free to Play",
             "Multiplayer",
+            "Automobile Sim",
             "Singleplayer",
-            "Local Multiplayer"
+            "Level Editor",
+            "Arcade",
+            "Competitive",
+            "Simulation",
+            "Controller",
+            "Massively Multiplayer",
+            "Action",
+            "Sports",
+            "Difficult",
+            "Sandbox",
+            "Casual",
+            "Adventure",
+            "Co-op",
+            "First-Person",
+            "Indie"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "polish": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "tchinese": "IS"
-        }
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "czech": "I",
+            "dutch": "I",
+            "hungarian": "I",
+            "korean": "I",
+            "polish": "I",
+            "portuguese": "I",
+            "russian": "I",
+            "slovakian": "I",
+            "tchinese": "I"
+        },
+        "information": "How to fix white screen issues:\n[https://steamcommunity.com/sharedfiles/filedetails/?id=448953593](https://steamcommunity.com/sharedfiles/filedetails/?id=448953593)\n"
     }
 }
-var gameOfDayDate = '2026-02-19'
+var gameOfDayDate = '2026-02-20'
