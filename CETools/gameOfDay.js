11f12e62a55b46cdeb7a86c9b4fd0fcb1ebf5e17
Icondor <ivanfigus@gmail.com>
2026-02-21 08:46:49 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,66 +1,73 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 11020,
-        "CEId": "66647036-1771-4f51-a233-ba6abf5aef32",
-        "name": "TrackMania Nations Forever",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/11020/header.jpg?t=1548090419",
+        "gameType": 0,
+        "platformId": 1528880,
+        "CEId": "4a9f7e74-2e61-46e6-b788-66a42b212d86",
+        "name": "Time to GOLF",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1528880/header.jpg?t=1613660718",
         "genre": "Arcade",
-        "tier": 3,
-        "points": 40,
-        "createdAt": "2023-02-20T00:40:11.000Z",
+        "tier": 2,
+        "points": 30,
+        "createdAt": "2022-06-07T03:11:15.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 19,
-        "playersStarted": 12,
-        "playersTotal": 576,
+        "playersCompleted": 9,
+        "playersStarted": 0,
+        "playersTotal": 50,
+        "priceData": {
+            "USD": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 52000,
+                "final": 52000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 750,
+                "final": 750,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 569,
+                "final": 569,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 2,
-            "primaryText": "The Definition of Insanity [15\u2605]\nObtain all E track Author Medals.\n\nStadium Stan [25\u2605]\nObtain the Author Medal on all A through D tracks.",
-            "community": 2,
-            "communityText": "Trial by Tire\n Achieve T\u00b3 Recruitment.\n\nTrack Attack\nComplete 10 Trials maps, with at least one being Advanced level (9-10).",
-            "achievements": 0
+            "primary": 1,
+            "primaryText": "No Mulligan Necessary [30\u2605]\nClear in under 100 seconds.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 11
         },
         "gameTags": [
-            "Racing",
-            "Free to Play",
-            "Multiplayer",
-            "Automobile Sim",
-            "Singleplayer",
-            "Level Editor",
-            "Arcade",
-            "Competitive",
-            "Simulation",
-            "Controller",
-            "Massively Multiplayer",
             "Action",
-            "Sports",
-            "Difficult",
-            "Sandbox",
-            "Casual",
             "Adventure",
-            "Co-op",
-            "First-Person",
+            "Casual",
+            "3D",
+            "Singleplayer",
             "Indie"
         ],
         "languages": {
             "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
             "japanese": "I",
             "spanish": "I",
-            "czech": "I",
-            "dutch": "I",
-            "hungarian": "I",
-            "korean": "I",
-            "polish": "I",
-            "portuguese": "I",
-            "russian": "I",
-            "slovakian": "I",
-            "tchinese": "I"
-        },
-        "information": "How to fix white screen issues:\n[https://steamcommunity.com/sharedfiles/filedetails/?id=448953593](https://steamcommunity.com/sharedfiles/filedetails/?id=448953593)\n"
+            "koreana": "I",
+            "schinese": "I"
+        }
     }
 }
-var gameOfDayDate = '2026-02-20'
+var gameOfDayDate = '2026-02-21'
