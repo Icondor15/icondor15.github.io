8e8eb823f1f20225ae2506163c85a676a910443a
Icondor <ivanfigus@gmail.com>
2026-02-22 08:48:28 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,73 +1,50 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1528880,
-        "CEId": "4a9f7e74-2e61-46e6-b788-66a42b212d86",
-        "name": "Time to GOLF",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1528880/header.jpg?t=1613660718",
-        "genre": "Arcade",
+        "gameType": 1,
+        "platformId": 2501550,
+        "CEId": "d9d3c086-6020-4564-8aa0-85ff45996239",
+        "name": "Bullet Hell Monday: Finale",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2501550/header.jpg?t=1693793997",
+        "genre": "Bullet Hell",
         "tier": 2,
         "points": 30,
-        "createdAt": "2022-06-07T03:11:15.000Z",
+        "createdAt": "2025-02-22T18:50:20.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 9,
-        "playersStarted": 0,
-        "playersTotal": 50,
-        "priceData": {
-            "USD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 52000,
-                "final": 52000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 750,
-                "final": 750,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 569,
-                "final": 569,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 2,
+        "playersStarted": 2,
+        "playersTotal": 79,
         "milestones": {
-            "primary": 1,
-            "primaryText": "No Mulligan Necessary [30\u2605]\nClear in under 100 seconds.",
+            "primary": 2,
+            "primaryText": "Advance to Black [25\u2605]\nComplete all missions.\n\nHefty Weekend [5\u2605]\nPass the True Chapter.",
             "community": 0,
             "communityText": "",
-            "achievements": 11
+            "achievements": 16
         },
         "gameTags": [
+            "Shoot 'Em Up",
+            "Bullet Hell",
+            "Top-Down Shooter",
             "Action",
-            "Adventure",
-            "Casual",
-            "3D",
-            "Singleplayer",
-            "Indie"
+            "Tutorial",
+            "Difficult",
+            "Indie",
+            "Shooter",
+            "Arcade",
+            "2D",
+            "Top-Down",
+            "Controller",
+            "Free to Play",
+            "Score Attack",
+            "Abstract",
+            "Psychedelic",
+            "Singleplayer"
         ],
         "languages": {
             "english": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "koreana": "I",
-            "schinese": "I"
-        }
+            "japanese": "I"
+        },
+        "information": "[All missions](https://docs.google.com/spreadsheets/d/1BKIMgBRc1Ak6xEv8OfLXx2CvXZen8bpaCZzDacgd2ss/edit?usp=sharing)\n\nIt's not possible to beat all missions on your first run through.\nJust force your way through the chapters (beating \"X10\" levels gives lots of currency) and try again later when upgraded."
     }
 }
-var gameOfDayDate = '2026-02-21'
+var gameOfDayDate = '2026-02-22'
