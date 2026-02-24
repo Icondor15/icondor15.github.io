fcd9396cca8e5d22f0f074fbb6c9bc71dda43f8d
Icondor <ivanfigus@gmail.com>
2026-02-24 08:47:33 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,66 +2,67 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 625630,
-        "CEId": "ad2f21ab-aa82-4a84-8512-f9b78ed053c7",
-        "name": "Journey of Johann",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/625630/header.jpg?t=1495155420",
+        "platformId": 473720,
+        "CEId": "122a6d0e-1720-45c7-8683-291ea7757246",
+        "name": "Mutant Mudds Super Challenge",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/473720/header.jpg?t=1707193169",
         "genre": "Platformer",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2024-02-23T14:17:06.000Z",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2024-02-24T15:32:08.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 1,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 3,
+        "playersTotal": 25,
         "priceData": {
             "USD": {
-                "initial": 799,
-                "final": 799,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 799,
-                "final": 799,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 559,
-                "final": 559,
+                "initial": 699,
+                "final": 699,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 79800,
-                "final": 79800,
+                "initial": 98000,
+                "final": 98000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1150,
-                "final": 1150,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 879,
-                "final": 879,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Journey's End[25\u2605 (Partial: 15\u2605)]\nSeparately; Obtain all Gold Medals, have a sum of best under 30 minutes for all main levels, collect all Goblets and Secrets; complete all levels without getting hit.",
+            "primaryText": "Mud Cleaner [5\u2605]\nBeat the game in under 100 deaths.",
             "community": 0,
             "communityText": "",
-            "achievements": 42
+            "achievements": 13
         },
         "gameTags": [
             "Action",
             "Indie",
             "Adventure",
-            "Platformer"
+            "Platformer",
+            "2D"
         ],
         "languages": {
             "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-02-23'
+var gameOfDayDate = '2026-02-24'
