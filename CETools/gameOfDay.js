f96b4fd878ba76e22c91a64540402b0ea21ba227
Icondor <ivanfigus@gmail.com>
2026-02-25 08:47:14 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,68 +1,39 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 473720,
-        "CEId": "122a6d0e-1720-45c7-8683-291ea7757246",
-        "name": "Mutant Mudds Super Challenge",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/473720/header.jpg?t=1707193169",
-        "genre": "Platformer",
-        "tier": 1,
-        "points": 5,
-        "createdAt": "2024-02-24T15:32:08.000Z",
+        "gameType": 1,
+        "platformId": 2619210,
+        "CEId": "b6c466f3-8a57-47f5-b135-ac296a4d8b5f",
+        "name": "Sympathia",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2619210/header.jpg?t=1705330842",
+        "genre": "Arcade",
+        "tier": 4,
+        "points": 180,
+        "createdAt": "2024-02-25T03:47:19.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 4,
-        "playersStarted": 0,
-        "playersTotal": 25,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 699,
-                "final": 699,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 98000,
-                "final": 98000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1450,
-                "final": 1450,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1099,
-                "final": 1099,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 1,
+        "playersStarted": 7,
+        "playersTotal": 119,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Mud Cleaner [5\u2605]\nBeat the game in under 100 deaths.",
+            "primary": 5,
+            "primaryText": "Magicgazer [90\u2605]\nAchieve a Full Combo on all of the Severe difficulty stages.\n\nHarmonia [40\u2605]\nAchieve a Full Combo on all of the Mild difficulty stages.\n\nDenpandemic [20\u2605]\nAchieve a Full Combo on all of the VeryMild difficulty stages.\n\n\u4e07\u83ef\u7c60 [20\u2605]\nClear all of the Severe difficulty stages without healing.\n\nGroovy Sympathy [10\u2605]\nClear all of the Mild difficulty stages without healing.",
             "community": 0,
             "communityText": "",
-            "achievements": 13
+            "achievements": 47
         },
         "gameTags": [
             "Action",
-            "Indie",
-            "Adventure",
-            "Platformer",
-            "2D"
+            "Music",
+            "Rhythm",
+            "Bullet Hell",
+            "Pixel Graphics",
+            "Shooter",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "I"
-        }
+            "japanese": "I"
+        },
+        "information": "\"Stage Turk\" can be unlocked by entering a secret command after reaching the ending once. Go to the \"How to Play\" screen and  enter Up 7, Down 8, Up 1, Right 2, Tap 1, Right 2, Tap 1, Right 1, Left 3, Tap 1, Left 2, Tap 1, Left 1, Right 1, Tap 8."
     }
 }
-var gameOfDayDate = '2026-02-24'
+var gameOfDayDate = '2026-02-25'
