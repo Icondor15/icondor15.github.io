d52cd3140991b1617ac03e4f89bd8d3ec33b6c85
Icondor <ivanfigus@gmail.com>
2026-02-26 08:47:30 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,39 +1,85 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 2619210,
-        "CEId": "b6c466f3-8a57-47f5-b135-ac296a4d8b5f",
-        "name": "Sympathia",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2619210/header.jpg?t=1705330842",
-        "genre": "Arcade",
-        "tier": 4,
-        "points": 180,
-        "createdAt": "2024-02-25T03:47:19.000Z",
+        "gameType": 0,
+        "platformId": 721350,
+        "CEId": "da5df49d-bcd4-48f0-a662-f10be68185ea",
+        "name": "Where Are My Friends?",
+        "header": "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/721350/header.jpg?t=1736986576",
+        "genre": "Platformer",
+        "tier": 3,
+        "points": 40,
+        "createdAt": "2025-02-26T08:30:25.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 7,
-        "playersTotal": 119,
+        "playersCompleted": 0,
+        "playersStarted": 0,
+        "playersTotal": 13,
+        "priceData": {
+            "USD": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 289,
+                "final": 289,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 41000,
+                "final": 41000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 495,
+                "final": 495,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 465,
+                "final": 465,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 5,
-            "primaryText": "Magicgazer [90\u2605]\nAchieve a Full Combo on all of the Severe difficulty stages.\n\nHarmonia [40\u2605]\nAchieve a Full Combo on all of the Mild difficulty stages.\n\nDenpandemic [20\u2605]\nAchieve a Full Combo on all of the VeryMild difficulty stages.\n\n\u4e07\u83ef\u7c60 [20\u2605]\nClear all of the Severe difficulty stages without healing.\n\nGroovy Sympathy [10\u2605]\nClear all of the Mild difficulty stages without healing.",
+            "primary": 1,
+            "primaryText": "Found Them! [40\u2605]\nComplete every Deathless run.",
             "community": 0,
             "communityText": "",
-            "achievements": 47
+            "achievements": 17
         },
         "gameTags": [
-            "Action",
-            "Music",
-            "Rhythm",
-            "Bullet Hell",
-            "Pixel Graphics",
-            "Shooter",
-            "Singleplayer"
+            "Adventure",
+            "Indie",
+            "Platformer",
+            "Side Scroller",
+            "2D",
+            "Singleplayer",
+            "Aliens",
+            "Story Rich",
+            "Metroidvania",
+            "Point & Click",
+            "Great Soundtrack",
+            "Atmospheric",
+            "Cute",
+            "Drama",
+            "Controller",
+            "Historical"
         ],
         "languages": {
-            "japanese": "I"
-        },
-        "information": "\"Stage Turk\" can be unlocked by entering a secret command after reaching the ending once. Go to the \"How to Play\" screen and  enter Up 7, Down 8, Up 1, Right 2, Tap 1, Right 2, Tap 1, Right 1, Left 3, Tap 1, Left 2, Tap 1, Left 1, Right 1, Tap 8."
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "spanish": "I",
+            "russian": "I",
+            "schinese": "I"
+        }
     }
 }
-var gameOfDayDate = '2026-02-25'
+var gameOfDayDate = '2026-02-26'
