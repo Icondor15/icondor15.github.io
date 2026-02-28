ce876eaac33d7405059e46e3d1a3a51bd7ba1938
Icondor <ivanfigus@gmail.com>
2026-02-28 07:35:38 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,80 +2,86 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 658780,
-        "CEId": "51d7bbd1-69a2-4c12-9c05-274285ba0cd7",
-        "name": "Bomb Chicken",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/658780/header.jpg?t=1557348285",
-        "genre": "Action",
-        "tier": 1,
-        "points": 5,
-        "createdAt": "2022-02-27T22:53:25.000Z",
+        "platformId": 1071190,
+        "CEId": "b7c0b762-b9f2-43bd-9cd7-d45d3fbdf374",
+        "name": "MineRalph",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1071190/header.jpg?t=1667429112",
+        "genre": "Platformer",
+        "tier": 5,
+        "points": 350,
+        "createdAt": "2022-02-28T01:41:31.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 19,
-        "playersStarted": 0,
-        "playersTotal": 49,
+        "playersCompleted": 2,
+        "playersStarted": 6,
+        "playersTotal": 57,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1479,
-                "final": 1479,
+                "initial": 975,
+                "final": 975,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1279,
-                "final": 1279,
+                "initial": 850,
+                "final": 850,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 170000,
-                "final": 170000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2195,
-                "final": 2195,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1949,
-                "final": 1949,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Arise, Chicken [5\u2605]\nProve yourself.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 10
+            "primary": 5,
+            "primaryText": "The Fabled Savior[190\u2605 (Partial: 50\u2605)]\nBeat the game without dying, restarting or quitting.\n\nKing of the Caves[90\u2605 (Partial: 60\u2605)]\nObtain the diamond time/death medals on all normal and bonus levels. *Partial points are awarded as a progress milestone.\n\nGetting On the Ball[40\u2605 (Partial: 25\u2605)]\nObtain the gold time/death medals on all normal and bonus levels. *Partial points are awarded as a progress milestone.\n\nRolling Stone [20\u2605]\nFinish an any% speedrun in under 1 hour.\n\nBall Buster [10\u2605]\nClear all bonus levels.",
+            "community": 3,
+            "communityText": "Punenthusiastic\nComplete an all secrets run without dying, restarting or quitting.\n\nIronic\nBeat the bonus world in level order without dying, restarting or quitting.\n\nMeteoRalph\nBeat the original diamond times for all normal and bonus levels.",
+            "achievements": 49
         },
         "gameTags": [
+            "Precision Platformer",
+            "Unforgiving",
+            "Difficult",
+            "Fast-Paced",
+            "Pixel Graphics",
+            "Platformer",
+            "Time Attack",
+            "Physics",
+            "2D Platformer",
+            "Arcade",
+            "Robots",
+            "Runner",
+            "Underground",
+            "4 Player Local",
+            "Co-op",
+            "Controller",
+            "Singleplayer",
             "Action",
             "Indie",
-            "Adventure",
-            "Casual",
-            "Pixel Graphics",
-            "Puzzle Platformer",
-            "2D",
-            "Platformer"
+            "2D"
         ],
         "languages": {
-            "english": "IS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "dutch": "IS",
-            "koreana": "IS",
-            "portuguese": "IS",
-            "russian": "IS",
-            "schinese": "IS"
+            "english": "I",
+            "german": "I",
+            "danish": "I",
+            "russian": "I",
+            "schinese": "I"
         }
     }
 }
-var gameOfDayDate = '2026-02-27'
+var gameOfDayDate = '2026-02-28'
