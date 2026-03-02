940d3bd522a04a09c7e1f0e987f403aea3c3dc06
Icondor <ivanfigus@gmail.com>
2026-03-02 08:50:51 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,90 +2,86 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1124560,
-        "CEId": "b34df342-e454-4d72-9154-c76a27fd4cd6",
-        "name": "Fallen Angel",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1124560/header.jpg?t=1627248749",
-        "genre": "Action",
+        "platformId": 394430,
+        "CEId": "867440a4-2298-4a6a-853f-a4889bfa530d",
+        "name": "Dimension Drive",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/394430/header.jpg?t=1682982853",
+        "genre": "Bullet Hell",
         "tier": 1,
-        "points": 11,
-        "createdAt": "2024-03-01T16:44:59.000Z",
+        "points": 16,
+        "createdAt": "2024-03-02T21:17:03.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
         "playersCompleted": 0,
-        "playersStarted": 1,
-        "playersTotal": 7,
+        "playersStarted": 3,
+        "playersTotal": 44,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 152000,
-                "final": 152000,
+                "initial": 128000,
+                "final": 128000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 1850,
+                "final": 1850,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 1399,
+                "final": 1399,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 3,
-            "primaryText": "Cast From Heaven [5\u2605]\nProve yourself.\n\nFall From Grace [5\u2605]\nProve Yourself.\n\nAscension (UNCLEARED) [1\u2605]\nSeparately; Complete the game with the 1HP modifier, the No Drops modifier, and beat Punishment Mode without dying.",
+            "primaryText": "Driven Insane [10\u2605]\nComplete the game on Insane difficulty. \n\nSplit Personality [5\u2605]\nObtain an S rank on all levels.\n\nReality Bender (UNCLEARED) [1\u2605]\nObtain all S ranks on Insane difficulty.",
             "community": 0,
             "communityText": "",
-            "achievements": 51
+            "achievements": 30
         },
         "gameTags": [
-            "Metroidvania",
-            "Combat",
-            "Beat 'em up",
-            "Villain Protagonist",
-            "Pixel Graphics",
-            "Hack and Slash",
-            "Action RPG",
-            "Top-Down",
-            "Action",
-            "Local Multiplayer",
-            "RPG",
-            "Singleplayer",
-            "Demons",
-            "2D",
+            "Shoot 'Em Up",
             "Local Co-Op",
-            "Fantasy",
+            "Anime",
+            "Split Screen",
             "Indie",
+            "Action",
+            "Arcade",
+            "Female Protagonist",
+            "Local Multiplayer",
+            "Shooter",
+            "Top-Down Shooter",
+            "Comic Book",
+            "2D",
+            "Top-Down",
+            "Score Attack",
+            "Story Rich",
             "Co-op",
-            "Colorful",
-            "Old School"
+            "Multiplayer",
+            "Singleplayer",
+            "Real-Time"
         ],
         "languages": {
             "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
             "japanese": "IS",
             "spanish": "IS",
-            "polish": "IS",
-            "russian": "IS",
-            "schinese": "IS"
+            "koreana": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-03-01'
+var gameOfDayDate = '2026-03-02'
