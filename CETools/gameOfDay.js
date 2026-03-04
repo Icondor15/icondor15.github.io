47f94c07cf8e6c034c48a519fe2a65826a0cd8fb
Icondor <ivanfigus@gmail.com>
2026-03-04 08:46:28 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,86 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 254700,
-        "CEId": "3aa4a76f-2c49-436a-89df-20151640da8a",
-        "name": "Resident Evil 4 (2005)",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/254700/header.jpg?t=1644282793",
-        "genre": "Action",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2022-03-03T10:29:45.000Z",
+        "platformId": 1828900,
+        "CEId": "eaec4ce7-3af4-41f3-ab7c-d18060f7548d",
+        "name": "Stickit",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1828900/header.jpg?t=1641902802",
+        "genre": "First-Person",
+        "tier": 4,
+        "points": 80,
+        "createdAt": "2022-03-04T23:12:13.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 15,
-        "playersStarted": 95,
-        "playersTotal": 553,
+        "playersCompleted": 7,
+        "playersStarted": 1,
+        "playersTotal": 86,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 299,
+                "final": 299,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 239,
+                "final": 239,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1598,
-                "final": 1598,
+                "initial": 209,
+                "final": 209,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 199000,
-                "final": 199000,
+                "initial": 31000,
+                "final": 31000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2796,
-                "final": 2796,
+                "initial": 450,
+                "final": 450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 339,
+                "final": 339,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "A Heart of Steel [15\u2605]\nClear the game on the highest difficulty.\n\nHandcannon [10\u2605]\nGet 5 stars on all stages with all characters in the Mercenaries mode.",
+            "primaryText": "Stuck It Out [30\u2605]\nObtain the Golden Stick without using practice lamps.\n\nSticking With It [50\u2605]\nObtain the Golden Stick.",
             "community": 0,
             "communityText": "",
-            "achievements": 12
+            "achievements": 28
         },
         "gameTags": [
-            "Inventory Management",
-            "Survival Horror",
-            "Action",
-            "Zombies",
-            "Horror",
-            "Third-Person Shooter",
-            "Singleplayer",
-            "Adventure",
-            "Classic",
-            "Third Person",
-            "Quick-Time Events",
-            "Atmospheric",
-            "Survival",
-            "Shooter",
-            "Gore",
-            "Replay Value",
-            "Great Soundtrack",
-            "Remake",
             "Difficult",
-            "VR"
+            "Precision Platformer",
+            "Psychological Horror",
+            "3D Platformer",
+            "Parkour",
+            "Nonlinear",
+            "Ninja",
+            "3D",
+            "First-Person",
+            "Exploration",
+            "Platformer",
+            "Minimalist",
+            "Alternate History",
+            "Dark Humor",
+            "Funny",
+            "Historical",
+            "Horror",
+            "Choices Matter",
+            "Action",
+            "Open World"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "spanish": "IS"
+            "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-03-03'
+var gameOfDayDate = '2026-03-04'
