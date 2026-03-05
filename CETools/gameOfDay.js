68085a3ecb182116f30df0b4ffd41fb2104169b9
Icondor <ivanfigus@gmail.com>
2026-03-05 08:47:38 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,82 +2,96 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1828900,
-        "CEId": "eaec4ce7-3af4-41f3-ab7c-d18060f7548d",
-        "name": "Stickit",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1828900/header.jpg?t=1641902802",
-        "genre": "First-Person",
-        "tier": 4,
-        "points": 80,
-        "createdAt": "2022-03-04T23:12:13.000Z",
+        "platformId": 1245620,
+        "CEId": "06af14b9-161b-4d47-bf84-f028fe2a39ca",
+        "name": "ELDEN RING",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1245620/header.jpg?t=1683618443",
+        "genre": "Action",
+        "tier": 2,
+        "points": 25,
+        "createdAt": "2022-03-05T05:42:40.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 7,
-        "playersStarted": 1,
-        "playersTotal": 86,
+        "playersCompleted": 63,
+        "playersStarted": 426,
+        "playersTotal": 1089,
         "priceData": {
             "USD": {
-                "initial": 299,
-                "final": 299,
+                "initial": 5999,
+                "final": 5999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 239,
-                "final": 239,
+                "initial": 5999,
+                "final": 5999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 209,
-                "final": 209,
+                "initial": 4999,
+                "final": 4999,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 31000,
-                "final": 31000,
+                "initial": 902000,
+                "final": 902000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 450,
-                "final": 450,
+                "initial": 8995,
+                "final": 8995,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 339,
-                "final": 339,
+                "initial": 7999,
+                "final": 7999,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Stuck It Out [30\u2605]\nObtain the Golden Stick without using practice lamps.\n\nSticking With It [50\u2605]\nObtain the Golden Stick.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 28
+            "primaryText": "Promised Consort [10\u2605]\nDefeat all DLC remembrance bosses (including Bayle).\n\nElden Lord [15\u2605]\nDefeat all non-missable bosses.",
+            "community": 4,
+            "communityText": "Iron Rune\nCollect all boss remembrances while at Rune level 1.\n\nLord of the Ring\nBeat the game without dying.\n\nOld Gods Felled by a Mortal Unnamed\nBeat the game on NG+7.\n\nThe Dark Soul\nComplete all primary objectives from all site-listed games in the FromSoft Soulslike franchise. See verification requirements for complete list.",
+            "achievements": 42
         },
         "gameTags": [
+            "Souls-like",
+            "Open World",
+            "Dark Fantasy",
+            "RPG",
             "Difficult",
-            "Precision Platformer",
-            "Psychological Horror",
-            "3D Platformer",
-            "Parkour",
-            "Nonlinear",
-            "Ninja",
-            "3D",
-            "First-Person",
-            "Exploration",
-            "Platformer",
-            "Minimalist",
-            "Alternate History",
-            "Dark Humor",
-            "Funny",
-            "Historical",
-            "Horror",
-            "Choices Matter",
+            "Action RPG",
+            "Multiplayer",
+            "Third Person",
+            "Fantasy",
+            "Singleplayer",
+            "Online Co-Op",
             "Action",
-            "Open World"
+            "Co-op",
+            "Atmospheric",
+            "Great Soundtrack",
+            "PvP",
+            "Violent",
+            "3D",
+            "Character Customization",
+            "Family Friendly"
         ],
         "languages": {
-            "english": "IAS"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "arabic": "I",
+            "brazilian": "IS",
+            "koreana": "IS",
+            "latam": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS",
+            "thai": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-03-04'
+var gameOfDayDate = '2026-03-05'
