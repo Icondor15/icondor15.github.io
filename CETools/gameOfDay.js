2ba85f19a626c6ba4fe3d390046965e15c9f74f7
Icondor <ivanfigus@gmail.com>
2026-03-06 08:46:18 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,96 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1245620,
-        "CEId": "06af14b9-161b-4d47-bf84-f028fe2a39ca",
-        "name": "ELDEN RING",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1245620/header.jpg?t=1683618443",
+        "platformId": 1629530,
+        "CEId": "f71e1a82-d846-4ace-8f98-4ba22d8e64e0",
+        "name": "Spark the Electric Jester 3",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1629530/header.jpg?t=1660491947",
         "genre": "Action",
-        "tier": 2,
-        "points": 25,
-        "createdAt": "2022-03-05T05:42:40.000Z",
+        "tier": 1,
+        "points": 6,
+        "createdAt": "2023-03-06T16:02:31.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 63,
-        "playersStarted": 426,
-        "playersTotal": 1089,
+        "playersCompleted": 0,
+        "playersStarted": 2,
+        "playersTotal": 34,
         "priceData": {
             "USD": {
-                "initial": 5999,
-                "final": 5999,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 5999,
-                "final": 5999,
+                "initial": 2099,
+                "final": 2099,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 4999,
-                "final": 4999,
+                "initial": 1949,
+                "final": 1949,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 902000,
-                "final": 902000,
+                "initial": 257000,
+                "final": 257000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 8995,
-                "final": 8995,
+                "initial": 2495,
+                "final": 2495,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 7999,
-                "final": 7999,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Promised Consort [10\u2605]\nDefeat all DLC remembrance bosses (including Bayle).\n\nElden Lord [15\u2605]\nDefeat all non-missable bosses.",
-            "community": 4,
-            "communityText": "Iron Rune\nCollect all boss remembrances while at Rune level 1.\n\nLord of the Ring\nBeat the game without dying.\n\nOld Gods Felled by a Mortal Unnamed\nBeat the game on NG+7.\n\nThe Dark Soul\nComplete all primary objectives from all site-listed games in the FromSoft Soulslike franchise. See verification requirements for complete list.",
-            "achievements": 42
+            "primaryText": "Sooth (UNCLEARED) [1\u2605]\nUnlock the secret boss at the bottom of Endless Dive and beat it.\n\nSpark Adventure [5\u2605]\nClear the game on Challenge Jester difficulty. / Get all platinum speed medals.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 6
         },
         "gameTags": [
-            "Souls-like",
-            "Open World",
-            "Dark Fantasy",
-            "RPG",
-            "Difficult",
-            "Action RPG",
-            "Multiplayer",
-            "Third Person",
-            "Fantasy",
-            "Singleplayer",
-            "Online Co-Op",
-            "Action",
-            "Co-op",
-            "Atmospheric",
-            "Great Soundtrack",
-            "PvP",
-            "Violent",
+            "3D Platformer",
             "3D",
-            "Character Customization",
-            "Family Friendly"
+            "Action",
+            "Platformer",
+            "Hack and Slash",
+            "Third Person",
+            "Exploration",
+            "Singleplayer",
+            "Beat 'em up",
+            "Combat",
+            "Driving",
+            "Sci-fi",
+            "Cartoony",
+            "Colorful",
+            "Fantasy",
+            "1990's",
+            "Indie",
+            "Fast-Paced",
+            "Retro",
+            "Futuristic"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "arabic": "I",
-            "brazilian": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "polish": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "tchinese": "IS",
-            "thai": "IS"
+            "english": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-03-05'
+var gameOfDayDate = '2026-03-06'
