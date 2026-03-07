a19a61fc9f89cb27b349e3410820d983fffae1aa
Icondor <ivanfigus@gmail.com>
2026-03-07 08:47:55 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,82 +2,80 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1629530,
-        "CEId": "f71e1a82-d846-4ace-8f98-4ba22d8e64e0",
-        "name": "Spark the Electric Jester 3",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1629530/header.jpg?t=1660491947",
-        "genre": "Action",
+        "platformId": 956560,
+        "CEId": "846e6649-5eb8-4b7a-b3b1-1ca2772dd648",
+        "name": "StarCrossed",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/956560/header.jpg?t=1680723453",
+        "genre": "Arcade",
         "tier": 1,
-        "points": 6,
-        "createdAt": "2023-03-06T16:02:31.000Z",
+        "points": 5,
+        "createdAt": "2024-03-07T01:09:41.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 2,
-        "playersTotal": 34,
+        "playersCompleted": 1,
+        "playersStarted": 0,
+        "playersTotal": 60,
         "priceData": {
             "USD": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2099,
-                "final": 2099,
+                "initial": 819,
+                "final": 819,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1949,
-                "final": 1949,
+                "initial": 719,
+                "final": 719,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 257000,
-                "final": 257000,
+                "initial": 101000,
+                "final": 101000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2495,
-                "final": 2495,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 1149,
+                "final": 1149,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Sooth (UNCLEARED) [1\u2605]\nUnlock the secret boss at the bottom of Endless Dive and beat it.\n\nSpark Adventure [5\u2605]\nClear the game on Challenge Jester difficulty. / Get all platinum speed medals.",
+            "primary": 1,
+            "primaryText": "Double Crossed [5\u2605]\nProve yourself.",
             "community": 0,
             "communityText": "",
-            "achievements": 6
+            "achievements": 30
         },
         "gameTags": [
-            "3D Platformer",
-            "3D",
+            "Female Protagonist",
             "Action",
-            "Platformer",
-            "Hack and Slash",
-            "Third Person",
-            "Exploration",
-            "Singleplayer",
-            "Beat 'em up",
-            "Combat",
-            "Driving",
-            "Sci-fi",
-            "Cartoony",
-            "Colorful",
-            "Fantasy",
-            "1990's",
+            "Local Co-Op",
             "Indie",
-            "Fast-Paced",
+            "Cute",
+            "Arcade",
+            "LGBTQ+",
+            "Co-op",
+            "Colorful",
+            "Bullet Hell",
+            "Anime",
+            "Multiplayer",
+            "Great Soundtrack",
             "Retro",
-            "Futuristic"
+            "Visual Novel",
+            "Shoot 'Em Up",
+            "2D",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "IS"
+            "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-03-06'
+var gameOfDayDate = '2026-03-07'
