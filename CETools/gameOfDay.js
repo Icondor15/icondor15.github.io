927e833493ec70f17c8c3fab3bf648df43df8797
Icondor <ivanfigus@gmail.com>
2026-03-12 08:47:28 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,95 +2,63 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1818690,
-        "CEId": "6b902e87-7795-499f-ad12-920e005afee4",
-        "name": "Grindstone",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1818690/header.jpg?t=1678295049",
-        "genre": "Strategy",
+        "platformId": 316180,
+        "CEId": "7c973319-5a08-4b19-8bab-67ba6337eed9",
+        "name": "A City Sleeps",
+        "header": "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/316180/header.jpg?t=1447362884",
+        "genre": "Bullet Hell",
         "tier": 1,
-        "points": 10,
-        "createdAt": "2024-03-11T15:50:36.000Z",
+        "points": 6,
+        "createdAt": "2025-03-12T18:00:49.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 2,
-        "playersStarted": 4,
-        "playersTotal": 39,
+        "playersCompleted": 0,
+        "playersStarted": 2,
+        "playersTotal": 17,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1679,
-                "final": 1679,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1549,
-                "final": 1549,
+                "initial": 699,
+                "final": 699,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 205000,
-                "final": 205000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 2895,
-                "final": 2895,
+                "initial": 98000,
+                "final": 98000,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2279,
-                "final": 2279,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Creep Crush [5\u2605]\nComplete all Extra worlds and the boss rush.\n\nNine to Five [5\u2605]\nComplete all levels and complete a greed grind without dying.",
-            "community": 1,
-            "communityText": "No Stone Unturned\n100% everything.",
-            "achievements": 49
+            "primaryText": "May You LIve in Interesting Times (UNCLEARED) [1\u2605]\nComplete the last difficulty of Stage 1.\n\nFortune's Friend [5\u2605]\nComplete the last difficulty of Stage 3.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 16
         },
         "gameTags": [
-            "Casual",
-            "Arcade",
-            "Puzzle",
-            "Collectathon",
-            "Clicker",
-            "Hack and Slash",
-            "Puzzle Platformer",
-            "2D",
-            "Cartoon",
-            "Cartoony",
-            "Colorful",
-            "Cooking",
-            "Crafting",
-            "Dark",
-            "Dark Comedy",
-            "Dark Humor",
-            "Loot",
-            "Relaxing",
-            "Character Customization",
-            "Choices Matter"
+            "Indie",
+            "Action",
+            "Bullet Hell",
+            "Rhythm",
+            "Shoot 'Em Up",
+            "Female Protagonist"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "arabic": "I",
-            "brazilian": "I",
-            "dutch": "I",
-            "koreana": "I",
-            "russian": "I",
-            "schinese": "I",
-            "tchinese": "I",
-            "turkish": "I"
+            "english": "IA"
         }
     }
 }
-var gameOfDayDate = '2026-03-11'
+var gameOfDayDate = '2026-03-12'
