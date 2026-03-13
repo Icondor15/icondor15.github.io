c9b993676fce47631a06c0357297f48350ef371a
Icondor <ivanfigus@gmail.com>
2026-03-13 09:06:21 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,63 +2,90 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 316180,
-        "CEId": "7c973319-5a08-4b19-8bab-67ba6337eed9",
-        "name": "A City Sleeps",
-        "header": "https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/316180/header.jpg?t=1447362884",
-        "genre": "Bullet Hell",
-        "tier": 1,
-        "points": 6,
-        "createdAt": "2025-03-12T18:00:49.000Z",
+        "platformId": 7200,
+        "CEId": "5337f128-8ca1-4cf8-9032-73ab63616c24",
+        "name": "Trackmania United Forever",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/7200/header.jpg?t=1611670003",
+        "genre": "Arcade",
+        "tier": 3,
+        "points": 60,
+        "createdAt": "2023-03-13T23:27:52.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 2,
-        "playersTotal": 17,
+        "playersCompleted": 1,
+        "playersStarted": 8,
+        "playersTotal": 81,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 699,
-                "final": 699,
+                "initial": 2599,
+                "final": 2599,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 98000,
-                "final": 98000,
+                "initial": 396000,
+                "final": 396000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 4495,
+                "final": 4495,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "May You LIve in Interesting Times (UNCLEARED) [1\u2605]\nComplete the last difficulty of Stage 1.\n\nFortune's Friend [5\u2605]\nComplete the last difficulty of Stage 3.",
-            "community": 0,
-            "communityText": "",
-            "achievements": 16
+            "primary": 4,
+            "primaryText": "Shoot for the Stars [25\u2605]\nGet all 588 medals in the Star Track Campaign.\n\nMixed Bag [20\u2605]\nGet all 588 medals in the United/Race Campaign.\n\nTrack Attacker [10\u2605]\nGet all Stunt Author Medals.\n\nSuper Trackmania Boy [5\u2605]\nGet all 63 medals in the United/Platform campaign.",
+            "community": 5,
+            "communityText": "Iron Tires\nGet all Platform campaign Gold Medals in a row, in order, and without ever resetting or restarting a map.\n\nTrickmaniac\nGet all Puzzle Author Medals.\n\nTMN_ESWC Old Enthusiast\nGet all medals in the old TrackMania Nations ESWC campaign. (See information)\n\nTMO Old Enthusiast\nGet all medals in the old TrackMania Original campaign. (See information)\n\nTMSX Old Enthusiast\nGet all medals in the old TrackMania Sunrise eXtreme campaign. (See information)",
+            "achievements": 0
         },
         "gameTags": [
-            "Indie",
+            "Racing",
+            "Multiplayer",
+            "Level Editor",
+            "Singleplayer",
+            "Time Attack",
+            "Arcade",
+            "Competitive",
+            "Classic",
+            "Driving",
+            "Difficult",
+            "Local Multiplayer",
             "Action",
-            "Bullet Hell",
-            "Rhythm",
-            "Shoot 'Em Up",
-            "Female Protagonist"
+            "Simulation"
         ],
         "languages": {
-            "english": "IA"
-        }
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "czech": "I",
+            "dutch": "I",
+            "hungarian": "I",
+            "korean": "I",
+            "polish": "I",
+            "portuguese": "I",
+            "russian": "I",
+            "slovakian": "I",
+            "tchinese": "I"
+        },
+        "information": "[Direct link to download the old TrackMania campaigns For Old Enthusiast COs (53 MB)](https://upload.cedb.me/TMUnitedF_Old_Campaings.zip), install steps inside or [here](https://steamcommunity.com/sharedfiles/filedetails/?id=2134162935)."
     }
 }
-var gameOfDayDate = '2026-03-12'
+var gameOfDayDate = '2026-03-13'
