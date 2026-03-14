8ff4fd08e347ed2fbf4669da923d1c30a59927f5
Icondor <ivanfigus@gmail.com>
2026-03-14 09:11:38 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,18 +2,18 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 7200,
-        "CEId": "5337f128-8ca1-4cf8-9032-73ab63616c24",
-        "name": "Trackmania United Forever",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/7200/header.jpg?t=1611670003",
-        "genre": "Arcade",
-        "tier": 3,
-        "points": 60,
-        "createdAt": "2023-03-13T23:27:52.000Z",
+        "platformId": 464450,
+        "CEId": "e1b61d7f-1ad8-46b1-86fc-20bd5f403978",
+        "name": "DoDonPachi Resurrection",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/464450/header.jpg?t=1563876814",
+        "genre": "Bullet Hell",
+        "tier": 6,
+        "points": 710,
+        "createdAt": "2020-03-14T12:55:44.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 8,
-        "playersTotal": 81,
+        "playersCompleted": 3,
+        "playersStarted": 17,
+        "playersTotal": 254,
         "priceData": {
             "USD": {
                 "initial": 2999,
@@ -21,71 +21,55 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 2899,
+                "final": 2899,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 2599,
-                "final": 2599,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 396000,
-                "final": 396000,
+                "initial": 303500,
+                "final": 303500,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 4495,
-                "final": 4495,
+                "initial": 4395,
+                "final": 4395,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 3899,
+                "final": 3899,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Shoot for the Stars [25\u2605]\nGet all 588 medals in the Star Track Campaign.\n\nMixed Bag [20\u2605]\nGet all 588 medals in the United/Race Campaign.\n\nTrack Attacker [10\u2605]\nGet all Stunt Author Medals.\n\nSuper Trackmania Boy [5\u2605]\nGet all 63 medals in the United/Platform campaign.",
-            "community": 5,
-            "communityText": "Iron Tires\nGet all Platform campaign Gold Medals in a row, in order, and without ever resetting or restarting a map.\n\nTrickmaniac\nGet all Puzzle Author Medals.\n\nTMN_ESWC Old Enthusiast\nGet all medals in the old TrackMania Nations ESWC campaign. (See information)\n\nTMO Old Enthusiast\nGet all medals in the old TrackMania Original campaign. (See information)\n\nTMSX Old Enthusiast\nGet all medals in the old TrackMania Sunrise eXtreme campaign. (See information)",
-            "achievements": 0
+            "primary": 9,
+            "primaryText": "Zatsuza [190\u2605]\nDefeat Zatsuza in BL with Strong (NO continues).\n\nBL Strong Master[145\u2605 (Partial: 40\u2605)]\nDefeat Hibachi in BL with Strong and score 500bil (NO continues). Can be done in separate runs.\n\nFirst Loop TLB Master [80\u2605]\nDefeat Hibachi in BL with Bomb and Power and defeat Hivac in BL Arrange (no continues).\n\nBL Bomb+Power Master [80\u2605]\nDefeat Hibachi in BL with Bomb and Power and score 300bil with Bomb and 600bil with Power in BL (continues allowed). Can all be achieved in individual runs.\n\nUra 2-All Master [100\u2605]\nClear the Ura loop of Arrange A and 1.5 (no continues).\n\nHumanity's Triumph [30\u2605]\nDefeat Hibachi in Arrange A (continues allowed).\n\nEvaccaneer Doom [25\u2605]\nDefeat the TLB in BL Arrange (continues allowed).\n\n1.5 2-All [50\u2605]\nClear either second loop of 1.5 (no continues).\n\nBlissful Death [10\u2605]\nClear the first loop of 1.5 or Arrange A. (No continues used)",
+            "community": 0,
+            "communityText": "",
+            "achievements": 100
         },
         "gameTags": [
-            "Racing",
-            "Multiplayer",
-            "Level Editor",
-            "Singleplayer",
-            "Time Attack",
+            "Bullet Hell",
+            "Shoot 'Em Up",
             "Arcade",
-            "Competitive",
-            "Classic",
-            "Driving",
-            "Difficult",
-            "Local Multiplayer",
             "Action",
-            "Simulation"
+            "Anime",
+            "Great Soundtrack",
+            "Difficult",
+            "Shooter",
+            "2D",
+            "Classic"
         ],
         "languages": {
             "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "czech": "I",
-            "dutch": "I",
-            "hungarian": "I",
-            "korean": "I",
-            "polish": "I",
-            "portuguese": "I",
-            "russian": "I",
-            "slovakian": "I",
-            "tchinese": "I"
+            "japanese": "I"
         },
-        "information": "[Direct link to download the old TrackMania campaigns For Old Enthusiast COs (53 MB)](https://upload.cedb.me/TMUnitedF_Old_Campaings.zip), install steps inside or [here](https://steamcommunity.com/sharedfiles/filedetails/?id=2134162935)."
+        "information": "Can have some replay desync issues, more noticeable on second loops. It's recommended to record videos of live runs.\n"
     }
 }
-var gameOfDayDate = '2026-03-13'
+var gameOfDayDate = '2026-03-14'
