21b2c83cb53a64710ea97d11c0284d2b965daf21
Icondor <ivanfigus@gmail.com>
2026-03-15 08:48:27 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,74 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 464450,
-        "CEId": "e1b61d7f-1ad8-46b1-86fc-20bd5f403978",
-        "name": "DoDonPachi Resurrection",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/464450/header.jpg?t=1563876814",
-        "genre": "Bullet Hell",
-        "tier": 6,
-        "points": 710,
-        "createdAt": "2020-03-14T12:55:44.000Z",
+        "platformId": 2424650,
+        "CEId": "50f1fbd3-aa45-44c7-87c3-94acad04523c",
+        "name": "Froggy's Battle",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2424650/header.jpg?t=1689775335",
+        "genre": "Action",
+        "tier": 1,
+        "points": 5,
+        "createdAt": "2024-03-15T21:51:50.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 3,
-        "playersStarted": 17,
-        "playersTotal": 254,
+        "playersCompleted": 28,
+        "playersStarted": 0,
+        "playersTotal": 70,
         "priceData": {
             "USD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 199,
+                "final": 199,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2899,
-                "final": 2899,
+                "initial": 199,
+                "final": 199,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 169,
+                "final": 169,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 303500,
-                "final": 303500,
+                "initial": 23500,
+                "final": 23500,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 4395,
-                "final": 4395,
+                "initial": 295,
+                "final": 295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 3899,
-                "final": 3899,
+                "initial": 259,
+                "final": 259,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 9,
-            "primaryText": "Zatsuza [190\u2605]\nDefeat Zatsuza in BL with Strong (NO continues).\n\nBL Strong Master[145\u2605 (Partial: 40\u2605)]\nDefeat Hibachi in BL with Strong and score 500bil (NO continues). Can be done in separate runs.\n\nFirst Loop TLB Master [80\u2605]\nDefeat Hibachi in BL with Bomb and Power and defeat Hivac in BL Arrange (no continues).\n\nBL Bomb+Power Master [80\u2605]\nDefeat Hibachi in BL with Bomb and Power and score 300bil with Bomb and 600bil with Power in BL (continues allowed). Can all be achieved in individual runs.\n\nUra 2-All Master [100\u2605]\nClear the Ura loop of Arrange A and 1.5 (no continues).\n\nHumanity's Triumph [30\u2605]\nDefeat Hibachi in Arrange A (continues allowed).\n\nEvaccaneer Doom [25\u2605]\nDefeat the TLB in BL Arrange (continues allowed).\n\n1.5 2-All [50\u2605]\nClear either second loop of 1.5 (no continues).\n\nBlissful Death [10\u2605]\nClear the first loop of 1.5 or Arrange A. (No continues used)",
+            "primary": 1,
+            "primaryText": "One Tricky Frog [5\u2605]\nSeparately; Beat Hard mode in less than 7 minutes, and with 5 or more hearts remaining.",
             "community": 0,
             "communityText": "",
-            "achievements": 100
+            "achievements": 15
         },
         "gameTags": [
-            "Bullet Hell",
-            "Shoot 'Em Up",
+            "Action Roguelike",
+            "Skateboarding",
+            "Roguelite",
             "Arcade",
-            "Action",
-            "Anime",
-            "Great Soundtrack",
-            "Difficult",
-            "Shooter",
+            "Indie",
             "2D",
-            "Classic"
+            "Cute",
+            "Relaxing",
+            "Action",
+            "Funny",
+            "Controller",
+            "Casual",
+            "Colorful",
+            "Roguelike",
+            "Fantasy",
+            "Magic",
+            "Swordplay",
+            "Nature",
+            "Combat",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "I",
-            "japanese": "I"
-        },
-        "information": "Can have some replay desync issues, more noticeable on second loops. It's recommended to record videos of live runs.\n"
+            "english": "I"
+        }
     }
 }
-var gameOfDayDate = '2026-03-14'
+var gameOfDayDate = '2026-03-15'
