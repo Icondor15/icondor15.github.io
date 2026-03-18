855a2a4bd5a9c91374f02e359bc2ffc67c06c0a5
Icondor <ivanfigus@gmail.com>
2026-03-18 08:47:05 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,90 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 773370,
-        "CEId": "39c14553-79be-4e08-84c6-5fae6c5f862a",
-        "name": "Exo One",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/773370/header.jpg?t=1706782417",
-        "genre": "Arcade",
-        "tier": 1,
-        "points": 5,
-        "createdAt": "2024-03-17T03:35:34.000Z",
+        "platformId": 1081650,
+        "CEId": "0e2424a7-6b22-4cd7-b487-095eab7f2034",
+        "name": "Celaria",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1081650/header.jpg?t=1672588966",
+        "genre": "First-Person",
+        "tier": 2,
+        "points": 32,
+        "createdAt": "2024-03-18T11:20:14.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 4,
-        "playersStarted": 0,
-        "playersTotal": 33,
+        "playersCompleted": 0,
+        "playersStarted": 2,
+        "playersTotal": 35,
         "priceData": {
             "USD": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1649,
-                "final": 1649,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1429,
-                "final": 1429,
+                "initial": 669,
+                "final": 669,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 190000,
-                "final": 190000,
+                "initial": 92000,
+                "final": 92000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2495,
-                "final": 2495,
+                "initial": 1179,
+                "final": 1179,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2199,
-                "final": 2199,
+                "initial": 1049,
+                "final": 1049,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Event Horizon [5\u2605]\nComplete the game in under an hour.",
+            "primary": 3,
+            "primaryText": "Mirrored Edge [30\u2605]\nSeparately; Obtain all Platinum medals on singleplayer maps, collect all Spheres in singleplayer.\n\nKing of the Skyline (UNCLEARED) [1\u2605]\nObtain the Platinum medal for any% speedrun mode.\n\nUltimate Ninja Warrior (UNCLEARED) [1\u2605]\nObtain the Platinum medal on AllSpheres speedrun mode.",
             "community": 0,
             "communityText": "",
-            "achievements": 28
+            "achievements": 48
         },
         "gameTags": [
-            "Atmospheric",
-            "Exploration",
-            "Space",
-            "Flight",
-            "Adventure",
-            "Relaxing",
-            "Physics",
-            "Walking Simulator",
+            "Parkour",
+            "Precision Platformer",
             "3D Platformer",
-            "Space Sim",
-            "Controller",
-            "Sci-fi",
-            "Story Rich",
-            "3D",
-            "Simulation",
-            "Realistic",
-            "Psychedelic",
-            "Philosophical",
-            "Mystery",
-            "Linear"
+            "First-Person",
+            "Runner",
+            "Racing",
+            "Level Editor",
+            "Multiplayer",
+            "Action",
+            "Time Attack",
+            "Colorful",
+            "Stylized",
+            "Platformer",
+            "Singleplayer",
+            "Fast-Paced",
+            "Abstract",
+            "Minimalist",
+            "Competitive",
+            "FPS",
+            "Indie"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IAS",
-            "german": "IAS",
-            "japanese": "IAS",
-            "spanish": "IAS",
-            "brazilian": "IAS",
-            "russian": "IAS",
-            "schinese": "IAS",
-            "tchinese": "IAS"
+            "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-03-17'
+var gameOfDayDate = '2026-03-18'
