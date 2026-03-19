16bb6f93876e0cf1d050086559fdefb931b44aea
Icondor <ivanfigus@gmail.com>
2026-03-19 08:47:00 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,83 +1,52 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1081650,
-        "CEId": "0e2424a7-6b22-4cd7-b487-095eab7f2034",
-        "name": "Celaria",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1081650/header.jpg?t=1672588966",
-        "genre": "First-Person",
-        "tier": 2,
-        "points": 32,
-        "createdAt": "2024-03-18T11:20:14.000Z",
+        "gameType": 1,
+        "platformId": 2093940,
+        "CEId": "493bdf6a-3596-4f16-ac67-de8372682370",
+        "name": "vivid/stasis",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2093940/header.jpg?t=1752489918",
+        "genre": "Arcade",
+        "tier": 6,
+        "points": 460,
+        "createdAt": "2024-03-19T18:22:07.000Z",
         "updatedAt": "2026-02-11T06:01:38.000Z",
-        "playersCompleted": 0,
-        "playersStarted": 2,
-        "playersTotal": 35,
-        "priceData": {
-            "USD": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 669,
-                "final": 669,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 92000,
-                "final": 92000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1179,
-                "final": 1179,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1049,
-                "final": 1049,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 1,
+        "playersStarted": 19,
+        "playersTotal": 451,
         "milestones": {
-            "primary": 3,
-            "primaryText": "Mirrored Edge [30\u2605]\nSeparately; Obtain all Platinum medals on singleplayer maps, collect all Spheres in singleplayer.\n\nKing of the Skyline (UNCLEARED) [1\u2605]\nObtain the Platinum medal for any% speedrun mode.\n\nUltimate Ninja Warrior (UNCLEARED) [1\u2605]\nObtain the Platinum medal on AllSpheres speedrun mode.",
-            "community": 0,
-            "communityText": "",
+            "primary": 11,
+            "primaryText": "Limit Shatter [60\u2605]\nPass Horizon Class Kaiden.\n\nVirtual Supremacy [110\u2605]\nObtain a V rank on all 17+ and 18 songs across all difficulties.\n\nBreak the Loop [70\u2605]\nObtain a V rank on all classes and all level 16, 16+, and 17 songs across all difficulties, AND pass Horizon Class C1, C2 and D and below.\n\nChronostasis [60\u2605]\nObtain an SS rank on all level 17+ and 18 songs across all difficulties, AND pass Horizon Class B2 and below.\n\nFate Inversion [50\u2605]\nObtain an SS rank on Class 11, 12 and Omega, and all level 16+ and 17 songs across all difficulties, AND pass Horizon Class B1 and below.\n\nSaturday Night Fever [40\u2605]\nObtain an SS rank on Class 9 and 10 and all level 15, 15+, and 16 songs across all difficulties, AND pass Horizon Class A2 and below.\n\nVividology [30\u2605]\nObtain an SS rank on Class 6, 7, and 8 and all level 13, 13+, 14, and 14+ songs across all difficulties, AND pass Horizon Class A1.\n\nThe First Encore [20\u2605]\nObtain an SS rank on Class 4 and 5 and all level 11, 11+, 12 and 12+ songs across all difficulties.\n\nFinale Starting to Hurt [10\u2605]\nObtain an SS rank on Class 2 and 3 and all level 9, 9+, 10, and 10+ songs across all difficulties.\n\nPrecision Tuning [5\u2605]\nObtain an SS rank on Class 1 and all level 8 songs and below across all difficulties.\n\nInitialize the Sequence [5\u2605]\nObtain an S rank on all songs level 8 and below across all difficulties.",
+            "community": 10,
+            "communityText": "Beyond Comprehension (S)\nObtain a VS rank on all songs in Boundary Shatter. (May be split into multiple objectives)\n\nCritical Eye (S)\nObtain a VS rank on all songs between level 15 and 16+ on Finale and Encore (whichever version of the song is harder).\n\nEnduring Perfectionist (S)\nObtain a VS rank on all Classes in Course Mode.\n\nOnce More (S)\nObtain an AC on all level 17 songs across all difficulties.\n\nPerfection Tuning (S)\nObtain a VS rank on all songs between level 9+ and 12+ on Finale and Encore (whichever version of the song is harder).\n\nResurrection (S)\nObtain a VS rank on all songs between level 13 and 14+ on Finale and Encore (whichever version of the song is harder).\n\nShattering Expectations (S)\nObtain an SS rank on all songs in Boundary Shatter. (May be split into multiple objectives)\n\nTwice More (S)\nObtain an AC on all level 17+ and 18 songs across all difficulties.\n\nUltimation (S)\nObtain a VS rank on all level 17 songs on Finale and Encore.\n\nVivid Stasis (S)\nObtain a VS rank on all level 17+ and 18 songs on Finale and Encore.",
             "achievements": 48
         },
         "gameTags": [
-            "Parkour",
-            "Precision Platformer",
-            "3D Platformer",
-            "First-Person",
-            "Runner",
-            "Racing",
-            "Level Editor",
-            "Multiplayer",
-            "Action",
-            "Time Attack",
+            "Rhythm",
+            "Visual Novel",
+            "LGBTQ+",
+            "Surreal",
+            "Score Attack",
+            "Cute",
+            "Female Protagonist",
+            "Anime",
+            "Adventure",
+            "2D",
             "Colorful",
-            "Stylized",
-            "Platformer",
+            "Pixel Graphics",
+            "Hand-drawn",
+            "Choose Your Own Adventure",
+            "Story Rich",
+            "Casual",
+            "Psychological Horror",
+            "Indie",
             "Singleplayer",
-            "Fast-Paced",
-            "Abstract",
-            "Minimalist",
-            "Competitive",
-            "FPS",
-            "Indie"
+            "Puzzle"
         ],
         "languages": {
-            "english": "I"
-        }
+            "english": "IAS"
+        },
+        "information": "[https://discord.com/channels/639112509445505044/1394317393060298992](https://discord.com/channels/639112509445505044/1394317393060298992)\n\nWhen you submit verification for any of the objectives, follow the Discord link above and provide your point estimate for the objectives you have completed, what your playtime is, and how much experience you have in other VSRGs.\n\n*This is an experimental attempt at asking for data points to contribute towards a more accurate point assessment for objectives.* \n\n----\n\nFor organizing purposes, MARENOL is given a difficulty number based on the highest difficulty level on each difficulty.\n-__MARENOL opening:__ 10\n-__MARENOL middle:__ 13+\n-__MARENOL finale:__ 17+"
     }
 }
-var gameOfDayDate = '2026-03-18'
+var gameOfDayDate = '2026-03-19'
