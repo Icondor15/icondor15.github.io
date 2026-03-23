f952fcf2806721f1cef69f3ea8b928ecf6f6a182
Icondor <ivanfigus@gmail.com>
2026-03-23 08:54:08 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,68 +2,75 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 429780,
-        "CEId": "5d3e3747-a88c-4c8f-a320-d0dffdd23caf",
-        "name": "Bunny Hop League",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/429780/header.jpg?t=1560789144",
+        "platformId": 851950,
+        "CEId": "e5b91554-215a-41b9-8974-e921044b2081",
+        "name": "Heroine of the Sniper",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/851950/header.jpg?t=1569030020",
         "genre": "First-Person",
-        "tier": 4,
-        "points": 90,
-        "createdAt": "2022-03-22T15:13:21.000Z",
-        "updatedAt": "2026-03-09T10:11:25.000Z",
-        "playersCompleted": 26,
-        "playersStarted": 18,
-        "playersTotal": 120,
+        "tier": 1,
+        "points": 10,
+        "createdAt": "2022-03-23T20:57:07.000Z",
+        "updatedAt": "2026-03-22T18:18:05.000Z",
+        "playersCompleted": 52,
+        "playersStarted": 0,
+        "playersTotal": 89,
         "priceData": {
             "USD": {
-                "initial": 599,
-                "final": 299,
-                "discountPercent": 50
+                "initial": 999,
+                "final": 339,
+                "discountPercent": 66
             },
             "EUR": {
-                "initial": 599,
-                "final": 299,
-                "discountPercent": 50
+                "initial": 819,
+                "final": 278,
+                "discountPercent": 66
             },
             "GBP": {
-                "initial": 479,
-                "final": 239,
-                "discountPercent": 50
+                "initial": 719,
+                "final": 244,
+                "discountPercent": 66
             },
             "JPY": {
-                "initial": 59800,
-                "final": 29900,
-                "discountPercent": 50
+                "initial": 98000,
+                "final": 33300,
+                "discountPercent": 66
             },
             "AUD": {
-                "initial": 850,
-                "final": 425,
-                "discountPercent": 50
+                "initial": 1450,
+                "final": 493,
+                "discountPercent": 66
             },
             "CAD": {
-                "initial": 649,
-                "final": 324,
-                "discountPercent": 50
+                "initial": 1149,
+                "final": 390,
+                "discountPercent": 66
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Paleological Prancer [30\u2605]\nFinish the Ruins bhop map.\n\nSnowboarding Enthusiast [30\u2605]\nFinish the hardest surf maps.\n\nRamping it Up [20\u2605]\nFinish the hardest ramp maps.\n\nHop to It [10\u2605]\nFinish all Hard and Very Hard bhop maps (except Ruins).",
+            "primary": 1,
+            "primaryText": "Legendawy Snipuwu [10\u2605]\nGet S rank in all missions on Hard.",
             "community": 0,
             "communityText": "",
-            "achievements": 57
+            "achievements": 26
         },
         "gameTags": [
+            "Action",
             "Indie",
-            "Sports",
-            "Parkour",
-            "Fast-Paced",
-            "Multiplayer"
+            "Anime",
+            "Female Protagonist",
+            "FPS",
+            "First-Person",
+            "Shooter",
+            "Sniper",
+            "Cute"
         ],
         "languages": {
-            "english": "IAS"
-        },
-        "information": "For the map \"Fast\", lock your FPS to 60fps in the settings in-game to ensure you get the proper launch at the start. Other FPS settings don't seem to work correctly.\n\n\nThe \"Welkin Blue\" achievement (ramp map) is broken. This can still be unlocked using a depot version of the game.  \nWithin the Steam Console, you can utilise the following command to download the relevant version with the achievement working:\n\n``download_depot 429780 429781 1557215148060610103``"
+            "english": "IS",
+            "japanese": "IS",
+            "koreana": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
+        }
     }
 }
-var gameOfDayDate = '2026-03-22'
+var gameOfDayDate = '2026-03-23'
