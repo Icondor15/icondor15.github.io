7e9e06618d0d41e03b887f5aa0bab3aff2fe23d6
Icondor <ivanfigus@gmail.com>
2026-03-25 08:49:22 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,85 +2,74 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 641080,
-        "CEId": "1e9a0bcc-4211-4ec4-8e93-6cc275dab6db",
-        "name": "Trials\u00ae Rising",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/641080/header.jpg?t=1680017258",
-        "genre": "Arcade",
-        "tier": 7,
-        "points": 800,
-        "createdAt": "2022-03-24T18:58:29.000Z",
+        "platformId": 2091540,
+        "CEId": "29a6ac99-8eab-4cd5-acea-8ae2519c8e4b",
+        "name": "RUSHAWAY",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2091540/header.jpg?t=1687195582",
+        "genre": "Platformer",
+        "tier": 1,
+        "points": 15,
+        "createdAt": "2024-03-25T14:50:26.000Z",
         "updatedAt": "2026-03-22T18:18:05.000Z",
-        "playersCompleted": 6,
-        "playersStarted": 47,
-        "playersTotal": 332,
+        "playersCompleted": 4,
+        "playersStarted": 0,
+        "playersTotal": 9,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 399,
-                "discountPercent": 80
+                "initial": 1099,
+                "final": 274,
+                "discountPercent": 75
             },
             "EUR": {
-                "initial": 1999,
-                "final": 399,
-                "discountPercent": 80
+                "initial": 1079,
+                "final": 269,
+                "discountPercent": 75
             },
             "GBP": {
-                "initial": 1699,
-                "final": 339,
-                "discountPercent": 80
+                "initial": 929,
+                "final": 232,
+                "discountPercent": 75
             },
             "JPY": {
-                "initial": 264000,
-                "final": 52800,
-                "discountPercent": 80
+                "initial": 130000,
+                "final": 32500,
+                "discountPercent": 75
             },
             "AUD": {
-                "initial": 2995,
-                "final": 599,
-                "discountPercent": 80
+                "initial": 1625,
+                "final": 406,
+                "discountPercent": 75
             },
             "CAD": {
-                "initial": 2699,
-                "final": 539,
-                "discountPercent": 80
+                "initial": 1399,
+                "final": 349,
+                "discountPercent": 75
             }
         },
         "milestones": {
-            "primary": 9,
-            "primaryText": "To End All Trials Part III [125\u2605]\nObtain 217 Diamond medals.\n\nTo End All Trials Part II [100\u2605]\nObtain 150 Diamond medals.\n\nTo End All Trials Part I [75\u2605]\nObtain 75 Diamond medals. \n\nTitan on Two Wheels [75\u2605]\nObtain all Ninja Diamonds.\n\nTrials J\u014dnin [125\u2605]\nPass all Ninja tracks.\n\nWay of the Ninja [100\u2605]\nPass Green Belt, Blue Belt, and Curse of the Demon.\n\nRise Above [25\u2605]\nPass White Belt, Orange Belt, Yellow Belt, and Rise of the Dragon.\n\nThe Mantis [125\u2605]\nObtain all 209 non-ninja Platinum medals.\n\nWorld Tour [50\u2605]\nObtain all 209 non-ninja Gold medals.",
-            "community": 15,
-            "communityText": "A Champion in the Making\nObtain any community Champion medal from a track other than Ninjas or Skill Games.\n\nA Champion Made\nObtain all community Champion medals.\n\nBreaking the Sound Barrier\nObtain any community Ultimate medal from a track other than Ninjas or Skill Games.\n\nCavemaniac\nComplete one of the baseline Level 5 tracks on either Fusion or Rising.\n\nDoughnuts and Bolts\nComplete one of the baseline Level 3 tracks on either Fusion or Rising.\n\nFinal Deal with The Devil\nComplete all 425 contracts.\n\nGiga Chadwick\nComplete one of the baseline Level 2 tracks on either Fusion or Rising.\n\nOut of Scope\nObtain all community Ultimate medals.\n\nRising Star\nComplete one of the baseline Level 6 tracks on either Fusion or Rising.\n\nSelling Your Soul\nComplete all non-ninja contracts.\n\nSensei of the Streets\nComplete each official Ninja track with 0 faults.\n\nTrials Legend\nComplete a Level 8 track on either Fusion or Rising.\n\nTrials Prodigy\nComplete one of the baseline Level 7 tracks on either Fusion or Rising.\n\nUltimate Ninja Warrior\nPass all level 7 baselines for either Fusion or Rising.\n\nZack Attack\nComplete one of the baseline Level 4 tracks on either Fusion or Rising.",
-            "achievements": 53
+            "primary": 2,
+            "primaryText": "Speck's Hell Rush [10\u2605]\nObtain a Platinum medal on every tower. \n\nCutting Corners [5\u2605]\nComplete the final tower.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 27
         },
         "gameTags": [
-            "Racing",
-            "Bikes",
-            "Physics",
-            "Multiplayer",
-            "Platformer",
-            "Competitive",
-            "Funny",
-            "Singleplayer",
-            "Sports",
             "Fast-Paced",
-            "Difficult"
+            "Precision Platformer",
+            "Physics",
+            "2.5D",
+            "Platformer",
+            "Action",
+            "Singleplayer",
+            "Indie",
+            "Retro",
+            "Visual Novel",
+            "2D"
         ],
         "languages": {
             "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "arabic": "IS",
-            "brazilian": "IS",
-            "dutch": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "tchinese": "IS"
-        },
-        "information": "Trials Rising is partially linked with Trials Fusion and Trials Evolution in terms of value. The value of each game should not be considered correct in isolation, and the value of the franchise should be seen as a solid unit worth 1400 points, divided amongst the three games\n\nThere is no recommended order to play the games in, however, the order in which you play will have a huge impact on the perceived difficulty of the others due to skill transfer. \n\nAmong games where skill transfer is a complicated problem to incorporate into the value, trials uniquely has two sets of skills that are built separately, speedrunning and ninjas. Because of this, attempting to balance the value across several games and several skillsets is too difficult to reliably reference any given objective in the franchise as an accurate comparison point.\n\n\n\nYou can find a sheet containing all baselines for Fusion and Rising here: \n\n[https://docs.google.com/spreadsheets/d/1MxPlhkVTz18hmWxdp5RnjnkWCU3LYPAWoNES2xZHA_Q/edit?usp=sharing](https://docs.google.com/spreadsheets/d/1MxPlhkVTz18hmWxdp5RnjnkWCU3LYPAWoNES2xZHA_Q/edit?usp=sharing)\n\nYou can find a link to the website for community medals here: https://www.trialscommunity.com/"
+            "portuguese": "IAS"
+        }
     }
 }
-var gameOfDayDate = '2026-03-24'
+var gameOfDayDate = '2026-03-25'
