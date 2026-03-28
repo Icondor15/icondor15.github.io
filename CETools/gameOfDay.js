c100bcf771de33d29deb5943bb348add18cd672c
Icondor <ivanfigus@gmail.com>
2026-03-28 07:50:20 +0100

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,85 +2,84 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1071280,
-        "CEId": "139ceec9-109c-4b12-8738-5ac08be0ac85",
-        "name": "Q - A Neon Platformer",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1071280/header.jpg?t=1626254689",
-        "genre": "Platformer",
+        "platformId": 1388230,
+        "CEId": "4d2aff40-2cf5-4e3a-b9b4-7d7a6586c2bd",
+        "name": "DANMAKAI: Red Forbidden Fruit",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1388230/header.jpg?t=1642839517",
+        "genre": "Bullet Hell",
         "tier": 3,
-        "points": 50,
-        "createdAt": "2022-03-27T20:42:56.000Z",
+        "points": 55,
+        "createdAt": "2022-03-28T07:38:58.000Z",
         "updatedAt": "2026-03-22T18:18:05.000Z",
-        "playersCompleted": 10,
-        "playersStarted": 7,
-        "playersTotal": 30,
+        "playersCompleted": 17,
+        "playersStarted": 0,
+        "playersTotal": 74,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 975,
+                "final": 975,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 850,
+                "final": 850,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 149000,
-                "final": 149000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Reinvent the Wheel [10\u2605]\nObtain both endings in under 4 hours.\n\nQ [20\u2605]\nComplete the final bonus level.\n\nSmooth Moves [10\u2605]\nObtain the dark ending.\n\nSquare Peg, Round Hole [10\u2605]\nBeat the game.",
+            "primary": 1,
+            "primaryText": "Danmakainya [55\u2605]\nClear Lunatic and Extra. Clear all spell cards with all characters.",
             "community": 0,
             "communityText": "",
-            "achievements": 23
+            "achievements": 11
         },
         "gameTags": [
-            "Platformer",
-            "Precision Platformer",
-            "Difficult",
-            "Colorful",
+            "Shoot 'Em Up",
+            "Bullet Hell",
+            "Shooter",
             "Arcade",
-            "2D Platformer",
+            "Female Protagonist",
             "2D",
-            "Retro",
-            "Cyberpunk",
-            "Controller",
-            "Stylized",
-            "Souls-like",
-            "Minimalist",
+            "Cute",
             "Action",
-            "Linear",
-            "Physics",
-            "Replay Value",
-            "Abstract",
-            "Sci-fi",
-            "Singleplayer"
+            "Pixel Graphics",
+            "Anime",
+            "Fantasy",
+            "Demons",
+            "Adventure",
+            "1990's",
+            "Singleplayer",
+            "Indie",
+            "Remake",
+            "Simulation",
+            "Top-Down Shooter",
+            "Controller"
         ],
         "languages": {
             "english": "IS",
-            "french": "IS",
-            "german": "IS",
-            "spanish": "IS"
+            "japanese": "IS",
+            "schinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-03-27'
+var gameOfDayDate = '2026-03-28'
