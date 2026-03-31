cabf90752ae5f13d58de07e64ac66c71891e201c
Icondor <ivanfigus@gmail.com>
2026-03-31 08:50:49 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,91 +2,82 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 553310,
-        "CEId": "c416132f-7098-435f-b752-523da1a3cda5",
-        "name": "Lethal League Blaze",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/553310/header.jpg?t=1630109321",
-        "genre": "Action",
+        "platformId": 388090,
+        "CEId": "849b5379-3bdc-4b4a-a786-f916c6ed2022",
+        "name": "Five Nights at Freddy's 4",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/388090/header.jpg?t=1579635988",
+        "genre": "Arcade",
         "tier": 1,
         "points": 5,
-        "createdAt": "2022-03-30T22:16:28.000Z",
+        "createdAt": "2022-08-01T21:15:33.000Z",
         "updatedAt": "2026-03-22T18:18:05.000Z",
-        "playersCompleted": 84,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 359,
+        "playersTotal": 183,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1799,
-                "final": 1799,
+                "initial": 579,
+                "final": 579,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 205000,
-                "final": 205000,
+                "initial": 82000,
+                "final": 82000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2895,
-                "final": 2895,
+                "initial": 1150,
+                "final": 1150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2279,
-                "final": 2279,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Funky Fresh [5\u2605]\nBeat the arcade mode without using any continues.",
+            "primaryText": "Teddy Bear Thriller [5\u2605]\nSurvive Night 8. / Survive Night 7 with \"Blind Mode\" + \"All Nightmare\" challenges enabled. / Survive Night 7 with \"Blind Mode\" + \"Mad Freddy\" + \"Insta-Foxy\" challenges enabled.",
             "community": 0,
             "communityText": "",
-            "achievements": 15
+            "achievements": 0
         },
         "gameTags": [
-            "Action",
+            "Horror",
+            "Survival Horror",
+            "Singleplayer",
+            "First-Person",
+            "Robots",
+            "Dark",
+            "Difficult",
+            "Point & Click",
+            "Psychological Horror",
+            "Survival",
+            "Strategy",
             "Indie",
-            "2D Fighter",
-            "Great Soundtrack",
-            "Sports",
-            "Fighting",
-            "Fast-Paced",
-            "Local Multiplayer",
-            "Competitive",
-            "Multiplayer",
-            "Arcade",
-            "Online Co-Op",
-            "Funny",
-            "4 Player Local",
-            "Soundtrack",
-            "eSports",
-            "2D",
-            "Stylized",
-            "Co-op",
-            "Anime"
+            "Story Rich",
+            "Atmospheric",
+            "Action",
+            "Family Friendly",
+            "Jump Scare",
+            "Simulation",
+            "Animation & Modeling",
+            "Illuminati"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "russian": "I",
-            "schinese": "I",
-            "tchinese": "I"
+            "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-03-30'
+var gameOfDayDate = '2026-03-31'
