ac9a0787f2307a8c98b2dea6ede96272ccaa3778
Icondor <ivanfigus@gmail.com>
2026-04-06 08:55:21 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,91 +2,91 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 282530,
-        "CEId": "4ec53840-b25e-4fb6-a043-9432e78c83b6",
-        "name": "Castlevania: Lords of Shadow \u2013 Mirror of Fate HD",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/282530/header.jpg?t=1631586604",
+        "platformId": 1334300,
+        "CEId": "91762f18-546d-4cba-9aaf-b55748841cae",
+        "name": "Charge Kid",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1334300/header.jpg?t=1624745112",
         "genre": [
-            "Action"
+            "Platformer"
         ],
-        "tier": 2,
-        "points": 25,
+        "tier": 1,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 2517,
-        "createdAt": "2022-04-05T13:12:15.000Z",
-        "updatedAt": "2026-04-04T20:49:30.000Z",
-        "playersCompleted": 10,
+        "medianPlaytime": 1098,
+        "createdAt": "2022-04-06T16:37:09.000Z",
+        "updatedAt": "2026-04-04T17:41:52.000Z",
+        "playersCompleted": 9,
         "playersStarted": 0,
-        "playersTotal": 144,
+        "playersTotal": 24,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 199,
+                "final": 199,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1199,
-                "final": 1199,
+                "initial": 159,
+                "final": 159,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 999,
-                "final": 999,
+                "initial": 169,
+                "final": 169,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 20500,
+                "final": 20500,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 295,
+                "final": 295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 229,
+                "final": 229,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Castle Crusher [20\u2605]\nBeat the boss rush on Hardcore mode.\n\nCastle Crasher [5\u2605]\nSeparately; finish Hardcore and beat the game in under 3 hours and 30 minutes.",
+            "primaryText": "Turing Approved [10\u2605]\nComplete the Secret% speedrun in under 12 minutes.\n\n\"I'm in.\" [5\u2605]\nBeat the secret level without dying. / Beat the 17th level without dying.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 23
+            "achievements": 15
         },
         "CETags": {
             "genre": [
-                "Metroidvania"
+                "Puzzle Platformer",
+                "2D Platformer"
             ],
             "info": []
         },
         "gameTags": [
-            "Action",
-            "Adventure",
-            "Metroidvania",
+            "Puzzle",
             "Platformer",
-            "Vampire",
-            "Hack and Slash",
-            "Singleplayer",
+            "Puzzle Platformer",
             "2D",
-            "Side Scroller",
-            "2.5D",
-            "Gothic",
-            "Fantasy",
-            "Atmospheric",
-            "Dark Fantasy",
-            "Dark",
-            "Horror"
+            "Pixel Graphics",
+            "Minimalist",
+            "Stylized",
+            "Retro",
+            "Cyberpunk",
+            "Indie",
+            "Adventure",
+            "Physics",
+            "Resource Management",
+            "Singleplayer",
+            "Precision Platformer",
+            "Difficult"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "spanish": "IS",
-            "brazilian": "IS"
-        },
-        "information": "To earn the hardcore achievement, DO NOT turn on navigation aids."
+            "english": "I"
+        }
     }
 }
-var gameOfDayDate = '2026-04-05'
+var gameOfDayDate = '2026-04-06'
