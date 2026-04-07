ff3b17f966a48ac9a6ae2c44dc3299e700cca153
Icondor <ivanfigus@gmail.com>
2026-04-07 08:50:18 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,57 +2,57 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1334300,
-        "CEId": "91762f18-546d-4cba-9aaf-b55748841cae",
-        "name": "Charge Kid",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1334300/header.jpg?t=1624745112",
+        "platformId": 213610,
+        "CEId": "55b8746d-0642-4d11-aca0-a9f8e7c4c6ef",
+        "name": "Sonic Adventure 2",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/213610/header.jpg?t=1572439482",
         "genre": [
             "Platformer"
         ],
-        "tier": 1,
-        "points": 15,
+        "tier": 2,
+        "points": 30,
         "secondaryPoints": 0,
-        "medianPlaytime": 1098,
-        "createdAt": "2022-04-06T16:37:09.000Z",
-        "updatedAt": "2026-04-04T17:41:52.000Z",
-        "playersCompleted": 9,
+        "medianPlaytime": 3858,
+        "createdAt": "2020-01-28T09:25:50.000Z",
+        "updatedAt": "2026-04-06T12:31:37.000Z",
+        "playersCompleted": 39,
         "playersStarted": 0,
-        "playersTotal": 24,
+        "playersTotal": 582,
         "priceData": {
             "USD": {
-                "initial": 199,
-                "final": 199,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 159,
-                "final": 159,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 169,
-                "final": 169,
+                "initial": 699,
+                "final": 699,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 20500,
-                "final": 20500,
+                "initial": 99000,
+                "final": 99000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 295,
-                "final": 295,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 229,
-                "final": 229,
+                "initial": 1349,
+                "final": 1349,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Turing Approved [10\u2605]\nComplete the Secret% speedrun in under 12 minutes.\n\n\"I'm in.\" [5\u2605]\nBeat the secret level without dying. / Beat the 17th level without dying.",
+            "primary": 1,
+            "primaryText": "Live and Learn [30\u2605]\nGet all A ranks in Story Mode / Beat all 3 Boss Attack Modes / Win an Expert Kart Race.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
@@ -61,32 +61,40 @@ var gameOfDay =
         },
         "CETags": {
             "genre": [
-                "Puzzle Platformer",
-                "2D Platformer"
+                "3D Platformer"
             ],
             "info": []
         },
         "gameTags": [
-            "Puzzle",
+            "Great Soundtrack",
+            "Action",
+            "3D Platformer",
             "Platformer",
-            "Puzzle Platformer",
-            "2D",
-            "Pixel Graphics",
-            "Minimalist",
-            "Stylized",
-            "Retro",
-            "Cyberpunk",
-            "Indie",
+            "Fast-Paced",
             "Adventure",
-            "Physics",
-            "Resource Management",
+            "Classic",
             "Singleplayer",
-            "Precision Platformer",
-            "Difficult"
+            "Replay Value",
+            "Third Person",
+            "Retro",
+            "Local Multiplayer",
+            "Story Rich",
+            "Family Friendly",
+            "3D",
+            "Multiplayer",
+            "Split Screen",
+            "Racing",
+            "Sci-fi",
+            "Comedy"
         ],
         "languages": {
-            "english": "I"
+            "english": "IA",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I"
         }
     }
 }
-var gameOfDayDate = '2026-04-06'
+var gameOfDayDate = '2026-04-07'
