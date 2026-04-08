4cbf88c8b5b153e130c49617470e9004fd88a628
Icondor <ivanfigus@gmail.com>
2026-04-08 08:50:37 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,100 +1,61 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 213610,
-        "CEId": "55b8746d-0642-4d11-aca0-a9f8e7c4c6ef",
-        "name": "Sonic Adventure 2",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/213610/header.jpg?t=1572439482",
+        "gameType": 1,
+        "platformId": 739290,
+        "CEId": "cea0c0ab-a21a-4999-b3e0-1c8d3675be36",
+        "name": "Dark Old Sun",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/739290/header.jpg?t=1600236570",
         "genre": [
-            "Platformer"
+            "Bullet Hell"
         ],
-        "tier": 2,
-        "points": 30,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
-        "medianPlaytime": 3858,
-        "createdAt": "2020-01-28T09:25:50.000Z",
-        "updatedAt": "2026-04-06T12:31:37.000Z",
-        "playersCompleted": 39,
+        "medianPlaytime": 874,
+        "createdAt": "2023-04-08T08:40:52.000Z",
+        "updatedAt": "2026-04-06T22:40:13.000Z",
+        "playersCompleted": 10,
         "playersStarted": 0,
-        "playersTotal": 582,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 699,
-                "final": 699,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 99000,
-                "final": 99000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1099,
-                "final": 1099,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1349,
-                "final": 1349,
-                "discountPercent": 0
-            }
-        },
+        "playersTotal": 103,
         "milestones": {
             "primary": 1,
-            "primaryText": "Live and Learn [30\u2605]\nGet all A ranks in Story Mode / Beat all 3 Boss Attack Modes / Win an Expert Kart Race.",
+            "primaryText": "I Walk a Lonely Void [10\u2605]\nBeat Arcade mode on \"Extreme\" difficulty. / Get the gold medal on all challenges. / Survive for more than 6 waves on Survival mode.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 15
+            "achievements": 44
         },
         "CETags": {
             "genre": [
-                "3D Platformer"
+                "Vertical Shoot'em'Up"
             ],
-            "info": []
+            "info": [
+                "Free"
+            ]
         },
         "gameTags": [
-            "Great Soundtrack",
             "Action",
-            "3D Platformer",
-            "Platformer",
-            "Fast-Paced",
-            "Adventure",
-            "Classic",
-            "Singleplayer",
-            "Replay Value",
-            "Third Person",
-            "Retro",
-            "Local Multiplayer",
-            "Story Rich",
-            "Family Friendly",
-            "3D",
-            "Multiplayer",
-            "Split Screen",
-            "Racing",
+            "Shoot 'Em Up",
+            "Shooter",
+            "2D",
+            "Bullet Hell",
+            "Indie",
+            "Space",
             "Sci-fi",
-            "Comedy"
+            "Singleplayer",
+            "Difficult",
+            "Replay Value",
+            "Controller",
+            "Soundtrack"
         ],
         "languages": {
-            "english": "IA",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I"
+            "english": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "tchinese": "I"
         }
     }
 }
-var gameOfDayDate = '2026-04-07'
+var gameOfDayDate = '2026-04-08'
