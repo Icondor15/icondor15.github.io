13934ac9b9737e331c4ff62597018d553d0489f9
Icondor <ivanfigus@gmail.com>
2026-04-12 08:51:18 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,89 +2,100 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 243160,
-        "CEId": "e89e6f7b-77ea-47d3-aab3-4c52a080a41e",
-        "name": "Mushroom 11",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/243160/header.jpg?t=1572465769",
+        "platformId": 312610,
+        "CEId": "5d6f2e97-7218-4376-b14d-33f4f011f78f",
+        "name": "METAL SLUG X",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/312610/header.jpg?t=1584640863",
         "genre": [
-            "Arcade"
+            "Action"
         ],
-        "tier": 2,
-        "points": 20,
+        "tier": 1,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 1561,
-        "createdAt": "2022-04-11T17:09:21.000Z",
-        "updatedAt": "2026-04-09T00:37:23.000Z",
-        "playersCompleted": 8,
+        "medianPlaytime": 947,
+        "createdAt": "2023-04-12T21:54:22.000Z",
+        "updatedAt": "2026-04-09T00:37:33.000Z",
+        "playersCompleted": 9,
         "playersStarted": 0,
-        "playersTotal": 142,
+        "playersTotal": 269,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 799,
+                "final": 799,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 559,
+                "final": 559,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 148000,
-                "final": 148000,
+                "initial": 79800,
+                "final": 79800,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 1150,
+                "final": 1150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 879,
+                "final": 879,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Terrible Trip [20\u2605]\nComplete each chapter individually without dying.",
+            "primaryText": "X Marks Aliens [15\u2605]\nBeat the game without using continues.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 35
+            "achievements": 15
         },
         "CETags": {
             "genre": [
-                "Awkward Movement",
-                "Puzzle Platformer",
-                "2D Platformer"
+                "Run & Gun"
             ],
             "info": []
         },
         "gameTags": [
-            "Indie",
             "Action",
-            "Adventure",
-            "Puzzle",
-            "Platformer",
-            "Puzzle Platformer",
-            "Physics",
-            "Experimental",
+            "Arcade",
+            "Co-op",
+            "Classic",
+            "Shoot 'Em Up",
             "2D",
-            "Atmospheric",
-            "Post-apocalyptic",
-            "Music",
-            "Difficult"
+            "Retro",
+            "Multiplayer",
+            "Side Scroller",
+            "Pixel Graphics",
+            "Shooter",
+            "Local Co-Op",
+            "Great Soundtrack",
+            "Difficult",
+            "Singleplayer",
+            "Old School",
+            "Adventure",
+            "Platformer",
+            "Anime",
+            "Local Multiplayer"
         ],
         "languages": {
-            "english": "I"
+            "english": "IA",
+            "french": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "koreana": "I",
+            "schinese": "I"
         }
     }
 }
-var gameOfDayDate = '2026-04-11'
+var gameOfDayDate = '2026-04-12'
