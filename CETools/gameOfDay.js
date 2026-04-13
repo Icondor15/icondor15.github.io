41d4d79fa6702c22743e0dfecee139de2ad69183
Icondor <ivanfigus@gmail.com>
2026-04-13 08:54:24 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,100 +2,105 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 312610,
-        "CEId": "5d6f2e97-7218-4376-b14d-33f4f011f78f",
-        "name": "METAL SLUG X",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/312610/header.jpg?t=1584640863",
+        "platformId": 1046030,
+        "CEId": "4eeaf4a6-cb29-4b4c-85a5-2db67ba1d865",
+        "name": "ISLANDERS",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1046030/header.jpg?t=1688541994",
         "genre": [
-            "Action"
+            "Strategy"
         ],
-        "tier": 1,
-        "points": 15,
+        "tier": 2,
+        "points": 20,
         "secondaryPoints": 0,
-        "medianPlaytime": 947,
-        "createdAt": "2023-04-12T21:54:22.000Z",
-        "updatedAt": "2026-04-09T00:37:33.000Z",
-        "playersCompleted": 9,
+        "medianPlaytime": 1110,
+        "createdAt": "2024-04-13T17:11:04.000Z",
+        "updatedAt": "2026-04-09T00:37:31.000Z",
+        "playersCompleted": 19,
         "playersStarted": 0,
-        "playersTotal": 269,
+        "playersTotal": 188,
         "priceData": {
             "USD": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
+                "initial": 499,
+                "final": 199,
+                "discountPercent": 60
             },
             "EUR": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
+                "initial": 499,
+                "final": 199,
+                "discountPercent": 60
             },
             "GBP": {
-                "initial": 559,
-                "final": 559,
-                "discountPercent": 0
+                "initial": 479,
+                "final": 191,
+                "discountPercent": 60
             },
             "JPY": {
-                "initial": 79800,
-                "final": 79800,
-                "discountPercent": 0
+                "initial": 62000,
+                "final": 24800,
+                "discountPercent": 60
             },
             "AUD": {
-                "initial": 1150,
-                "final": 1150,
-                "discountPercent": 0
+                "initial": 850,
+                "final": 340,
+                "discountPercent": 60
             },
             "CAD": {
-                "initial": 879,
-                "final": 879,
-                "discountPercent": 0
+                "initial": 669,
+                "final": 267,
+                "discountPercent": 60
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "X Marks Aliens [15\u2605]\nBeat the game without using continues.",
+            "primaryText": "My Land is Island [20\u2605]\nReach the 21st Island",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 15
+            "achievements": 38
         },
         "CETags": {
             "genre": [
-                "Run & Gun"
+                "Resource Management"
             ],
             "info": []
         },
         "gameTags": [
-            "Action",
-            "Arcade",
-            "Co-op",
-            "Classic",
-            "Shoot 'Em Up",
-            "2D",
-            "Retro",
-            "Multiplayer",
-            "Side Scroller",
-            "Pixel Graphics",
-            "Shooter",
-            "Local Co-Op",
-            "Great Soundtrack",
-            "Difficult",
+            "Relaxing",
+            "City Builder",
+            "Strategy",
+            "Building",
+            "Puzzle",
+            "Indie",
+            "Minimalist",
             "Singleplayer",
-            "Old School",
-            "Adventure",
-            "Platformer",
-            "Anime",
-            "Local Multiplayer"
+            "Beautiful",
+            "Colony Sim",
+            "Atmospheric",
+            "Colorful",
+            "Procedural Generation",
+            "Addictive",
+            "Replay Value",
+            "Score Attack",
+            "3D",
+            "Top-Down",
+            "Turn-Based Strategy",
+            "Multiplayer"
         ],
         "languages": {
-            "english": "IA",
+            "english": "I",
             "french": "I",
-            "italian": "I",
+            "german": "I",
             "japanese": "I",
             "spanish": "I",
+            "brazilian": "I",
             "koreana": "I",
-            "schinese": "I"
+            "latam": "I",
+            "polish": "I",
+            "russian": "I",
+            "schinese": "I",
+            "turkish": "I"
         }
     }
 }
-var gameOfDayDate = '2026-04-12'
+var gameOfDayDate = '2026-04-13'
