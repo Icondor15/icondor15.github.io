44a9833acc35b16c1d5f7a668ae236d7f320302f
Icondor <ivanfigus@gmail.com>
2026-04-15 08:53:19 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,93 +2,89 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 212480,
-        "CEId": "15dad01b-d40b-4c41-a635-4025f440edc5",
-        "name": "Sonic & All-Stars Racing Transformed Collection",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/212480/header.jpg?t=1648829972",
+        "platformId": 1702880,
+        "CEId": "d699c43c-dee3-422e-a48f-0827db4afa3d",
+        "name": "LinearShooter Remixed",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1702880/header.jpg?t=1639517791",
         "genre": [
-            "Arcade"
+            "Bullet Hell"
         ],
-        "tier": 2,
-        "points": 30,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
-        "medianPlaytime": 2605,
-        "createdAt": "2022-04-14T16:40:15.000Z",
-        "updatedAt": "2026-04-09T00:37:18.000Z",
-        "playersCompleted": 34,
-        "playersStarted": 8,
-        "playersTotal": 396,
+        "medianPlaytime": 243,
+        "createdAt": "2022-04-15T09:04:34.000Z",
+        "updatedAt": "2026-04-09T00:37:37.000Z",
+        "playersCompleted": 22,
+        "playersStarted": 0,
+        "playersTotal": 49,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 299,
+                "final": 299,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 1999,
+                "initial": 239,
+                "final": 239,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 209,
+                "final": 209,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 31000,
+                "final": 31000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2399,
-                "final": 2399,
+                "initial": 450,
+                "final": 450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 2699,
-                "final": 2699,
+                "initial": 339,
+                "final": 339,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "You're Too Slow [20\u2605]\nBeat all staff ghosts in time attack.\n\nGotta Go Fast [10\u2605]\nObtain all stars in World Tour and finish 1st place in all grand prix tournaments on expert difficulty.",
+            "primary": 1,
+            "primaryText": "Linear Shooting [10\u2605]\nClear Hard mode.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 50
+            "achievements": 13
         },
         "CETags": {
             "genre": [
-                "Racing"
+                "Rogue-like",
+                "Vertical Shoot'em'Up"
             ],
             "info": []
         },
         "gameTags": [
-            "Racing",
-            "Local Multiplayer",
-            "Multiplayer",
-            "Great Soundtrack",
-            "Local Co-Op",
-            "Sports",
-            "Split Screen",
-            "Arcade",
-            "Replay Value",
-            "4 Player Local",
-            "Controller",
-            "Family Friendly",
-            "Online Co-Op",
-            "Singleplayer",
-            "Fast-Paced",
-            "Colorful",
+            "Shoot 'Em Up",
             "Action",
-            "Co-op",
-            "Adventure",
-            "Touch-Friendly"
+            "Roguelite",
+            "Bullet Hell",
+            "Minimalist",
+            "Shooter",
+            "Score Attack",
+            "Procedural Generation",
+            "Controller",
+            "Indie",
+            "2D",
+            "Abstract",
+            "Singleplayer"
         ],
         "languages": {
-            "english": "IA",
-            "french": "IA",
-            "german": "IA",
-            "italian": "IA",
-            "spanish": "IA"
+            "english": "I",
+            "japanese": "I"
         }
     }
 }
-var gameOfDayDate = '2026-04-14'
+var gameOfDayDate = '2026-04-15'
