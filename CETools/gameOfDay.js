2b58f03f22d8aa3e03071c14c398380e70d98b76
Icondor <ivanfigus@gmail.com>
2026-04-16 08:50:55 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,89 +2,98 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1702880,
-        "CEId": "d699c43c-dee3-422e-a48f-0827db4afa3d",
-        "name": "LinearShooter Remixed",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1702880/header.jpg?t=1639517791",
+        "platformId": 3406610,
+        "CEId": "edf5a9b1-9bbe-4c78-aef9-73596055ccb9",
+        "name": "Teleport Master",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3406610/header.jpg?t=1742137219",
         "genre": [
-            "Bullet Hell"
+            "Arcade"
         ],
         "tier": 1,
-        "points": 10,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 243,
-        "createdAt": "2022-04-15T09:04:34.000Z",
-        "updatedAt": "2026-04-09T00:37:37.000Z",
-        "playersCompleted": 22,
+        "medianPlaytime": 0,
+        "createdAt": "2025-04-16T22:54:49.000Z",
+        "updatedAt": "2026-04-06T12:31:37.000Z",
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 49,
+        "playersTotal": 18,
         "priceData": {
             "USD": {
-                "initial": 299,
-                "final": 299,
-                "discountPercent": 0
+                "initial": 499,
+                "final": 399,
+                "discountPercent": 20
             },
             "EUR": {
-                "initial": 239,
-                "final": 239,
-                "discountPercent": 0
+                "initial": 499,
+                "final": 399,
+                "discountPercent": 20
             },
             "GBP": {
-                "initial": 209,
-                "final": 209,
-                "discountPercent": 0
+                "initial": 429,
+                "final": 343,
+                "discountPercent": 20
             },
             "JPY": {
-                "initial": 31000,
-                "final": 31000,
-                "discountPercent": 0
+                "initial": 49900,
+                "final": 39900,
+                "discountPercent": 20
             },
             "AUD": {
-                "initial": 450,
-                "final": 450,
-                "discountPercent": 0
+                "initial": 749,
+                "final": 599,
+                "discountPercent": 20
             },
             "CAD": {
-                "initial": 339,
-                "final": 339,
-                "discountPercent": 0
+                "initial": 649,
+                "final": 519,
+                "discountPercent": 20
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Linear Shooting [10\u2605]\nClear Hard mode.",
+            "primaryText": "Tumbling Witch [15\u2605]\nComplete the game on Master difficulty.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 13
+            "achievements": 0
         },
         "CETags": {
             "genre": [
-                "Rogue-like",
-                "Vertical Shoot'em'Up"
+                "2D Platformer",
+                "Awkward Movement"
             ],
             "info": []
         },
         "gameTags": [
-            "Shoot 'Em Up",
-            "Action",
-            "Roguelite",
-            "Bullet Hell",
-            "Minimalist",
-            "Shooter",
-            "Score Attack",
-            "Procedural Generation",
-            "Controller",
-            "Indie",
+            "Cute",
+            "Casual",
             "2D",
-            "Abstract",
-            "Singleplayer"
+            "Pixel Graphics",
+            "2D Platformer",
+            "Adventure",
+            "Walking Simulator",
+            "Magic",
+            "God Game",
+            "Side Scroller",
+            "Cartoony",
+            "Hand-drawn",
+            "1990's",
+            "Emotional",
+            "Game Development",
+            "Retro",
+            "Family Friendly",
+            "Medieval",
+            "Fantasy",
+            "Parody "
         ],
         "languages": {
-            "english": "I",
-            "japanese": "I"
+            "english": "IS",
+            "japanese": "IS",
+            "koreana": "IS",
+            "schinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-04-15'
+var gameOfDayDate = '2026-04-16'
