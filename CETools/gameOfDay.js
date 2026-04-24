5cedb75d090aaac9a1ae02153ba3d7400697523b
Icondor <ivanfigus@gmail.com>
2026-04-24 08:50:30 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,108 +2,79 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1293170,
-        "CEId": "1c957f86-a960-4d6a-8c0a-3b4a421aed33",
-        "name": "BZZZT",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1293170/header.jpg?t=1712333380",
+        "platformId": 815110,
+        "CEId": "3c5f121d-5081-4b05-9d63-9fcbdc0cdcb7",
+        "name": "Conc Jump",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/815110/header.jpg?t=1589246743",
         "genre": [
-            "Platformer"
+            "First-Person"
         ],
-        "tier": 1,
-        "points": 5,
-        "secondaryPoints": 10,
-        "medianPlaytime": 667,
-        "createdAt": "2024-04-23T18:30:27.000Z",
+        "tier": 3,
+        "points": 60,
+        "secondaryPoints": 0,
+        "medianPlaytime": 906,
+        "createdAt": "2022-04-24T20:02:02.000Z",
         "updatedAt": "2026-04-20T18:31:22.000Z",
-        "playersCompleted": 25,
-        "playersStarted": 0,
-        "playersTotal": 113,
+        "playersCompleted": 5,
+        "playersStarted": 4,
+        "playersTotal": 27,
         "priceData": {
             "USD": {
-                "initial": 1199,
-                "final": 299,
-                "discountPercent": 75
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
             },
             "EUR": {
-                "initial": 1179,
-                "final": 294,
-                "discountPercent": 75
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
             },
             "GBP": {
-                "initial": 999,
-                "final": 249,
-                "discountPercent": 75
+                "initial": 399,
+                "final": 399,
+                "discountPercent": 0
             },
             "JPY": {
-                "initial": 140000,
-                "final": 35000,
-                "discountPercent": 75
+                "initial": 52000,
+                "final": 52000,
+                "discountPercent": 0
             },
             "AUD": {
-                "initial": 1775,
-                "final": 443,
-                "discountPercent": 75
+                "initial": 750,
+                "final": 750,
+                "discountPercent": 0
             },
             "CAD": {
-                "initial": 1549,
-                "final": 387,
-                "discountPercent": 75
+                "initial": 569,
+                "final": 569,
+                "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Machine Learning [5\u2605]\nSeparately; Finish the game on Insane difficulty and perform a golden flawless run on all story mode stages.",
-            "secondary": 1,
-            "secondaryText": "Badbert and the Terrible, Horrible, No Good, Very Bad Day [10\u2605]\nBeat every level on Insane difficulty.",
-            "community": 1,
-            "communityText": "Mr. Robot\nFinish the game on Insane difficulty without losing a life.",
-            "achievements": 25
+            "primary": 2,
+            "primaryText": "Conc Master [30\u2605]\nComplete the conc jumping campaign.\n\nMeet the Soldier [30\u2605]\nComplete the rocket jumping campaign.",
+            "secondary": 0,
+            "secondaryText": "",
+            "community": 0,
+            "communityText": "",
+            "achievements": 3
         },
         "CETags": {
             "genre": [
-                "2D Platformer"
+                "First Person Movement"
             ],
-            "info": [
-                "Has Community Objectives",
-                "Has Secondary Objectives"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Precision Platformer",
-            "Pixel Graphics",
-            "Score Attack",
-            "2D Platformer",
-            "Soundtrack",
-            "Difficult",
-            "Funny",
-            "Time Attack",
-            "Controller",
-            "2D",
-            "Cute",
-            "Colorful",
-            "Fast-Paced",
-            "Action",
-            "Platformer",
             "Indie",
-            "Robots",
-            "Singleplayer",
-            "Arcade",
-            "Perma Death"
+            "FPS",
+            "Difficult",
+            "Time Attack",
+            "Puzzle Platformer"
         ],
         "languages": {
-            "english": "IS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "czech": "IS",
-            "polish": "IS",
-            "schinese": "IS",
-            "swedish": "IS",
-            "turkish": "IS"
+            "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-04-23'
+var gameOfDayDate = '2026-04-24'
