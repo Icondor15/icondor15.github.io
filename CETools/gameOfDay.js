664549d5b406be7b1c1cb9f0e6c97c32c77b119a
Icondor <ivanfigus@gmail.com>
2026-04-25 08:50:48 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,79 +2,94 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 815110,
-        "CEId": "3c5f121d-5081-4b05-9d63-9fcbdc0cdcb7",
-        "name": "Conc Jump",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/815110/header.jpg?t=1589246743",
+        "platformId": 618690,
+        "CEId": "d39c3e9a-2f27-4dea-9777-95af2e82df5d",
+        "name": "Gorescript",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/618690/header.jpg?t=1581594317",
         "genre": [
             "First-Person"
         ],
         "tier": 3,
         "points": 60,
         "secondaryPoints": 0,
-        "medianPlaytime": 906,
-        "createdAt": "2022-04-24T20:02:02.000Z",
+        "medianPlaytime": 801,
+        "createdAt": "2022-04-25T20:13:15.000Z",
         "updatedAt": "2026-04-20T18:31:22.000Z",
         "playersCompleted": 5,
-        "playersStarted": 4,
-        "playersTotal": 27,
+        "playersStarted": 3,
+        "playersTotal": 58,
         "priceData": {
             "USD": {
+                "initial": 599,
+                "final": 599,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 589,
+                "final": 589,
+                "discountPercent": 0
+            },
+            "GBP": {
                 "initial": 499,
                 "final": 499,
                 "discountPercent": 0
             },
-            "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
             "JPY": {
-                "initial": 52000,
-                "final": 52000,
+                "initial": 70000,
+                "final": 70000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 879,
+                "final": 879,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 569,
-                "final": 569,
+                "initial": 779,
+                "final": 779,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Conc Master [30\u2605]\nComplete the conc jumping campaign.\n\nMeet the Soldier [30\u2605]\nComplete the rocket jumping campaign.",
+            "primary": 3,
+            "primaryText": "Shooting in the Dark [35\u2605]\nFinish the game on \"Masochistic Blackout Permadeath\" mode.\n\nQuick Draw [15\u2605]\nFinish all levels in under a minute separately.\n\nHardcore Gore [10\u2605]\nFinish the game on \"Masochistic\" difficulty.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 3
+            "achievements": 28
         },
         "CETags": {
             "genre": [
-                "First Person Movement"
+                "First Person Shooter"
             ],
             "info": []
         },
         "gameTags": [
-            "Indie",
             "FPS",
+            "Shooter",
+            "First-Person",
+            "Voxel",
+            "3D",
+            "Controller",
+            "Action",
+            "Colorful",
+            "Retro",
+            "Old School",
+            "Combat",
+            "Indie",
+            "Singleplayer",
+            "Great Soundtrack",
+            "Survival",
             "Difficult",
-            "Time Attack",
-            "Puzzle Platformer"
+            "Action-Adventure",
+            "Gore",
+            "Pixel Graphics",
+            "Free to Play"
         ],
         "languages": {
             "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-04-24'
+var gameOfDayDate = '2026-04-25'
