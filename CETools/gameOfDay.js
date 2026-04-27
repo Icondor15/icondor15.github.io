a61260d0b8ce77d23f4b1bdefb5b466e63578fe0
Icondor <ivanfigus@gmail.com>
2026-04-27 08:21:38 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,95 +2,89 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 509820,
-        "CEId": "ad06e4bb-f41b-4cd3-8357-c67c55f4d392",
-        "name": "Super Grav",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/509820/header.jpg?t=1713987493",
+        "platformId": 924650,
+        "CEId": "18f5b9b3-4cd6-4930-82b2-3f732b351038",
+        "name": "Hifuu Nightmare Diary ~ Violet Detector. ",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/924650/header.jpg?t=1667286247",
         "genre": [
-            "Arcade"
+            "Bullet Hell"
         ],
-        "tier": 4,
-        "points": 110,
+        "tier": 1,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 1961,
-        "createdAt": "2024-04-26T02:36:35.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2023-04-27T01:27:30.000Z",
         "updatedAt": "2026-04-25T16:11:02.000Z",
-        "playersCompleted": 1,
-        "playersStarted": 1,
-        "playersTotal": 13,
+        "playersCompleted": 10,
+        "playersStarted": 0,
+        "playersTotal": 48,
         "priceData": {
             "USD": {
-                "initial": 599,
-                "final": 599,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 589,
-                "final": 589,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 499,
-                "final": 499,
+                "initial": 829,
+                "final": 829,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 70000,
-                "final": 70000,
+                "initial": 110000,
+                "final": 110000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 879,
-                "final": 879,
+                "initial": 1550,
+                "final": 1550,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 779,
-                "final": 779,
+                "initial": 1249,
+                "final": 1249,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "spACEd [80\u2605]\nComplete a full run of all missions while acing every level.\n\nGraceful Space Racer [15\u2605]\nBeat the demo bogey time on all Race and Gravitation maps.\n\nSpacifist [10\u2605]\nComplete a full run of all missions without firing a weapon or crashing into enemies.\n\nIntergalactic Expedition [5\u2605]\nAce all missions.",
+            "primary": 1,
+            "primaryText": "Violet President [15\u2605]\nClear all Dream Diaries.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 14
+            "achievements": 0
         },
         "CETags": {
             "genre": [
-                "Racing",
-                "Awkward Movement"
+                "Vertical Shoot'em'Up"
             ],
-            "info": []
+            "info": [
+                "Female Protagonist"
+            ]
         },
         "gameTags": [
-            "Action",
-            "Racing",
-            "Pixel Graphics",
-            "Shoot 'Em Up",
-            "2D",
-            "Arcade",
-            "Shooter",
-            "Combat Racing",
-            "Retro",
             "Indie",
-            "PvP",
-            "4 Player Local",
-            "Co-op",
-            "Co-op Campaign",
-            "Local Co-Op",
-            "Local Multiplayer",
-            "Multiplayer",
+            "Action",
+            "Bullet Hell",
+            "Great Soundtrack",
+            "Faith",
+            "Shoot 'Em Up",
+            "Female Protagonist",
+            "Difficult",
             "Singleplayer",
-            "Combat",
-            "Bullet Hell"
+            "2D",
+            "Anime",
+            "Arcade",
+            "Cute"
         ],
         "languages": {
-            "english": "I"
+            "japanese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-04-26'
+var gameOfDayDate = '2026-04-27'
