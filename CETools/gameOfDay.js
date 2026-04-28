4cd4d130d250c284e20c113164da23e0e9b870f9
Icondor <ivanfigus@gmail.com>
2026-04-28 13:14:56 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,89 +2,99 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 924650,
-        "CEId": "18f5b9b3-4cd6-4930-82b2-3f732b351038",
-        "name": "Hifuu Nightmare Diary ~ Violet Detector. ",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/924650/header.jpg?t=1667286247",
+        "platformId": 345820,
+        "CEId": "f49a8fac-f5d5-497c-a672-003947e9e548",
+        "name": "Shantae and the Pirate's Curse",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/345820/header.jpg?t=1572910769",
         "genre": [
-            "Bullet Hell"
+            "Action"
         ],
         "tier": 1,
-        "points": 15,
+        "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2023-04-27T01:27:30.000Z",
+        "medianPlaytime": 1187,
+        "createdAt": "2022-04-28T06:13:16.000Z",
         "updatedAt": "2026-04-25T16:11:02.000Z",
-        "playersCompleted": 10,
+        "playersCompleted": 28,
         "playersStarted": 0,
-        "playersTotal": 48,
+        "playersTotal": 297,
         "priceData": {
             "USD": {
-                "initial": 1099,
-                "final": 1099,
-                "discountPercent": 0
+                "initial": 1999,
+                "final": 999,
+                "discountPercent": 50
             },
             "EUR": {
-                "initial": 899,
-                "final": 899,
-                "discountPercent": 0
+                "initial": 1999,
+                "final": 999,
+                "discountPercent": 50
             },
             "GBP": {
-                "initial": 829,
-                "final": 829,
-                "discountPercent": 0
+                "initial": 1499,
+                "final": 749,
+                "discountPercent": 50
             },
             "JPY": {
-                "initial": 110000,
-                "final": 110000,
-                "discountPercent": 0
+                "initial": 198000,
+                "final": 99000,
+                "discountPercent": 50
             },
             "AUD": {
-                "initial": 1550,
-                "final": 1550,
-                "discountPercent": 0
+                "initial": 2895,
+                "final": 1447,
+                "discountPercent": 50
             },
             "CAD": {
-                "initial": 1249,
-                "final": 1249,
-                "discountPercent": 0
+                "initial": 2199,
+                "final": 1099,
+                "discountPercent": 50
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Violet President [15\u2605]\nClear all Dream Diaries.",
+            "primaryText": "Dance Through the Danger [5\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 0
+            "achievements": 44
         },
         "CETags": {
             "genre": [
-                "Vertical Shoot'em'Up"
+                "Action-Platformer",
+                "Metroidvania"
             ],
-            "info": [
-                "Female Protagonist"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Indie",
-            "Action",
-            "Bullet Hell",
-            "Great Soundtrack",
-            "Faith",
-            "Shoot 'Em Up",
+            "Platformer",
+            "Metroidvania",
             "Female Protagonist",
-            "Difficult",
-            "Singleplayer",
+            "Great Soundtrack",
             "2D",
+            "Pixel Graphics",
+            "Action",
+            "Adventure",
             "Anime",
-            "Arcade",
-            "Cute"
+            "Indie",
+            "Cute",
+            "Singleplayer",
+            "Pirates",
+            "Funny",
+            "Side Scroller",
+            "RPG",
+            "Difficult",
+            "Sexual Content",
+            "Fantasy",
+            "Comedy"
         ],
         "languages": {
-            "japanese": "IS"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "spanish": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-04-27'
+var gameOfDayDate = '2026-04-28'
