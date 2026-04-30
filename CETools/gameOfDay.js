4f3a821ed72b4b747c93df2b9f1d089417c3ceb0
Icondor <ivanfigus@gmail.com>
2026-04-30 08:51:02 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,95 +2,108 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2305880,
-        "CEId": "2bcd0976-2735-48aa-878f-345ede14fb30",
-        "name": "Hexapoda",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2305880/header.jpg?t=1710863007",
+        "platformId": 210970,
+        "CEId": "93847607-3a88-46a1-8834-c75482290eb0",
+        "name": "The Witness",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/210970/header.jpg?t=1598383120",
         "genre": [
-            "Bullet Hell"
+            "Strategy"
         ],
         "tier": 1,
         "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 319,
-        "createdAt": "2024-04-29T04:55:17.000Z",
-        "updatedAt": "2026-04-25T16:11:02.000Z",
-        "playersCompleted": 2,
+        "medianPlaytime": 1908,
+        "createdAt": "2022-04-30T05:58:04.000Z",
+        "updatedAt": "2026-04-30T03:45:19.000Z",
+        "playersCompleted": 256,
         "playersStarted": 0,
-        "playersTotal": 11,
+        "playersTotal": 901,
         "priceData": {
             "USD": {
-                "initial": 1299,
-                "final": 1299,
-                "discountPercent": 0
+                "initial": 3999,
+                "final": 999,
+                "discountPercent": 75
             },
             "EUR": {
-                "initial": 1299,
-                "final": 1299,
-                "discountPercent": 0
+                "initial": 3699,
+                "final": 924,
+                "discountPercent": 75
             },
             "GBP": {
-                "initial": 1099,
-                "final": 1099,
-                "discountPercent": 0
+                "initial": 2999,
+                "final": 749,
+                "discountPercent": 75
             },
             "JPY": {
-                "initial": 150000,
-                "final": 150000,
-                "discountPercent": 0
+                "initial": 398000,
+                "final": 99500,
+                "discountPercent": 75
             },
             "AUD": {
-                "initial": 1895,
-                "final": 1895,
-                "discountPercent": 0
+                "initial": 5695,
+                "final": 1423,
+                "discountPercent": 75
             },
             "CAD": {
-                "initial": 1699,
-                "final": 1699,
-                "discountPercent": 0
+                "initial": 4399,
+                "final": 1099,
+                "discountPercent": 75
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Stick Bugged [5\u2605]\nBeat the boss rush without using continues.",
+            "primaryText": "Mountain King [5\u2605]\nComplete the Challenge.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 35
+            "achievements": 2
         },
         "CETags": {
             "genre": [
-                "Vertical Shoot'em'Up"
+                "Puzzle"
             ],
             "info": []
         },
         "gameTags": [
-            "Shoot 'Em Up",
-            "Bullet Hell",
-            "Arcade",
-            "2D",
-            "Action",
-            "Top-Down Shooter",
-            "Shooter",
-            "4 Player Local",
-            "Pixel Graphics",
-            "Stylized",
-            "Top-Down",
-            "Post-apocalyptic",
-            "Retro",
-            "1980s",
-            "1990's",
-            "Multiple Endings",
-            "Nature",
-            "Futuristic",
-            "Sci-fi",
-            "Old School"
+            "Puzzle",
+            "Exploration",
+            "First-Person",
+            "Open World",
+            "Singleplayer",
+            "Colorful",
+            "Indie",
+            "Atmospheric",
+            "Difficult",
+            "Walking Simulator",
+            "Adventure",
+            "Mystery",
+            "Nonlinear",
+            "Philosophical",
+            "Relaxing",
+            "Surreal",
+            "Casual",
+            "Great Soundtrack",
+            "Story Rich",
+            "Beautiful"
         ],
         "languages": {
-            "english": "IS",
-            "french": "IS"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "arabic": "IS",
+            "brazilian": "IS",
+            "hungarian": "IS",
+            "koreana": "IS",
+            "polish": "IS",
+            "portuguese": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-04-29'
+var gameOfDayDate = '2026-04-30'
