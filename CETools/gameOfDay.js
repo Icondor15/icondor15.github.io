131b71a3a614a8436eb614d0c4cbd13595c57a1c
Icondor <ivanfigus@gmail.com>
2026-05-03 08:50:04 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,96 +2,99 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 98400,
-        "CEId": "2ccddc1f-ab60-4b40-9d22-09c07e943414",
-        "name": "Hard Reset Extended Edition",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/98400/header.jpg?t=1505118773",
+        "platformId": 38740,
+        "CEId": "21f58fea-75de-4064-ae4e-38077f538cb8",
+        "name": "EDGE",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/38740/header.jpg?t=1596256602",
         "genre": [
-            "First-Person"
+            "Platformer"
         ],
-        "tier": 2,
-        "points": 25,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
-        "medianPlaytime": 2279,
-        "createdAt": "2022-05-02T19:32:31.000Z",
-        "updatedAt": "2026-05-01T17:24:45.000Z",
-        "playersCompleted": 9,
-        "playersStarted": 0,
-        "playersTotal": 320,
+        "medianPlaytime": 1407,
+        "createdAt": "2022-05-03T22:32:37.000Z",
+        "updatedAt": "2026-05-02T17:21:07.000Z",
+        "playersCompleted": 12,
+        "playersStarted": 20,
+        "playersTotal": 361,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 699,
+                "final": 699,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 689,
+                "final": 689,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1279,
-                "final": 1279,
+                "initial": 589,
+                "final": 589,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 170000,
-                "final": 170000,
+                "initial": 80000,
+                "final": 80000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2195,
-                "final": 2195,
+                "initial": 1025,
+                "final": 1025,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1949,
-                "final": 1949,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Exalted Exile [25\u2605]\nComplete all Bezoar and Barrens levels on Heroic mode.",
+            "primary": 2,
+            "primaryText": "Speedhypercuber [5\u2605]\nGet an S+ rank on all extended and bonus levels.\n\nSpeedcuber [5\u2605]\nGet an S+ rank on all normal levels.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 115
+            "achievements": 47
         },
         "CETags": {
             "genre": [
-                "First Person Shooter"
+                "Puzzle Platformer"
             ],
             "info": []
         },
         "gameTags": [
-            "FPS",
-            "Cyberpunk",
-            "Action",
-            "Sci-fi",
-            "Shooter",
+            "Puzzle Platformer",
+            "Puzzle",
             "Indie",
+            "Action",
+            "Platformer",
+            "Great Soundtrack",
             "Singleplayer",
-            "First-Person",
-            "Dystopian ",
-            "Robots",
-            "Difficult",
-            "3D Vision",
-            "Violent",
-            "Old School",
-            "Atmospheric",
-            "Gore",
-            "Boomer Shooter"
+            "Isometric",
+            "Casual",
+            "Abstract",
+            "3D Platformer",
+            "Retro",
+            "Time Attack",
+            "Minimalist",
+            "Difficult"
         ],
         "languages": {
-            "english": "IA",
-            "german": "IA",
-            "japanese": "I",
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "spanish": "I",
             "czech": "I",
+            "dutch": "I",
+            "finnish": "I",
             "polish": "I",
-            "russian": "I"
+            "portuguese": "I",
+            "turkish": "I"
         }
     }
 }
-var gameOfDayDate = '2026-05-02'
+var gameOfDayDate = '2026-05-03'
