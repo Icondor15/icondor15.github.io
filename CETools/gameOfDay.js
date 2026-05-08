7e4871034f34a967a3b7d8dd5bbdb2632bcc3bf8
Icondor <ivanfigus@gmail.com>
2026-05-08 08:50:13 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,98 +1,48 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 2761620,
-        "CEId": "9cf4364b-02ca-42d4-a094-6f9264a2a5ab",
-        "name": "13 Seconds",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2761620/header.jpg?t=1710307725",
+        "gameType": 3,
+        "platformId": 8868,
+        "CEId": "ad84a8cc-611f-4534-90d3-1fc2d588e9c6",
+        "name": "Ginga Fukei Densetsu Sapphire",
+        "header": "https://upload.cedb.me/ad84a8cc-611f-4534-90d3-1fc2d588e9c6.png",
         "genre": [
             "Bullet Hell"
         ],
         "tier": 1,
-        "points": 10,
+        "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 255,
-        "createdAt": "2024-05-07T02:19:33.000Z",
-        "updatedAt": "2026-05-03T23:06:35.000Z",
-        "playersCompleted": 7,
+        "medianPlaytime": 0,
+        "createdAt": "2024-05-08T21:21:16.000Z",
+        "updatedAt": "2026-05-03T19:20:40.000Z",
+        "playersCompleted": 2,
         "playersStarted": 0,
-        "playersTotal": 33,
-        "priceData": {
-            "USD": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 399,
-                "final": 399,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 339,
-                "final": 339,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 47000,
-                "final": 47000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 595,
-                "final": 595,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 499,
-                "final": 499,
-                "discountPercent": 0
-            }
+        "playersTotal": 6,
+        "gameTags": [
+            "Shoot 'em Up"
+        ],
+        "consoleData": {
+            "name": "PC Engine CD/TurboGrafx-CD",
+            "family": "NEC",
+            "icon": "https://static.retroachievements.org/assets/images/system/pccd.png"
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "v13 Bots [10\u2605]\nObtain an item 11 times in a row without missing it. / Reach level 13.",
-            "secondary": 0,
-            "secondaryText": "",
+            "primaryText": "Thin Blue Line [5\u2605]\nClear the game on Hard difficulty.",
+            "secondary": 1,
+            "secondaryText": "Time Solution [0\u2605]\nClear the game on Hard difficulty without continues.",
             "community": 0,
             "communityText": "",
-            "achievements": 7
+            "achievements": 21
         },
         "CETags": {
             "genre": [
-                "Twin Stick",
-                "Avoid'em'Up",
-                "Multitasking"
+                "Vertical Shoot'em'Up"
             ],
-            "info": []
-        },
-        "gameTags": [
-            "Action",
-            "Casual",
-            "Bullet Hell",
-            "Roguelike",
-            "Shooter",
-            "Arcade",
-            "Pixel Graphics",
-            "Cyberpunk",
-            "Shoot 'Em Up",
-            "2D",
-            "Cartoony",
-            "Sci-fi",
-            "Robots",
-            "PvE",
-            "Controller",
-            "Singleplayer"
-        ],
-        "languages": {
-            "english": "IAS",
-            "japanese": "IAS",
-            "spanish": "IAS",
-            "koreana": "IAS",
-            "schinese": "IAS",
-            "tchinese": "IAS"
+            "info": [
+                "Has Secondary Objectives"
+            ]
         }
     }
 }
-var gameOfDayDate = '2026-05-07'
+var gameOfDayDate = '2026-05-08'
