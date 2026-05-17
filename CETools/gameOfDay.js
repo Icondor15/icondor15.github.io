26d593e07cfc8144e7138506b9d3775840f243cb
Icondor <ivanfigus@gmail.com>
2026-05-17 08:49:47 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,94 +2,100 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2107520,
-        "CEId": "0aef72d9-c926-4096-aeb1-635dbc32e3a2",
-        "name": "Krimson",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2107520/header.jpg?t=1711050023",
+        "platformId": 1037130,
+        "CEId": "81360953-410d-4cdd-8cf7-baee2c3866b7",
+        "name": "Dandy Ace",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1037130/header.jpg?t=1620854766",
         "genre": [
-            "Platformer"
+            "Action"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 2,
+        "points": 30,
         "secondaryPoints": 0,
-        "medianPlaytime": 544,
-        "createdAt": "2025-05-16T05:30:55.000Z",
-        "updatedAt": "2026-05-15T01:35:55.000Z",
-        "playersCompleted": 1,
+        "medianPlaytime": 1067,
+        "createdAt": "2021-05-17T18:28:36.000Z",
+        "updatedAt": "2026-05-16T21:06:32.000Z",
+        "playersCompleted": 11,
         "playersStarted": 0,
-        "playersTotal": 11,
+        "playersTotal": 91,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 975,
-                "final": 975,
+                "initial": 1679,
+                "final": 1679,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 850,
-                "final": 850,
+                "initial": 1549,
+                "final": 1549,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 120000,
-                "final": 120000,
+                "initial": 205000,
+                "final": 205000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 2895,
+                "final": 2895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 2279,
+                "final": 2279,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Wait1ng 4 Y0u in Hell [10\u2605]\nComplete each level in the game without dying.",
+            "primaryText": "A True Magician [30\u2605]\nDefeat Lele on Nightmare difficulty.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 12
+            "achievements": 26
         },
         "CETags": {
             "genre": [
-                "2D Platformer"
+                "Rogue-like"
             ],
             "info": []
         },
         "gameTags": [
-            "Precision Platformer",
-            "Rhythm",
-            "Horror",
-            "Psychological Horror",
-            "2D Platformer",
-            "Platformer",
-            "2D",
-            "Dark",
-            "Psychedelic",
-            "Atmospheric",
             "Action",
-            "Abstract",
-            "Puzzle",
-            "Pixel Graphics",
-            "Dark Fantasy",
-            "Demons",
-            "Adventure",
-            "Psychological",
-            "Sci-fi",
-            "Thriller"
+            "Indie",
+            "Roguelite",
+            "Isometric",
+            "Roguelike",
+            "Dungeon Crawler",
+            "Difficult",
+            "Magic",
+            "Singleplayer",
+            "Procedural Generation",
+            "Perma Death",
+            "Replay Value",
+            "Atmospheric",
+            "2D",
+            "Hand-drawn",
+            "Action Roguelike"
         ],
         "languages": {
-            "english": "I"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "brazilian": "IAS",
+            "koreana": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-05-16'
+var gameOfDayDate = '2026-05-17'
