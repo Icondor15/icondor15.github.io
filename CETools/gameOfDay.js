7ed590f20d30257db452d6af53c7ff86233c636d
Icondor <ivanfigus@gmail.com>
2026-05-20 08:40:05 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,93 +2,110 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2336290,
-        "CEId": "63117566-1edb-4357-8e4b-29090ce8a0a3",
-        "name": "Well...",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/2336290/header.jpg?t=1680490592",
+        "platformId": 956140,
+        "CEId": "8c6551c6-e391-46c6-a028-db438c2e6ef0",
+        "name": "Lifeslide",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/956140/header.jpg?t=1673512195",
         "genre": [
-            "First-Person"
+            "Arcade"
         ],
         "tier": 1,
-        "points": 10,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 302,
-        "createdAt": "2023-05-19T00:43:28.000Z",
-        "updatedAt": "2026-05-17T13:19:55.000Z",
+        "medianPlaytime": 718,
+        "createdAt": "2023-05-20T00:06:41.000Z",
+        "updatedAt": "2026-05-15T01:35:55.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 11,
-        "playersStarted": 1,
-        "playersTotal": 20,
+        "playersCompleted": 6,
+        "playersStarted": 0,
+        "playersTotal": 30,
         "priceData": {
             "USD": {
-                "initial": 399,
-                "final": 399,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 399,
-                "final": 399,
+                "initial": 819,
+                "final": 819,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 339,
-                "final": 339,
+                "initial": 719,
+                "final": 719,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 47000,
-                "final": 47000,
+                "initial": 101000,
+                "final": 101000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 595,
-                "final": 595,
+                "initial": 1450,
+                "final": 1450,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 1149,
+                "final": 1149,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "If only Timmy was as skilled as you [5\u2605]\nBeat the game in under 10 minutes.\n\nWell... I did it! [5\u2605]\nBeat the game.",
+            "primary": 1,
+            "primaryText": "Zen Trip [15\u2605]\nBeat the entire game without dying.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 0,
-            "communityText": "",
-            "achievements": 10
+            "community": 1,
+            "communityText": "Professional Paper Pilot\nBeat the entire game without dying using the plane with maximum speed.",
+            "achievements": 31
         },
         "CETags": {
             "genre": [
-                "First Person Movement"
+                "Sports"
             ],
             "info": []
         },
         "gameTags": [
-            "Adventure",
-            "Casual",
-            "Action-Adventure",
-            "Platformer",
-            "3D Platformer",
-            "Exploration",
-            "3D",
-            "Cartoony",
-            "Colorful",
-            "First-Person",
-            "Drama",
-            "Fantasy",
-            "Funny",
-            "Mystery",
-            "Story Rich",
-            "Singleplayer",
             "Indie",
-            "Precision Platformer"
+            "Adventure",
+            "Flight",
+            "Arcade",
+            "3D",
+            "Abstract",
+            "Colorful",
+            "Stylized",
+            "Atmospheric",
+            "Emotional",
+            "Family Friendly",
+            "Nature",
+            "Controller",
+            "Linear",
+            "Physics",
+            "Procedural Generation",
+            "Simulation",
+            "Action",
+            "Collectathon",
+            "Time Management"
         ],
         "languages": {
-            "english": "IAS"
+            "english": "IS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "arabic": "IS",
+            "brazilian": "IS",
+            "bulgarian": "IS",
+            "dutch": "IS",
+            "koreana": "IS",
+            "portuguese": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS",
+            "turkish": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-05-19'
+var gameOfDayDate = '2026-05-20'
