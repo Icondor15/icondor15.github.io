490bc9518843516e3151720a3734fb0733f94416
Icondor <ivanfigus@gmail.com>
2026-05-22 08:39:21 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,111 +2,106 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 2073250,
-        "CEId": "473fc1c0-fe8b-4049-93ad-f25af100d664",
-        "name": "Rift of the NecroDancer",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2073250/header.jpg?t=1746470556",
+        "platformId": 1476170,
+        "CEId": "1609f25e-60bf-4d3e-8fdc-9daf19b367e3",
+        "name": "Godstrike",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1476170/header.jpg?t=1651245904",
         "genre": [
-            "Arcade"
+            "Bullet Hell"
         ],
-        "tier": 4,
-        "points": 110,
+        "tier": 1,
+        "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2025-05-21T19:09:25.000Z",
+        "medianPlaytime": 429,
+        "createdAt": "2022-05-22T22:32:42.000Z",
         "updatedAt": "2026-05-15T01:35:55.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 0,
-        "playersStarted": 12,
-        "playersTotal": 218,
+        "playersCompleted": 9,
+        "playersStarted": 0,
+        "playersTotal": 94,
         "priceData": {
             "USD": {
-                "initial": 1999,
-                "final": 1999,
-                "discountPercent": 0
+                "initial": 599,
+                "final": 389,
+                "discountPercent": 35
             },
             "EUR": {
-                "initial": 1950,
-                "final": 1950,
-                "discountPercent": 0
+                "initial": 549,
+                "final": 356,
+                "discountPercent": 35
             },
             "GBP": {
-                "initial": 1675,
-                "final": 1675,
-                "discountPercent": 0
+                "initial": 459,
+                "final": 298,
+                "discountPercent": 35
             },
             "JPY": {
-                "initial": 230000,
-                "final": 230000,
-                "discountPercent": 0
+                "initial": 72000,
+                "final": 46800,
+                "discountPercent": 35
             },
             "AUD": {
-                "initial": 2950,
-                "final": 2950,
-                "discountPercent": 0
+                "initial": 879,
+                "final": 571,
+                "discountPercent": 35
             },
             "CAD": {
-                "initial": 2599,
-                "final": 2599,
-                "discountPercent": 0
+                "initial": 779,
+                "final": 506,
+                "discountPercent": 35
             }
         },
         "milestones": {
-            "primary": 11,
-            "primaryText": "Ultimate Goblin Pleaser (UNCLEARED) [0\u2605]\nBeat 32 tracks in Remix Mode on Impossible difficulty with the CODA modifier active. (DLCs allowed)\n\nGoblin Pleaser (UNCLEARED) [0\u2605]\nBeat any track in Remix Mode on Impossible difficulty with the CODA modifier active.\n\n*Maniacal Laughter* YOU'VE DONE IT! (UNCLEARED) [0\u2605]\nBeat all base game tracks in Remix Mode with a Full Combo on Impossible difficulty.\n\n3-Lane DJ (UNCLEARED) [0\u2605]\nBeat all base game tracks in Remix Mode with a Full Combo on Hard difficulty (or higher).\n\nJust a Made-Up Rhythm [20\u2605]\nBeat all base game tracks in Remix Mode with a Full Combo on Medium difficulty (or higher).\n\nShop 'Til You Drop[35\u2605 (Partial: 10\u2605)]\nClear all Shopkeeper challenges.\n\nRift Shopping (UNVALUED) [0\u2605]\nGet an S+ on all Challenges.\n\nBet you miss the Crypt by now, huh? (UNCLEARED) [0\u2605]\nBeat all base game tracks with a Full Combo on Impossible difficulty.\n\nHard Rift to Counterblow [40\u2605]\nBeat all base game tracks with a Full Combo on Hard difficulty (or higher).\n\nRiftbuster [10\u2605]\nBeat all base game tracks with a Full Combo on Medium difficulty (or higher).\n\nStorybook Sequel [5\u2605]\nOn Hard difficulty, get a \"Perfect\" badge on all Minigames and get an S+ rank on all Bossfights.",
+            "primary": 1,
+            "primaryText": "Spark [5\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 1,
-            "communityText": "No Rift Adrift\nComplete all tracks (including DLCs) in Remix Mode on Impossible difficulty with the CODA modifier active.",
-            "achievements": 35
+            "community": 0,
+            "communityText": "",
+            "achievements": 22
         },
         "CETags": {
             "genre": [
-                "VSRG",
-                "Rhythm"
+                "Twin Stick",
+                "Rogue-like",
+                "Boss Rush"
             ],
-            "info": [
-                "Has Community Objectives",
-                "Uncleared"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Rhythm",
-            "Music",
-            "Indie",
-            "Arcade",
-            "Difficult",
-            "Female Protagonist",
+            "Roguelite",
+            "Twin Stick Shooter",
+            "Bullet Hell",
+            "Combat",
+            "Top-Down",
+            "Action Roguelike",
+            "Shooter",
             "Action",
-            "Great Soundtrack",
+            "Arcade",
+            "3D",
+            "Bullet Time",
+            "Anime",
             "Colorful",
-            "2D",
-            "Hand-drawn",
-            "RPG",
-            "Minigames",
-            "Level Editor",
+            "Indie",
+            "Adventure",
+            "Fantasy",
             "Singleplayer",
-            "Cute",
-            "Soundtrack",
-            "Cartoon",
-            "Music-Based Procedural Generation",
-            "LGBTQ+"
+            "Controller",
+            "Boss Rush",
+            "Difficult"
         ],
         "languages": {
             "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "tchinese": "IS"
-        },
-        "information": "The base game tracks required for the POs are the songs that are listed before Meat Boy when sorted by character."
+            "french": "IAS",
+            "german": "IAS",
+            "italian": "IAS",
+            "japanese": "IAS",
+            "spanish": "IAS",
+            "brazilian": "IAS",
+            "koreana": "IAS",
+            "russian": "IAS",
+            "schinese": "IAS"
+        }
     }
 }
-var gameOfDayDate = '2026-05-21'
+var gameOfDayDate = '2026-05-22'
