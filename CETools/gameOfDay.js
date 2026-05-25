ac5a992391ae85ba57f0e4a2889907d1bda1935f
Icondor <ivanfigus@gmail.com>
2026-05-25 08:43:27 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,96 +1,47 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1861290,
-        "CEId": "4cb96c41-ed6c-4364-9f3d-abf1552ad9f2",
-        "name": "Devil Spire",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1861290/header.jpg?t=1666433215",
+        "gameType": 3,
+        "platformId": 19043,
+        "CEId": "25d7ebf2-4888-4558-b852-0acecdef1bce",
+        "name": "Crash Bandicoot: The Wrath of Cortex",
+        "header": "https://upload.cedb.me/25d7ebf2-4888-4558-b852-0acecdef1bce.png",
         "genre": [
-            "First-Person"
+            "Platformer"
         ],
-        "tier": 2,
-        "points": 20,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
-        "medianPlaytime": 1760,
-        "createdAt": "2023-05-24T15:08:59.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2024-05-25T02:42:00.000Z",
         "updatedAt": "2026-05-15T01:35:55.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 3,
+        "playersCompleted": 9,
         "playersStarted": 0,
-        "playersTotal": 76,
-        "priceData": {
-            "USD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 899,
-                "final": 899,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 124900,
-                "final": 124900,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1399,
-                "final": 1399,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1250,
-                "final": 1250,
-                "discountPercent": 0
-            }
+        "playersTotal": 13,
+        "gameTags": [
+            "3D Platforming"
+        ],
+        "consoleData": {
+            "name": "PlayStation 2",
+            "family": "Sony",
+            "icon": "https://static.retroachievements.org/assets/images/system/ps2.png"
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "What goes up, must come down [20\u2605]\nBeat Ascent on Extreme. / Beat Descent on Extreme. / Kill 10 bosses in Arena on Extreme.",
+            "primaryText": "Elementalist [10\u2605]\nObtain all gems and platinum relics.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 1,
-            "communityText": "The Downward Spiral\nBeat Descent mode on Nightmare difficulty.",
-            "achievements": 111
+            "community": 0,
+            "communityText": "",
+            "achievements": 88
         },
         "CETags": {
             "genre": [
-                "Rogue-like"
+                "3D Platformer"
             ],
             "info": []
-        },
-        "gameTags": [
-            "Dungeon Crawler",
-            "RPG",
-            "Dark Fantasy",
-            "Roguelite",
-            "2.5D",
-            "First-Person",
-            "Atmospheric",
-            "Action RPG",
-            "Hand-drawn",
-            "Retro",
-            "Old School",
-            "Procedural Generation",
-            "Character Customization",
-            "Inventory Management",
-            "Perma Death",
-            "Multiple Endings",
-            "Loot",
-            "Souls-like",
-            "1990's",
-            "Medieval"
-        ],
-        "languages": {
-            "english": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-05-24'
+var gameOfDayDate = '2026-05-25'
