64ba09f8b06d4e4734738d8e29e63a73c021eb38
Icondor <ivanfigus@gmail.com>
2026-05-30 09:58:30 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,99 +2,112 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 12210,
-        "CEId": "95eaae55-9caa-4de7-8e6a-ab02db5f7e0a",
-        "name": "Grand Theft Auto IV: The Complete Edition",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/12210/header.jpg?t=1618853493",
+        "platformId": 1456760,
+        "CEId": "54c7c46a-48f9-47f1-b6e7-536153c37690",
+        "name": "ROBOBEAT",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1456760/60368d84372b1f8809de35d66811f2748dd01d53/header.jpg?t=1742553743",
         "genre": [
-            "Action"
+            "First-Person"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 2,
+        "points": 20,
         "secondaryPoints": 0,
-        "medianPlaytime": 7375,
-        "createdAt": "2023-05-29T20:57:35.000Z",
-        "updatedAt": "2026-05-28T06:09:57.000Z",
+        "medianPlaytime": 1145,
+        "createdAt": "2025-05-30T19:54:39.000Z",
+        "updatedAt": "2026-05-29T05:12:22.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 54,
-        "playersStarted": 7,
-        "playersTotal": 527,
+        "playersCompleted": 2,
+        "playersStarted": 0,
+        "playersTotal": 34,
         "priceData": {
             "USD": {
                 "initial": 1999,
-                "final": 599,
-                "discountPercent": 70
+                "final": 1999,
+                "discountPercent": 0
             },
             "EUR": {
-                "initial": 1999,
-                "final": 599,
-                "discountPercent": 70
+                "initial": 1949,
+                "final": 1949,
+                "discountPercent": 0
             },
             "GBP": {
                 "initial": 1699,
-                "final": 509,
-                "discountPercent": 70
+                "final": 1699,
+                "discountPercent": 0
             },
             "JPY": {
-                "initial": 220000,
-                "final": 66000,
-                "discountPercent": 70
+                "initial": 230000,
+                "final": 230000,
+                "discountPercent": 0
             },
             "AUD": {
-                "initial": 2995,
-                "final": 898,
-                "discountPercent": 70
+                "initial": 2895,
+                "final": 2895,
+                "discountPercent": 0
             },
             "CAD": {
-                "initial": 2999,
-                "final": 899,
-                "discountPercent": 70
+                "initial": 2279,
+                "final": 2279,
+                "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Ballad of a Radiant Star [5\u2605]\nScore 100% in all missions (in \"The Ballad of the Gay Tony\" DLC)\n\nDopamine Chaser [5\u2605]\nProve yourself. ",
+            "primary": 1,
+            "primaryText": "Robo Beatdown [20\u2605]\n Complete a full run on Frazzer's Room only using one revolver",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 55
+            "achievements": 38
         },
         "CETags": {
             "genre": [
-                "Action-Adventure"
+                "Rogue-like",
+                "First Person Shooter",
+                "Rhythm"
             ],
             "info": []
         },
         "gameTags": [
-            "Open World",
+            "Action Roguelike",
+            "Rhythm",
+            "FPS",
+            "Roguelike",
+            "Music",
             "Action",
-            "Crime",
-            "Automobile Sim",
-            "Multiplayer",
-            "Third Person",
+            "Psychedelic",
+            "Boomer Shooter",
+            "Futuristic",
             "Adventure",
-            "Singleplayer",
-            "Third-Person Shooter",
-            "Moddable",
-            "Sandbox",
             "Shooter",
-            "Physics",
-            "Atmospheric",
-            "Classic",
-            "Gore",
-            "Dark Humor",
-            "Funny",
-            "Satire",
-            "Co-op"
+            "Combat",
+            "Controller",
+            "Singleplayer",
+            "Robots",
+            "Arena Shooter",
+            "Shoot 'Em Up",
+            "First-Person",
+            "Arcade",
+            "Looter Shooter"
         ],
         "languages": {
             "english": "IAS",
             "french": "IS",
             "german": "IS",
             "italian": "IS",
-            "spanish": "IS"
+            "japanese": "IS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "filipino": "IS",
+            "koreana": "IS",
+            "latam": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS",
+            "turkish": "IS",
+            "ukrainian": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-05-29'
+var gameOfDayDate = '2026-05-30'
