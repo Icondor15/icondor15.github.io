68a8698fea21122bedaad6c959fd8d0c42404f8c
Icondor <ivanfigus@gmail.com>
2026-05-31 08:48:55 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,113 +1,48 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1456760,
-        "CEId": "54c7c46a-48f9-47f1-b6e7-536153c37690",
-        "name": "ROBOBEAT",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1456760/60368d84372b1f8809de35d66811f2748dd01d53/header.jpg?t=1742553743",
+        "gameType": 3,
+        "platformId": 10907,
+        "CEId": "2a49fe5c-90c2-4320-88b0-314eeec41a07",
+        "name": "Crash Bash",
+        "header": "https://upload.cedb.me/crashbash.png",
         "genre": [
-            "First-Person"
+            "Arcade"
         ],
-        "tier": 2,
-        "points": 20,
+        "tier": 3,
+        "points": 45,
         "secondaryPoints": 0,
-        "medianPlaytime": 1145,
-        "createdAt": "2025-05-30T19:54:39.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2024-05-31T20:33:58.000Z",
         "updatedAt": "2026-05-29T05:12:22.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 2,
+        "playersCompleted": 4,
         "playersStarted": 0,
-        "playersTotal": 34,
-        "priceData": {
-            "USD": {
-                "initial": 1999,
-                "final": 1999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 1949,
-                "final": 1949,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 1699,
-                "final": 1699,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 230000,
-                "final": 230000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 2895,
-                "final": 2895,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 2279,
-                "final": 2279,
-                "discountPercent": 0
-            }
+        "playersTotal": 12,
+        "gameTags": [
+            "Adventure",
+            "Party"
+        ],
+        "consoleData": {
+            "name": "PlayStation",
+            "family": "Sony",
+            "icon": "https://static.retroachievements.org/assets/images/system/ps1.png"
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Robo Beatdown [20\u2605]\n Complete a full run on Frazzer's Room only using one revolver",
+            "primary": 2,
+            "primaryText": "Minigame Maestro [35\u2605]\nObtain all 28 Platinum Relics.\n\nGimmicks and Golds [10\u2605]\nObtain all 28 Gems, Crystals, and Gold Relics.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 38
+            "achievements": 48
         },
         "CETags": {
             "genre": [
-                "Rogue-like",
-                "First Person Shooter",
-                "Rhythm"
+                "Mini-Games"
             ],
             "info": []
-        },
-        "gameTags": [
-            "Action Roguelike",
-            "Rhythm",
-            "FPS",
-            "Roguelike",
-            "Music",
-            "Action",
-            "Psychedelic",
-            "Boomer Shooter",
-            "Futuristic",
-            "Adventure",
-            "Shooter",
-            "Combat",
-            "Controller",
-            "Singleplayer",
-            "Robots",
-            "Arena Shooter",
-            "Shoot 'Em Up",
-            "First-Person",
-            "Arcade",
-            "Looter Shooter"
-        ],
-        "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "filipino": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "polish": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "tchinese": "IS",
-            "turkish": "IS",
-            "ukrainian": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-05-30'
+var gameOfDayDate = '2026-05-31'
