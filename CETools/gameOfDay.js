43366b7ed17a4921d5171f0dac9207124b65ba0f
Icondor <ivanfigus@gmail.com>
2026-06-01 08:58:43 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,48 +1,60 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 10907,
-        "CEId": "2a49fe5c-90c2-4320-88b0-314eeec41a07",
-        "name": "Crash Bash",
-        "header": "https://upload.cedb.me/crashbash.png",
+        "gameType": 2,
+        "platformId": 488300,
+        "CEId": "38b9cb19-dbc2-4940-b358-25ef25b6514b",
+        "name": "Infinite Air with Mark McMorris",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/488300/header.jpg?t=1572541032",
         "genre": [
             "Arcade"
         ],
         "tier": 3,
-        "points": 45,
+        "points": 50,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2024-05-31T20:33:58.000Z",
+        "medianPlaytime": 2704,
+        "createdAt": "2022-06-01T23:22:58.000Z",
         "updatedAt": "2026-05-29T05:12:22.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 4,
+        "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 12,
-        "gameTags": [
-            "Adventure",
-            "Party"
-        ],
-        "consoleData": {
-            "name": "PlayStation",
-            "family": "Sony",
-            "icon": "https://static.retroachievements.org/assets/images/system/ps1.png"
-        },
+        "playersTotal": 46,
         "milestones": {
             "primary": 2,
-            "primaryText": "Minigame Maestro [35\u2605]\nObtain all 28 Platinum Relics.\n\nGimmicks and Golds [10\u2605]\nObtain all 28 Gems, Crystals, and Gold Relics.",
+            "primaryText": "Mark of Skill [45\u2605]\nComplete 120 challenges.\n\nFrostbite [5\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 48
+            "achievements": 41
         },
         "CETags": {
             "genre": [
-                "Mini-Games"
+                "Sports"
             ],
-            "info": []
+            "info": [
+                "Delisted"
+            ]
+        },
+        "gameTags": [
+            "Sports",
+            "Snowboarding",
+            "Simulation",
+            "Physics",
+            "Action",
+            "Open World",
+            "Snow",
+            "Adventure",
+            "Action-Adventure",
+            "RPG"
+        ],
+        "languages": {
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "spanish": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-05-31'
+var gameOfDayDate = '2026-06-01'
