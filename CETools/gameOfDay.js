e036f93389e432e158a6b6823005ac2c66213098
Icondor <ivanfigus@gmail.com>
2026-06-02 08:53:12 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,60 +1,106 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 2,
-        "platformId": 488300,
-        "CEId": "38b9cb19-dbc2-4940-b358-25ef25b6514b",
-        "name": "Infinite Air with Mark McMorris",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/488300/header.jpg?t=1572541032",
+        "gameType": 0,
+        "platformId": 1766540,
+        "CEId": "574bbb72-f82f-4398-8c3b-2167af18b7eb",
+        "name": "Wonderputt Forever",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1766540/header.jpg?t=1693498552",
         "genre": [
             "Arcade"
         ],
-        "tier": 3,
-        "points": 50,
+        "tier": 2,
+        "points": 20,
         "secondaryPoints": 0,
-        "medianPlaytime": 2704,
-        "createdAt": "2022-06-01T23:22:58.000Z",
+        "medianPlaytime": 1702,
+        "createdAt": "2023-10-02T14:32:40.000Z",
         "updatedAt": "2026-05-29T05:12:22.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 46,
+        "playersCompleted": 1,
+        "playersStarted": 3,
+        "playersTotal": 57,
+        "priceData": {
+            "USD": {
+                "initial": 999,
+                "final": 999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 975,
+                "final": 975,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 850,
+                "final": 850,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 120000,
+                "final": 120000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 1450,
+                "final": 1450,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1299,
+                "final": 1299,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
             "primary": 2,
-            "primaryText": "Mark of Skill [45\u2605]\nComplete 120 challenges.\n\nFrostbite [5\u2605]\nProve yourself.",
+            "primaryText": "Hole-y Relics [10\u2605]\nObtain all golden relics.\n\nWonderputt [10\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 0,
-            "communityText": "",
-            "achievements": 41
+            "community": 3,
+            "communityText": "Decorated Golfer\nObtain all in-game awards.\n\nGolden Run\nObtain all goldens relics from any course in a single run.\n\nYou Only Golf Once\nComplete any trip using the \"roguelite\" modifier.",
+            "achievements": 25
         },
         "CETags": {
             "genre": [
-                "Sports"
+                "Sports",
+                "Score Attack"
             ],
             "info": [
-                "Delisted"
+                "Mouse Focused"
             ]
         },
         "gameTags": [
             "Sports",
-            "Snowboarding",
-            "Simulation",
+            "Casual",
+            "Mini Golf",
+            "Isometric",
+            "Indie",
+            "Puzzle",
+            "Golf",
+            "Arcade",
+            "Relaxing",
+            "Colorful",
             "Physics",
-            "Action",
-            "Open World",
-            "Snow",
-            "Adventure",
-            "Action-Adventure",
-            "RPG"
+            "Singleplayer",
+            "Nostalgia"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "spanish": "IS"
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "arabic": "I",
+            "brazilian": "I",
+            "koreana": "I",
+            "latam": "I",
+            "polish": "I",
+            "swedish": "I",
+            "tchinese": "I",
+            "thai": "I",
+            "turkish": "I"
         }
     }
 }
-var gameOfDayDate = '2026-06-01'
+var gameOfDayDate = '2026-06-02'
