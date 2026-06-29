95eac1a48e2eee36af732b5193d027d54cb55113
Icondor <ivanfigus@gmail.com>
2026-06-29 09:19:12 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,93 +2,84 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1566410,
-        "CEId": "7d0aae8e-6d89-490d-9ef4-ae9ad428cc4a",
-        "name": "Touhou Kouryudou ~ Unconnected Marketeers.",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1566410/header.jpg?t=1621839528",
+        "platformId": 384360,
+        "CEId": "16d16e46-a967-42ae-a82a-d1b42a842356",
+        "name": "Fingered",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/384360/header.jpg?t=1447376927",
         "genre": [
-            "Bullet Hell"
+            "Strategy"
         ],
-        "tier": 3,
-        "points": 65,
+        "tier": 1,
+        "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2023-06-27T00:26:39.000Z",
-        "updatedAt": "2026-06-21T05:21:53.000Z",
+        "medianPlaytime": 530,
+        "createdAt": "2023-06-29T12:18:20.000Z",
+        "updatedAt": "2026-06-26T15:31:09.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 6,
-        "playersStarted": 24,
-        "playersTotal": 120,
+        "playersCompleted": 33,
+        "playersStarted": 0,
+        "playersTotal": 221,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
-                "discountPercent": 0
+                "initial": 187,
+                "final": 71,
+                "discountPercent": 62
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
-                "discountPercent": 0
+                "initial": 187,
+                "final": 71,
+                "discountPercent": 62
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
-                "discountPercent": 0
+                "initial": 159,
+                "final": 60,
+                "discountPercent": 62
             },
             "JPY": {
-                "initial": 154000,
-                "final": 154000,
-                "discountPercent": 0
+                "initial": 22100,
+                "final": 8400,
+                "discountPercent": 62
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
-                "discountPercent": 0
+                "initial": 278,
+                "final": 105,
+                "discountPercent": 62
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
-                "discountPercent": 0
+                "initial": 244,
+                "final": 92,
+                "discountPercent": 62
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "Black Monday [40\u2605]\nBeat the game on Lunatic without deaths.\n\nMarket Crash [15\u2605]\nBeat the game on Lunatic without continues.\n\nCentipede Slayer [5\u2605]\nBeat the Extra mode.\n\nShopping Spree [5\u2605]\nBeat the game on Normal or higher without continues.",
+            "primary": 1,
+            "primaryText": "Golden Boia [5\u2605]\nBeat the game without killing an innocent person.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 2,
-            "communityText": "Golden Centipede\nBeat the Extra mode without dying, bombing and equipping cards.\n\nOut of Stock\nBeat the game on Lunatic without dying, bombing and equipping cards.",
-            "achievements": 0
+            "community": 0,
+            "communityText": "",
+            "achievements": 24
         },
         "CETags": {
             "genre": [
-                "Vertical Shoot'em'Up"
+                "Puzzle"
             ],
-            "info": [
-                "Female Protagonist",
-                "Has Community Objectives"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Bullet Hell",
-            "Shoot 'Em Up",
-            "Action",
-            "Faith",
-            "Great Soundtrack",
-            "Singleplayer",
-            "Shooter",
             "Indie",
-            "Cute",
-            "Difficult",
-            "Female Protagonist",
-            "Roguelite",
-            "Anime",
+            "Casual",
+            "Detective",
+            "Funny",
+            "Puzzle",
+            "Singleplayer",
             "2D",
-            "Arcade"
+            "Comedy",
+            "Crime"
         ],
         "languages": {
-            "japanese": "IS"
+            "english": "IA"
         }
     }
 }
-var gameOfDayDate = '2026-06-27'
+var gameOfDayDate = '2026-06-29'
