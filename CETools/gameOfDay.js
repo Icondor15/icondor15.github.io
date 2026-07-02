b0918757141e6ca9c77266e0626e269be12086e4
Icondor <ivanfigus@gmail.com>
2026-07-02 09:46:29 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,95 +2,99 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 233150,
-        "CEId": "90954c44-0e5c-433e-876f-a06f609d2b17",
-        "name": "LUFTRAUSERS",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/233150/header.jpg?t=1615922153",
+        "platformId": 24240,
+        "CEId": "d5eb5c07-2a2e-4312-ae4b-d2c393400d06",
+        "name": "PAYDAY\u2122 The Heist",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/24240/header.jpg?t=1574898258",
         "genre": [
-            "Arcade"
+            "First-Person"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 3,
+        "points": 40,
         "secondaryPoints": 0,
-        "medianPlaytime": 1486,
-        "createdAt": "2022-07-01T20:52:18.000Z",
-        "updatedAt": "2026-06-30T18:25:21.000Z",
+        "medianPlaytime": 3474,
+        "createdAt": "2022-07-02T20:55:10.000Z",
+        "updatedAt": "2026-06-21T05:21:53.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 26,
-        "playersStarted": 0,
-        "playersTotal": 257,
+        "playersCompleted": 67,
+        "playersStarted": 7,
+        "playersTotal": 619,
         "priceData": {
             "USD": {
                 "initial": 999,
-                "final": 199,
-                "discountPercent": 80
+                "final": 329,
+                "discountPercent": 67
             },
             "EUR": {
-                "initial": 975,
-                "final": 195,
-                "discountPercent": 80
+                "initial": 949,
+                "final": 313,
+                "discountPercent": 67
             },
             "GBP": {
-                "initial": 850,
-                "final": 170,
-                "discountPercent": 80
+                "initial": 799,
+                "final": 263,
+                "discountPercent": 67
             },
             "JPY": {
                 "initial": 120000,
-                "final": 24000,
-                "discountPercent": 80
+                "final": 39600,
+                "discountPercent": 67
             },
             "AUD": {
                 "initial": 1450,
-                "final": 290,
-                "discountPercent": 80
+                "final": 478,
+                "discountPercent": 67
             },
             "CAD": {
                 "initial": 1299,
-                "final": 259,
-                "discountPercent": 80
+                "final": 428,
+                "discountPercent": 67
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Flying Ace [10\u2605]\nBeat all missions and launch the [CLASSIFIED].",
+            "primary": 2,
+            "primaryText": "Crime Spree [30\u2605]\nComplete all Overkill 145+ achievements.\n\nThieving With Finesse [10\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 12
+            "achievements": 56
         },
         "CETags": {
             "genre": [
-                "Twin Stick"
+                "First Person Shooter"
             ],
             "info": []
         },
         "gameTags": [
-            "Indie",
-            "Bullet Hell",
-            "Arcade",
+            "FPS",
             "Action",
-            "Pixel Graphics",
-            "2D",
-            "Shoot 'Em Up",
-            "Great Soundtrack",
-            "Singleplayer",
-            "Retro",
+            "Co-op",
+            "Heist",
+            "Multiplayer",
+            "Crime",
+            "Online Co-Op",
             "Shooter",
-            "Fast-Paced",
-            "Flight",
-            "Addictive",
+            "First-Person",
+            "Great Soundtrack",
+            "Team-Based",
+            "Stealth",
             "Difficult",
-            "World War II",
+            "Tactical",
+            "Strategy",
+            "Singleplayer",
             "Simulation",
-            "Casual",
-            "Score Attack",
-            "Replay Value"
+            "Indie",
+            "RPG",
+            "Adventure"
         ],
         "languages": {
-            "english": "IS"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "spanish": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-07-01'
+var gameOfDayDate = '2026-07-02'
