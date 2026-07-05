4c7b24e3bbfbf005d7b161be53bce069c3559deb
Icondor <ivanfigus@gmail.com>
2026-07-05 09:42:56 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,92 +2,96 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 331190,
-        "CEId": "42633477-6588-4243-8feb-0662dff7cea9",
-        "name": "Master Spy",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/331190/header.jpg?t=1632849122",
+        "platformId": 1963510,
+        "CEId": "0eabba06-5b60-43d0-9714-e25be97c5547",
+        "name": "Herald of Havoc",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1963510/header.jpg?t=1748368630",
         "genre": [
-            "Platformer"
+            "First-Person"
         ],
         "tier": 2,
-        "points": 30,
+        "points": 20,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2023-07-04T00:36:02.000Z",
-        "updatedAt": "2026-06-21T05:21:53.000Z",
+        "medianPlaytime": 495,
+        "createdAt": "2025-07-05T07:48:47.000Z",
+        "updatedAt": "2026-07-04T19:03:39.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 1,
-        "playersStarted": 2,
-        "playersTotal": 121,
+        "playersCompleted": 3,
+        "playersStarted": 1,
+        "playersTotal": 11,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 99,
-                "discountPercent": 90
+                "initial": 1699,
+                "final": 679,
+                "discountPercent": 60
             },
             "EUR": {
-                "initial": 899,
-                "final": 89,
-                "discountPercent": 90
+                "initial": 1649,
+                "final": 659,
+                "discountPercent": 60
             },
             "GBP": {
-                "initial": 699,
-                "final": 69,
-                "discountPercent": 90
+                "initial": 1429,
+                "final": 571,
+                "discountPercent": 60
             },
             "JPY": {
-                "initial": 98000,
-                "final": 9800,
-                "discountPercent": 90
+                "initial": 190000,
+                "final": 76000,
+                "discountPercent": 60
             },
             "AUD": {
-                "initial": 1450,
-                "final": 145,
-                "discountPercent": 90
+                "initial": 2495,
+                "final": 998,
+                "discountPercent": 60
             },
             "CAD": {
-                "initial": 1099,
-                "final": 109,
-                "discountPercent": 90
+                "initial": 2199,
+                "final": 879,
+                "discountPercent": 60
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Stealth Master [15\u2605]\nComplete each mission with a Master rank on Operative difficulty.\n\nUndercover Virtuoso [10\u2605]\nComplete the game on Master difficulty or above.\n\nEntry-Level Espionage [5\u2605]\nComplete the game and all VR levels on Operative difficulty or above (level select is allowed).",
+            "primary": 2,
+            "primaryText": "Arena Havoc [10\u2605]\nBeat wave 20 on all endless levels.\n\nEpisode Havoc [10\u2605]\nClear all levels on Havoc difficulty. / Do misc achievements on these levels.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 1,
-            "communityText": "Mission: Impossible\nStart a new game and complete every mission with a Master rank on Operative difficulty or above.",
-            "achievements": 0
+            "community": 0,
+            "communityText": "",
+            "achievements": 30
         },
         "CETags": {
             "genre": [
-                "2D Platformer"
+                "First Person Shooter",
+                "Boomer Shooter"
             ],
             "info": []
         },
         "gameTags": [
-            "Platformer",
-            "Indie",
-            "Action",
-            "Stealth",
-            "Pixel Graphics",
-            "Difficult",
+            "Boomer Shooter",
+            "Stylized",
+            "Sci-fi",
+            "FPS",
+            "Fantasy",
+            "Fast-Paced",
             "Retro",
-            "Great Soundtrack",
-            "2D",
+            "Shooter",
+            "Combat",
+            "Action",
+            "3D",
+            "Futuristic",
             "Singleplayer",
-            "Noir",
-            "Ninja",
-            "Story Rich",
-            "Cinematic",
-            "Precision Platformer",
-            "Side Scroller"
+            "Magic",
+            "Gore",
+            "Medieval",
+            "Swordplay",
+            "PvE",
+            "Violent",
+            "Dark Fantasy"
         ],
         "languages": {
-            "english": "IS"
-        },
-        "information": "The game only shows Master ranks upon completing a chapter in the campaign, which is highly inconvenient. So, you're allowed to simulate them through level select. For Master ranks, you are not allowed to get caught or use resets. For Mission 1, the sum of level times must be 1:30 or less. For Missions 2 through 5 individually, 8:00 or less."
+            "english": "I"
+        }
     }
 }
-var gameOfDayDate = '2026-07-04'
+var gameOfDayDate = '2026-07-05'
