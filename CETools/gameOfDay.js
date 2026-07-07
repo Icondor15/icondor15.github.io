920a6dca94706441c876d0ef7318f3a1a4f9e8b2
Icondor <ivanfigus@gmail.com>
2026-07-07 10:45:55 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,54 +1,94 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 1961770,
-        "CEId": "66011c02-3adb-460b-b20b-98cc0bc6e55c",
-        "name": "Ball Go Home",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1961770/header.jpg?t=1675958332",
+        "gameType": 0,
+        "platformId": 640290,
+        "CEId": "8342ba10-667e-4776-a343-840e3c659cdb",
+        "name": "Super Duper Multitasking",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/640290/header.jpg?t=1739480400",
         "genre": [
-            "Platformer"
+            "Arcade"
         ],
-        "tier": 1,
-        "points": 5,
+        "tier": 3,
+        "points": 40,
         "secondaryPoints": 0,
-        "medianPlaytime": 30,
-        "createdAt": "2024-07-06T17:48:26.000Z",
+        "medianPlaytime": 1432,
+        "createdAt": "2022-07-07T11:20:09.000Z",
         "updatedAt": "2026-07-04T19:03:39.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 11,
-        "playersStarted": 0,
-        "playersTotal": 87,
+        "playersCompleted": 6,
+        "playersStarted": 8,
+        "playersTotal": 60,
+        "priceData": {
+            "USD": {
+                "initial": 999,
+                "final": 749,
+                "discountPercent": 25
+            },
+            "EUR": {
+                "initial": 975,
+                "final": 731,
+                "discountPercent": 25
+            },
+            "GBP": {
+                "initial": 850,
+                "final": 637,
+                "discountPercent": 25
+            },
+            "JPY": {
+                "initial": 120000,
+                "final": 90000,
+                "discountPercent": 25
+            },
+            "AUD": {
+                "initial": 1450,
+                "final": 1087,
+                "discountPercent": 25
+            },
+            "CAD": {
+                "initial": 1299,
+                "final": 974,
+                "discountPercent": 25
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Homecoming [5\u2605]\nFinish all levels.",
+            "primary": 3,
+            "primaryText": "The HunTask[20\u2605 (Partial: 5\u2605)]\nClear all \"The Hundred\" and \"Super Multitasking GO!\" missions on 100% difficulty.\n\nFeet Required [15\u2605]\nProve yourself.\n\nLocal Legend [5\u2605]\nBeat all local #1 leaderboards on every difficulty in Quickplay (Any slot and minigame types). / Get to play 5 simultaneous games in Quickplay (no Maximum difficulty).",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 0
+            "achievements": 62
         },
         "CETags": {
             "genre": [
-                "Awkward Movement",
-                "2D Platformer"
+                "Mini-Games",
+                "Multitasking"
             ],
-            "info": [
-                "Free"
-            ]
+            "info": []
         },
         "gameTags": [
+            "Arcade",
+            "Short",
+            "Score Attack",
+            "Puzzle",
+            "Replay Value",
+            "Difficult",
+            "Fast-Paced",
+            "Indie",
             "Action",
-            "2D Platformer",
-            "Physics",
-            "Minimalist",
-            "Platformer",
+            "Casual",
+            "Family Friendly",
+            "Pixel Graphics",
             "2D",
+            "Controller",
+            "Local Multiplayer",
+            "Multiplayer",
             "Singleplayer"
         ],
         "languages": {
-            "english": "IA"
+            "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-07-06'
+var gameOfDayDate = '2026-07-07'
