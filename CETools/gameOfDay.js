75db7ae8b70d061a5248afad3591e68b8a4c7f04
Icondor <ivanfigus@gmail.com>
2026-07-13 09:54:36 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,23 +2,23 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 250580,
-        "CEId": "057827ec-de03-4d6c-9791-509255e1970d",
-        "name": "Paranautical Activity: Deluxe Atonement Edition",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/250580/header.jpg?t=1593028220",
+        "platformId": 232910,
+        "CEId": "bf9f47c9-e668-4a9a-9483-de0de5d33223",
+        "name": "TrackMania\u00b2 Stadium",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/232910/header.jpg?t=1602605537",
         "genre": [
-            "First-Person"
+            "Arcade"
         ],
-        "tier": 2,
-        "points": 25,
+        "tier": 3,
+        "points": 60,
         "secondaryPoints": 0,
-        "medianPlaytime": 2303,
-        "createdAt": "2024-07-12T04:07:18.000Z",
-        "updatedAt": "2026-07-04T19:03:39.000Z",
+        "medianPlaytime": 2195,
+        "createdAt": "2022-07-13T21:32:23.000Z",
+        "updatedAt": "2026-07-06T00:54:25.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 176,
+        "playersCompleted": 23,
+        "playersStarted": 9,
+        "playersTotal": 202,
         "priceData": {
             "USD": {
                 "initial": 999,
@@ -26,70 +26,92 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 699,
-                "final": 699,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 699,
-                "final": 699,
+                "initial": 859,
+                "final": 859,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 98000,
-                "final": 98000,
+                "initial": 132000,
+                "final": 132000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 1495,
+                "final": 1495,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Picky Eater [15\u2605]\nBeat the game without taking any items.\n\nAtonement [10\u2605]\nProve yourself.",
+            "primaryText": "Suicide Circuit [45\u2605]\nObtain 65 Author Medals.\n\nSquared Off [15\u2605]\nObtain 65 Gold Medals.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 0,
-            "communityText": "",
-            "achievements": 45
+            "community": 1,
+            "communityText": "Stadium All-Star\nGet all Author Medals in the StarTrack Title Pack.",
+            "achievements": 0
         },
         "CETags": {
             "genre": [
-                "Rogue-like",
-                "First Person Shooter"
+                "Racing"
             ],
-            "info": []
+            "info": [
+                "Curated"
+            ]
         },
         "gameTags": [
-            "Action Roguelike",
-            "Indie",
-            "FPS",
-            "Roguelike",
-            "Action",
-            "Voxel",
-            "Roguelite",
+            "Racing",
+            "Multiplayer",
+            "Automobile Sim",
+            "Level Editor",
+            "Time Attack",
+            "Sports",
+            "Arcade",
             "Singleplayer",
-            "Great Soundtrack",
-            "Shooter",
-            "Difficult",
-            "RPG",
-            "First-Person",
+            "Local Multiplayer",
+            "eSports",
+            "Competitive",
             "Fast-Paced",
-            "Procedural Generation",
-            "Pixel Graphics",
-            "Horror",
-            "Perma Death"
+            "Sandbox",
+            "Driving",
+            "Split Screen",
+            "Action",
+            "Simulation",
+            "Great Soundtrack",
+            "Controller"
         ],
         "languages": {
-            "english": "IA"
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "czech": "I",
+            "danish": "I",
+            "dutch": "I",
+            "finnish": "I",
+            "hungarian": "I",
+            "koreana": "I",
+            "norwegian": "I",
+            "polish": "I",
+            "portuguese": "I",
+            "romanian": "I",
+            "russian": "I",
+            "schinese": "I",
+            "swedish": "I",
+            "turkish": "I"
         }
     }
 }
-var gameOfDayDate = '2026-07-12'
+var gameOfDayDate = '2026-07-13'
