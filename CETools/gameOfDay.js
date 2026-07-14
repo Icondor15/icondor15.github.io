c95746ff84769700f005523c087fc8b0829c55b9
Icondor <ivanfigus@gmail.com>
2026-07-14 09:47:51 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,116 +2,104 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 232910,
-        "CEId": "bf9f47c9-e668-4a9a-9483-de0de5d33223",
-        "name": "TrackMania\u00b2 Stadium",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/232910/header.jpg?t=1602605537",
+        "platformId": 1304420,
+        "CEId": "ef102261-6ec4-4325-819a-620f7b70c793",
+        "name": "Super Cable Boy",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1304420/header.jpg?t=1635417304",
         "genre": [
-            "Arcade"
+            "Platformer"
         ],
-        "tier": 3,
-        "points": 60,
+        "tier": 1,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 2195,
-        "createdAt": "2022-07-13T21:32:23.000Z",
-        "updatedAt": "2026-07-06T00:54:25.000Z",
+        "medianPlaytime": 829,
+        "createdAt": "2022-07-14T11:55:22.000Z",
+        "updatedAt": "2026-07-04T19:03:39.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 23,
-        "playersStarted": 9,
-        "playersTotal": 202,
+        "playersCompleted": 14,
+        "playersStarted": 0,
+        "playersTotal": 79,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1399,
+                "final": 1399,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 859,
-                "final": 859,
+                "initial": 1239,
+                "final": 1239,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 132000,
-                "final": 132000,
+                "initial": 164000,
+                "final": 164000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1495,
-                "final": 1495,
+                "initial": 2295,
+                "final": 2295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1849,
+                "final": 1849,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Suicide Circuit [45\u2605]\nObtain 65 Author Medals.\n\nSquared Off [15\u2605]\nObtain 65 Gold Medals.",
+            "primary": 1,
+            "primaryText": "Omusubi Connoisseur [15\u2605]\nCollect all Onigiris.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 1,
-            "communityText": "Stadium All-Star\nGet all Author Medals in the StarTrack Title Pack.",
-            "achievements": 0
+            "community": 0,
+            "communityText": "",
+            "achievements": 41
         },
         "CETags": {
             "genre": [
-                "Racing"
+                "Grappler",
+                "2D Platformer"
             ],
-            "info": [
-                "Curated"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Racing",
-            "Multiplayer",
-            "Automobile Sim",
-            "Level Editor",
-            "Time Attack",
-            "Sports",
-            "Arcade",
+            "Precision Platformer",
+            "2D Platformer",
+            "Platformer",
+            "Indie",
+            "Retro",
+            "Pixel Graphics",
+            "Experimental",
+            "Cute",
+            "Local Co-Op",
+            "Action",
+            "Atmospheric",
             "Singleplayer",
             "Local Multiplayer",
-            "eSports",
-            "Competitive",
-            "Fast-Paced",
-            "Sandbox",
-            "Driving",
+            "Multiplayer",
+            "Co-op",
+            "2D",
             "Split Screen",
-            "Action",
-            "Simulation",
-            "Great Soundtrack",
-            "Controller"
+            "Colorful",
+            "Choices Matter",
+            "Multiple Endings"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "czech": "I",
-            "danish": "I",
-            "dutch": "I",
-            "finnish": "I",
-            "hungarian": "I",
-            "koreana": "I",
-            "norwegian": "I",
-            "polish": "I",
-            "portuguese": "I",
-            "romanian": "I",
-            "russian": "I",
-            "schinese": "I",
-            "swedish": "I",
-            "turkish": "I"
+            "english": "IAS",
+            "german": "IAS",
+            "japanese": "IAS",
+            "spanish": "IAS",
+            "koreana": "IAS",
+            "portuguese": "IAS",
+            "schinese": "IAS",
+            "tchinese": "IAS",
+            "thai": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-07-13'
+var gameOfDayDate = '2026-07-14'
