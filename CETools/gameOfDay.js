05ae39e9c37be1bb463711c96d68846a60d40e55
Icondor <ivanfigus@gmail.com>
2026-07-16 10:32:46 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,23 +2,23 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1779180,
-        "CEId": "ebae404f-0ea1-4b51-b624-949bc0c90aef",
-        "name": "Knight's Try",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1779180/header.jpg?t=1643317706",
+        "platformId": 1797680,
+        "CEId": "67572fba-5a2b-45fe-ad40-2a8ff2dd7f26",
+        "name": "Wee Tanks!",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1797680/header.jpg?t=1688732888",
         "genre": [
-            "Platformer"
+            "Action"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 4,
+        "points": 185,
         "secondaryPoints": 0,
-        "medianPlaytime": 610,
-        "createdAt": "2022-07-15T23:02:50.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2023-07-16T20:24:38.000Z",
         "updatedAt": "2026-07-04T19:03:39.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 20,
+        "playersCompleted": 4,
+        "playersStarted": 11,
+        "playersTotal": 96,
         "priceData": {
             "USD": {
                 "initial": 999,
@@ -26,18 +26,18 @@ var gameOfDay =
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 819,
-                "final": 819,
+                "initial": 999,
+                "final": 999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 719,
-                "final": 719,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 101000,
-                "final": 101000,
+                "initial": 120000,
+                "final": 120000,
                 "discountPercent": 0
             },
             "AUD": {
@@ -52,45 +52,62 @@ var gameOfDay =
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "First Try [10\u2605]\nComplete the game in a single try.",
-            "secondary": 0,
-            "secondaryText": "",
+            "primary": 3,
+            "primaryText": "One Last Weep [125\u2605]\nFinish the full campaign on any difficulty without getting killed once. / Complete the True Run challenge solo on Grandpa difficulty.\n\nLucky Cadet [45\u2605]\nFinish the full campaign on Kid difficulty (or higher) in one session. / Defeat the King Tank on Adult difficulty without any armor.\n\nCrisis Core [15\u2605]\nObtain all achievements in Survival Mode.",
+            "secondary": 1,
+            "secondaryText": "Wee Are SO Screwed [0\u2605]\nFinish the full campaign on Ancient difficulty.",
             "community": 0,
             "communityText": "",
-            "achievements": 13
+            "achievements": 51
         },
         "CETags": {
             "genre": [
-                "3D Platformer"
+                "Arena Shooter",
+                "Twin Stick"
             ],
-            "info": []
+            "info": [
+                "Has Secondary Objectives"
+            ]
         },
         "gameTags": [
             "Action",
+            "Action-Adventure",
+            "Shoot 'Em Up",
+            "Hero Shooter",
+            "Vehicular Combat",
+            "Family Friendly",
+            "Level Editor",
             "Adventure",
-            "Platformer",
-            "3D Platformer",
-            "Precision Platformer",
-            "3D",
-            "Stylized",
-            "Third Person",
-            "1990's",
-            "Atmospheric",
-            "Emotional",
-            "Fantasy",
-            "Medieval",
-            "Old School",
-            "Retro",
-            "Controller",
-            "Linear",
-            "Singleplayer",
-            "Indie",
-            "Difficult"
+            "PvE",
+            "Combat",
+            "4 Player Local",
+            "Co-op",
+            "Local Co-Op",
+            "Tanks",
+            "Shooter",
+            "Cartoony",
+            "Bullet Hell",
+            "Colorful",
+            "Cute",
+            "Building"
         ],
         "languages": {
-            "english": "I"
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "galician": "IS",
+            "koreana": "IS",
+            "latam": "IS",
+            "luxembourgish": "IS",
+            "polish": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "turkish": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-07-15'
+var gameOfDayDate = '2026-07-16'
