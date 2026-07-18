0f4cd4ccb7def977f4f795b91df3278990f524b3
Icondor <ivanfigus@gmail.com>
2026-07-18 09:46:33 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,91 +2,78 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 522240,
-        "CEId": "46067c7f-8757-421f-be39-85e70d8ecf18",
-        "name": "Pool Panic",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/522240/header.jpg?t=1584658980",
+        "platformId": 756200,
+        "CEId": "c40c927c-99a8-493b-a837-4a524a9b344b",
+        "name": "Impossibox",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/756200/header.jpg?t=1607007871",
         "genre": [
-            "Arcade"
+            "Platformer"
         ],
-        "tier": 1,
-        "points": 15,
+        "tier": 3,
+        "points": 40,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2022-07-17T22:32:42.000Z",
+        "medianPlaytime": 741,
+        "createdAt": "2024-07-18T04:48:55.000Z",
         "updatedAt": "2026-07-04T19:03:39.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 0,
-        "playersStarted": 4,
-        "playersTotal": 191,
+        "playersCompleted": 4,
+        "playersStarted": 0,
+        "playersTotal": 22,
         "priceData": {
             "USD": {
-                "initial": 1499,
-                "final": 1499,
+                "initial": 99,
+                "final": 99,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 99,
+                "final": 99,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1139,
-                "final": 1139,
+                "initial": 89,
+                "final": 89,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 152000,
-                "final": 152000,
+                "initial": 12000,
+                "final": 12000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 150,
+                "final": 150,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1749,
-                "final": 1749,
+                "initial": 129,
+                "final": 129,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Clean the Table (UNCLEARED) [0\u2605]\nGet all Gold medals on hard mode.\n\n8-Ball [15\u2605]\nProve yourself.",
+            "primary": 1,
+            "primaryText": "Don't Go Home in a Box [40\u2605]\nBeat the game without dying.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 32
+            "achievements": 9
         },
         "CETags": {
             "genre": [
-                "Sports"
+                "2D Platformer"
             ],
-            "info": [
-                "Uncleared"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Sports",
             "Action",
-            "Adventure",
-            "Funny",
-            "Puzzle",
-            "Billiards",
-            "4 Player Local",
-            "Surreal",
-            "Local Multiplayer"
+            "Indie",
+            "Platformer"
         ],
         "languages": {
-            "english": "IS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "spanish": "IS",
-            "brazilian": "IS"
+            "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-07-17'
+var gameOfDayDate = '2026-07-18'
