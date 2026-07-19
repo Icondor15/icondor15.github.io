f81affb600fcd52ab81f85ef33f82915aa0b60f8
Icondor <ivanfigus@gmail.com>
2026-07-19 09:53:52 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,78 +2,109 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 756200,
-        "CEId": "c40c927c-99a8-493b-a837-4a524a9b344b",
-        "name": "Impossibox",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/756200/header.jpg?t=1607007871",
+        "platformId": 1144200,
+        "CEId": "ed24952c-ab02-4adb-8305-fdf54e0166de",
+        "name": "Ready or Not",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1144200/header.jpg?t=1775492374",
         "genre": [
-            "Platformer"
+            "First-Person"
         ],
         "tier": 3,
-        "points": 40,
+        "points": 70,
         "secondaryPoints": 0,
-        "medianPlaytime": 741,
-        "createdAt": "2024-07-18T04:48:55.000Z",
-        "updatedAt": "2026-07-04T19:03:39.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2025-07-19T21:16:13.000Z",
+        "updatedAt": "2026-07-19T00:09:25.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 4,
-        "playersStarted": 0,
-        "playersTotal": 22,
+        "playersCompleted": 1,
+        "playersStarted": 10,
+        "playersTotal": 143,
         "priceData": {
             "USD": {
-                "initial": 99,
-                "final": 99,
+                "initial": 4999,
+                "final": 4999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 99,
-                "final": 99,
+                "initial": 4999,
+                "final": 4999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 89,
-                "final": 89,
+                "initial": 4499,
+                "final": 4499,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 12000,
-                "final": 12000,
+                "initial": 715000,
+                "final": 715000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 150,
-                "final": 150,
+                "initial": 7350,
+                "final": 7350,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 129,
-                "final": 129,
+                "initial": 5999,
+                "final": 5999,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Don't Go Home in a Box [40\u2605]\nBeat the game without dying.",
-            "secondary": 0,
-            "secondaryText": "",
+            "primary": 6,
+            "primaryText": "You Can't Break Those Cuffs [15\u2605]\nComplete all missions in the base game, the \"Home Invasion\" and \"Dark Waters\" DLC on Hard difficulty with an S rank.\n\nOpen Fire [5\u2605]\nComplete all Boiling Point missions with an S rank on Hard difficulty.\n\nI'm Too Old for This...  [10\u2605]\nComplete all base game missions on Hard difficulty.\n\nHold the Line[25\u2605 (Partial: 5\u2605)]\nComplete Ironman Mode on Standard difficulty (or higher) without losing a single officer.\n\nBrave Boys in Blue [10\u2605]\nComplete all missions on Standard difficulty (or higher) with an S rank.\n\nWe Start at 9 [5\u2605]\nProve yourself.",
+            "secondary": 1,
+            "secondaryText": "Gray Steeled [0\u2605]\nComplete Ironman Mode on Hard difficulty.",
             "community": 0,
             "communityText": "",
-            "achievements": 9
+            "achievements": 66
         },
         "CETags": {
             "genre": [
-                "2D Platformer"
+                "First Person Shooter"
             ],
-            "info": []
+            "info": [
+                "Has Secondary Objectives"
+            ]
         },
         "gameTags": [
+            "Tactical",
+            "Realistic",
+            "FPS",
+            "Shooter",
+            "Multiplayer",
+            "Singleplayer",
+            "Co-op",
+            "Horror",
+            "First-Person",
             "Action",
-            "Indie",
-            "Platformer"
+            "Psychological Horror",
+            "Violent",
+            "Gun Customization",
+            "Combat",
+            "Crime",
+            "Gore",
+            "Modern",
+            "Atmospheric",
+            "Co-op Campaign",
+            "VR"
         ],
         "languages": {
-            "english": "IAS"
-        }
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "koreana": "IS",
+            "polish": "IS",
+            "portuguese": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "turkish": "IS"
+        },
+        "information": "Miyu: \"new M32A1 flashbang launcher is op\""
     }
 }
-var gameOfDayDate = '2026-07-18'
+var gameOfDayDate = '2026-07-19'
