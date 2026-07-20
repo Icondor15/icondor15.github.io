3a985e1ac43965c8eab9775adeb96beeac8d57a8
Icondor <ivanfigus@gmail.com>
2026-07-20 10:22:15 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,110 +1,38 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1144200,
-        "CEId": "ed24952c-ab02-4adb-8305-fdf54e0166de",
-        "name": "Ready or Not",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1144200/header.jpg?t=1775492374",
+        "gameType": 5,
+        "CEId": "3b3daf18-ff41-4c92-9432-1fe7b950f96d",
+        "name": "Nintendo Switch\u2122 2 Welcome Tour",
+        "header": "https://upload.cedb.me/welcometour.png",
         "genre": [
-            "First-Person"
+            "Arcade"
         ],
-        "tier": 3,
-        "points": 70,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
         "medianPlaytime": 0,
-        "createdAt": "2025-07-19T21:16:13.000Z",
-        "updatedAt": "2026-07-19T00:09:25.000Z",
+        "createdAt": "2025-07-20T23:25:12.000Z",
+        "updatedAt": "2026-07-19T14:09:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 1,
-        "playersStarted": 10,
-        "playersTotal": 143,
-        "priceData": {
-            "USD": {
-                "initial": 4999,
-                "final": 4999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 4999,
-                "final": 4999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 4499,
-                "final": 4499,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 715000,
-                "final": 715000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 7350,
-                "final": 7350,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 5999,
-                "final": 5999,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 4,
+        "playersStarted": 0,
+        "playersTotal": 9,
         "milestones": {
-            "primary": 6,
-            "primaryText": "You Can't Break Those Cuffs [15\u2605]\nComplete all missions in the base game, the \"Home Invasion\" and \"Dark Waters\" DLC on Hard difficulty with an S rank.\n\nOpen Fire [5\u2605]\nComplete all Boiling Point missions with an S rank on Hard difficulty.\n\nI'm Too Old for This...  [10\u2605]\nComplete all base game missions on Hard difficulty.\n\nHold the Line[25\u2605 (Partial: 5\u2605)]\nComplete Ironman Mode on Standard difficulty (or higher) without losing a single officer.\n\nBrave Boys in Blue [10\u2605]\nComplete all missions on Standard difficulty (or higher) with an S rank.\n\nWe Start at 9 [5\u2605]\nProve yourself.",
-            "secondary": 1,
-            "secondaryText": "Gray Steeled [0\u2605]\nComplete Ironman Mode on Hard difficulty.",
+            "primary": 1,
+            "primaryText": "Welcome Tour the Future [10\u2605]\nObtain 3 gold medals on every minigame.",
+            "secondary": 0,
+            "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 66
+            "achievements": 0
         },
         "CETags": {
             "genre": [
-                "First Person Shooter"
+                "Mini-Games"
             ],
-            "info": [
-                "Has Secondary Objectives"
-            ]
-        },
-        "gameTags": [
-            "Tactical",
-            "Realistic",
-            "FPS",
-            "Shooter",
-            "Multiplayer",
-            "Singleplayer",
-            "Co-op",
-            "Horror",
-            "First-Person",
-            "Action",
-            "Psychological Horror",
-            "Violent",
-            "Gun Customization",
-            "Combat",
-            "Crime",
-            "Gore",
-            "Modern",
-            "Atmospheric",
-            "Co-op Campaign",
-            "VR"
-        ],
-        "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "koreana": "IS",
-            "polish": "IS",
-            "portuguese": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "turkish": "IS"
-        },
-        "information": "Miyu: \"new M32A1 flashbang launcher is op\""
+            "info": []
+        }
     }
 }
-var gameOfDayDate = '2026-07-19'
+var gameOfDayDate = '2026-07-20'
