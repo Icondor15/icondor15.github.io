a3649b643d922f2a4b804f2469303d6c555dbe99
Icondor <ivanfigus@gmail.com>
2026-07-21 09:42:04 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,38 +1,89 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 5,
-        "CEId": "3b3daf18-ff41-4c92-9432-1fe7b950f96d",
-        "name": "Nintendo Switch\u2122 2 Welcome Tour",
-        "header": "https://upload.cedb.me/welcometour.png",
+        "gameType": 0,
+        "platformId": 601810,
+        "CEId": "0e6e530b-58c3-4525-8e2d-cebb00541d4a",
+        "name": "Spark the Electric Jester",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/601810/header.jpg?t=1572743521",
         "genre": [
-            "Arcade"
+            "Action"
         ],
         "tier": 1,
         "points": 10,
         "secondaryPoints": 0,
         "medianPlaytime": 0,
-        "createdAt": "2025-07-20T23:25:12.000Z",
+        "createdAt": "2024-07-21T04:54:16.000Z",
         "updatedAt": "2026-07-19T14:09:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 4,
-        "playersStarted": 0,
-        "playersTotal": 9,
+        "playersCompleted": 0,
+        "playersStarted": 2,
+        "playersTotal": 37,
+        "priceData": {
+            "USD": {
+                "initial": 799,
+                "final": 799,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 659,
+                "final": 659,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 579,
+                "final": 579,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 82000,
+                "final": 82000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 999,
+                "final": 999,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 899,
+                "final": 899,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Welcome Tour the Future [10\u2605]\nObtain 3 gold medals on every minigame.",
+            "primary": 2,
+            "primaryText": "Fark My Life [5\u2605]\nBeat Fark's story on Hard mode.\n\nThe Better Yellow One [5\u2605]\nFinish all Normal modes.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 0
+            "achievements": 4
         },
         "CETags": {
             "genre": [
-                "Mini-Games"
+                "Action-Platformer"
             ],
             "info": []
+        },
+        "gameTags": [
+            "Action",
+            "Platformer",
+            "Great Soundtrack",
+            "2D",
+            "Pixel Graphics",
+            "Fast-Paced",
+            "Singleplayer",
+            "Indie",
+            "Retro",
+            "Adventure",
+            "Colorful",
+            "Side Scroller",
+            "Difficult"
+        ],
+        "languages": {
+            "english": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-07-20'
+var gameOfDayDate = '2026-07-21'
