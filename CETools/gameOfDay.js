606b2a1a9566b81f2570a5ea089b2d82aa54edad
Icondor <ivanfigus@gmail.com>
2026-07-22 09:27:15 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,88 +2,125 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 601810,
-        "CEId": "0e6e530b-58c3-4525-8e2d-cebb00541d4a",
-        "name": "Spark the Electric Jester",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/601810/header.jpg?t=1572743521",
+        "platformId": 1519710,
+        "CEId": "8429d649-882c-4760-9f00-d9f229a5c401",
+        "name": "SCHiM",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1519710/header.jpg?t=1721289638",
         "genre": [
-            "Action"
+            "Platformer"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 2,
+        "points": 20,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2024-07-21T04:54:16.000Z",
+        "medianPlaytime": 1142,
+        "createdAt": "2024-07-22T14:49:02.000Z",
         "updatedAt": "2026-07-19T14:09:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 0,
-        "playersStarted": 2,
-        "playersTotal": 37,
+        "playersCompleted": 1,
+        "playersStarted": 0,
+        "playersTotal": 11,
         "priceData": {
             "USD": {
-                "initial": 799,
-                "final": 799,
-                "discountPercent": 0
+                "initial": 1645,
+                "final": 822,
+                "discountPercent": 50
             },
             "EUR": {
-                "initial": 659,
-                "final": 659,
-                "discountPercent": 0
+                "initial": 1645,
+                "final": 822,
+                "discountPercent": 50
             },
             "GBP": {
-                "initial": 579,
-                "final": 579,
-                "discountPercent": 0
+                "initial": 1499,
+                "final": 749,
+                "discountPercent": 50
             },
             "JPY": {
-                "initial": 82000,
-                "final": 82000,
-                "discountPercent": 0
+                "initial": 198000,
+                "final": 99000,
+                "discountPercent": 50
             },
             "AUD": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
+                "initial": 2895,
+                "final": 1447,
+                "discountPercent": 50
             },
             "CAD": {
-                "initial": 899,
-                "final": 899,
-                "discountPercent": 0
+                "initial": 2279,
+                "final": 1139,
+                "discountPercent": 50
             }
         },
         "milestones": {
             "primary": 2,
-            "primaryText": "Fark My Life [5\u2605]\nBeat Fark's story on Hard mode.\n\nThe Better Yellow One [5\u2605]\nFinish all Normal modes.",
+            "primaryText": "Bask in Darkness [10\u2605]\nBeat the game with Risky mode on.\n\nLiving in the Shadows [10\u2605]\nFinish each level without respawning.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 0,
-            "communityText": "",
-            "achievements": 4
+            "community": 1,
+            "communityText": "The Closer to Light, the Greater the Shadow\nBeat the game with Extra Jump off, Risky mode on, and respawns off.",
+            "achievements": 24
         },
         "CETags": {
             "genre": [
-                "Action-Platformer"
+                "3D Platformer"
             ],
             "info": []
         },
         "gameTags": [
-            "Action",
-            "Platformer",
-            "Great Soundtrack",
-            "2D",
-            "Pixel Graphics",
-            "Fast-Paced",
-            "Singleplayer",
-            "Indie",
-            "Retro",
-            "Adventure",
+            "Exploration",
+            "3D Platformer",
+            "Relaxing",
+            "Puzzle Platformer",
+            "Wholesome",
+            "Collectathon",
+            "Puzzle",
             "Colorful",
-            "Side Scroller",
-            "Difficult"
+            "Family Friendly",
+            "Stylized",
+            "Isometric",
+            "Casual",
+            "3D",
+            "Adventure",
+            "Cute",
+            "Third Person",
+            "Top-Down",
+            "Singleplayer",
+            "Abstract",
+            "Minimalist"
         ],
         "languages": {
-            "english": "IS"
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "arabic": "I",
+            "brazilian": "I",
+            "bulgarian": "I",
+            "croatian": "I",
+            "czech": "I",
+            "danish": "I",
+            "dutch": "I",
+            "finnish": "I",
+            "greek": "I",
+            "hungarian": "I",
+            "indonesian": "I",
+            "koreana": "I",
+            "latam": "I",
+            "norwegian": "I",
+            "polish": "I",
+            "portuguese": "I",
+            "romanian": "I",
+            "russian": "I",
+            "schinese": "I",
+            "swedish": "I",
+            "tchinese": "I",
+            "thai": "I",
+            "turkish": "I",
+            "ukrainian": "I",
+            "vietnamese": "I"
         }
     }
 }
-var gameOfDayDate = '2026-07-21'
+var gameOfDayDate = '2026-07-22'
