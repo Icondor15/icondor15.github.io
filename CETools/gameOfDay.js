09a8d2dffc7e31a606baa0f42806e8c6ef4071ab
Icondor <ivanfigus@gmail.com>
2026-07-27 11:31:01 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,108 +2,95 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1701520,
-        "CEId": "a8783c0a-ac89-4d33-ae1d-1b387c10bd42",
-        "name": "Afterimage",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1701520/header.jpg?t=1669795839",
+        "platformId": 3667230,
+        "CEId": "4d4ff35b-dac3-4372-8272-15d0fbb14132",
+        "name": "The Deadseat",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3667230/6fec8f4b856c515bd053112ab5fe8d3e1b84fc35/header.jpg?t=1752886112",
         "genre": [
-            "Action"
+            "Arcade"
         ],
-        "tier": 3,
-        "points": 55,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
-        "medianPlaytime": 3550,
-        "createdAt": "2023-07-26T23:57:01.000Z",
+        "medianPlaytime": 573,
+        "createdAt": "2025-07-27T08:19:09.000Z",
         "updatedAt": "2026-07-19T14:09:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 3,
-        "playersStarted": 1,
-        "playersTotal": 299,
+        "playersCompleted": 4,
+        "playersStarted": 0,
+        "playersTotal": 23,
         "priceData": {
             "USD": {
-                "initial": 2499,
-                "final": 2499,
-                "discountPercent": 0
+                "initial": 299,
+                "final": 197,
+                "discountPercent": 34
             },
             "EUR": {
-                "initial": 2499,
-                "final": 2499,
-                "discountPercent": 0
+                "initial": 299,
+                "final": 197,
+                "discountPercent": 34
             },
             "GBP": {
-                "initial": 2099,
-                "final": 2099,
-                "discountPercent": 0
+                "initial": 249,
+                "final": 164,
+                "discountPercent": 34
             },
             "JPY": {
-                "initial": 280000,
-                "final": 280000,
-                "discountPercent": 0
+                "initial": 35000,
+                "final": 23100,
+                "discountPercent": 34
             },
             "AUD": {
-                "initial": 3595,
-                "final": 3595,
-                "discountPercent": 0
+                "initial": 450,
+                "final": 297,
+                "discountPercent": 34
             },
             "CAD": {
-                "initial": 2999,
-                "final": 2999,
-                "discountPercent": 0
+                "initial": 389,
+                "final": 256,
+                "discountPercent": 34
             }
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Pantheon of Engardin [30\u2605]\nDefeat the All Bosses Tag Match in Trial of Soul with a \"Perfect\" hit score.\n\nTitan Vanquisher [15\u2605]\nDefeat all bosses individually in Trial of Soul with \"Perfect\" hit scores.\n\nTrials of the Ancient [10\u2605]\nDefeat all boss Combinations in Trial of Soul with \"Perfect\" hit scores.",
+            "primary": 1,
+            "primaryText": "One Hell of a Ride [10\u2605]\nComplete a challenge run with the Suffer preset.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 46
+            "achievements": 11
         },
         "CETags": {
             "genre": [
-                "Metroidvania",
-                "Boss Rush"
+                "Horror"
             ],
-            "info": [
-                "Female Protagonist"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Metroidvania",
-            "Female Protagonist",
-            "2D Platformer",
-            "Side Scroller",
-            "Anime",
-            "Exploration",
-            "Souls-like",
-            "Hack and Slash",
-            "Action RPG",
-            "Difficult",
-            "Platformer",
-            "Action-Adventure",
-            "RPG",
-            "2D",
-            "Cute",
-            "Open World",
-            "Lore-Rich",
+            "Singleplayer",
             "Story Rich",
-            "Multiple Endings",
-            "Choices Matter"
+            "Survival Horror",
+            "Horror",
+            "Dark",
+            "Action",
+            "Roguelike",
+            "3D",
+            "Indie",
+            "Resource Management",
+            "First-Person",
+            "Psychological Horror",
+            "Point & Click",
+            "Retro",
+            "Simulation",
+            "2D",
+            "Atmospheric",
+            "Supernatural",
+            "Pixel Graphics",
+            "Lore-Rich"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IAS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "koreana": "IS",
-            "russian": "IS",
-            "schinese": "IAS",
-            "tchinese": "IAS"
+            "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-07-26'
+var gameOfDayDate = '2026-07-27'
