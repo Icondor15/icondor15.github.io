5bad15c383045354ceedb8cec5bee132d588f993
Icondor <ivanfigus@gmail.com>
2026-07-28 10:07:24 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,96 +1,48 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 3667230,
-        "CEId": "4d4ff35b-dac3-4372-8272-15d0fbb14132",
-        "name": "The Deadseat",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/3667230/6fec8f4b856c515bd053112ab5fe8d3e1b84fc35/header.jpg?t=1752886112",
+        "gameType": 3,
+        "platformId": 3438,
+        "CEId": "e6b724e6-c98b-4a4f-9f63-42d60d43af21",
+        "name": "Rockman & Forte",
+        "header": "https://upload.cedb.me/e6b724e6-c98b-4a4f-9f63-42d60d43af21.png",
         "genre": [
-            "Arcade"
+            "Action"
         ],
         "tier": 1,
-        "points": 10,
+        "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 573,
-        "createdAt": "2025-07-27T08:19:09.000Z",
-        "updatedAt": "2026-07-19T14:09:49.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2024-07-28T20:08:42.000Z",
+        "updatedAt": "2026-07-27T15:20:57.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 4,
+        "playersCompleted": 3,
         "playersStarted": 0,
-        "playersTotal": 23,
-        "priceData": {
-            "USD": {
-                "initial": 299,
-                "final": 197,
-                "discountPercent": 34
-            },
-            "EUR": {
-                "initial": 299,
-                "final": 197,
-                "discountPercent": 34
-            },
-            "GBP": {
-                "initial": 249,
-                "final": 164,
-                "discountPercent": 34
-            },
-            "JPY": {
-                "initial": 35000,
-                "final": 23100,
-                "discountPercent": 34
-            },
-            "AUD": {
-                "initial": 450,
-                "final": 297,
-                "discountPercent": 34
-            },
-            "CAD": {
-                "initial": 389,
-                "final": 256,
-                "discountPercent": 34
-            }
+        "playersTotal": 4,
+        "gameTags": [
+            "2D Platforming"
+        ],
+        "consoleData": {
+            "name": "SNES/Super Famicom",
+            "family": "Nintendo",
+            "icon": "https://static.retroachievements.org/assets/images/system/snes.png"
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "One Hell of a Ride [10\u2605]\nComplete a challenge run with the Suffer preset.",
+            "primaryText": "Fall of the Robot King [5\u2605]\nBeat the game as both characters.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 11
+            "achievements": 37
         },
         "CETags": {
             "genre": [
-                "Horror"
+                "Action-Platformer",
+                "Run & Gun"
             ],
             "info": []
-        },
-        "gameTags": [
-            "Singleplayer",
-            "Story Rich",
-            "Survival Horror",
-            "Horror",
-            "Dark",
-            "Action",
-            "Roguelike",
-            "3D",
-            "Indie",
-            "Resource Management",
-            "First-Person",
-            "Psychological Horror",
-            "Point & Click",
-            "Retro",
-            "Simulation",
-            "2D",
-            "Atmospheric",
-            "Supernatural",
-            "Pixel Graphics",
-            "Lore-Rich"
-        ],
-        "languages": {
-            "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-07-27'
+var gameOfDayDate = '2026-07-28'
