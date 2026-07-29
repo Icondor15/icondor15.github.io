638419c7efccce834cf8ad58e3d46087a8b86737
Icondor <ivanfigus@gmail.com>
2026-07-29 09:47:21 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,48 +1,103 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 3438,
-        "CEId": "e6b724e6-c98b-4a4f-9f63-42d60d43af21",
-        "name": "Rockman & Forte",
-        "header": "https://upload.cedb.me/e6b724e6-c98b-4a4f-9f63-42d60d43af21.png",
+        "gameType": 0,
+        "platformId": 1731120,
+        "CEId": "b17bd0ef-95cd-4b17-a868-29eb4bdf7922",
+        "name": "Cloud Escape",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1731120/header_alt_assets_0.jpg?t=1662321295",
         "genre": [
-            "Action"
+            "Platformer"
         ],
         "tier": 1,
-        "points": 5,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2024-07-28T20:08:42.000Z",
+        "medianPlaytime": 568,
+        "createdAt": "2022-07-29T19:06:18.000Z",
         "updatedAt": "2026-07-27T15:20:57.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 4,
-        "gameTags": [
-            "2D Platforming"
-        ],
-        "consoleData": {
-            "name": "SNES/Super Famicom",
-            "family": "Nintendo",
-            "icon": "https://static.retroachievements.org/assets/images/system/snes.png"
+        "playersCompleted": 2,
+        "playersStarted": 2,
+        "playersTotal": 11,
+        "priceData": {
+            "USD": {
+                "initial": 499,
+                "final": 374,
+                "discountPercent": 25
+            },
+            "EUR": {
+                "initial": 399,
+                "final": 299,
+                "discountPercent": 25
+            },
+            "GBP": {
+                "initial": 399,
+                "final": 299,
+                "discountPercent": 25
+            },
+            "JPY": {
+                "initial": 52000,
+                "final": 39000,
+                "discountPercent": 25
+            },
+            "AUD": {
+                "initial": 750,
+                "final": 562,
+                "discountPercent": 25
+            },
+            "CAD": {
+                "initial": 569,
+                "final": 426,
+                "discountPercent": 25
+            }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Fall of the Robot King [5\u2605]\nBeat the game as both characters.",
+            "primary": 2,
+            "primaryText": "Cloud Nine [10\u2605]\nReach the goal within 6 minutes on Hard Mode without dying.\n\nTower Escape [5\u2605]\nSeparately; Reach the top of the tower within 6 minutes, and without accumulating more than 850 meters of altitude.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 37
+            "achievements": 28
         },
         "CETags": {
             "genre": [
-                "Action-Platformer",
-                "Run & Gun"
+                "3D Platformer"
             ],
             "info": []
+        },
+        "gameTags": [
+            "Precision Platformer",
+            "3D Platformer",
+            "Runner",
+            "Parkour",
+            "Platformer",
+            "Female Protagonist",
+            "3D",
+            "Third Person",
+            "Physics",
+            "Funny",
+            "Action",
+            "Controller",
+            "Casual",
+            "Linear",
+            "Stylized",
+            "Pirates",
+            "Comedy",
+            "Cartoon",
+            "Memes",
+            "Singleplayer"
+        ],
+        "languages": {
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "dutch": "I",
+            "russian": "I"
         }
     }
 }
-var gameOfDayDate = '2026-07-28'
+var gameOfDayDate = '2026-07-29'
