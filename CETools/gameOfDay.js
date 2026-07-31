b2d0fd4cab9c6ffbc2113e4e7bb6e6ec66e17fc0
Icondor <ivanfigus@gmail.com>
2026-07-31 11:10:11 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,85 +2,101 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1100170,
-        "CEId": "c22b0045-6a26-4e0c-9937-e5f5a3aee0d9",
-        "name": "Touhou Bunkachou \uff5e Double Spoiler",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1100170/header.jpg?t=1597739913",
+        "platformId": 1400910,
+        "CEId": "7918bc97-0c7b-48f9-b833-e9d781b91b95",
+        "name": "Soundodger 2",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1400910/header.jpg?t=1681393861",
         "genre": [
             "Bullet Hell"
         ],
-        "tier": 2,
-        "points": 20,
+        "tier": 4,
+        "points": 125,
         "secondaryPoints": 0,
-        "medianPlaytime": 2579,
-        "createdAt": "2022-07-30T00:56:02.000Z",
-        "updatedAt": "2026-07-27T15:20:57.000Z",
+        "medianPlaytime": 13257,
+        "createdAt": "2023-07-31T21:52:38.000Z",
+        "updatedAt": "2026-07-30T23:48:59.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 23,
-        "playersStarted": 0,
-        "playersTotal": 76,
+        "playersCompleted": 2,
+        "playersStarted": 11,
+        "playersTotal": 136,
         "priceData": {
             "USD": {
-                "initial": 1099,
-                "final": 1099,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 899,
-                "final": 899,
+                "initial": 1950,
+                "final": 1950,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 829,
-                "final": 829,
+                "initial": 1675,
+                "final": 1675,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 110000,
-                "final": 110000,
+                "initial": 230000,
+                "final": 230000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1550,
-                "final": 1550,
+                "initial": 2950,
+                "final": 2950,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1249,
-                "final": 1249,
+                "initial": 2599,
+                "final": 2599,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Mysterious Journalist [20\u2605]\nClear all scenes. (Both characters allowed)",
+            "primary": 3,
+            "primaryText": "Bullet Dancer[100\u2605 (Partial: 20\u2605)]\nObtain an S+ rank on the advanced and challenge versions of every track while obtaining the heart, not getting hit, and without using powers simultaneously.\n\nAudio Avoider [20\u2605]\nObtain an S+ rank on the normal version of every track while obtaining the heart, not getting hit, and without using powers simultaneously.\n\nLost in Sound [5\u2605]\nObtain an S+ rank on every track in the Lost Levels while obtaining the heart, not getting hit, and without using powers simultaneously.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 1,
-            "communityText": "Double Enthusiast\nClear all scenes with Hatate.",
-            "achievements": 0
+            "community": 8,
+            "communityText": "Master Choreographer\nComplete any three levels on the SD2 list rated at 8 points or above (any level that is already included under a primary objective will not count).\n\nMelodic Movements\nComplete any three levels on the SD2 list rated between 7 to 8 points (any level that is already included under a primary objective will not count).\n\nBeat Bypass\nComplete any three levels on the SD2 list rated between 5 to 7 points (any level that is already included under a primary objective will not count).\n\nSymphonic Tour\nComplete any three levels on the SD2 list rated between 3 to 5 points (any level that is already included under a primary objective will not count).\n\nAudio Excursion\nComplete any three levels on the SD2 list rated between 1 to 3 points (any level that is already included under a primary objective will not count).\n\nBreaking Point\nComplete any three levels on the SD2 Challenges list rated at 4 points or above (any level that is already included under a primary objective will not count).\n\nJingle Jeopardy\nComplete any three levels on the SD2 Challenges list rated between 2 to 4 points (any level that is already included under a primary objective will not count).\n\nDodge This\nComplete any three levels on the SD2 Challenges list rated between 1 to 2 points (any level that is already included under a primary objective will not count).",
+            "achievements": 27
         },
         "CETags": {
             "genre": [
-                "Vertical Shoot'em'Up"
+                "Avoid'em'Up",
+                "Rhythm"
             ],
             "info": [
                 "Has Community Objectives",
-                "Female Protagonist"
+                "Mouse Focused",
+                "Curated"
             ]
         },
         "gameTags": [
-            "Indie",
-            "Action",
             "Bullet Hell",
+            "Rhythm",
             "Shoot 'Em Up",
-            "Faith",
-            "Great Soundtrack",
-            "Female Protagonist"
+            "Difficult",
+            "Arena Shooter",
+            "Arcade",
+            "Colorful",
+            "Action",
+            "Relaxing",
+            "Bullet Time",
+            "Time Manipulation",
+            "2D",
+            "2.5D",
+            "Noir",
+            "Controller",
+            "Casual",
+            "Abstract",
+            "Minimalist",
+            "Stylized",
+            "Level Editor"
         ],
         "languages": {
-            "japanese": "IS"
-        }
+            "english": "I"
+        },
+        "information": "Community curated list of levels for COs can be found here: https://docs.google.com/spreadsheets/d/1ersIV9GSeeXMI4SQDH-cTu3VjlvsYVuCcfUwsT6pjmE/edit?gid=957659803#gid=957659803.\n\nHero of the March 18 Massacre."
     }
 }
-var gameOfDayDate = '2026-07-30'
+var gameOfDayDate = '2026-07-31'
