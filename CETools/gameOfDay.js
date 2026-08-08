c61fe32096d818192e62261bc952686f165007a4
Icondor <ivanfigus@gmail.com>
2026-08-08 10:10:11 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,101 +2,105 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1856910,
-        "CEId": "182bb6dc-9808-4efc-92a9-03688dcb3977",
-        "name": "Ganryu 2",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1856910/header.jpg?t=1656056966",
+        "platformId": 1462040,
+        "CEId": "d82090c5-f478-4fcd-83e0-0e3dc1b946c0",
+        "name": "FINAL FANTASY VII REMAKE INTERGRADE",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1462040/header.jpg?t=1663767129",
         "genre": [
             "Action"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 2,
+        "points": 20,
         "secondaryPoints": 0,
-        "medianPlaytime": 576,
-        "createdAt": "2023-08-07T01:26:17.000Z",
-        "updatedAt": "2026-08-02T19:13:49.000Z",
+        "medianPlaytime": 4971,
+        "createdAt": "2023-08-08T04:25:13.000Z",
+        "updatedAt": "2026-07-27T15:20:57.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 2,
+        "playersCompleted": 21,
         "playersStarted": 0,
-        "playersTotal": 33,
+        "playersTotal": 190,
         "priceData": {
             "USD": {
-                "initial": 1799,
-                "final": 1799,
-                "discountPercent": 0
+                "initial": 3999,
+                "final": 1399,
+                "discountPercent": 65
             },
             "EUR": {
-                "initial": 1499,
-                "final": 1499,
-                "discountPercent": 0
+                "initial": 3999,
+                "final": 1399,
+                "discountPercent": 65
             },
             "GBP": {
-                "initial": 1399,
-                "final": 1399,
-                "discountPercent": 0
+                "initial": 3499,
+                "final": 1224,
+                "discountPercent": 65
             },
             "JPY": {
-                "initial": 184000,
-                "final": 184000,
-                "discountPercent": 0
+                "initial": 547800,
+                "final": 191700,
+                "discountPercent": 65
             },
             "AUD": {
-                "initial": 2595,
-                "final": 2595,
-                "discountPercent": 0
+                "initial": 5495,
+                "final": 1923,
+                "discountPercent": 65
             },
             "CAD": {
-                "initial": 2049,
-                "final": 2049,
-                "discountPercent": 0
+                "initial": 5349,
+                "final": 1872,
+                "discountPercent": 65
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "The Stone And The Sword [10\u2605]\nComplete the game without dying and under 75 minutes.",
+            "primaryText": "Master of Fate [20\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 25
+            "achievements": 63
         },
         "CETags": {
             "genre": [
-                "Action-Platformer"
+                "Action-Adventure"
             ],
             "info": []
         },
         "gameTags": [
-            "Hack and Slash",
-            "2D Platformer",
-            "Martial Arts",
-            "Platformer",
-            "Arcade",
+            "RPG",
+            "Action RPG",
             "JRPG",
-            "2D",
-            "Action",
-            "Controller",
-            "Swordplay",
-            "Ninja",
-            "Pixel Graphics",
             "Singleplayer",
-            "Assassins",
-            "Indie",
-            "Retro",
-            "Soundtrack"
+            "Action-Adventure",
+            "Party-Based RPG",
+            "Third Person",
+            "Spectacle fighter",
+            "Action",
+            "Fantasy",
+            "Controller",
+            "Adventure",
+            "3D",
+            "Magic",
+            "Story Rich",
+            "3D Platformer",
+            "Cinematic",
+            "Realistic",
+            "Sci-fi",
+            "Great Soundtrack"
         ],
         "languages": {
-            "english": "IS",
-            "french": "IS",
-            "german": "IS",
+            "english": "IAS",
+            "french": "IAS",
+            "german": "IAS",
             "italian": "IS",
-            "japanese": "IS",
+            "japanese": "IAS",
             "spanish": "IS",
             "brazilian": "IS",
             "koreana": "IS",
-            "russian": "IS",
-            "schinese": "IS"
+            "latam": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-08-07'
+var gameOfDayDate = '2026-08-08'
