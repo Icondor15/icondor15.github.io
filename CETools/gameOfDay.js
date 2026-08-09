6bbd0a58d71e6f1bb0040ec87efcedc70fe6018e
Icondor <ivanfigus@gmail.com>
2026-08-09 10:16:23 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,105 +2,84 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1462040,
-        "CEId": "d82090c5-f478-4fcd-83e0-0e3dc1b946c0",
-        "name": "FINAL FANTASY VII REMAKE INTERGRADE",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1462040/header.jpg?t=1663767129",
+        "platformId": 468310,
+        "CEId": "c5077033-dce5-40cf-87a8-419952b744d4",
+        "name": "She Wants Me Dead",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/468310/header.jpg?t=1670597930",
         "genre": [
-            "Action"
+            "Platformer"
         ],
-        "tier": 2,
-        "points": 20,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
-        "medianPlaytime": 4971,
-        "createdAt": "2023-08-08T04:25:13.000Z",
+        "medianPlaytime": 280,
+        "createdAt": "2024-08-09T19:43:53.000Z",
         "updatedAt": "2026-07-27T15:20:57.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 21,
+        "playersCompleted": 1,
         "playersStarted": 0,
-        "playersTotal": 190,
+        "playersTotal": 22,
         "priceData": {
             "USD": {
-                "initial": 3999,
-                "final": 1399,
-                "discountPercent": 65
+                "initial": 799,
+                "final": 799,
+                "discountPercent": 0
             },
             "EUR": {
-                "initial": 3999,
-                "final": 1399,
-                "discountPercent": 65
+                "initial": 779,
+                "final": 779,
+                "discountPercent": 0
             },
             "GBP": {
-                "initial": 3499,
-                "final": 1224,
-                "discountPercent": 65
+                "initial": 669,
+                "final": 669,
+                "discountPercent": 0
             },
             "JPY": {
-                "initial": 547800,
-                "final": 191700,
-                "discountPercent": 65
+                "initial": 92000,
+                "final": 92000,
+                "discountPercent": 0
             },
             "AUD": {
-                "initial": 5495,
-                "final": 1923,
-                "discountPercent": 65
+                "initial": 1179,
+                "final": 1179,
+                "discountPercent": 0
             },
             "CAD": {
-                "initial": 5349,
-                "final": 1872,
-                "discountPercent": 65
+                "initial": 1049,
+                "final": 1049,
+                "discountPercent": 0
             }
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Master of Fate [20\u2605]\nProve yourself.",
+            "primaryText": "She Can't Always Have Her Way [10\u2605]\nSeparately; complete the \"One Life Run\" challenge, and complete the \"Time Attack\" challenge in less than 9 minutes.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 63
+            "achievements": 10
         },
         "CETags": {
             "genre": [
-                "Action-Adventure"
+                "2D Platformer"
             ],
             "info": []
         },
         "gameTags": [
-            "RPG",
-            "Action RPG",
-            "JRPG",
-            "Singleplayer",
-            "Action-Adventure",
-            "Party-Based RPG",
-            "Third Person",
-            "Spectacle fighter",
             "Action",
-            "Fantasy",
-            "Controller",
-            "Adventure",
-            "3D",
-            "Magic",
-            "Story Rich",
-            "3D Platformer",
-            "Cinematic",
-            "Realistic",
-            "Sci-fi",
-            "Great Soundtrack"
+            "Gore",
+            "Violent",
+            "Platformer",
+            "2D",
+            "Indie",
+            "Great Soundtrack",
+            "Difficult",
+            "Cats"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IAS",
-            "german": "IAS",
-            "italian": "IS",
-            "japanese": "IAS",
-            "spanish": "IS",
-            "brazilian": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "schinese": "IS",
-            "tchinese": "IS"
+            "english": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-08-08'
+var gameOfDayDate = '2026-08-09'
