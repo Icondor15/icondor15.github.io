a31213f0427ca65ad6481944468f394e894f568a
Icondor <ivanfigus@gmail.com>
2026-08-12 10:20:53 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,106 +1,48 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1480560,
-        "CEId": "b834061d-34b8-4c73-8017-d65780e2ead3",
-        "name": "Lawn Mowing Simulator",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1480560/header.jpg?t=1683890027",
+        "gameType": 3,
+        "platformId": 14853,
+        "CEId": "efc10f10-df8e-46b6-932a-d83d46019cfb",
+        "name": "Pokemon Mystery Dungeon: Explorers of Sky",
+        "header": "https://upload.cedb.me/efc10f10-df8e-46b6-932a-d83d46019cfb.png",
         "genre": [
-            "Arcade"
+            "Strategy"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 2,
+        "points": 25,
         "secondaryPoints": 0,
-        "medianPlaytime": 7437,
-        "createdAt": "2023-08-11T00:12:25.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2024-08-12T16:29:35.000Z",
         "updatedAt": "2026-07-27T15:20:57.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 5,
-        "playersStarted": 0,
-        "playersTotal": 216,
-        "priceData": {
-            "USD": {
-                "initial": 1999,
-                "final": 499,
-                "discountPercent": 75
-            },
-            "EUR": {
-                "initial": 1999,
-                "final": 499,
-                "discountPercent": 75
-            },
-            "GBP": {
-                "initial": 1599,
-                "final": 399,
-                "discountPercent": 75
-            },
-            "JPY": {
-                "initial": 255000,
-                "final": 63700,
-                "discountPercent": 75
-            },
-            "AUD": {
-                "initial": 2899,
-                "final": 724,
-                "discountPercent": 75
-            },
-            "CAD": {
-                "initial": 2599,
-                "final": 649,
-                "discountPercent": 75
-            }
+        "playersCompleted": 2,
+        "playersStarted": 2,
+        "playersTotal": 21,
+        "gameTags": [
+            "Roguelike"
+        ],
+        "consoleData": {
+            "name": "Nintendo DS",
+            "family": "Nintendo",
+            "icon": "https://static.retroachievements.org/assets/images/system/ds.png"
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Touch Grass [10\u2605]\nComplete all challenges.",
+            "primary": 4,
+            "primaryText": "Destiny's Ascent [10\u2605]\nClear Destiny Tower.\n\nPerfect Purist [5\u2605]\nClear Zero Isle South.\n\nAdept Purist [5\u2605]\nClear Zero Isle East and Zero Isle West.\n\nCenter of the Earth [5\u2605]\nClear Zero Isle North and Zero Isle Center.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 0,
-            "communityText": "",
-            "achievements": 54
+            "community": 1,
+            "communityText": "No Stone Left Unturned\nReach Guildmaster Rank and recruit all Pokemon.",
+            "achievements": 130
         },
         "CETags": {
             "genre": [
-                "Miscellaneous"
+                "Traditional Rogue-like"
             ],
             "info": []
         },
-        "gameTags": [
-            "Simulation",
-            "Relaxing",
-            "Realistic",
-            "Farming Sim",
-            "Immersive Sim",
-            "Management",
-            "Physics",
-            "Driving",
-            "Agriculture",
-            "Time Management",
-            "Automobile Sim",
-            "Economy",
-            "Nature",
-            "Character Customization",
-            "3D",
-            "First-Person",
-            "Third Person",
-            "Controller",
-            "Multiplayer",
-            "Family Friendly"
-        ],
-        "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "japanese": "I",
-            "spanish": "I",
-            "brazilian": "I",
-            "koreana": "I",
-            "polish": "I",
-            "russian": "I",
-            "schinese": "I",
-            "tchinese": "I"
-        }
+        "information": "For those interested in RA mastery: there are 5 missable achievements."
     }
 }
-var gameOfDayDate = '2026-08-11'
+var gameOfDayDate = '2026-08-12'
