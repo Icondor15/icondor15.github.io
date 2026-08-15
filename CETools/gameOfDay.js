61075fd5732a163420df8e6f773f57c62222f9b5
Icondor <ivanfigus@gmail.com>
2026-08-15 09:55:53 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,98 +2,104 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1301230,
-        "CEId": "79d73929-1a91-4695-8626-e6503b384c2b",
-        "name": "Astrodogs",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1301230/header.jpg?t=1617894064",
+        "platformId": 1627570,
+        "CEId": "320cf073-420b-4ae1-9756-cffc45a53e06",
+        "name": "The Plucky Squire",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1627570/c349049add507bd6d62bed84c4ba6fba9b53c839/header_alt_assets_1.jpg?t=1751526090",
         "genre": [
-            "Arcade"
+            "Action"
         ],
         "tier": 1,
-        "points": 10,
+        "points": 5,
         "secondaryPoints": 0,
         "medianPlaytime": 0,
-        "createdAt": "2022-08-14T02:49:48.000Z",
+        "createdAt": "2025-08-15T00:47:37.000Z",
         "updatedAt": "2026-08-12T01:31:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 3,
-        "playersStarted": 0,
-        "playersTotal": 30,
+        "playersCompleted": 0,
+        "playersStarted": 5,
+        "playersTotal": 130,
         "priceData": {
             "USD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 1079,
-                "final": 1079,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 1029,
-                "final": 1029,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 132000,
-                "final": 132000,
+                "initial": 340000,
+                "final": 340000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1850,
-                "final": 1850,
+                "initial": 4399,
+                "final": 4399,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1449,
-                "final": 1449,
+                "initial": 3899,
+                "final": 3899,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Star Doge [10\u2605]\nGet all S ranks.",
+            "primary": 2,
+            "primaryText": "Journey of a Thousand Miles (UNCLEARED) [0\u2605]\nComplete the game on Iron Squire Mode.\n\nStorybook Savior [5\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 15
+            "achievements": 24
         },
         "CETags": {
             "genre": [
-                "Third Person Shooter",
-                "Horizontal Shoot'em'Up",
-                "Score Attack",
                 "Action-Adventure"
             ],
-            "info": []
+            "info": [
+                "Uncleared"
+            ]
         },
         "gameTags": [
-            "Action",
-            "Colorful",
-            "3D",
-            "On-Rails Shooter",
-            "Shoot 'Em Up",
-            "Bullet Hell",
-            "Third-Person Shooter",
-            "Linear",
-            "Arcade",
-            "Shooter",
-            "Cute",
-            "Comic Book",
             "Adventure",
-            "Funny",
-            "Bullet Time",
+            "3D Platformer",
+            "Action-Adventure",
+            "2D Platformer",
+            "Puzzle Platformer",
+            "2D",
+            "Cute",
+            "3D",
+            "Cartoony",
+            "Family Friendly",
+            "Singleplayer",
+            "Fantasy",
             "Controller",
-            "Vehicular Combat",
-            "Tutorial",
-            "Psychedelic",
-            "Old School"
+            "Combat",
+            "Indie",
+            "2.5D",
+            "Platformer"
         ],
         "languages": {
-            "english": "IAS"
+            "english": "IAS",
+            "french": "IAS",
+            "german": "IAS",
+            "japanese": "IAS",
+            "spanish": "IAS",
+            "brazilian": "IAS",
+            "koreana": "IAS",
+            "latam": "IAS",
+            "russian": "IAS",
+            "schinese": "IAS",
+            "tchinese": "IAS"
         }
     }
 }
-var gameOfDayDate = '2026-08-14'
+var gameOfDayDate = '2026-08-15'
