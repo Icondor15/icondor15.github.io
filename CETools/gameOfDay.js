3a6e0955a94dbc536b6211608ec9a03e1564fafa
Icondor <ivanfigus@gmail.com>
2026-08-18 09:08:51 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,47 +2,47 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 500,
-        "CEId": "e51888ca-756f-4b6f-a00e-1417ffcc8d0a",
-        "name": "Left 4 Dead",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/500/header.jpg?t=1745368560",
+        "platformId": 422110,
+        "CEId": "014c18c0-34e1-4bea-8693-69367eada6df",
+        "name": "Wand Wars",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/422110/header.jpg?t=1667248117",
         "genre": [
-            "First-Person"
+            "Action"
         ],
         "tier": 1,
-        "points": 15,
+        "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 2134,
-        "createdAt": "2025-08-17T20:24:31.000Z",
-        "updatedAt": "2026-08-16T09:18:34.000Z",
+        "medianPlaytime": 213,
+        "createdAt": "2024-08-18T22:05:51.000Z",
+        "updatedAt": "2026-08-12T01:31:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 13,
+        "playersCompleted": 5,
         "playersStarted": 0,
-        "playersTotal": 692,
+        "playersTotal": 33,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 1199,
+                "final": 1199,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 975,
-                "final": 975,
+                "initial": 1199,
+                "final": 1199,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 850,
-                "final": 850,
+                "initial": 899,
+                "final": 899,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 120000,
-                "final": 120000,
+                "initial": 118000,
+                "final": 118000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 1695,
+                "final": 1695,
                 "discountPercent": 0
             },
             "CAD": {
@@ -53,74 +53,44 @@ var gameOfDay =
         },
         "milestones": {
             "primary": 1,
-            "primaryText": "Legendary Survivor [15\u2605]\nAchieve gold times on each survival map.",
+            "primaryText": "New Magic Wand [5\u2605]\nFinish all campaigns on Legendary difficulty. ",
             "secondary": 0,
             "secondaryText": "",
-            "community": 3,
-            "communityText": "Lone Survivor\nAchieve gold times on each survival map while solo (bots allowed):\n\nPlatinum Survivor\nAchieve 30 minutes on each official survival map.\n\nTrue Survivor\nAchieve 20 minutes on each official survival map.",
-            "achievements": 73
+            "community": 0,
+            "communityText": "",
+            "achievements": 28
         },
         "CETags": {
             "genre": [
-                "Arena Shooter",
-                "First Person Shooter",
-                "Horror"
+                "Arena Shooter"
             ],
             "info": []
         },
         "gameTags": [
-            "Zombies",
+            "Sports",
+            "PvP",
+            "Bullet Hell",
             "Co-op",
-            "Multiplayer",
-            "FPS",
-            "Action",
-            "Horror",
-            "Shooter",
-            "Online Co-Op",
-            "Survival",
-            "Team-Based",
-            "First-Person",
-            "Post-apocalyptic",
-            "Singleplayer",
-            "Moddable",
-            "Atmospheric",
-            "Adventure",
-            "Tactical",
-            "Replay Value",
             "Competitive",
-            "Female Protagonist"
+            "4 Player Local",
+            "Arcade",
+            "Difficult",
+            "Fighting",
+            "Real-Time",
+            "2D",
+            "Cute",
+            "Funny",
+            "Action",
+            "Pixel Graphics",
+            "Cats",
+            "Magic",
+            "Colorful",
+            "Atmospheric",
+            "Supernatural"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IAS",
-            "german": "IAS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IAS",
-            "brazilian": "IS",
-            "bulgarian": "IS",
-            "czech": "IS",
-            "danish": "IS",
-            "dutch": "IS",
-            "finnish": "IS",
-            "greek": "IS",
-            "hungarian": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "norwegian": "IS",
-            "polish": "IS",
-            "portuguese": "IS",
-            "romanian": "IS",
-            "russian": "IAS",
-            "schinese": "IS",
-            "swedish": "IS",
-            "tchinese": "IS",
-            "thai": "IS",
-            "turkish": "IS",
-            "ukrainian": "IS",
-            "vietnamese": "IS"
-        },
-        "information": "Don't use godspots in survival.\nStats are not detailed enough + survival maps from The Sacrifice and Crash Course are not shown in stats and not required for LEGENDARY SURVIVOR achievement, so make sure you screenshot all gold medals after the end of the rounds for proof."
+            "english": "IAS"
+        }
     }
 }
-var gameOfDayDate = '2026-08-17'
+var gameOfDayDate = '2026-08-18'
