f0d0dabfc458392036c1a14783b6d3c1ad671e80
Icondor <ivanfigus@gmail.com>
2026-08-23 10:20:22 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,101 +1,49 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 495050,
-        "CEId": "f8517c43-8de2-4e71-89d2-f0065dee36fa",
-        "name": "Mega Man Legacy Collection 2",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/495050/header.jpg?t=1644283244",
+        "gameType": 3,
+        "platformId": 751,
+        "CEId": "865e316c-90ef-4960-8f12-28b64fee68b1",
+        "name": "Donkey Kong Country 2",
+        "header": "https://upload.cedb.me/865e316c-90ef-4960-8f12-28b64fee68b1.jpg",
         "genre": [
-            "Action"
+            "Platformer"
         ],
-        "tier": 4,
-        "points": 80,
+        "tier": 1,
+        "points": 10,
         "secondaryPoints": 0,
-        "medianPlaytime": 5114,
-        "createdAt": "2023-10-19T06:22:43.000Z",
-        "updatedAt": "2026-08-20T16:04:12.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2025-08-23T16:55:18.000Z",
+        "updatedAt": "2026-08-17T23:15:01.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 4,
-        "playersStarted": 3,
-        "playersTotal": 270,
-        "priceData": {
-            "USD": {
-                "initial": 1999,
-                "final": 1999,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 1999,
-                "final": 1999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 1598,
-                "final": 1598,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 199000,
-                "final": 199000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 2796,
-                "final": 2796,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 2999,
-                "final": 2999,
-                "discountPercent": 0
-            }
+        "playersCompleted": 3,
+        "playersStarted": 0,
+        "playersTotal": 4,
+        "gameTags": [
+            "2D Platforming"
+        ],
+        "consoleData": {
+            "name": "Game Boy Advance",
+            "family": "Nintendo",
+            "icon": "https://static.retroachievements.org/assets/images/system/gba.png"
         },
         "milestones": {
-            "primary": 3,
-            "primaryText": "Metal Hero [55\u2605]\nClear Mega Man 9 without getting damaged. Checkpoint Save usage is disallowed.\n\nMega Busted [15\u2605]\nClear all the hard mode Boss and Miniboss stages in the Mega Man 10 Original Challenges.\n\nGilded Bomber[10\u2605 (Partial: 5\u2605)]\nComplete all four Mega Man games. / Earn gold medals on 50 challenges, including all four \"BOSS RUSH \u2605BUSTER ONLY!\" challenges.",
+            "primary": 1,
+            "primaryText": "Cranking It [10\u2605]\nBeat all of Cranky's Times in Diddy's Dash.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 2,
-            "communityText": "Outer Space Hazard\nClear Mega Man 10 without getting damaged and without loading save files. Checkpoint Save usage is disallowed.\n\nAmbition Resurges\nClear Mega Man 9 without getting damaged and without loading save files. Checkpoint Save usage is disallowed.",
-            "achievements": 21
+            "community": 0,
+            "communityText": "",
+            "achievements": 80
         },
         "CETags": {
             "genre": [
-                "Run & Gun",
-                "Action-Platformer"
+                "2D Platformer"
             ],
-            "info": []
-        },
-        "gameTags": [
-            "Action",
-            "Platformer",
-            "Classic",
-            "Retro",
-            "Great Soundtrack",
-            "2D",
-            "Singleplayer",
-            "Pixel Graphics",
-            "Difficult",
-            "Side Scroller",
-            "Anime",
-            "1990's",
-            "Shooter",
-            "Robots",
-            "Sci-fi",
-            "Remake",
-            "2D Platformer",
-            "Colorful"
-        ],
-        "languages": {
-            "english": "IAS",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "japanese": "IAS",
-            "spanish": "I",
-            "tchinese": "I"
+            "info": [
+                "Animal Protagonist"
+            ]
         }
     }
 }
-var gameOfDayDate = '2026-08-22'
+var gameOfDayDate = '2026-08-23'
