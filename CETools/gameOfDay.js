309a46211afaa7cfdd166e14efaf2e8dffc16a02
Icondor <ivanfigus@gmail.com>
2026-08-25 09:34:46 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,51 +1,97 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 3,
-        "platformId": 5268,
-        "CEId": "011101b7-4e31-44c8-b7c2-02fbf7577fa1",
-        "name": "Splatterhouse",
-        "header": "https://upload.cedb.me/011101b7-4e31-44c8-b7c2-02fbf7577fa1.jpg",
+        "gameType": 0,
+        "platformId": 78000,
+        "CEId": "f1eab776-645c-42d1-b640-244cbf4a590d",
+        "name": "Bejeweled\u00ae 3",
+        "header": "https://steamcdn-a.akamaihd.net/steam/apps/78000/header.jpg?t=1584804978",
         "genre": [
-            "Action"
+            "Arcade"
         ],
-        "tier": 0,
-        "points": 0,
-        "secondaryPoints": 5,
-        "medianPlaytime": 0,
-        "createdAt": "2025-08-24T17:18:39.000Z",
-        "updatedAt": "2026-08-12T01:31:49.000Z",
-        "playersOvercompleted": 2,
-        "playersCompleted": 0,
+        "tier": 1,
+        "points": 15,
+        "secondaryPoints": 0,
+        "medianPlaytime": 1727,
+        "createdAt": "2020-08-25T17:35:19.000Z",
+        "updatedAt": "2026-08-15T20:08:31.000Z",
+        "playersOvercompleted": 0,
+        "playersCompleted": 74,
         "playersStarted": 0,
-        "playersTotal": 3,
-        "gameTags": [
-            "Beat 'em Up"
-        ],
-        "consoleData": {
-            "name": "PC Engine/TurboGrafx-16",
-            "family": "NEC",
-            "icon": "https://static.retroachievements.org/assets/images/system/pce.png"
+        "playersTotal": 456,
+        "priceData": {
+            "USD": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 425,
+                "final": 425,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 49800,
+                "final": 49800,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 750,
+                "final": 750,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 499,
+                "final": 499,
+                "discountPercent": 0
+            }
         },
         "milestones": {
-            "primary": 0,
-            "primaryText": "",
-            "secondary": 1,
-            "secondaryText": "Ch ch ch ah ah ah [5\u2605]\nComplete the game on Hard Mode without using a continue.",
+            "primary": 1,
+            "primaryText": "Bedazzling [15\u2605]\nProve yourself.",
+            "secondary": 0,
+            "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 45
+            "achievements": 65
         },
         "CETags": {
             "genre": [
-                "Horror",
-                "Beat'em'Up"
+                "Score Attack",
+                "Arcade Puzzler"
             ],
-            "info": [
-                "Has Secondary Objectives"
-            ]
+            "info": []
         },
-        "information": "To access Hard Mode, hold the SELECT button on the title screen for 5 seconds until \"HARD\" appears under the title"
+        "gameTags": [
+            "Puzzle",
+            "Casual",
+            "Match 3",
+            "Singleplayer",
+            "Strategy",
+            "Addictive",
+            "Relaxing",
+            "Classic",
+            "Great Soundtrack",
+            "Touch-Friendly",
+            "Colorful",
+            "Family Friendly",
+            "Arcade",
+            "2D",
+            "Mouse Only",
+            "Indie"
+        ],
+        "languages": {
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "spanish": "I"
+        }
     }
 }
-var gameOfDayDate = '2026-08-24'
+var gameOfDayDate = '2026-08-25'
