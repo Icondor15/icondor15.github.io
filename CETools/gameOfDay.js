e1817881e97b9e297a1f32f7045cad9b6efc1a12
Icondor <ivanfigus@gmail.com>
2026-08-26 10:05:37 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,96 +2,110 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 78000,
-        "CEId": "f1eab776-645c-42d1-b640-244cbf4a590d",
-        "name": "Bejeweled\u00ae 3",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/78000/header.jpg?t=1584804978",
+        "platformId": 1110910,
+        "CEId": "869b0b40-e4c8-404e-8648-0b4ec86a55ed",
+        "name": "Mortal Shell",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1110910/header.jpg?t=1629787787",
         "genre": [
-            "Arcade"
+            "Action"
         ],
-        "tier": 1,
-        "points": 15,
+        "tier": 3,
+        "points": 40,
         "secondaryPoints": 0,
-        "medianPlaytime": 1727,
-        "createdAt": "2020-08-25T17:35:19.000Z",
-        "updatedAt": "2026-08-15T20:08:31.000Z",
+        "medianPlaytime": 3029,
+        "createdAt": "2021-08-26T17:12:54.000Z",
+        "updatedAt": "2026-08-15T00:30:26.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 74,
-        "playersStarted": 0,
-        "playersTotal": 456,
+        "playersCompleted": 19,
+        "playersStarted": 2,
+        "playersTotal": 252,
         "priceData": {
             "USD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 2999,
+                "final": 2999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 499,
-                "final": 499,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 425,
-                "final": 425,
+                "initial": 2499,
+                "final": 2499,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 49800,
-                "final": 49800,
+                "initial": 308000,
+                "final": 308000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 750,
-                "final": 750,
+                "initial": 4495,
+                "final": 4495,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 499,
-                "final": 499,
+                "initial": 3299,
+                "final": 3299,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Bedazzling [15\u2605]\nProve yourself.",
+            "primary": 2,
+            "primaryText": "Immortal Shell [25\u2605]\nComplete a run in The Virtuous Cycle with the Dark Form Obsidian Shell Shade.\n\nEternal Suffering [15\u2605]\nSeparately, beat the game without bonding with a shell, and beat the game in Obsidian Dark form.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 65
+            "achievements": 37
         },
         "CETags": {
             "genre": [
-                "Score Attack",
-                "Arcade Puzzler"
+                "Action-Adventure",
+                "Souls-like"
             ],
             "info": []
         },
         "gameTags": [
-            "Puzzle",
-            "Casual",
-            "Match 3",
+            "Souls-like",
+            "Dark Fantasy",
+            "Action",
+            "RPG",
+            "Difficult",
             "Singleplayer",
-            "Strategy",
-            "Addictive",
-            "Relaxing",
-            "Classic",
-            "Great Soundtrack",
-            "Touch-Friendly",
-            "Colorful",
-            "Family Friendly",
-            "Arcade",
-            "2D",
-            "Mouse Only",
-            "Indie"
+            "Violent",
+            "Action RPG",
+            "Third Person",
+            "Hack and Slash",
+            "Atmospheric",
+            "Open World",
+            "Adventure",
+            "Multiplayer",
+            "Indie",
+            "Gore",
+            "Horror",
+            "Sexual Content",
+            "Co-op",
+            "Nudity"
         ],
         "languages": {
-            "english": "I",
-            "french": "I",
-            "german": "I",
-            "italian": "I",
-            "spanish": "I"
-        }
+            "english": "IAS",
+            "french": "IS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "koreana": "IS",
+            "latam": "IS",
+            "portuguese": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS",
+            "ukrainian": "IS"
+        },
+        "information": "DLC Required:\n-The Virtuous Cycle"
     }
 }
-var gameOfDayDate = '2026-08-25'
+var gameOfDayDate = '2026-08-26'
