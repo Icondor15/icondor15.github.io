f4bb21cc1e99e20c32395c4d4c3da51ffaa9b243
Icondor <ivanfigus@gmail.com>
2026-08-27 09:40:24 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,110 +2,101 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1110910,
-        "CEId": "869b0b40-e4c8-404e-8648-0b4ec86a55ed",
-        "name": "Mortal Shell",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1110910/header.jpg?t=1629787787",
+        "platformId": 200260,
+        "CEId": "01078398-a4ae-45b8-81df-75c2c950a05e",
+        "name": "Batman: Arkham City - Game of the Year Edition",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/200260/header.jpg?t=1745964057",
         "genre": [
             "Action"
         ],
-        "tier": 3,
-        "points": 40,
+        "tier": 1,
+        "points": 15,
         "secondaryPoints": 0,
-        "medianPlaytime": 3029,
-        "createdAt": "2021-08-26T17:12:54.000Z",
-        "updatedAt": "2026-08-15T00:30:26.000Z",
+        "medianPlaytime": 5523,
+        "createdAt": "2025-08-27T21:37:04.000Z",
+        "updatedAt": "2026-08-12T01:31:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 19,
-        "playersStarted": 2,
-        "playersTotal": 252,
+        "playersCompleted": 11,
+        "playersStarted": 0,
+        "playersTotal": 513,
         "priceData": {
             "USD": {
-                "initial": 2999,
-                "final": 2999,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 2499,
-                "final": 2499,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 308000,
-                "final": 308000,
+                "initial": 198000,
+                "final": 198000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 4495,
-                "final": 4495,
+                "initial": 2895,
+                "final": 2895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 3299,
-                "final": 3299,
+                "initial": 1999,
+                "final": 1999,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Immortal Shell [25\u2605]\nComplete a run in The Virtuous Cycle with the Dark Form Obsidian Shell Shade.\n\nEternal Suffering [15\u2605]\nSeparately, beat the game without bonding with a shell, and beat the game in Obsidian Dark form.",
+            "primary": 1,
+            "primaryText": "Perfect Knight[15\u2605 (Partial: 10\u2605)]\nComplete all relevant campaigns on all characters.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 37
+            "achievements": 64
         },
         "CETags": {
             "genre": [
-                "Action-Adventure",
-                "Souls-like"
+                "Action-Adventure"
             ],
             "info": []
         },
         "gameTags": [
-            "Souls-like",
-            "Dark Fantasy",
             "Action",
-            "RPG",
-            "Difficult",
-            "Singleplayer",
-            "Violent",
-            "Action RPG",
-            "Third Person",
-            "Hack and Slash",
-            "Atmospheric",
             "Open World",
+            "Stealth",
+            "Superhero",
             "Adventure",
-            "Multiplayer",
-            "Indie",
-            "Gore",
-            "Horror",
-            "Sexual Content",
-            "Co-op",
-            "Nudity"
+            "Third Person",
+            "Singleplayer",
+            "Beat 'em up",
+            "Comic Book",
+            "Detective",
+            "Story Rich",
+            "Atmospheric",
+            "Fighting",
+            "Action-Adventure",
+            "Cinematic",
+            "Controller",
+            "Sandbox",
+            "Metroidvania",
+            "Great Soundtrack",
+            "RPG"
         ],
         "languages": {
             "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "italian": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
+            "french": "IAS",
+            "german": "IAS",
+            "italian": "IAS",
+            "spanish": "IAS",
             "brazilian": "IS",
-            "koreana": "IS",
-            "latam": "IS",
-            "portuguese": "IS",
-            "russian": "IS",
-            "schinese": "IS",
-            "tchinese": "IS",
-            "ukrainian": "IS"
-        },
-        "information": "DLC Required:\n-The Virtuous Cycle"
+            "russian": "IS"
+        }
     }
 }
-var gameOfDayDate = '2026-08-26'
+var gameOfDayDate = '2026-08-27'
