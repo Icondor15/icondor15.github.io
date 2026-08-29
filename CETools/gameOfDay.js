639168b0b6d0242094cc0772705204c1e573923c
Icondor <ivanfigus@gmail.com>
2026-08-29 10:17:01 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,52 +1,105 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 4,
-        "platformId": 41010,
-        "CEId": "3eaf956f-7874-432b-8b4d-0c51f809a633",
-        "name": "Serious Sam HD: The Second Encounter",
-        "header": "https://cdn.cloudflare.steamstatic.com/steam/apps/41014/header.jpg?t=1573728535",
+        "gameType": 0,
+        "platformId": 243360,
+        "CEId": "ba80ac2a-186d-4b55-8981-d4bce6a34663",
+        "name": "TrackMania\u00b2 Valley",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/243360/header.jpg?t=1588603133",
         "genre": [
-            "First-Person"
+            "Arcade"
         ],
         "tier": 3,
-        "points": 75,
+        "points": 40,
         "secondaryPoints": 0,
-        "medianPlaytime": 4052,
-        "createdAt": "2020-08-28T17:22:24.000Z",
-        "updatedAt": "2026-08-12T01:53:48.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2022-08-29T07:21:24.000Z",
+        "updatedAt": "2026-08-12T01:31:49.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 16,
-        "playersStarted": 6,
-        "playersTotal": 396,
+        "playersCompleted": 10,
+        "playersStarted": 7,
+        "playersTotal": 104,
+        "priceData": {
+            "USD": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1699,
+                "final": 1699,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 264000,
+                "final": 264000,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2995,
+                "final": 2995,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
             "primary": 2,
-            "primaryText": "Serious Co-optitude[45\u2605 (Partial: 15\u2605)]\nObtain a gold medal on all cooperative Survival levels. / Complete a Coin-op cooperative game on Normal difficulty (or higher).\n\nSecond Sam[30\u2605 (Partial: 15\u2605)]\nComplete the game in single player on Serious difficulty. / Complete \"The Grand Cathedral\" on Serious difficulty without dying or loading. / Obtain a gold medal on all single player Survival levels.",
+            "primaryText": "Well-Stacked in Tracks [30\u2605]\nObtain 65 Author Medals.\n\nRoad Rager [10\u2605]\nObtain 65 Gold Medals.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 2,
-            "communityText": "Bent on Sand\nObtain the gold medal with a time of 3:30 on \"Bend on Sand\" in single player Survival.\n\nSerious Seconds\nComplete the game in single player on Serious difficulty without dying or loading.",
-            "achievements": 70
+            "community": 3,
+            "communityText": "Alpine All-Star\nGet all Author Medals in the TM1 Alpine Title Pack.\n\nSpeed All-Star\nGet all Author Medals in the TM1 Speed Title Pack.\n\nValley All-Star\nGet all Author Medals in the StarTrack Title Pack.",
+            "achievements": 0
         },
         "CETags": {
             "genre": [
-                "First Person Shooter",
-                "Arena Shooter"
+                "Racing"
             ],
             "info": []
         },
         "gameTags": [
-            "Action",
-            "Indie"
+            "Racing",
+            "Sports",
+            "Multiplayer",
+            "Level Editor",
+            "Time Attack",
+            "Arcade",
+            "Singleplayer",
+            "Local Multiplayer",
+            "Split Screen"
         ],
         "languages": {
             "english": "I",
             "french": "I",
             "german": "I",
             "italian": "I",
-            "spanish": "I"
-        },
-        "information": "The actual store page is located here: [https://store.steampowered.com/app/41014](https://store.steampowered.com/app/41014)"
+            "japanese": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "czech": "I",
+            "danish": "I",
+            "dutch": "I",
+            "finnish": "I",
+            "hungarian": "I",
+            "koreana": "I",
+            "norwegian": "I",
+            "polish": "I",
+            "portuguese": "I",
+            "romanian": "I",
+            "russian": "I",
+            "schinese": "I",
+            "swedish": "I",
+            "turkish": "I"
+        }
     }
 }
-var gameOfDayDate = '2026-08-28'
+var gameOfDayDate = '2026-08-29'
