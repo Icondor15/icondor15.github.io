dfc54b2967d4be50eb8b2e5bb6fc1df87ff7f8ee
Icondor <ivanfigus@gmail.com>
2026-09-02 10:12:34 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,100 +2,106 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1884300,
-        "CEId": "d88b865b-b991-4d5f-bc05-5d6193470b87",
-        "name": "Deathsmiles I\u30fbII",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1884300/header.jpg?t=1676875376",
+        "platformId": 342180,
+        "CEId": "036b16db-e20b-4b51-a0bf-e3ecfb5b5e26",
+        "name": "Arizona Sunshine\u00ae",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/342180/header.jpg?t=1725006637",
         "genre": [
-            "Bullet Hell"
+            "First-Person"
         ],
         "tier": 2,
-        "points": 20,
+        "points": 30,
         "secondaryPoints": 0,
-        "medianPlaytime": 1096,
-        "createdAt": "2023-09-01T03:32:53.000Z",
+        "medianPlaytime": 1859,
+        "createdAt": "2024-09-02T23:03:49.000Z",
         "updatedAt": "2026-08-30T06:36:35.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 2,
-        "playersStarted": 2,
-        "playersTotal": 33,
+        "playersCompleted": 5,
+        "playersStarted": 4,
+        "playersTotal": 77,
         "priceData": {
             "USD": {
-                "initial": 3990,
-                "final": 3990,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 3490,
-                "final": 3490,
+                "initial": 1499,
+                "final": 1499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 3099,
-                "final": 3099,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 495000,
-                "final": 495000,
+                "initial": 168000,
+                "final": 168000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 5695,
-                "final": 5695,
+                "initial": 2295,
+                "final": 2295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 4549,
-                "final": 4549,
+                "initial": 2099,
+                "final": 2099,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Fever Christmas [15\u2605]\nProve yourself on the X mode.\n\nHeaping Snow [5\u2605]\nClear DSII's Arcade mode without continuing.",
+            "primary": 3,
+            "primaryText": "Don't Dead, Open Inside [15\u2605]\nComplete the main game on Apocalyptic difficulty.\n\nDamn It [10\u2605]\nComplete The Damned DLC on Apocalyptic difficulty.\n\nDead Man Walking [5\u2605]\nComplete Dead Man DLC on Apocalyptic difficulty.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 43
+            "achievements": 48
         },
         "CETags": {
             "genre": [
-                "Horizontal Shoot'em'Up"
+                "First Person Shooter"
             ],
-            "info": []
+            "info": [
+                "VR Required"
+            ]
         },
         "gameTags": [
             "Action",
-            "Arcade",
-            "Shooter",
-            "Shoot 'Em Up",
-            "Bullet Hell",
-            "2D Platformer",
-            "Side Scroller",
-            "2D",
-            "Cute",
-            "Anime",
-            "Pixel Graphics",
-            "Fantasy",
-            "Magic",
+            "VR",
+            "Zombies",
+            "Multiplayer",
             "Horror",
-            "Gothic",
-            "Multiple Endings",
-            "Female Protagonist",
-            "Choices Matter",
+            "Survival",
+            "Co-op Campaign",
+            "Online Co-Op",
+            "Adventure",
+            "Gore",
+            "FPS",
+            "Shooter",
+            "First-Person",
+            "Survival Horror",
+            "Indie",
+            "Co-op",
+            "Open World",
+            "Violent",
             "Singleplayer",
-            "Multiplayer"
+            "Post-apocalyptic"
         ],
         "languages": {
-            "english": "IS",
-            "french": "IS",
+            "english": "IAS",
+            "french": "IAS",
+            "german": "IAS",
+            "italian": "IAS",
             "japanese": "IAS",
-            "spanish": "IS",
-            "koreana": "IS",
-            "tchinese": "IS"
-        }
+            "spanish": "IAS",
+            "koreana": "IAS",
+            "russian": "IAS",
+            "schinese": "IAS"
+        },
+        "information": "This game requires a VR headset in order to play."
     }
 }
-var gameOfDayDate = '2026-09-01'
+var gameOfDayDate = '2026-09-02'
