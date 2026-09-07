33810e5c8f5a97960b131e5a0b768aca588427ac
Icondor <ivanfigus@gmail.com>
2026-09-07 09:54:16 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,89 +1,52 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 0,
-        "platformId": 1492420,
-        "CEId": "fcb2bef9-e847-4390-9c12-6315fa149e3f",
-        "name": "EQUALINE",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1492420/header.jpg?t=1612777216",
+        "gameType": 1,
+        "platformId": 796380,
+        "CEId": "9d36a781-72fa-4529-ae78-bc14b5ae2f9b",
+        "name": "Terra Feminarum",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/796380/header.jpg?t=1588712837",
         "genre": [
-            "Arcade"
+            "Bullet Hell"
         ],
-        "tier": 3,
-        "points": 65,
+        "tier": 4,
+        "points": 120,
         "secondaryPoints": 0,
-        "medianPlaytime": 2114,
-        "createdAt": "2022-09-06T17:37:06.000Z",
-        "updatedAt": "2026-09-06T07:12:35.000Z",
+        "medianPlaytime": 2418,
+        "createdAt": "2021-09-07T20:19:56.000Z",
+        "updatedAt": "2026-08-30T06:36:35.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 64,
-        "playersStarted": 0,
-        "playersTotal": 171,
-        "priceData": {
-            "USD": {
-                "initial": 1199,
-                "final": 1199,
-                "discountPercent": 0
-            },
-            "EUR": {
-                "initial": 999,
-                "final": 999,
-                "discountPercent": 0
-            },
-            "GBP": {
-                "initial": 929,
-                "final": 929,
-                "discountPercent": 0
-            },
-            "JPY": {
-                "initial": 122000,
-                "final": 122000,
-                "discountPercent": 0
-            },
-            "AUD": {
-                "initial": 1695,
-                "final": 1695,
-                "discountPercent": 0
-            },
-            "CAD": {
-                "initial": 1349,
-                "final": 1349,
-                "discountPercent": 0
-            }
-        },
+        "playersCompleted": 5,
+        "playersStarted": 1,
+        "playersTotal": 188,
         "milestones": {
-            "primary": 1,
-            "primaryText": "Order of Operations [65\u2605]\nClear a mission with more than 120 seconds remaining. / Get a score of 1 million or more in every mission.",
+            "primary": 3,
+            "primaryText": "The Light Comes From The North [80\u2605]\nComplete all Brutal trials.\n\nBeyond The Dark Star [25\u2605]\nProve yourself.\n\nRoad to Pohjola [15\u2605]\nClear all Brutal story chapters.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 3,
-            "communityText": "Divide By Zero\nAchieve a Maxout (999,999,999 points) in any of the Endless modes.\n\nEuler\nDefeat a Lv. MAX CPU with \"\"999 Sec.\", \"Easy\" and \"100 Questions\" selected in Calculation Battle.\n\nGauss\nDefeat a Lv. MAX CPU with \"999 Sec.\", \"Expert\" and \"100 Questions\" selected in Calculation Battle.",
-            "achievements": 24
+            "community": 0,
+            "communityText": "",
+            "achievements": 30
         },
         "CETags": {
             "genre": [
-                "Miscellaneous",
-                "Score Attack"
+                "Vertical Shoot'em'Up"
             ],
             "info": [
-                "Has Community Objectives"
+                "Free"
             ]
         },
         "gameTags": [
             "Action",
-            "Puzzle",
-            "Singleplayer",
-            "Multiplayer",
-            "PvP",
-            "2D",
             "Indie",
-            "Psychedelic",
-            "Abstract"
+            "Bullet Hell",
+            "Shoot 'Em Up",
+            "Anime",
+            "Free to Play"
         ],
         "languages": {
-            "english": "I",
-            "japanese": "I"
+            "english": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-09-06'
+var gameOfDayDate = '2026-09-07'
