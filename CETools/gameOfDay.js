6e55f2651af3306d6f091b4dab1435ce9f92b5f0
Icondor <ivanfigus@gmail.com>
2026-09-10 09:40:30 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -1,66 +1,104 @@
 var gameOfDay =
 {
     "game": {
-        "gameType": 1,
-        "platformId": 1993830,
-        "CEId": "91796946-6a46-41e5-930c-f607897e126d",
-        "name": "ROTA",
-        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1993830/header.jpg?t=1712599127",
+        "gameType": 0,
+        "platformId": 314710,
+        "CEId": "e65f151c-37d4-4ea6-aae7-18d78ba88d89",
+        "name": "Mighty No. 9",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/314710/header.jpg?t=1608197459",
         "genre": [
-            "Platformer"
+            "Action"
         ],
-        "tier": 1,
-        "points": 10,
+        "tier": 3,
+        "points": 40,
         "secondaryPoints": 0,
-        "medianPlaytime": 648,
-        "createdAt": "2024-09-09T18:48:49.000Z",
+        "medianPlaytime": 4330,
+        "createdAt": "2024-09-10T18:47:32.000Z",
         "updatedAt": "2026-08-30T06:36:35.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 5,
-        "playersStarted": 0,
-        "playersTotal": 115,
+        "playersCompleted": 2,
+        "playersStarted": 2,
+        "playersTotal": 125,
+        "priceData": {
+            "USD": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "EUR": {
+                "initial": 1999,
+                "final": 1999,
+                "discountPercent": 0
+            },
+            "GBP": {
+                "initial": 1779,
+                "final": 1779,
+                "discountPercent": 0
+            },
+            "JPY": {
+                "initial": 238900,
+                "final": 238900,
+                "discountPercent": 0
+            },
+            "AUD": {
+                "initial": 2995,
+                "final": 2995,
+                "discountPercent": 0
+            },
+            "CAD": {
+                "initial": 2749,
+                "final": 2749,
+                "discountPercent": 0
+            }
+        },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Directionally Challenged [10\u2605]\nSeparately, complete all time trials and beat the game in under 1 hour.",
+            "primary": 3,
+            "primaryText": "Bright Ray [15\u2605]\nGet S ranks on each stage on Hyper & Maniac difficulties as Ray. / Get S rank on the \"Vermillion Destroyer\" stage on Hyper & Maniac difficulties.\n\nBeefed Up [15\u2605]\nGet S ranks on each stage of the base game on Hyper & Maniac difficulties (as Beck).\n\nI'll See You In Xel [10\u2605]\nProve yourself.",
             "secondary": 0,
             "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 14
+            "achievements": 70
         },
         "CETags": {
             "genre": [
-                "Puzzle Platformer"
+                "Action-Platformer"
             ],
-            "info": [
-                "Free"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Puzzle",
-            "Puzzle Platformer",
-            "Logic",
-            "Cute",
-            "Precision Platformer",
-            "Atmospheric",
-            "Relaxing",
-            "Female Protagonist",
-            "Open World",
-            "Colorful",
-            "Soundtrack",
-            "2D Platformer",
             "Platformer",
-            "2D",
-            "Controller",
-            "Casual",
+            "Action",
+            "Anime",
+            "Side Scroller",
+            "2.5D",
+            "Retro",
+            "Robots",
             "Singleplayer",
-            "Family Friendly",
-            "Nature",
-            "Indie"
+            "Indie",
+            "Multiplayer",
+            "Co-op",
+            "Casual",
+            "Difficult",
+            "Sci-fi",
+            "Comedy",
+            "2D",
+            "Great Soundtrack",
+            "Futuristic"
         ],
         "languages": {
-            "english": "I"
+            "english": "IAS",
+            "french": "IAS",
+            "german": "IS",
+            "italian": "IS",
+            "japanese": "IAS",
+            "spanish": "IS",
+            "brazilian": "IS",
+            "koreana": "IS",
+            "russian": "IS",
+            "schinese": "IS",
+            "tchinese": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-09-09'
+var gameOfDayDate = '2026-09-10'
