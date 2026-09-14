5b72849d6b9da1ca10727234386481e59ea53e9d
Icondor <ivanfigus@gmail.com>
2026-09-14 11:00:10 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,86 +2,110 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 367140,
-        "CEId": "8b57a61c-e33a-4bc3-a5c2-bb06967a5a8d",
-        "name": "Wish Project",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/367140/header.jpg?t=1459976554",
+        "platformId": 2354000,
+        "CEId": "ea897b12-27bd-43cd-a04e-9ae4a06b9f08",
+        "name": "Slackers - Carts of Glory",
+        "header": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/2354000/header.jpg?t=1727693537",
         "genre": [
-            "Bullet Hell"
+            "Arcade"
         ],
-        "tier": 3,
-        "points": 45,
+        "tier": 2,
+        "points": 30,
         "secondaryPoints": 0,
-        "medianPlaytime": 2070,
-        "createdAt": "2022-09-13T17:43:49.000Z",
+        "medianPlaytime": 0,
+        "createdAt": "2024-11-09T03:33:45.000Z",
         "updatedAt": "2026-09-12T05:11:18.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 2,
-        "playersStarted": 2,
-        "playersTotal": 37,
+        "playersCompleted": 0,
+        "playersStarted": 9,
+        "playersTotal": 32,
         "priceData": {
             "USD": {
-                "initial": 199,
-                "final": 199,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 199,
-                "final": 199,
+                "initial": 1299,
+                "final": 1299,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 159,
-                "final": 159,
+                "initial": 1099,
+                "final": 1099,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 19800,
-                "final": 19800,
+                "initial": 149000,
+                "final": 149000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 295,
-                "final": 295,
+                "initial": 1895,
+                "final": 1895,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 219,
-                "final": 219,
+                "initial": 1699,
+                "final": 1699,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Wish Upon a Horn [35\u2605]\nClear the game on Harder difficulty without using any continues.\n\nMedal Enthusiast [10\u2605]\nSeparately, earn all six Gold Medals on any character on Harder difficulty.",
+            "primary": 3,
+            "primaryText": "Drunken Daredevil (UNCLEARED) [0\u2605]\nObtain the platinum medal on all Freestyle levels.\n\nAbsolutely Trolleyed [15\u2605]\nObtain the platinum medal on all other Time Attack levels.\n\nTired of Bein' Sober [15\u2605]\nObtain the platinum medal on any 16 Time Attack levels.",
             "secondary": 0,
             "secondaryText": "",
             "community": 1,
-            "communityText": "Medal Overachiever\nSeparately, earn all six Gold Medals on ALL characters (including hidden ones) on Harder difficulty.",
-            "achievements": 21
+            "communityText": "King of Winging It\nComplete all the original 16 levels in level order without dying, restarting, or quitting.",
+            "achievements": 17
         },
         "CETags": {
             "genre": [
-                "Vertical Shoot'em'Up"
+                "Racing"
             ],
-            "info": []
+            "info": [
+                "Has Community Objectives",
+                "Uncleared"
+            ]
         },
         "gameTags": [
-            "Action",
             "Indie",
-            "Shoot 'Em Up",
-            "Bullet Hell",
-            "Local Co-Op",
-            "Shooter",
-            "Arcade",
-            "2D",
+            "Simulation",
+            "Casual",
+            "Driving",
+            "Funny",
+            "Skateboarding",
+            "Physics",
+            "Third Person",
+            "Colorful",
+            "Stylized",
+            "Multiplayer",
             "Difficult",
+            "Time Attack",
+            "Arcade",
+            "Tutorial",
             "Singleplayer",
-            "Controller"
+            "3D",
+            "Controller",
+            "First-Person",
+            "Racing"
         ],
         "languages": {
-            "english": "I"
-        }
+            "english": "I",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "greek": "I",
+            "hindi": "I",
+            "koreana": "I",
+            "russian": "I",
+            "schinese": "I"
+        },
+        "information": "The original 16 levels are listed in the following order:\n\u00ad1. Groove\n\u00ad2. Monolith\n\u00ad3. Outskirts\n\u00ad4. Hillbomb\n\u00ad5. The Breeze\n\u00ad6. Coasting\n\u00ad7. Dam it!\n\u00ad8. Downtown\n\u00ad9. Derailed\n\u00ad10. Downfall\n\u00ad11. Roundabout\n\u00ad12. Road rash\n\u00ad13. Night crawler\n\u00ad14. Bottleneck\n\u00ad15. Trainwreck\n\u00ad16. Roadkill"
     }
 }
-var gameOfDayDate = '2026-09-13'
+var gameOfDayDate = '2026-09-14'
