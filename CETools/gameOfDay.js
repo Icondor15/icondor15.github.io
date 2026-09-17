ef94fe22c6789b9d85ce7e26c5e0551b49ed8084
Icondor <ivanfigus@gmail.com>
2026-09-17 09:30:42 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,92 +2,97 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 1325470,
-        "CEId": "1372fbcf-73a1-42a7-a76a-ea2bd2fda3ff",
-        "name": "Pianistic",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1325470/header.jpg?t=1681124629",
+        "platformId": 1996470,
+        "CEId": "27231b8f-889d-4b78-9286-ff0cf434d97a",
+        "name": "Skator Gator 3D",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1996470/header.jpg?t=1694548750",
         "genre": [
-            "Arcade"
+            "Platformer"
         ],
-        "tier": 3,
-        "points": 50,
+        "tier": 1,
+        "points": 5,
         "secondaryPoints": 0,
-        "medianPlaytime": 0,
-        "createdAt": "2023-09-16T20:59:57.000Z",
+        "medianPlaytime": 358,
+        "createdAt": "2023-09-17T15:05:31.000Z",
         "updatedAt": "2026-09-15T17:17:54.000Z",
         "playersOvercompleted": 0,
-        "playersCompleted": 0,
-        "playersStarted": 2,
-        "playersTotal": 95,
+        "playersCompleted": 7,
+        "playersStarted": 0,
+        "playersTotal": 41,
         "priceData": {
             "USD": {
-                "initial": 199,
-                "final": 199,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 199,
-                "final": 199,
+                "initial": 499,
+                "final": 499,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 169,
-                "final": 169,
+                "initial": 429,
+                "final": 429,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 23500,
-                "final": 23500,
+                "initial": 58000,
+                "final": 58000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 295,
-                "final": 295,
+                "initial": 750,
+                "final": 750,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 259,
-                "final": 259,
+                "initial": 649,
+                "final": 649,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 4,
-            "primaryText": "One More Encore (UNCLEARED) [0\u2605]\nObtain an SSS rank or higher on all songs.\n\nGrand Pianist [20\u2605]\nObtain an SS rank or higher on all songs.\n\nPhilharmonic [20\u2605]\nObtain an S rank or higher on all songs.\n\nChord Progression [10\u2605]\nObtain a B+ rank or higher or more on all songs.",
+            "primary": 1,
+            "primaryText": "Later, Speed Gator [5\u2605]\nGet all staff times.",
             "secondary": 0,
             "secondaryText": "",
-            "community": 1,
-            "communityText": "Concerto of the Century\nObtain a U rank on all songs.",
-            "achievements": 27
+            "community": 0,
+            "communityText": "",
+            "achievements": 24
         },
         "CETags": {
             "genre": [
-                "Rhythm"
+                "3D Platformer"
             ],
             "info": [
-                "Uncleared",
-                "Has Community Objectives"
+                "Animal Protagonist"
             ]
         },
         "gameTags": [
-            "Rhythm",
-            "Difficult",
-            "Level Editor",
-            "Atmospheric",
-            "Music",
-            "Singleplayer",
-            "Moddable",
-            "Minimalist",
-            "Casual",
-            "2D",
+            "3D Platformer",
+            "Collectathon",
+            "Short",
             "Action",
-            "Indie",
-            "Cyberpunk",
-            "Music-Based Procedural Generation"
+            "Family Friendly",
+            "Skateboarding",
+            "Singleplayer",
+            "Cartoon",
+            "Action-Adventure",
+            "Platformer",
+            "3D",
+            "Cute",
+            "Colorful",
+            "Skating",
+            "Cartoony",
+            "Adventure",
+            "Sports",
+            "Linear",
+            "Precision Platformer",
+            "Arcade"
         ],
         "languages": {
             "english": "I"
         }
     }
 }
-var gameOfDayDate = '2026-09-16'
+var gameOfDayDate = '2026-09-17'
