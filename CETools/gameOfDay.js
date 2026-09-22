ef363b76e11300d83383ea31ef07e6a4a9ab56c4
Icondor <ivanfigus@gmail.com>
2026-09-22 09:34:00 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,103 +2,91 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 248310,
-        "CEId": "a57d1929-f26f-4b90-8917-3184977698f4",
-        "name": "Freedom Planet",
-        "header": "https://steamcdn-a.akamaihd.net/steam/apps/248310/header.jpg?t=1560750049",
+        "platformId": 1856900,
+        "CEId": "127c7f28-8644-4e49-bda4-6118520cf17f",
+        "name": "Andro Dunos II",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1856900/header.jpg?t=1648470911",
         "genre": [
-            "Platformer"
+            "Bullet Hell"
         ],
-        "tier": 1,
-        "points": 10,
-        "secondaryPoints": 0,
-        "medianPlaytime": 2233,
-        "createdAt": "2020-04-14T19:28:15.000Z",
+        "tier": 0,
+        "points": 0,
+        "secondaryPoints": 10,
+        "medianPlaytime": 225,
+        "createdAt": "2023-09-22T00:45:05.000Z",
         "updatedAt": "2026-09-15T17:17:54.000Z",
-        "playersOvercompleted": 0,
-        "playersCompleted": 21,
+        "playersOvercompleted": 6,
+        "playersCompleted": 0,
         "playersStarted": 0,
-        "playersTotal": 283,
+        "playersTotal": 74,
         "priceData": {
             "USD": {
+                "initial": 1799,
+                "final": 1799,
+                "discountPercent": 0
+            },
+            "EUR": {
                 "initial": 1499,
                 "final": 1499,
                 "discountPercent": 0
             },
-            "EUR": {
+            "GBP": {
                 "initial": 1399,
                 "final": 1399,
                 "discountPercent": 0
             },
-            "GBP": {
-                "initial": 1099,
-                "final": 1099,
-                "discountPercent": 0
-            },
             "JPY": {
-                "initial": 148000,
-                "final": 148000,
+                "initial": 184000,
+                "final": 184000,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 2150,
-                "final": 2150,
+                "initial": 2595,
+                "final": 2595,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1699,
-                "final": 1699,
+                "initial": 2049,
+                "final": 2049,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 1,
-            "primaryText": "Guardian of Avalice [10\u2605]\nClear the game without losing any lives. / Clear the game in less than 90 minutes.",
-            "secondary": 0,
-            "secondaryText": "",
-            "community": 1,
-            "communityText": "Grand Gem Hoarder\nBeat the original version of the \"Gem Hoarder\" achievement by completing the game with over 7000 crystal shards.",
-            "achievements": 56
+            "primary": 0,
+            "primaryText": "",
+            "secondary": 1,
+            "secondaryText": "From 1992 with Love [10\u2605]\nBeat the game on Android difficulty without using continues.",
+            "community": 0,
+            "communityText": "",
+            "achievements": 0
         },
         "CETags": {
             "genre": [
-                "2D Platformer"
+                "Horizontal Shoot'em'Up"
             ],
             "info": [
-                "Overwhelmingly Positive",
-                "Has Community Objectives"
+                "Has Secondary Objectives"
             ]
         },
         "gameTags": [
-            "Platformer",
-            "Great Soundtrack",
+            "Shoot 'Em Up",
+            "Bullet Hell",
             "Action",
-            "Female Protagonist",
-            "2D",
-            "Indie",
-            "Pixel Graphics",
-            "Retro",
-            "Fast-Paced",
-            "Singleplayer",
-            "Adventure",
-            "Cute",
+            "Arcade",
+            "Shooter",
             "Difficult",
-            "Anime",
-            "Classic",
-            "Side Scroller",
-            "Story Rich",
-            "Aliens",
-            "Funny",
-            "2D Platformer"
+            "2D",
+            "Pixel Graphics",
+            "Linear",
+            "Controller",
+            "Singleplayer",
+            "Futuristic",
+            "Soundtrack",
+            "Indie"
         ],
         "languages": {
-            "english": "IAS",
-            "french": "IS",
-            "german": "IS",
-            "japanese": "IS",
-            "spanish": "IS",
-            "koreana": "IS"
+            "english": "IS"
         }
     }
 }
-var gameOfDayDate = '2026-09-21'
+var gameOfDayDate = '2026-09-22'
