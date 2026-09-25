a6538baf7954f1b29afa80c19ed0119a1791331c
Icondor <ivanfigus@gmail.com>
2026-09-25 09:48:31 +0200

    Regular game data update. (EXTRAS)

--- CETools/gameOfDay.js
+++ CETools/gameOfDay.js
@@ -2,98 +2,108 @@ var gameOfDay =
 {
     "game": {
         "gameType": 0,
-        "platformId": 719930,
-        "CEId": "e17a963d-bc43-4286-98bf-bc9c43ab1d83",
-        "name": "Super High Ball: Pinball Platformer",
-        "header": "https://cdn.akamai.steamstatic.com/steam/apps/719930/header.jpg?t=1627027446",
+        "platformId": 1520330,
+        "CEId": "1b2a5997-9812-4099-bcb7-b215e17de447",
+        "name": "TechBeat Heart",
+        "header": "https://cdn.akamai.steamstatic.com/steam/apps/1520330/header.jpg?t=1654351385",
         "genre": [
-            "Platformer"
+            "Bullet Hell"
         ],
-        "tier": 3,
-        "points": 65,
-        "secondaryPoints": 30,
-        "medianPlaytime": 2578,
-        "createdAt": "2021-09-24T06:36:49.000Z",
+        "tier": 2,
+        "points": 30,
+        "secondaryPoints": 0,
+        "medianPlaytime": 651,
+        "createdAt": "2021-09-25T21:35:44.000Z",
         "updatedAt": "2026-09-21T05:01:27.000Z",
-        "playersOvercompleted": 4,
-        "playersCompleted": 23,
-        "playersStarted": 2,
-        "playersTotal": 112,
+        "playersOvercompleted": 0,
+        "playersCompleted": 5,
+        "playersStarted": 0,
+        "playersTotal": 91,
         "priceData": {
             "USD": {
-                "initial": 999,
-                "final": 999,
+                "initial": 199,
+                "final": 199,
                 "discountPercent": 0
             },
             "EUR": {
-                "initial": 975,
-                "final": 975,
+                "initial": 199,
+                "final": 199,
                 "discountPercent": 0
             },
             "GBP": {
-                "initial": 850,
-                "final": 850,
+                "initial": 169,
+                "final": 169,
                 "discountPercent": 0
             },
             "JPY": {
-                "initial": 120000,
-                "final": 120000,
+                "initial": 23500,
+                "final": 23500,
                 "discountPercent": 0
             },
             "AUD": {
-                "initial": 1450,
-                "final": 1450,
+                "initial": 295,
+                "final": 295,
                 "discountPercent": 0
             },
             "CAD": {
-                "initial": 1299,
-                "final": 1299,
+                "initial": 259,
+                "final": 259,
                 "discountPercent": 0
             }
         },
         "milestones": {
-            "primary": 2,
-            "primaryText": "Absolute Baller [40\u2605]\nBeat all Diamond times.\n\nNo Tilt Required [25\u2605]\nGet all the Sapphire balls and cubes.",
-            "secondary": 1,
-            "secondaryText": "Pinball Wizard [30\u2605]\nComplete every individual level without dying.",
+            "primary": 1,
+            "primaryText": "With Love [30\u2605]\nProve yourself!",
+            "secondary": 0,
+            "secondaryText": "",
             "community": 0,
             "communityText": "",
-            "achievements": 64
+            "achievements": 55
         },
         "CETags": {
             "genre": [
-                "Awkward Movement"
+                "Score Attack",
+                "Vertical Shoot'em'Up"
             ],
-            "info": [
-                "Has Secondary Objectives",
-                "Curated"
-            ]
+            "info": []
         },
         "gameTags": [
-            "Precision Platformer",
-            "Pinball",
+            "Shoot 'Em Up",
+            "Bullet Hell",
+            "Arena Shooter",
+            "Procedural Generation",
+            "Replay Value",
             "Difficult",
-            "Arcade",
-            "Fast-Paced",
-            "Platformer",
-            "Physics",
-            "Action",
-            "Time Attack",
             "Score Attack",
+            "Arcade",
+            "Perma Death",
+            "Top-Down Shooter",
+            "Action",
+            "Spectacle fighter",
             "Singleplayer",
-            "Side Scroller",
-            "Parkour",
-            "Character Customization",
-            "Indie",
-            "2D Platformer",
-            "Stylized",
-            "Minimalist",
-            "Adventure",
-            "3D"
+            "Robots",
+            "Shooter",
+            "Fast-Paced",
+            "Pixel Graphics",
+            "2D",
+            "Retro",
+            "Exploration"
         ],
         "languages": {
-            "english": "I"
+            "english": "IAS",
+            "french": "I",
+            "german": "I",
+            "italian": "I",
+            "japanese": "I",
+            "spanish": "I",
+            "brazilian": "I",
+            "dutch": "I",
+            "latam": "I",
+            "portuguese": "I",
+            "russian": "I",
+            "schinese": "I",
+            "tchinese": "I"
         }
     }
 }
-var gameOfDayDate = '2026-09-24'
+var gameOfDayDate = '2026-09-25'
